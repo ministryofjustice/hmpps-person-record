8ed58310116bd255386a5dec8517ed49fdfe05f4
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2023-07-21 15:40:50 +0100

    PIC-3158 further ktlint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/DeliusOffenderEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/DeliusOffenderEntity.kt
@@ -1,6 +1,13 @@
 package uk.gov.justice.digital.hmpps.personrecord.jpa.entity
 
-import jakarta.persistence.*
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import org.hibernate.envers.Audited
 
 @Entity
