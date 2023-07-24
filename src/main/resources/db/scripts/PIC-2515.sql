99ad07f9927245404aabaf4284168a7ef66a4ec0
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-07-24 11:18:01 +0100

    PIC-3160
    Old script for reference.

--- src/main/resources/db/scripts/PIC-2515.sql
+++ src/main/resources/db/scripts/PIC-2515.sql
@@ -1,9 +1,9 @@
 /**
   Scripts to run before deploying person record service in to dev,pre-prod and prod.
  */
-insert into personrecordservice.person (person_id,pnc_number,crn,created_by,created_date,last_updated_by ,date_of_birth,family_name,given_name)
-select person_id,pnc,crn,'migration', current_date , 'migration', date_of_birth , d.name ->>'surname' , d.name->>'forename1'
-from personrecordservice.defendant d;
+insert into personrecordservice.person (crn,person_id,pnc_number,created_by,created_date,last_updated_by ,date_of_birth,family_name,given_name)
+select  distinct on (d.crn) crn , person_id,pnc,'migration', current_date , 'migration', date_of_birth , d.name ->>'surname' , d.name->>'forename1'
+from personrecordservice.defendant d where d.crn is not null;
 commit;
 
 drop table personrecordservice.defendant;
