6f0ae8d275f4ea6ee8fdb8479e9278fdab6d852a
cairnsp <101661307+cairnsp@users.noreply.github.com>
2023-08-22 12:57:12 +0100

    PIC-3159 Feign client test (#24)
    * PIC-3159 Feign client test
    
    * PIC-3159 Feign client test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/HmppsPersonRecord.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/HmppsPersonRecord.kt
@@ -2,9 +2,10 @@ package uk.gov.justice.digital.hmpps.personrecord
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-// @EnableFeignClients
+@EnableFeignClients
 @SpringBootApplication()
 @EnableJpaAuditing
 class HmppsPersonRecord
