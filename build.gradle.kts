5a599c597e75416a9cf10716b1cb6ef07b76c7c5
cairnsp <101661307+cairnsp@users.noreply.github.com>
2023-11-06 09:56:12 +0000

    CPR-45 Add IRSA service account name to k8s (#29)
    * CPR-45 Add IRSA service account name to k8s
    
    * CPR-45 Bump up helm chart versions
    
    * CPR-45 Bump up api versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
   kotlin("plugin.spring") version "1.9.0"
   kotlin("jvm") version "1.9.0"
   kotlin("plugin.jpa") version "1.9.0"
@@ -18,26 +18,26 @@ dependencies {
   implementation("jakarta.validation:jakarta.validation-api:3.0.2")
   implementation("com.microsoft.azure:applicationinsights-spring-boot-starter:2.6.4")
   implementation("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
-  implementation("org.hibernate.orm:hibernate-envers:6.1.7.Final")
-  implementation("io.swagger.core.v3:swagger-annotations:2.2.8")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+  implementation("org.hibernate.orm:hibernate-envers:6.3.1.Final")
+  implementation("io.swagger.core.v3:swagger-annotations:2.2.18")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
   implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.4")
   implementation("org.springframework.cloud:spring-cloud-dependencies:2022.0.4")
 
   runtimeOnly("org.postgresql:postgresql:42.5.4")
-  runtimeOnly("org.flywaydb:flyway-core:9.16.0")
+  runtimeOnly("org.flywaydb:flyway-core:9.22.3")
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:junit-jupiter:1.17.6")
-  testImplementation("org.testcontainers:postgresql:1.17.6")
-  testImplementation("com.github.tomakehurst:wiremock:3.0.0-beta-7")
-  testImplementation("io.jsonwebtoken:jjwt-api:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.20-RC")
+  testImplementation("org.testcontainers:junit-jupiter:1.19.1")
+  testImplementation("org.testcontainers:postgresql:1.19.1")
+  testImplementation("org.wiremock:wiremock:3.2.0")
+  testImplementation("io.jsonwebtoken:jjwt-api:0.12.3")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.3")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.20")
 }
 
 java {
