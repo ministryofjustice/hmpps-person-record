a6d8141f1c26b379a942f4ef56925c7c3f1fb4af
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2023-11-21 14:55:09 +0000

    CPR-53 Flyway script for initial table structure

--- src/main/resources/db/migration/V1__add_initial_tables.sql
+++ src/main/resources/db/migration/V1__add_initial_tables.sql
@@ -37,9 +37,9 @@ CREATE TABLE person_aud
 alter table if exists person_aud add constraint person_aud_revinfo_FK foreign key (rev) references revinfo;
 
 
-DROP TABLE IF EXISTS hmcts_defendant;
+DROP TABLE IF EXISTS defendant;
 
-CREATE TABLE IF NOT EXISTS hmcts_defendant
+CREATE TABLE IF NOT EXISTS defendant
 (
     id                                      SERIAL      PRIMARY KEY,
     defendant_id                            TEXT        DEFAULT NULL,
@@ -69,11 +69,11 @@ CREATE TABLE IF NOT EXISTS hmcts_defendant
     fk_person_id                            bigInt      NOT NULL
 );
 
-alter table if exists hmcts_defendant add constraint fk_defendant_person_id foreign key (fk_person_id) references person;
+alter table if exists defendant add constraint fk_defendant_person_id foreign key (fk_person_id) references person;
 
-DROP TABLE IF EXISTS hmcts_defendant_aud;
+DROP TABLE IF EXISTS defendant_aud;
 
-CREATE TABLE hmcts_defendant_aud
+CREATE TABLE defendant_aud
 (
     rev integer not null,
     revtype smallint,
@@ -100,14 +100,19 @@ CREATE TABLE hmcts_defendant_aud
     date_of_birth                           DATE
 );
 
-alter table if exists hmcts_defendant_aud add constraint hmcts_defendant_aud_revinfo_FK foreign key (rev) references revinfo;
+alter table if exists defendant_aud add constraint defendant_aud_revinfo_FK foreign key (rev) references revinfo;
 
-DROP TABLE IF EXISTS delius_offender;
+DROP TABLE IF EXISTS offender;
 
-CREATE TABLE IF NOT EXISTS delius_offender
+CREATE TABLE IF NOT EXISTS offender
 (
     id                                      SERIAL      PRIMARY KEY,
     crn                                     TEXT        NOT NULL,
+    pnc_number                              TEXT        NULL,
+    lao                                     BOOLEAN     NULL,
+    first_name                              TEXT        NULL,
+    last_name                               TEXT        NULL,
+    date_of_birth                           DATE        NULL,
     created_by                              TEXT        NOT NULL,
     created_date                            TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_updated_by                         TEXT        NOT NULL,
@@ -116,15 +121,49 @@ CREATE TABLE IF NOT EXISTS delius_offender
     fk_person_id                            bigInt      NOT NULL
 );
 
-DROP TABLE IF EXISTS delius_offender_aud;
+DROP TABLE IF EXISTS offender_aud;
 
-CREATE TABLE IF NOT EXISTS delius_offender_aud
+CREATE TABLE IF NOT EXISTS offender_aud
 (
     rev integer not null,
     revtype smallint,
     id bigint not null,
     crn TEXT,
+    pnc_number TEXT,
     primary key (rev, id)
 );
 
-alter table if exists delius_offender_aud add constraint delius_offender_aud_revinfo_FK foreign key (rev) references revinfo;
+alter table if exists offender_aud add constraint offender_aud_revinfo_FK foreign key (rev) references revinfo;
+
+DROP TABLE IF EXISTS prisoner;
+
+CREATE TABLE IF NOT EXISTS prisoner
+(
+    id                                      SERIAL      PRIMARY KEY,
+    offender_id                             TEXT        NULL,
+    pnc_number                              TEXT        NULL,
+    first_name                              TEXT        NULL,
+    last_name                               TEXT        NULL,
+    date_of_birth                           DATE        NULL,
+    created_by                              TEXT        NOT NULL,
+    created_date                            TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated_by                         TEXT        NOT NULL,
+    last_updated_date                       TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    version                                 int4        NOT NULL DEFAULT 0,
+    fk_person_id                            bigInt      NOT NULL
+);
+
+DROP TABLE IF EXISTS prisoner_aud;
+
+CREATE TABLE IF NOT EXISTS prisoner_aud
+(
+    rev integer not null,
+    revtype smallint,
+    id bigint not null,
+    offender_id TEXT,
+    pnc_number TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    date_of_birth DATE,
+    primary key (rev, id)
+);
