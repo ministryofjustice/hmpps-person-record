1d0feb4624707987036a3307dcffaa53a16d38ae
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-11-21 16:37:06 +0000

    CPR-84-87
    . Added court case events queue config.
    . Added retry dlq cron job.
    . Added queue and topic kube secrets

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/integration/LocalStackIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/integration/LocalStackIntTest.kt
@@ -11,7 +11,7 @@ import uk.gov.justice.hmpps.sqs.countMessagesOnQueue
 class LocalStackIntTest : IntegrationTestBase() {
 
   @Test
-  fun `should send and receive message using local stack`() {
+  fun `should send and receive message using local stack with test queue and topic`() {
     // given
     val testTopic by lazy {
       hmppsQueueService.findByTopicId("testtopic")
@@ -31,5 +31,28 @@ class LocalStackIntTest : IntegrationTestBase() {
     assertThat(publishResponse?.messageId()).isNotNull()
 
     await untilCallTo { testQueue?.sqsClient?.countMessagesOnQueue(testQueue!!.queueUrl)?.get() } matches { it == 1 }
+  }
+
+  @Test
+  fun `should receive message from court_case_events_topic`() {
+    // given
+    val courtCaseEventsTopic by lazy {
+      hmppsQueueService.findByTopicId("courtcaseeventstopic")
+    }
+    val cprCourtCaseEventsQueue by lazy {
+      hmppsQueueService.findByQueueId("cprcourtcaseeventsqueue")
+    }
+
+    val publishRequest = PublishRequest.builder()
+      .topicArn(courtCaseEventsTopic?.arn)
+      .message("test message")
+      .build()
+    // when
+    val publishResponse = courtCaseEventsTopic?.snsClient?.publish(publishRequest)?.get()
+    // then
+    assertThat(publishResponse?.sdkHttpResponse()?.isSuccessful).isTrue()
+    assertThat(publishResponse?.messageId()).isNotNull()
+
+    await untilCallTo { cprCourtCaseEventsQueue?.sqsClient?.countMessagesOnQueue(cprCourtCaseEventsQueue!!.queueUrl)?.get() } matches { it == 1 }
   }
 }
