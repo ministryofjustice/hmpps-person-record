a023fc47dc96b3b1440a4452a65223b7d99b2191
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2023-11-27 14:23:47 +0000

    CPR-81 Create initial Telemetry event type

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/type/TelemetryEventType.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.type
+
+enum class TelemetryEventType(val eventName: String) {
+  NEW_CASE_MISSING_PNC("CprNewCaseMissingPnc"),
+  NEW_CASE_INVALID_PNC("CprNewCaseInvalidPnc"),
+  NEW_CASE_EXACT_MATCH("CprNewCaseExactMatch"),
+  NEW_CASE_PARTIAL_MATCH("CprNewCasePartialMatch")
+}
