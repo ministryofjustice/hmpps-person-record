6795162c573868c65ebd55b5fbf570b01d7338da
cairnsp <101661307+cairnsp@users.noreply.github.com>
2023-11-27 16:02:34 +0000

    CPR 81 - Create Initial Telemetry Service  (#45)
    * CPR-81 Create initial Telemetry event type
    
    * CPR-81 Create initial skeleton Telemetry Service and test
    
    * CPR-81 Create Spring Boot telemetry configuration

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/TelemetryServiceTest.kt
@@ -0,0 +1,32 @@
+package uk.gov.justice.digital.hmpps.personrecord.service
+
+import com.microsoft.applicationinsights.TelemetryClient
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.verify
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
+
+@ExtendWith(MockitoExtension::class)
+class TelemetryServiceTest {
+
+  @Mock
+  private lateinit var telemetryClient: TelemetryClient
+
+  @InjectMocks
+  lateinit var telemetryService: TelemetryService
+
+  @Test
+  fun `should track event when provided with custom dimensions and event type`() {
+    // Given
+    val customDimensions = mapOf("key" to "value")
+
+    // When
+    telemetryService.trackEvent(TelemetryEventType.NEW_CASE_EXACT_MATCH, customDimensions)
+
+    // Then
+    verify(telemetryClient).trackEvent("CprNewCaseExactMatch", customDimensions, null)
+  }
+}
