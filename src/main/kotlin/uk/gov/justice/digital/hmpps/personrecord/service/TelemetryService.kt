34bbe9319b18b6b0d914e5a7fd27f01461148966
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2023-11-27 14:31:16 +0000

    CPR-81 Create initial skeleton Telemetry Service and test

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/TelemetryService.kt
@@ -0,0 +1,14 @@
+package uk.gov.justice.digital.hmpps.personrecord.service
+
+import com.microsoft.applicationinsights.TelemetryClient
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
+
+@Service
+class TelemetryService(val telemetryClient: TelemetryClient) {
+
+  fun trackEvent(eventType: TelemetryEventType, customDimensions: Map<String, String?>) {
+    telemetryClient.trackEvent(eventType.eventName, customDimensions, null)
+  }
+
+}
