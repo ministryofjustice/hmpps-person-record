85d171226cdd4f2369b16817b9c49dbfa95477ab
vb-moj <104899072+vb-moj@users.noreply.github.com>
2023-12-05 10:23:41 +0000

    CPR-99-app-insight-events (#51)
    * CPR-99
    
    . Added various telemetry events
    
    . Updated tests to verify.
    
    * CPR-99
    
    . Added various telemetry events
    
    . Updated tests to verify.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processor/CourtCaseEventsProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processor/CourtCaseEventsProcessor.kt
@@ -10,11 +10,14 @@ import uk.gov.justice.digital.hmpps.personrecord.model.hmcts.MessageType
 import uk.gov.justice.digital.hmpps.personrecord.model.hmcts.event.CommonPlatformHearingEvent
 import uk.gov.justice.digital.hmpps.personrecord.model.hmcts.event.LibraHearingEvent
 import uk.gov.justice.digital.hmpps.personrecord.service.CourtCaseEventsService
+import uk.gov.justice.digital.hmpps.personrecord.service.TelemetryService
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
 
 @Service
 class CourtCaseEventsProcessor(
   private val objectMapper: ObjectMapper,
   private val courtCaseEventsService: CourtCaseEventsService,
+  private val telemetryService: TelemetryService,
 ) {
 
   companion object {
@@ -26,13 +29,17 @@ class CourtCaseEventsProcessor(
     when (sqsMessage.getMessageType()) {
       MessageType.LIBRA_COURT_CASE -> processLibraHearingEvent(objectMapper.readValue<LibraHearingEvent>(sqsMessage.message))
       MessageType.COMMON_PLATFORM_HEARING -> processCommonPlatformHearingEvent(objectMapper.readValue<CommonPlatformHearingEvent>(sqsMessage.message))
-      else -> { log.debug("Received case type ${MessageType.UNKNOWN.name}") }
+      else -> {
+        log.debug("Received case type ${MessageType.UNKNOWN.name}")
+        telemetryService.trackEvent(TelemetryEventType.UNKNOWN_CASE_RECEIVED, emptyMap())
+      }
     }
   }
 
   fun processLibraHearingEvent(libraHearingEvent: LibraHearingEvent) {
     log.debug("Processing LIBRA event")
     log.debug(libraHearingEvent.toString())
+    telemetryService.trackEvent(TelemetryEventType.NEW_LIBRA_CASE_RECEIVED, mapOf("PNC" to libraHearingEvent.pnc, "CRO" to libraHearingEvent.cro))
     courtCaseEventsService.processPersonFromCourtCaseEvent(Person.from(libraHearingEvent))
   }
 
@@ -41,6 +48,7 @@ class CourtCaseEventsProcessor(
     log.debug(commonPlatformHearingEvent.toString())
     commonPlatformHearingEvent.hearing.prosecutionCases.forEach { prosecutionCase ->
       prosecutionCase.defendants.forEach {
+        telemetryService.trackEvent(TelemetryEventType.NEW_CP_CASE_RECEIVED, mapOf("PNC" to it.pncId, "CRO" to it.croNumber))
         courtCaseEventsService.processPersonFromCourtCaseEvent(Person.from(it))
       }
     }
