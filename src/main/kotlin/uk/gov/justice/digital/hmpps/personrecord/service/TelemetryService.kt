34ab1ad9f216e534096378ebfb9beb60f90d6e1a
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-12-08 15:47:08 +0000

    CPR-102
    . Added crn and uuid to exact match event
    
    . Updated test.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/TelemetryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/TelemetryService.kt
@@ -1,13 +1,19 @@
 package uk.gov.justice.digital.hmpps.personrecord.service
 
 import com.microsoft.applicationinsights.TelemetryClient
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
 
 @Service
 class TelemetryService(private val telemetryClient: TelemetryClient) {
 
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   fun trackEvent(eventType: TelemetryEventType, customDimensions: Map<String, String?>) {
+    log.debug("Sending telemetry event ${ eventType.eventName} ")
     telemetryClient.trackEvent(eventType.eventName, customDimensions, null)
   }
 }
