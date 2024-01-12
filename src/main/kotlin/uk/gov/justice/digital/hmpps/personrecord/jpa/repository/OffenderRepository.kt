f9a75dbdaca98d9f9c88ac94cea556b454833253
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-01-12 10:15:00 +0000

    FIX: fixing ktlint errors

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/OffenderRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/OffenderRepository.kt
@@ -6,6 +6,6 @@ import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.OffenderEntity
 
 @Repository
 interface OffenderRepository : JpaRepository<OffenderEntity, Long> {
-  fun findByCrn(crn: String): OffenderEntity ?
+  fun findByCrn(crn: String): OffenderEntity?
   fun existsByCrn(crn: String): Boolean
 }
