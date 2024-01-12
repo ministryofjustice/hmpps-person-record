11eba1311dbc51d3e9b20fdc972effe7c222f6e5
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2024-01-12 15:23:00 +0000

    CPR-133 Refactor OAuth2 security config for Feign

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/SecurityConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/SecurityConfiguration.kt
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -44,15 +44,15 @@ class SecurityConfiguration {
   @Bean
   fun authorizedClientManager(
     clientRegistrationRepository: ClientRegistrationRepository?,
-    authorizedClientRepository: OAuth2AuthorizedClientRepository?,
+    clientService: OAuth2AuthorizedClientService,
   ): OAuth2AuthorizedClientManager {
     val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
       .clientCredentials()
       .build()
 
-    val authorizedClientManager = DefaultOAuth2AuthorizedClientManager(
+    val authorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(
       clientRegistrationRepository,
-      authorizedClientRepository,
+      clientService,
     )
 
     authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
