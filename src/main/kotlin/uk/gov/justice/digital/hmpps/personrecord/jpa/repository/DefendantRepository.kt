cf657ad1a1bd4ca5ed35108b184c65112f7dfc28
cairnsp <101661307+cairnsp@users.noreply.github.com>
2024-01-15 13:32:21 +0000

    CPR-133 Add transactional isolation level to repository method (#80)
    * CPR-133 Add transactional isolation level to repository method
    
    * CPR-133 ktlint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/DefendantRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/DefendantRepository.kt
@@ -2,11 +2,14 @@ package uk.gov.justice.digital.hmpps.personrecord.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.DefendantEntity
 
 @Repository
 interface DefendantRepository : JpaRepository<DefendantEntity, Long> {
   fun findByDefendantId(defendantId: String): DefendantEntity?
 
+  @Transactional(isolation = Isolation.READ_COMMITTED)
   fun findAllByPncNumber(pncNumber: String): List<DefendantEntity>
 }
