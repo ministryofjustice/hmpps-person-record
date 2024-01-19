24b5a512d2e065e4bc3c049daffac588ed6f70d2
cairnsp <101661307+cairnsp@users.noreply.github.com>
2024-01-19 12:41:55 +0000

    CPR-54 Add 'JsonInclude' annotation to Nomis search pojo (#97)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/PossibleMatchCriteria.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/PossibleMatchCriteria.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.hmpps.personrecord.client.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import uk.gov.justice.digital.hmpps.personrecord.model.Person
 import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class PossibleMatchCriteria(
   val firstName: String? = null,
   val lastName: String? = null,
