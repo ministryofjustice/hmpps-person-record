e5f2887c55030f1aa508771fe0390942ab2248ff
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-02-06 15:37:01 +0000

    CPR-141 renaming for clarity

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/validate/PNCIdValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/validate/PNCIdValidator.kt
@@ -4,24 +4,21 @@ import uk.gov.justice.digital.hmpps.personrecord.service.TelemetryService
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.INVALID_PNC
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.VALID_PNC
 
-const val PNC_REGEX = "\\d{4}(/?)\\d{7}[A-Z]{1}\$"
+private const val VALID_LETTERS = "ZABCDEFGHJKLMNPQRTUVWXY"
+private const val SLASH = "/"
+private val PNC_REGEX = Regex("\\d{4}(/?)\\d{7}[A-Z]{1}\$")
 
 class PNCIdValidator(private val telemetryService: TelemetryService) {
-  fun isValid(pncId: PNCIdentifier): Boolean {
-    val pncIdentifier = pncId.pncId!!
-    return (pncIdentifier.matches(Regex(PNC_REGEX)) && correctModulus(pncIdentifier))
+  fun isValid(pncIdentifier: PNCIdentifier): Boolean {
+    val pnc = pncIdentifier.pncId!!
+    return (pnc.matches(PNC_REGEX) && correctModulus(pnc))
       .also {
-        telemetryService.trackEvent(if (it) VALID_PNC else INVALID_PNC, mapOf("PNC" to pncIdentifier, "inputPNC" to pncId.inputPnc))
+        telemetryService.trackEvent(if (it) VALID_PNC else INVALID_PNC, mapOf("PNC" to pnc, "inputPNC" to pncIdentifier.inputPnc))
       }
   }
 
   private fun correctModulus(pncIdentifier: String): Boolean {
     val modulusLetter = pncIdentifier[12]
     return VALID_LETTERS[pncIdentifier.replace(SLASH, "").substring(2, 11).toLong().mod(23)] == modulusLetter
-  }
-
-  companion object {
-    private const val VALID_LETTERS = "ZABCDEFGHJKLMNPQRTUVWXY"
-    private const val SLASH = "/"
   }
 }
