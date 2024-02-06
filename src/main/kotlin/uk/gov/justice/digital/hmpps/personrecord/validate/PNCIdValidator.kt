afb2964fea7b37f5b3caf8b1b1c9efb5e8f59c39
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-02-06 14:58:50 +0000

    CPR-141 refactored for clarity

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/validate/PNCIdValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/validate/PNCIdValidator.kt
@@ -9,10 +9,15 @@ const val PNC_REGEX = "\\d{4}(/?)\\d{7}[A-Z]{1}\$"
 class PNCIdValidator(private val telemetryService: TelemetryService) {
   fun isValid(pncId: PNCIdentifier): Boolean {
     val pncIdentifier = pncId.pncId!!
-    return (pncIdentifier.matches(Regex(PNC_REGEX)) && VALID_LETTERS[pncIdentifier.replace(SLASH, "").substring(2, 11).toLong().mod(23)] == pncIdentifier[12])
+    return (pncIdentifier.matches(Regex(PNC_REGEX)) && correctModulus(pncIdentifier))
       .also {
         telemetryService.trackEvent(if (it) VALID_PNC else INVALID_PNC, mapOf("PNC" to pncIdentifier, "inputPNC" to pncId.inputPnc))
       }
+  }
+
+  private fun correctModulus(pncIdentifier: String): Boolean {
+    val modulusLetter = pncIdentifier[12]
+    return VALID_LETTERS[pncIdentifier.replace(SLASH, "").substring(2, 11).toLong().mod(23)] == modulusLetter
   }
 
   companion object {
