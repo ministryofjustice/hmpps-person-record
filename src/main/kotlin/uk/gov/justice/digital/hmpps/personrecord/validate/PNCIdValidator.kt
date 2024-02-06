7fcdbe5ab14ba8f4b893f74aebb3463eed679485
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-02-06 17:15:02 +0000

    CPR-141 move PNC validation tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/validate/PNCIdValidator.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.personrecord.validate
-
-class PNCIdValidator() {
-  fun isValid(pncIdentifier: PNCIdentifier): Boolean {
-    return pncIdentifier.isValid()
-  }
-}
