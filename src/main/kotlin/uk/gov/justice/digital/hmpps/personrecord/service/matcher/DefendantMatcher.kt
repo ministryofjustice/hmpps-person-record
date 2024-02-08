06ebca8c903500da5167170e6c39279035c25a39
vb-moj <104899072+vb-moj@users.noreply.github.com>
2024-02-08 15:02:11 +0000

    Cpr 154 defendant matcher (#158)
    * CPR-154 Added abstract class Matcher
    
    Refactored prisoner and offender matchers.
    
    Added Defendant matcher.
    
    * CPR-154 Refactored CourtCaseEventsService
    
    Updated prisoner and offender services.
    
    * CPR-154 Renamed variable.
    
    * CPR-154 Static import even types.
    
    * CPR-154 Corrected function name.
    
    * CPR-154 Renamed matcher function.
    
    * CPR-154 Removed unnecessary function.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/matcher/DefendantMatcher.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.matcher
+
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.DefendantEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.Person
+import uk.gov.justice.digital.hmpps.personrecord.validate.PNCIdentifier
+
+class DefendantMatcher(defendants: List<DefendantEntity>?, person: Person) :
+  Matcher<DefendantEntity>(defendants, person) {
+
+  override fun isPartialMatchItem(item: DefendantEntity): Boolean {
+    return person.otherIdentifiers?.pncIdentifier == PNCIdentifier(item.pncNumber?.pncId) &&
+      item.forenameOne.equals(person.givenName, true) || item.surname.equals(person.familyName, true) || item.dateOfBirth == person.dateOfBirth
+  }
+
+  override fun isMatchingItem(item: DefendantEntity): Boolean {
+    return person.otherIdentifiers?.pncIdentifier == PNCIdentifier(item.pncNumber?.pncId.toString()) &&
+      item.forenameOne.equals(person.givenName, ignoreCase = true) &&
+      item.surname.equals(person.familyName, ignoreCase = true) &&
+      item.dateOfBirth == person.dateOfBirth
+  }
+
+  fun extractMatchingFields(defendant: DefendantEntity): Map<String, String?> =
+    mapOf(
+      "Surname" to if (defendant.surname.equals(person.familyName)) defendant.surname else null,
+      "Forename" to if (defendant.forenameOne.equals(person.givenName)) defendant.forenameOne else null,
+      "Date of birth" to if (defendant.dateOfBirth?.equals(person.dateOfBirth) == true) defendant.dateOfBirth.toString() else null,
+    ).filterValues { it != null }
+}
