77e746100b59fa01c2590e800cd1a5d9e7f94416
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-02-09 09:00:29 +0000

    CPR-141 another test moved off SQL initialisation

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonRepositoryCustomImplIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonRepositoryCustomImplIntTest.kt
@@ -1,28 +1,30 @@
 package uk.gov.justice.digital.hmpps.personrecord.jpa.repository
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.test.context.jdbc.Sql
-import org.springframework.test.context.jdbc.SqlConfig
 import uk.gov.justice.digital.hmpps.personrecord.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.DefendantEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.OtherIdentifiers
+import uk.gov.justice.digital.hmpps.personrecord.model.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.PersonSearchRequest
+import uk.gov.justice.digital.hmpps.personrecord.validate.PNCIdentifier
+import java.time.LocalDate
 import java.util.*
 
-@Sql(
-  scripts = ["classpath:sql/before-test.sql"],
-  config = SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED),
-  executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-)
-@Sql(
-  scripts = ["classpath:sql/after-test.sql"],
-  config = SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED),
-  executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
-)
 class PersonRepositoryCustomImplIntTest : IntegrationTestBase() {
+
+  @BeforeEach
+  fun setup() {
+    personRepository.deleteAll()
+  }
 
   @Test
   fun `should return person record for known provided surname`() {
     // Given
+    aDefendant(UUID.fromString("ddf11834-e2a3-11ed-b5ea-0242ac120002"), "Bob", "Mortimer")
+
     val searchRequest = PersonSearchRequest(surname = "Mortimer")
 
     // When
@@ -34,24 +36,10 @@ class PersonRepositoryCustomImplIntTest : IntegrationTestBase() {
   }
 
   @Test
-  fun `should return person record for known provided middle names`() {
-    // Given
-    val searchRequest = PersonSearchRequest(
-      surname = "Mahoney",
-      forenameThree = "bob",
-    )
-
-    // When
-    val results = personRepository.searchByRequestParameters(searchRequest)
-
-    // Then
-    assertThat(results).hasSize(1)
-    assertThat(results[0].personId).isEqualTo(UUID.fromString("eed4a9a4-d853-11ed-afa1-0242ac120002"))
-  }
-
-  @Test
   fun `should return all person records for provided search criteria`() {
     // Given
+    aDefendant(UUID.fromString("eed4a9a4-d853-11ed-afa1-0242ac120002"), "Iestyn", "Mahoney")
+    aDefendant(UUID.fromString("d75a9374-e2a3-11ed-b5ea-0242ac120002"), "Garry", "Mahoney")
     val searchRequest = PersonSearchRequest(
       surname = "Mahoney",
     )
@@ -70,6 +58,7 @@ class PersonRepositoryCustomImplIntTest : IntegrationTestBase() {
   @Test
   fun `should return a single person record for an exact name match ignoring case`() {
     // Given
+    aDefendant(UUID.randomUUID(), "John", "Mahoney")
     val searchRequest = PersonSearchRequest(
       forenameOne = "jOhN",
       surname = "mAhOnEy",
@@ -92,5 +81,17 @@ class PersonRepositoryCustomImplIntTest : IntegrationTestBase() {
 
     // Then
     assertThat(results).isEmpty()
+  }
+
+  private fun aDefendant(personId: UUID, givenName: String, familyName: String) {
+    val person = Person(otherIdentifiers = OtherIdentifiers(pncIdentifier = PNCIdentifier.from("2001/0171310W"), crn = "CRN1234"), givenName = givenName, familyName = familyName, dateOfBirth = LocalDate.of(1965, 6, 18))
+
+    val newPersonEntity = PersonEntity(personId = personId)
+    newPersonEntity.createdBy = "test"
+    newPersonEntity.lastUpdatedBy = "test"
+    val newDefendantEntity = DefendantEntity.from(person)
+    newDefendantEntity.person = newPersonEntity
+    newPersonEntity.defendants.add(newDefendantEntity)
+    personRepository.saveAndFlush(newPersonEntity)
   }
 }
