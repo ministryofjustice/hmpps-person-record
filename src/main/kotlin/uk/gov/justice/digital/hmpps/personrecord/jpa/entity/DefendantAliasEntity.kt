857a325e49f507aca4d1e0b9b6cdc8d88a0b907e
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2024-02-15 11:21:04 +0000

    CPR-159 Added defendant alias entity.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/DefendantAliasEntity.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.personrecord.jpa.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.envers.Audited
+
+@Entity
+@Table(name = "defendant_alias")
+@Audited
+class DefendantAliasEntity(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = null,
+
+  @ManyToOne(optional = false, cascade = [CascadeType.ALL])
+  @JoinColumn(
+    name = "fk_defendant_id",
+    referencedColumnName = "id",
+    nullable = false,
+  )
+  var defendant: DefendantEntity? = null,
+
+  @Column(name = "first_name")
+  val firstName: String? = null,
+
+  @Column(name = "middle_name")
+  val middleName: String? = null,
+
+  @Column(name = "last_name")
+  val lastName: String? = null,
+) : BaseAuditedEntity()
