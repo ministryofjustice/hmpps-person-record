83d76fbcfd85a5c44d14d9d2a6efeb2bd9c470b2
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2024-02-19 12:44:30 +0000

    CPR-159 removed envers from the build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
   implementation("jakarta.validation:jakarta.validation-api:3.0.2")
   implementation("com.microsoft.azure:applicationinsights-spring-boot-starter:2.6.4")
   implementation("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
-  implementation("org.hibernate.orm:hibernate-envers:6.4.4.Final")
   implementation("io.swagger.core.v3:swagger-annotations:2.2.20")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
   implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.0")
