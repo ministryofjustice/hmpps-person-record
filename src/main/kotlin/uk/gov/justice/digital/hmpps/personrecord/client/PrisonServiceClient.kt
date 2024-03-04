6b9214dd010fa62e8c8cf032201d75f6218db98d
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2024-03-04 08:35:19 +0000

    CPR-131 Added prison service rest client.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonServiceClient.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.personrecord.client
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.PrisonerDetails
+import uk.gov.justice.digital.hmpps.personrecord.config.PrisonServiceOAuth2Config
+
+@FeignClient(
+  name = "prison-service",
+  url = "\${prison-service.base-url}",
+  configuration = [PrisonServiceOAuth2Config::class],
+)
+interface PrisonServiceClient {
+
+  @GetMapping(value = ["\${prison-service.prisoner-details}/{prisonerNumber}"])
+  fun getPrisonerDetails(@PathVariable("prisonerNumber") prisonerNumber: String): PrisonerDetails?
+}
