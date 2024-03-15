8e8647fe6a4490c8a742a7da8d5e78fa981abbb3
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-03-15 10:03:16 +0000

    CPR-199 fix case

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/PersonCreationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/PersonCreationIntTest.kt
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.personrecord.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.personrecord.message.listeners.processors.NEW_OFFENDER_CREATED
 import uk.gov.justice.digital.hmpps.personrecord.model.hmcts.MessageType.COMMON_PLATFORM_HEARING
-import uk.gov.justice.digital.hmpps.personrecord.service.helper.commonPlatformHearingWIthOneDefendant
+import uk.gov.justice.digital.hmpps.personrecord.service.helper.commonPlatformHearingWithOneDefendant
 
 class PersonCreationIntTest : IntegrationTestBase() {
 
   @Test
   fun `should allow creation and retrieval of 2 defendants with same PNC and different name`() {
     val pncNumber = "1981/0154257C"
-    publishHMCTSMessage(commonPlatformHearingWIthOneDefendant(pncNumber, "Bob"), COMMON_PLATFORM_HEARING)
+    publishHMCTSMessage(commonPlatformHearingWithOneDefendant(pncNumber, "Bob"), COMMON_PLATFORM_HEARING)
 
     publishDeliusNewOffenderEvent(createDomainEvent(NEW_OFFENDER_CREATED, "C1234"))
   }
