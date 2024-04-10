9f6904ad5ec82e3db4f84a6118a7007f0e957efb
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-04-10 17:24:09 +0100

    CPR-165 Cononocalise CRO SF format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifier.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifier.kt
@@ -7,7 +7,10 @@ class CROIdentifier(inputCroId: String, inputFingerprint: Boolean) {
 
   companion object {
     private const val EMPTY_CRO = ""
-    private const val SF_PREFIX = "SF"
+    private const val SERIAL_NUM_LENGTH = 6
+
+    private val SF_CRO_REGEX = Regex("^SF\\d{2}/\\d{1,6}[A-Z]\$")
+    private val CRO_REGEX = Regex("^\\d{1,6}/\\d{2}[A-Z]\$")
 
     fun from(inputCroId: String? = ""): CROIdentifier {
       if (inputCroId.isNullOrEmpty()) {
@@ -17,14 +20,34 @@ class CROIdentifier(inputCroId: String, inputFingerprint: Boolean) {
     }
 
     private fun toCanonicalForm(inputCroId: String): CROIdentifier {
-      if (isSfFormat(inputCroId)) {
-        return CROIdentifier("", false)
+      return when {
+        isSfFormat(inputCroId) -> CROIdentifier(canonicalizeSfFormat(inputCroId), false)
+        isExpectedFormat(inputCroId) -> CROIdentifier(inputCroId, true)
+        else -> CROIdentifier("", false)
       }
-      return CROIdentifier("", true)
     }
 
-    private fun isSfFormat(croId: String): Boolean {
-      return croId.startsWith(SF_PREFIX)
+    private fun canonicalizeSfFormat(inputCroId: String): String {
+      val checkChar = inputCroId.takeLast(1)
+      val strippedCRO = inputCroId.drop(2).split("/") // splits into [YY, NNNNNND]
+      val yearDigits = strippedCRO[0]
+      val serialNumber = padSerialNumber(strippedCRO[1].dropLast(1))
+      return "$serialNumber/$yearDigits$checkChar"
+    }
+
+    private fun padSerialNumber(serialNumber: String): String {
+      if (serialNumber.length < SERIAL_NUM_LENGTH) {
+        return serialNumber.padStart(SERIAL_NUM_LENGTH, '0')
+      }
+      return serialNumber
+    }
+
+    private fun isExpectedFormat(inputCroId: String): Boolean {
+      return inputCroId.matches(CRO_REGEX)
+    }
+
+    private fun isSfFormat(inputCroId: String): Boolean {
+      return inputCroId.matches(SF_CRO_REGEX)
     }
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifierTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifierTest.kt
@@ -2,15 +2,7 @@ package uk.gov.justice.digital.hmpps.personrecord.model.identifiers
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
-import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.InvalidPNCIdentifier
-import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.PNCIdentifier
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.stream.Stream
+
 import kotlin.test.assertEquals
 
 class CROIdentifierTest {
@@ -18,20 +10,34 @@ class CROIdentifierTest {
   fun `should process an empty string`() {
     val identifier = CROIdentifier.from("")
     assertThat(identifier.croId.isEmpty())
-    assertEquals(identifier.fingerprint, false)
+    assertEquals(false, identifier.fingerprint)
   }
 
   @Test
   fun `should process null id`() {
     val identifier = CROIdentifier.from(null)
     assertThat(identifier.croId.isEmpty())
-    assertEquals(identifier.fingerprint, false)
+    assertEquals(false, identifier.fingerprint)
   }
 
   @Test
   fun `should process a SF format CRO`() {
-    val identifier = CROIdentifier.from("SF20/01234523")
-    assertThat(identifier.croId.isEmpty())
-    assertEquals(identifier.fingerprint, false)
+    val identifier = CROIdentifier.from("SF20/012345A")
+    assertEquals("012345/20A", identifier.croId)
+    assertEquals(false, identifier.fingerprint)
+  }
+
+  @Test
+  fun `should process a SF format CRO with limit serial section`() {
+    val identifier = CROIdentifier.from("SF20/0145A")
+    assertEquals("000145/20A", identifier.croId)
+    assertEquals(false, identifier.fingerprint)
+  }
+
+  @Test
+  fun `should process a standard format CRO`() {
+    val identifier = CROIdentifier.from("056810/65Y")
+    assertEquals("056810/65Y", identifier.croId)
+    assertEquals(true, identifier.fingerprint)
   }
 }
