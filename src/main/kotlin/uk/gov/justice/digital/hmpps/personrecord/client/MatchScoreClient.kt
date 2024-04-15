9e2ec2385d894784ebe5688de3e05c293636578a
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-04-15 10:44:59 +0100

    CPR-188 populate match request with values to match

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/MatchScoreClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/MatchScoreClient.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.personrecord.client
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 @FeignClient(
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody
   url = "\${match-score.base-url}",
 )
 interface MatchScoreClient {
-
-  @GetMapping("match")
+  @PostMapping("match")
   fun getMatchScore(@RequestBody matchRequest: MatchRequest): MatchResponse?
 }
 
@@ -24,4 +23,18 @@ data class MatchResponse(
 
 data class MatchData(@JsonProperty("0") val value: String)
 
-class MatchRequest
+class MatchRequest(
+  @JsonProperty("unique_id")
+  val uniqueId: PersonMatchScoreParameter,
+  @JsonProperty("first_name")
+  val firstName: PersonMatchScoreParameter,
+  val surname: PersonMatchScoreParameter,
+  @JsonProperty("dob")
+  val dateOfBirth: PersonMatchScoreParameter,
+  @JsonProperty("pnc_number")
+  val pncNumber: PersonMatchScoreParameter,
+  @JsonProperty("source_dataset")
+  val sourceDataSet: PersonMatchScoreParameter = PersonMatchScoreParameter("libra", "delius"),
+)
+
+class PersonMatchScoreParameter(@JsonProperty("0") val value0: String? = "", @JsonProperty("1") val value1: String? = "")
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/MatchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/MatchService.kt
@@ -3,20 +3,35 @@ package uk.gov.justice.digital.hmpps.personrecord.service
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.personrecord.client.MatchRequest
 import uk.gov.justice.digital.hmpps.personrecord.client.MatchScoreClient
+import uk.gov.justice.digital.hmpps.personrecord.client.PersonMatchScoreParameter
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.DefendantEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.Person
 
 @Service
 class MatchService(val matchScoreClient: MatchScoreClient) {
-  fun score(allMatchingItems: List<DefendantEntity>, person: Person): MatchResult {
-    val matchScore = matchScoreClient.getMatchScore(matchRequest = MatchRequest())
+  fun score(allMatchingItems: List<DefendantEntity>, newRecord: Person): MatchResult {
+    val candidateRecord = allMatchingItems[0]
+    // TODO a lot of tidying up
+    val candidateRecordIdentifier = candidateRecord.defendantId!!
+    val newRecordIdentifier = newRecord.defendantId!!
+
+    val matchScore = matchScoreClient.getMatchScore(
+      MatchRequest(
+        uniqueId = PersonMatchScoreParameter(candidateRecordIdentifier, newRecordIdentifier),
+        firstName = PersonMatchScoreParameter(candidateRecord.firstName, newRecord.givenName),
+        surname = PersonMatchScoreParameter(candidateRecord.surname, newRecord.familyName),
+        dateOfBirth = PersonMatchScoreParameter(candidateRecord.dateOfBirth.toString(), newRecord.dateOfBirth.toString()),
+        pncNumber = PersonMatchScoreParameter(candidateRecord.pncNumber?.pncId, newRecord.otherIdentifiers?.pncIdentifier?.pncId),
+      ),
+    )
+
     return MatchResult(
       matchScore?.matchProbability?.value!!,
-      candidateRecordUUID = allMatchingItems[0].person?.personId.toString(),
+      candidateRecordUUID = candidateRecord.person?.personId.toString(),
       candidateRecordIdentifierType = "defendantId",
-      candidateRecordIdentifier = allMatchingItems[0].defendantId!!,
+      candidateRecordIdentifier = candidateRecordIdentifier,
       newRecordIdentifierType = "defendantId",
-      newRecordIdentifier = person.defendantId!!,
+      newRecordIdentifier = newRecordIdentifier,
     )
   }
 }
