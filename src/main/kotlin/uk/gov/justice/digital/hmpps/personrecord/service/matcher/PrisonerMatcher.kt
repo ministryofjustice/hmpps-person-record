1aa8d47c1e1102f7851e974b6f859b9e5b3d30e5
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-04-17 14:59:45 +0100

    CPR-282 Add PNC check for partial matches (#247)
    * CPR-282 Remove pnc validation
    
    * CPR-282 Update tests
    
    * CPR-282 Process regardless
    
    * CPR-182 Remove redundant event
    
    * CPR-182 Remove redundant event
    
    * CPR-282 Remove event
    
    * CPR-282 Remove PNC event types
    
    * CPR-282 Update pnc matching
    
    * CPR-282 Get test working
    
    * CPR-282 Update processor
    
    * CPR-282 Fixed failing tests.
    
    * CPR-282 Tidy up code.
    
    * CPR-282 Add pnc check for partial matches
    
    ---------
    
    Co-authored-by: vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/matcher/PrisonerMatcher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/matcher/PrisonerMatcher.kt
@@ -2,11 +2,15 @@ package uk.gov.justice.digital.hmpps.personrecord.service.matcher
 
 import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.Prisoner
 import uk.gov.justice.digital.hmpps.personrecord.model.Person
+import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.PNCIdentifier
 
 class PrisonerMatcher(prisonerDetails: List<Prisoner>?, person: Person) :
   Matcher<Prisoner>(prisonerDetails, person) {
   override fun isPartialMatchItem(item: Prisoner): Boolean {
-    return item.firstName.equals(person.givenName, true) || item.lastName.equals(person.familyName, true) || item.dateOfBirth == person.dateOfBirth
+    return person.otherIdentifiers?.pncIdentifier == PNCIdentifier.from(item.pncNumber) ||
+      item.firstName.equals(person.givenName, true) ||
+      item.lastName.equals(person.familyName, true) ||
+      item.dateOfBirth == person.dateOfBirth
   }
 
   override fun isMatchingItem(item: Prisoner): Boolean {
