52f84c16d08814f75dc4b813c82c3912c47a63ae
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-04-19 10:56:52 +0100

    CPR-282 PNC identifier refactor (#251)
    * CPR-282 First pass at PNC refactor
    
    * CPR-282 Fix tests
    
    * CPR-282 Update to not record invalid
    
    * CPR-282 Review comments
    
    * CPR-282 Fix lint
    
    * CPR-282 Add check to match only if pnc present
    
    * CPR-289 switch off external calls (#257)
    
    * CPR-289 Switch of nomis and nDelius calls
    
    * CPR-289 Remove vscode file
    
    * CPR-282 Reduce match score calls (#258)
    
    * CPR-282 Reduce match score calls
    
    * CPR-282 Update test
    
    * CPR-282 Merge main
    
    * CPR-282 Remove bin folder
    
    * CPR-282 Remove redundant check now
    
    * CPR-282 Re-add valid function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/PNCIdentifier.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/PNCIdentifier.kt
@@ -1,64 +1,75 @@
 package uk.gov.justice.digital.hmpps.personrecord.model.identifiers
 
+import org.apache.commons.lang3.builder.EqualsBuilder
+import org.apache.commons.lang3.builder.HashCodeBuilder
 import java.time.LocalDate
 
-interface PNCIdentifier {
+class PNCIdentifier(val pncId: String) {
 
-  val pncId: String
+  val valid: Boolean
+    get() = pncId.isNotEmpty()
+
+  override fun equals(other: Any?): Boolean {
+    return EqualsBuilder.reflectionEquals(this, other)
+  }
+
+  override fun hashCode(): Int {
+    return HashCodeBuilder.reflectionHashCode(this)
+  }
+
+  override fun toString(): String {
+    return pncId
+  }
+
   companion object {
-    fun from(inputPncId: String? = ""): PNCIdentifier {
-      if (inputPncId.isNullOrEmpty()) {
-        return MissingPNCIdentifier()
-      }
-      return validOrInvalid(inputPncId)
-    }
 
-    private const val VALID_LETTERS = "ZABCDEFGHJKLMNPQRTUVWXY"
-    private const val SLASH = "/"
-    private val PNC_REGEX = Regex("\\d{4}(/?)\\d{7}[A-Z]\$")
-    private const val LONG_PNC_ID_LENGTH = 10
-    private const val CENTURY = 100
-    private const val LAST_CHARACTER = 12
+    internal const val SERIAL_NUM_LENGTH = 7
     private const val YEAR_END = 4
 
-    private fun validOrInvalid(inputPncId: String): PNCIdentifier {
-      val pncId = toCanonicalForm(inputPncId)
+    private val PNC_REGEX = Regex("\\d{2,$YEAR_END}(/?)\\d{1,$SERIAL_NUM_LENGTH}[A-Z]\$")
+
+    private const val EMPTY_PNC = ""
+    private const val SLASH = "/"
+    private const val LONG_PNC_ID_LENGTH = 10
+    private const val CENTURY = 100
+
+    fun from(inputPncId: String? = EMPTY_PNC): PNCIdentifier {
       return when {
-        (pncId.matches(PNC_REGEX) && correctModulus(pncId)) -> ValidPNCIdentifier(pncId)
-        else -> InvalidPNCIdentifier(inputPncId)
+        inputPncId.isNullOrEmpty() -> PNCIdentifier(EMPTY_PNC)
+        isExpectedFormat(inputPncId) -> toCanonicalForm(inputPncId)
+        else -> PNCIdentifier(EMPTY_PNC)
       }
     }
-    private fun correctModulus(pncId: String): Boolean {
-      val modulusLetter = pncId[LAST_CHARACTER]
-      return VALID_LETTERS[pncId.replace(SLASH, "").substring(2, LAST_CHARACTER - 1).toLong().mod(VALID_LETTERS.length)] == modulusLetter
-    }
-    private fun toCanonicalForm(pnc: String): String {
-      val sanitizedPncId = pnc.replace("/", "")
 
-      if (sanitizedPncId.length < LONG_PNC_ID_LENGTH) { // this is a short form PNC e.g. 79/123456Z
-        val yearDigits = sanitizedPncId.take(2).also { digits ->
-          if (!digits.all { character -> character.isDigit() }) {
-            return pnc
-          }
-        }
+    private fun isExpectedFormat(pnc: String): Boolean = pnc.matches(PNC_REGEX)
 
-        val year = getYearFromLastTwoDigits(yearDigits.toInt()) // E.g. 79 becomes 1979
-        val remainingIdChars = sanitizedPncId.substring(2) // the non-year id part 123456Z
-        // pad out with zeros: 123456Z becomes 0123456Z
-        val standardizedId = remainingIdChars.padStart(LONG_PNC_ID_LENGTH - 2, '0')
-        return "$year/$standardizedId" // 1979/0123456Z
+    private fun toCanonicalForm(pnc: String): PNCIdentifier {
+      val sanitizedPncId = pnc.replace(SLASH, "")
+      val canonicalPnc = when {
+        isShortFormFormat(sanitizedPncId) -> canonicalShortForm(sanitizedPncId)
+        else -> canonicalLongForm(sanitizedPncId)
       }
-      return withForwardSlash(pnc)
-    }
-
-    private fun withForwardSlash(pnc: String): String {
       return when {
-        !pnc.contains("/") -> {
-          pnc.substring(0, YEAR_END) + "/" + pnc.substring(YEAR_END)
-        }
-        else -> pnc
+        (canonicalPnc.valid) -> PNCIdentifier(canonicalPnc.value)
+        else -> PNCIdentifier(EMPTY_PNC)
       }
     }
+
+    private fun canonicalShortForm(pnc: String): PNC {
+      val checkChar = pnc.takeLast(1)
+      val year = getYearFromLastTwoDigits(pnc.take(2).toInt()) // E.g. 79 becomes 1979
+      val serialNum = pnc.substring(2).dropLast(1) // the non-year id part 123456Z
+      return PNC(checkChar, serialNum, year)
+    }
+
+    private fun canonicalLongForm(pnc: String): PNC {
+      val checkChar = pnc.takeLast(1)
+      val year = pnc.take(YEAR_END)
+      val serialNum = pnc.dropLast(1).takeLast(SERIAL_NUM_LENGTH)
+      return PNC(checkChar, serialNum, year)
+    }
+
+    private fun isShortFormFormat(pnc: String): Boolean = pnc.length < LONG_PNC_ID_LENGTH
 
     private fun isYearThisCentury(year: Int): Boolean {
       return year in 0..currentYearLastTwoDigits()
@@ -84,43 +95,25 @@ interface PNCIdentifier {
   }
 }
 
-class MissingPNCIdentifier : PNCIdentifier {
-  override val pncId: String
-    get() = ""
+class PNC(private val checkChar: String, serialNum: String, private val yearDigits: String) {
 
-  override fun toString(): String {
-    return pncId
-  }
-}
+  private val paddedSerialNum: String = padSerialNumber(serialNum)
 
-class InvalidPNCIdentifier(private val inputPncId: String) : PNCIdentifier {
-  override val pncId: String
-    get() = ""
+  val value: String
+    get() = "$yearDigits/$paddedSerialNum$checkChar"
 
-  override fun toString(): String {
-    return pncId
+  val valid: Boolean
+    get() = correctModulus(checkChar.single())
+
+  private fun correctModulus(checkChar: Char): Boolean {
+    val modulus = VALID_LETTERS[(yearDigits.takeLast(2) + paddedSerialNum).toInt().mod(VALID_LETTERS.length)]
+    return modulus == checkChar
   }
 
-  fun invalidValue(): String = inputPncId
-}
+  private fun padSerialNumber(serialNumber: String): String =
+    serialNumber.padStart(PNCIdentifier.SERIAL_NUM_LENGTH, '0')
 
-class ValidPNCIdentifier(private val inputPncId: String) : PNCIdentifier {
-
-  override val pncId: String
-    get() = inputPncId
-
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (javaClass != other?.javaClass) return false
-    other as PNCIdentifier
-    return pncId == other.pncId
-  }
-
-  override fun hashCode(): Int {
-    return pncId.hashCode()
-  }
-
-  override fun toString(): String {
-    return pncId
+  companion object {
+    private const val VALID_LETTERS = "ZABCDEFGHJKLMNPQRTUVWXY"
   }
 }
