1218ef2657d58e02cb90c267d7d89b0f6a1aabdd
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-04-29 13:43:16 +0100

    CPR-262 retry call to getPrisonerNumbers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/populatefromnomis/PopulateFromNomis.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/populatefromnomis/PopulateFromNomis.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.populatefromnomis
 
+import feign.FeignException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -7,13 +8,21 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
 import uk.gov.justice.digital.hmpps.personrecord.client.PageParams
 import uk.gov.justice.digital.hmpps.personrecord.client.PrisonServiceClient
 import uk.gov.justice.digital.hmpps.personrecord.client.PrisonerSearchClient
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.DefendantEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.DefendantRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor
 
 private const val OK = "OK"
+
+private const val RETRIES = 10
+private const val DELAY_MILLIS = 5000L
+
+private val retryables = listOf(HttpClientErrorException::class, HttpServerErrorException::class, FeignException.InternalServerError::class)
 
 @RestController
 class PopulateFromNomis(
@@ -30,21 +39,28 @@ class PopulateFromNomis(
   }
 
   suspend fun populatePages() {
+    // TODO understand scoping here, how many threads?
     CoroutineScope(Job()).launch {
+      // if this call fails we will just restart the process, no need to retry
       val prisonerNumbers = prisonServiceClient.getPrisonerNumbers(PageParams(0, pageSize))!!
       val totalPages = prisonerNumbers.totalPages // is this 0 based?
+      // Code assumes that it is 1 based.
+      // I have not been able to verify this from API docs or code
       var numbers = prisonerNumbers.numbers
 
       for (page in 1..totalPages) {
         numbers.forEach {
+          // TODO retries
           val prisoner = prisonerSearchClient.getPrisoner(it)
 
           val prisonerEntity = DefendantEntity(firstName = prisoner.firstName)
           defendantRepository.save(prisonerEntity)
         }
+        // don't really like this, but it saves 1 call to getPrisonerNumbers
         if (page < totalPages) {
-          // don't really like this but it saves 1 call to getPrisonerNumbers
-          numbers = prisonServiceClient.getPrisonerNumbers(PageParams(page, pageSize))!!.numbers
+          RetryExecutor.runWithRetry(retryables, RETRIES, DELAY_MILLIS) {
+            numbers = prisonServiceClient.getPrisonerNumbers(PageParams(page, pageSize))!!.numbers
+          }
         }
       }
     }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/populatefromnomis/PopulateFromNomisIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/populatefromnomis/PopulateFromNomisIntTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.populatefromnomis
 
+import com.github.tomakehurst.wiremock.client.WireMock
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.untilAsserted
@@ -11,6 +12,47 @@ class PopulateFromNomisIntTest : IntegrationTestBase() {
 
   @Test
   fun `populate from nomis`() {
+    webTestClient.post()
+      .uri("/populatefromnomis")
+      .exchange()
+      .expectStatus()
+      .isOk
+
+    // will have to change to personrepository but this will be near enough
+
+    await.atMost(15, SECONDS) untilAsserted {
+      assertThat(defendantRepository.findAll().size).isEqualTo(7)
+    }
+    val prisoners = defendantRepository.findAll()
+    assertThat(prisoners[0].firstName).isEqualTo("PrisonerOneFirstName")
+    assertThat(prisoners[1].firstName).isEqualTo("PrisonerTwoFirstName")
+    assertThat(prisoners[2].firstName).isEqualTo("PrisonerThreeFirstName")
+    assertThat(prisoners[3].firstName).isEqualTo("PrisonerFourFirstName")
+    assertThat(prisoners[4].firstName).isEqualTo("PrisonerFiveFirstName")
+    assertThat(prisoners[5].firstName).isEqualTo("PrisonerSixFirstName")
+    assertThat(prisoners[6].firstName).isEqualTo("PrisonerSevenFirstName")
+  }
+
+  @Test
+  fun `populate from nomis retries getPrisonerNumbers`() {
+    // first call fails
+    wireMockExtension.stubFor(
+      WireMock.get("/api/prisoners/getPrisonerNumbers?size=1&page=0").willReturn(
+        WireMock.aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(503),
+      ),
+    )
+    // second call succeeds
+    wireMockExtension.stubFor(
+      WireMock.get("/api/prisoners/getPrisonerNumbers?size=1&page=0").willReturn(
+        WireMock.aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody("{\n  \"totalPages\": 7,\n  \"totalElements\": 7,\n  \"first\": true,\n  \"last\": false,\n  \"size\": 0,\n  \"content\": [\n    \"prisonerNumberOne\"\n  ],\n  \"number\": 0,\n  \"sort\": [\n    {\n      \"direction\": \"string\",\n      \"nullHandling\": \"string\",\n      \"ascending\": true,\n      \"property\": \"string\",\n      \"ignoreCase\": true\n    }\n  ],\n  \"numberOfElements\": 0,\n  \"pageable\": {\n    \"offset\": 0,\n    \"sort\": [\n      {\n        \"direction\": \"string\",\n        \"nullHandling\": \"string\",\n        \"ascending\": true,\n        \"property\": \"string\",\n        \"ignoreCase\": true\n      }\n    ],\n    \"pageSize\": 0,\n    \"pageNumber\": 0,\n    \"unpaged\": true,\n    \"paged\": true\n  },\n  \"empty\": true\n}")
+          .withStatus(200),
+
+      ),
+    )
     webTestClient.post()
       .uri("/populatefromnomis")
       .exchange()
