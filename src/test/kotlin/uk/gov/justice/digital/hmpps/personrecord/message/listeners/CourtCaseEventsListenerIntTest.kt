faa2146e704e00e676c57ae41265715e77b99f97
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-05-03 15:34:05 +0100

    CPR-310 Update number of times

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/CourtCaseEventsListenerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/CourtCaseEventsListenerIntTest.kt
@@ -86,7 +86,7 @@ class CourtCaseEventsListenerIntTest : IntegrationTestBase() {
     checkTelemetry(
       CPR_RECORD_UPDATED,
       mapOf("SourceSystem" to "HMCTS"),
-      times(999),
+      times(99),
     )
   }
 
