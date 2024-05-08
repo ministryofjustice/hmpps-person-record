5d73ce5d1311db595e0ea20bcab6fb2c2c3c5b13
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-05-08 13:15:20 +0100

    CPR-328 Drop old tables

--- src/main/resources/db/migration/V23__drop_old_tables.sql
+++ src/main/resources/db/migration/V23__drop_old_tables.sql
@@ -1,10 +1,15 @@
 BEGIN;
 -------------------------------------------------------
 
-DROP TABLE offender CASCADE;
-DROP TABLE address CASCADE;
-DROP TABLE contact;
-DROP TABLE offender_alias;
+TRUNCATE TABLE person_identifier CASCADE;
+DROP TABLE IF EXISTS address CASCADE;
+DROP TABLE IF EXISTS contact CASCADE;
+DROP TABLE IF EXISTS offender_alias CASCADE;
+DROP TABLE IF EXISTS offender CASCADE;
+DROP TABLE IF EXISTS defendant_alias CASCADE;
+DROP TABLE IF EXISTS defendant CASCADE;
+DROP TABLE IF EXISTS prisoner_alias CASCADE;
+DROP TABLE IF EXISTS prisoner CASCADE;
 
 -----------------------------------------------------
 COMMIT;
