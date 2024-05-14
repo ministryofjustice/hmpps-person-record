4086c587a5ebe67cba6816b56261b6ed692f24cd
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-05-14 15:41:43 +0100

    CPR-332 Re-try on locking exception

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/hmcts/CourtCaseEventsProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/hmcts/CourtCaseEventsProcessor.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.slf4j.LoggerFactory
 import org.springframework.dao.CannotAcquireLockException
+import org.springframework.orm.ObjectOptimisticLockingFailureException
 import org.springframework.orm.jpa.JpaSystemException
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
@@ -85,6 +86,10 @@ class CourtCaseEventsProcessor(
       when (e) {
         is CannotAcquireLockException, is JpaSystemException -> {
           log.warn("Expected error when processing $e.message")
+        }
+        is ObjectOptimisticLockingFailureException -> {
+          log.info("Locking exception, retrying message")
+          process(person)
         }
         else -> throw e
       }
