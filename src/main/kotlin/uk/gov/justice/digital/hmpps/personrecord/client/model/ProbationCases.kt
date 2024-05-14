305c5ac5d73aa0b7ba6d8eb7343294932e0f0ee4
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-05-14 16:18:41 +0100

    CPR-276 tests all passing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/ProbationCases.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/ProbationCases.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.personrecord.client.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.DeliusOffenderDetail
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ProbationCases(
@@ -11,5 +11,5 @@ data class ProbationCases(
   val first: Boolean,
   val last: Boolean,
   @JsonProperty("content")
-  val cases: List<DeliusOffenderDetail>,
+  val cases: List<ProbationCase>,
 )
