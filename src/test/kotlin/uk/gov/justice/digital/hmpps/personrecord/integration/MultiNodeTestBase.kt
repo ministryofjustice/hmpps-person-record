cc3e2b1845b8ee11ca30cb7af2f2b597d94ba9ad
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-05-15 16:01:51 +0100

    CPR-333 Reproduce the issue

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/integration/MultiNodeTestBase.kt
@@ -0,0 +1,83 @@
+package uk.gov.justice.digital.hmpps.personrecord.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilAsserted
+import org.awaitility.kotlin.untilCallTo
+import org.jmock.lib.concurrent.Blitzer
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.verification.VerificationMode
+import org.springframework.boot.builder.SpringApplicationBuilder
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue
+import software.amazon.awssdk.services.sns.model.PublishRequest
+import uk.gov.justice.digital.hmpps.personrecord.HmppsPersonRecord
+import uk.gov.justice.digital.hmpps.personrecord.model.hmcts.MessageType.COMMON_PLATFORM_HEARING
+import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.PNCIdentifier
+import uk.gov.justice.digital.hmpps.personrecord.service.helper.commonPlatformHearingWithSameDefendantIdTwice
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_RECORD_CREATED
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_RECORD_UPDATED
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.countMessagesOnQueue
+import java.time.Duration
+
+
+class MultiNodeTestBase {
+
+
+  @Test
+  fun `should have requests from 2 instances`() {
+    val instance1: SpringApplicationBuilder = SpringApplicationBuilder(HmppsPersonRecord::class.java)
+      .profiles("test")
+    instance1.run()
+
+    val courtCaseEventsTopic =
+      (instance1.context().getBean("hmppsQueueService") as HmppsQueueService)
+      .findByTopicId("courtcaseeventstopic")
+
+    val courtCaseEventsQueue =
+      (instance1.context().getBean("hmppsQueueService") as HmppsQueueService)
+        .findByQueueId("cprcourtcaseeventsqueue")
+
+    val instance2: SpringApplicationBuilder = SpringApplicationBuilder(HmppsPersonRecord::class.java)
+      .profiles("test", "test-different-port")
+    instance2.run()
+
+    // given
+    val pncNumber = PNCIdentifier.from("2003/0062845E")
+
+    val publishRequest = PublishRequest.builder()
+      .topicArn(courtCaseEventsTopic?.arn)
+      .message(commonPlatformHearingWithSameDefendantIdTwice(pncNumber = pncNumber.pncId))
+      .messageAttributes(
+        mapOf(
+          "messageType" to MessageAttributeValue.builder().dataType("String")
+            .stringValue(COMMON_PLATFORM_HEARING.name).build(),
+        ),
+      )
+      .build()
+    // when
+    val blitzer = Blitzer(100, 10)
+    try {
+      blitzer.blitz {
+        courtCaseEventsTopic?.snsClient?.publish(publishRequest)?.get()
+      }
+    } finally {
+      blitzer.shutdown()
+    }
+
+    // then
+    await untilCallTo {
+      courtCaseEventsQueue?.sqsClient?.countMessagesOnQueue(courtCaseEventsQueue.queueUrl)?.get()
+    } matches { it == 0 }
+
+    await untilCallTo {
+      courtCaseEventsQueue?.sqsDlqClient?.countMessagesOnQueue(courtCaseEventsQueue.dlqUrl!!)?.get()
+    } matches { it == 0 }
+  }
+
+}
