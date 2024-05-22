92347a58fcd86e0bd01daa51202e529c025392c4
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-05-22 12:23:15 +0100

    CPR-305 one of PopulateFromProbation tests passing

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateFromProbationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateFromProbationIntTest.kt
@@ -5,13 +5,17 @@ import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.untilAsserted
+import org.awaitility.kotlin.untilNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.personrecord.integration.WebTestBase
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.CROIdentifier
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
+import uk.gov.justice.digital.hmpps.personrecord.service.helper.allProbationCasesResponse
+import uk.gov.justice.digital.hmpps.personrecord.service.helper.allProbationCasesSingleResponse
 import java.time.LocalDate
+import java.util.UUID.randomUUID
 import java.util.concurrent.TimeUnit.SECONDS
 
 class PopulateFromProbationIntTest : WebTestBase() {
@@ -21,37 +25,49 @@ class PopulateFromProbationIntTest : WebTestBase() {
 
   @Test
   fun `populate from probation`() {
+    val scenarioName = "populate"
+    val crnOne: String = randomUUID().toString()
+    val crnTwo: String = randomUUID().toString()
+    val crnThree: String = randomUUID().toString()
+    val crnFour: String = randomUUID().toString()
+    val crnFive: String = randomUUID().toString()
+    val crnSix: String = randomUUID().toString()
+    val crnSeven: String = randomUUID().toString()
+    stubResponse(crnOne, "POPOne", crnTwo, "POPTwo", 0, scenarioName, STARTED)
+    stubResponse(crnThree, "POPThree", crnFour, "POPFour", 1, scenarioName, STARTED)
+    stubResponse(crnFive, "POPFive", crnSix, "POPSix", 2, scenarioName, STARTED)
+    stubSingleResponse(crnSeven, "POPSeven", 3, scenarioName, STARTED)
+
     webTestClient.post()
       .uri("/populatefromprobation")
       .exchange()
       .expectStatus()
       .isOk
 
-    await.atMost(15, SECONDS) untilAsserted {
-      assertThat(personRepository.findAll().size).isEqualTo(7)
+    await.atMost(15, SECONDS) untilNotNull {
+      personRepository.findByCrn(crnSeven)
     }
-    val pops = personRepository.findAll()
-    val pop = pops[0]
-    assertThat(pop.firstName).isEqualTo("POPOneFirstName")
-    assertThat(pop.middleNames).isEqualTo("POPOneMiddleNameOne POPOneMiddleNameTwo")
-    assertThat(pop.lastName).isEqualTo("POPOneLastName")
-    assertThat(pop.crn).isEqualTo("D001022")
-    assertThat(pop.dateOfBirth).isEqualTo(LocalDate.of(1980, 8, 29))
-    assertThat(pop.aliases[0].firstName).isEqualTo("POPOneAliasOneFirstName")
-    assertThat(pop.aliases[0].middleNames).isEqualTo("POPOneAliasOneMiddleNameOne POPOneAliasOneMiddleNameTwo")
-    assertThat(pop.aliases[0].lastName).isEqualTo("POPOneAliasOneLastName")
-    assertThat(pop.aliases[1].firstName).isEqualTo("POPOneAliasTwoFirstName")
-    assertThat(pop.aliases[1].middleNames).isEqualTo("POPOneAliasTwoMiddleNameOne POPOneAliasTwoMiddleNameTwo")
-    assertThat(pop.aliases[1].lastName).isEqualTo("POPOneAliasTwoLastName")
-    assertThat(pop.sourceSystem).isEqualTo(DELIUS)
-    assertThat(pops[1].firstName).isEqualTo("POPTwoFirstName")
-    assertThat(pops[2].firstName).isEqualTo("POPThreeFirstName")
-    assertThat(pops[3].firstName).isEqualTo("POPFourFirstName")
-    assertThat(pops[4].firstName).isEqualTo("POPFiveFirstName")
-    assertThat(pops[5].firstName).isEqualTo("POPSixFirstName")
-    assertThat(pops[6].firstName).isEqualTo("POPSevenFirstName")
-    assertThat(pops[6].middleNames).isEqualTo("")
-    assertThat(pops[6].cro).isEqualTo(CROIdentifier.from(""))
+    val popOne = personRepository.findByCrn(crnOne)!!
+    assertThat(popOne.firstName).isEqualTo("POPOneFirstName")
+    assertThat(popOne.middleNames).isEqualTo("POPOneMiddleNameOne POPOneMiddleNameTwo")
+    assertThat(popOne.lastName).isEqualTo("POPOneLastName")
+    assertThat(popOne.crn).isEqualTo(crnOne)
+    assertThat(popOne.dateOfBirth).isEqualTo(LocalDate.of(1980, 8, 29))
+    assertThat(popOne.aliases[0].firstName).isEqualTo("POPOneAliasOneFirstName")
+    assertThat(popOne.aliases[0].middleNames).isEqualTo("POPOneAliasOneMiddleNameOne POPOneAliasOneMiddleNameTwo")
+    assertThat(popOne.aliases[0].lastName).isEqualTo("POPOneAliasOneLastName")
+    assertThat(popOne.aliases[1].firstName).isEqualTo("POPOneAliasTwoFirstName")
+    assertThat(popOne.aliases[1].middleNames).isEqualTo("POPOneAliasTwoMiddleNameOne POPOneAliasTwoMiddleNameTwo")
+    assertThat(popOne.aliases[1].lastName).isEqualTo("POPOneAliasTwoLastName")
+    assertThat(popOne.sourceSystem).isEqualTo(DELIUS)
+    assertThat(personRepository.findByCrn(crnTwo)!!.firstName).isEqualTo("POPTwoFirstName")
+    assertThat(personRepository.findByCrn(crnThree)!!.firstName).isEqualTo("POPThreeFirstName")
+    assertThat(personRepository.findByCrn(crnFour)!!.firstName).isEqualTo("POPFourFirstName")
+    assertThat(personRepository.findByCrn(crnFive)!!.firstName).isEqualTo("POPFiveFirstName")
+    assertThat(personRepository.findByCrn(crnSix)!!.firstName).isEqualTo("POPSixFirstName")
+    assertThat(personRepository.findByCrn(crnSeven)!!.firstName).isEqualTo("POPSevenFirstName")
+    assertThat(personRepository.findByCrn(crnSeven)!!.middleNames).isEqualTo("")
+    assertThat(personRepository.findByCrn(crnSeven)!!.cro).isEqualTo(CROIdentifier.from(""))
   }
 
   @Test
@@ -132,5 +148,33 @@ class PopulateFromProbationIntTest : WebTestBase() {
     assertThat(pops[5].firstName).isEqualTo("POPSixFirstName")
     assertThat(pops[6].firstName).isEqualTo("POPSevenFirstName")
     assertThat(pops[6].aliases.size).isEqualTo(0)
+  }
+
+  private fun stubResponse(firstCrn: String, firstPrefix: String, secondCrn: String, secondPrefix: String, page: Int, scenarioName: String, scenarioState: String) {
+    wiremock.stubFor(
+      WireMock.get("/all-probation-cases?size=2&page=$page&sort=id%2Casc")
+        .inScenario(scenarioName)
+        .whenScenarioStateIs(scenarioState)
+        .willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(allProbationCasesResponse(firstCrn, firstPrefix, secondCrn, secondPrefix))
+            .withStatus(200),
+        ),
+    )
+  }
+
+  private fun stubSingleResponse(firstCrn: String, firstPrefix: String, page: Int, scenarioName: String, scenarioState: String) {
+    wiremock.stubFor(
+      WireMock.get("/all-probation-cases?size=2&page=$page&sort=id%2Casc")
+        .inScenario(scenarioName)
+        .whenScenarioStateIs(scenarioState)
+        .willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(allProbationCasesSingleResponse(firstCrn, firstPrefix))
+            .withStatus(200),
+        ),
+    )
   }
 }
