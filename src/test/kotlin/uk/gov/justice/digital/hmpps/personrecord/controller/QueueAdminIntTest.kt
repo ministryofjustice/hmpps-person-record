f2e0ddd3d0bdf98a3882464dd259bab07fd87ba9
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-05-28 16:48:55 +0100

    CPR-293 failing test to purge queue

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/controller/QueueAdminIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/controller/QueueAdminIntTest.kt
@@ -6,10 +6,33 @@ import uk.gov.justice.digital.hmpps.personrecord.integration.WebTestBase
 class QueueAdminIntTest() : WebTestBase() {
 
   @Test
-  fun `should return HTTP Location header containing the URL of new person`() {
+  fun `should retry dead letter queue`() {
     webTestClient
       .put()
       .uri("/queue-admin/retry-all-dlqs")
+      .authorised()
+      .exchange()
+      .expectStatus()
+      .isOk
+  }
+
+  @Test
+  fun `should return 401 for unauthorised request to purge dead letter queue`() {
+    webTestClient
+      .put()
+      .uri("/queue-admin/purge-queue/cpr_court_case_events_queue_dlq")
+      .authorised(listOf("WRONG_ROLE"))
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+
+  @Test
+  fun `should return 200 for authorised request to purge dead letter queue`() {
+    webTestClient
+      .put()
+      .uri("/queue-admin/purge-queue/cpr_court_case_events_queue_dlq")
+      .authorised()
       .exchange()
       .expectStatus()
       .isOk
