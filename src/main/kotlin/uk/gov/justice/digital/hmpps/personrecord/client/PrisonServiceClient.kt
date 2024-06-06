78631d80722a04d1cfe84ef64d3af6b9d0cf5032
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2024-06-06 09:32:18 +0100

    FIX: remove unused wiremock mappings (#373)
    * FIX: remove unused wiremock mappings
    
    * FIX: remove unused prisoner address mapping
    
    * FIX: remove unused prisoner address response
    
    * FIX: remove more mappings and redundant test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonServiceClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonServiceClient.kt
@@ -3,10 +3,7 @@ package uk.gov.justice.digital.hmpps.personrecord.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.cloud.openfeign.SpringQueryMap
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import uk.gov.justice.digital.hmpps.personrecord.client.model.PrisonerNumbers
-import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.Address
-import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.PrisonerDetails
 import uk.gov.justice.digital.hmpps.personrecord.config.PrisonServiceOAuth2Config
 
 @FeignClient(
@@ -15,12 +12,6 @@ import uk.gov.justice.digital.hmpps.personrecord.config.PrisonServiceOAuth2Confi
   configuration = [PrisonServiceOAuth2Config::class],
 )
 interface PrisonServiceClient {
-
-  @GetMapping(value = ["/api/offenders/{prisonerNumber}"])
-  fun getPrisonerDetails(@PathVariable("prisonerNumber") prisonerNumber: String): PrisonerDetails?
-
-  @GetMapping(value = ["/api/offenders/{prisonerNumber}/addresses"])
-  fun getPrisonerAddresses(@PathVariable("prisonerNumber") prisonerNumber: String): List<Address>?
 
   @GetMapping(value = ["/api/prisoners/prisoner-numbers"])
   fun getPrisonerNumbers(@SpringQueryMap params: PageParams): PrisonerNumbers?
