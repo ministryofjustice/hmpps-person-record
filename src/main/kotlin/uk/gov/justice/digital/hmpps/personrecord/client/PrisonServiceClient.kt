fc8922ef76c132a11575a54c332a91745dcd6c5b
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-06-06 09:11:11 +0100

    FIX: remove more mappings and redundant test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonServiceClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonServiceClient.kt
@@ -3,9 +3,7 @@ package uk.gov.justice.digital.hmpps.personrecord.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.cloud.openfeign.SpringQueryMap
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import uk.gov.justice.digital.hmpps.personrecord.client.model.PrisonerNumbers
-import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.PrisonerDetails
 import uk.gov.justice.digital.hmpps.personrecord.config.PrisonServiceOAuth2Config
 
 @FeignClient(
@@ -14,9 +12,6 @@ import uk.gov.justice.digital.hmpps.personrecord.config.PrisonServiceOAuth2Confi
   configuration = [PrisonServiceOAuth2Config::class],
 )
 interface PrisonServiceClient {
-
-  @GetMapping(value = ["/api/offenders/{prisonerNumber}"])
-  fun getPrisonerDetails(@PathVariable("prisonerNumber") prisonerNumber: String): PrisonerDetails?
 
   @GetMapping(value = ["/api/prisoners/prisoner-numbers"])
   fun getPrisonerNumbers(@SpringQueryMap params: PageParams): PrisonerNumbers?
