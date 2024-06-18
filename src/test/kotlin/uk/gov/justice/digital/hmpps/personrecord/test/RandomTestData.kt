d208548d6727311ffa56a8871378f321ec57de6d
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2024-06-18 16:03:33 +0100

    Cpr 264 further randomisation of tests (#416)
    * CPR-264 make tests more random
    
    * CPR-264 do not use GUIDS for names as they do not soundex very well

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/RandomTestData.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/RandomTestData.kt
@@ -3,21 +3,23 @@ package uk.gov.justice.digital.hmpps.personrecord.test
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.UUID
+import kotlin.text.Charsets.UTF_8
 
 fun randomPnc(): String {
-  val allPNCs = Files.readAllLines(Paths.get("src/test/resources/valid_pncs.csv"), Charsets.UTF_8)
-
+  val allPNCs = Files.readAllLines(Paths.get("src/test/resources/valid_pncs.csv"), UTF_8)
   return allPNCs.get((0..allPNCs.size).random())
 }
 
 fun randomCro(): String {
-  val allCROs = Files.readAllLines(Paths.get("src/test/resources/valid_cros.csv"), Charsets.UTF_8)
-
+  val allCROs = Files.readAllLines(Paths.get("src/test/resources/valid_cros.csv"), UTF_8)
   return allCROs.get((0..allCROs.size).random())
 }
 
-fun randomFirstName(): String = UUID.randomUUID().toString()
-
-fun randomLastName(): String = UUID.randomUUID().toString()
+fun randomFirstName(): String = randomString()
+fun randomLastName(): String = randomString()
 fun randomNationalInsuranceNumber(): String = UUID.randomUUID().toString()
 fun randomDriverLicenseNumber(): String = UUID.randomUUID().toString()
+
+private fun randomString(): String = (1..(6 + (Math.random() * 7).toInt())).map {
+  ('a' + (Math.random() * 26).toInt())
+}.joinToString("")
