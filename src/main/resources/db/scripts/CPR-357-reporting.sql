242ac8f7e89b50e8c33c2f86b8039da526faca96
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-06-20 15:54:50 +0100

    CPR-357 add a markdown report

--- src/main/resources/db/scripts/CPR-357-reporting.sql
+++ src/main/resources/db/scripts/CPR-357-reporting.sql
@@ -1,26 +1,20 @@
--- total number of person records with a UUID -- 1359844
+-- total number of person records with a UUID
 select count (*) from personrecordservice.person where fk_person_identifier_id is not null;
 
--- total number of UUIDs -- 781335
+-- total number of UUIDs
 select count(*) from personrecordservice.person_identifier;
 
 -- records with a UUID by source system
--- DELIUS 712030
--- NOMIS	 647814
 select source_system, count(fk_person_identifier_id) from personrecordservice.person p1
 where p1.fk_person_identifier_id is not null
 group by p1.source_system;
 
 -- records with no UUID by source system
--- DELIUS	1747627
--- HMCTS	18004
--- NOMIS	166487
 select source_system, count(*) from personrecordservice.person p1
 where p1.fk_person_identifier_id is null
 group by p1.source_system;
 
 -- linked records
--- 615330
 select count(*) from personrecordservice.person_identifier pi
                          join personrecordservice.person probation
                               on pi.id = probation.fk_person_identifier_id
@@ -30,7 +24,6 @@ select count(*) from personrecordservice.person_identifier pi
                                   and prison.source_system = 'NOMIS';
 
 -- UUID on Probation record only
--- 96700
 select count(*) from personrecordservice.person_identifier pi
                          join personrecordservice.person probation
                               on pi.id = probation.fk_person_identifier_id
@@ -40,7 +33,6 @@ where not exists(select * from personrecordservice.person prison
                    and prison.source_system = 'NOMIS');
 
 -- UUID on Prison record only
--- 32484
 select count(*) from personrecordservice.person_identifier pi
                          join personrecordservice.person prison
                               on pi.id = prison.fk_person_identifier_id
