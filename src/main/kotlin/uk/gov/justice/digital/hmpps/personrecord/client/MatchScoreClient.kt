ff4b32f4a5b0138e625fbf29eab2bb2d27d32edd
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-06-21 11:45:31 +0100

    CPR-264 define import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/MatchScoreClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/MatchScoreClient.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import java.util.*
+import java.util.UUID
 
 @FeignClient(
   name = "match-score",
