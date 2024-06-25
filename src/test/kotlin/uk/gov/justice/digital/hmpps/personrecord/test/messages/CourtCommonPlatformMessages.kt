a8748e7cc75ba04a52f54f5e2b02bc89e5341eb6
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-06-25 17:08:49 +0100

    CPR-353 final court message converted

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/messages/CourtCommonPlatformMessages.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/messages/CourtCommonPlatformMessages.kt
@@ -5,8 +5,14 @@ import uk.gov.justice.digital.hmpps.personrecord.test.randomFirstName
 import uk.gov.justice.digital.hmpps.personrecord.test.randomLastName
 import java.util.UUID
 
-data class CommonPlatformHearingSetup(val pnc: String? = null, val firstName: String? = randomFirstName(), val lastName: String = randomLastName(), val dateOfBirth: String = "1975-01-01", val cro: String = randomCro(), val defendantId: String = UUID.randomUUID().toString())
-
+data class CommonPlatformHearingSetup(val pnc: String? = null, val firstName: String? = randomFirstName(), val middleName: String? = null, val lastName: String = randomLastName(), val dateOfBirth: String = "1975-01-01", val cro: String = randomCro(), val defendantId: String = UUID.randomUUID().toString(), val aliases: List<CommonPlatformHearingSetupAlias>? = null, val contact: CommonPlatformHearingSetupContact? = null)
+data class CommonPlatformHearingSetupAlias(val firstName: String, val lastName: String)
+data class CommonPlatformHearingSetupContact(
+  val home: String = "0207345678",
+  val work: String = "0203788776",
+  val mobile: String = "078590345677",
+  val primaryEmail: String = "email@email.com",
+)
 fun commonPlatformHearing(commonPlatformHearingSetup: List<CommonPlatformHearingSetup>) = """
     {
       "hearing": {
@@ -74,194 +80,45 @@ private fun defendant(commonPlatformHearingSetup: CommonPlatformHearingSetup) =
                   "personDetails": {
                     "address": {
                       "address1": "13 Wind Street",
-                      "address2": "Swansea",
+                      "address2": "Cardiff",
                       "address3": "Wales",
                       "address4": "UK",
                       "address5": "Earth",
-                      "postcode": "SA1 1FU"
+                      "postcode": "CF10 1FU"
                     },
+                    ${commonPlatformHearingSetup.contact?.let {
+    """
+                      "contact": {
+                       "home": "${commonPlatformHearingSetup.contact.home}",
+                        "work": "${commonPlatformHearingSetup.contact.work}",
+                        "mobile": "${commonPlatformHearingSetup.contact.mobile}",
+                        "primaryEmail": "${commonPlatformHearingSetup.contact.primaryEmail}"
+                       }, 
+    """.trimIndent()
+  } ?: ""}   
                     "dateOfBirth": "${commonPlatformHearingSetup.dateOfBirth}",
                     ${commonPlatformHearingSetup.firstName?.let { """ "firstName": "${commonPlatformHearingSetup.firstName}", """.trimIndent() } ?: ""}
                     "gender": "MALE",
+                    ${commonPlatformHearingSetup.middleName?.let { """ "middleName": "${commonPlatformHearingSetup.middleName}", """.trimIndent() } ?: ""}
                     "lastName": "${commonPlatformHearingSetup.lastName}",
-                    "title": "Mr"
-                  }
-                },
-                "aliases": [
-                  {
-                    "firstName": "aliasFirstName1",
-                    "lastName": "alisLastName1"
-                  },
-                  {
-                    "firstName": "aliasFirstName2",
-                    "lastName": "alisLastName2"
-                  }
-                ],
-                "prosecutionCaseId": "D2B61C8A-0684-4764-B401-F0A788BC7CCF"
-              }
-  """.trimIndent()
-
-fun commonPlatformHearingWithAdditionalFields(defendantIds: List<String>) = """
-    {
-      "hearing": {
-        "type": {
-          "description": "sentence"
-        },
-        "courtCentre": {
-          "id": "9b583616-049b-30f9-a14f-028a53b7cfe8",
-          "roomId": "7cb09222-49e1-3622-a5a6-ad253d2b3c39",
-          "roomName": "Crown Court 3-1",
-          "code": "B10JQ00"
-        },
-        "hearingDays": [
-          {
-            "listedDurationMinutes": 60,
-            "listingSequence": 0,
-            "sittingDay": "2021-09-08T09:00:00.000Z"
-          },
-          {
-            "listedDurationMinutes": 30,
-            "listingSequence": 1,
-            "sittingDay": "2021-09-09T10:30:00.000Z"
-          }
-        ],
-        "id": "E10E3EF3-8637-40E3-BDED-8ED104A380AC",
-        "jurisdictionType": "CROWN",
-        "prosecutionCases": [
-          {
-            "defendants": [
-              { 
-                "id": "${defendantIds[0]}",
-                "masterDefendantId": "eeb71c73-573b-444e-9dc3-4e5998d1be65",
-                "pncId": "2003/0062845E",
-                "croNumber": "33577/63G",
-                "offences": [
-                  {
-                    "id": "a63d9020-aa6b-4997-92fd-72a692b036de",
-                    "offenceLegislation": "Contrary to section 20 of the Offences Against the    Person Act 1861.",
-                    "offenceTitle": "Wound / inflict grievous bodily harm without intent",
-                    "wording": "on 01/08/2009 at  the County public house, unlawfully and maliciously wounded, John Smith",
-                    "listingNumber": 30,
-                    "offenceCode": "ABC001"
-                  }
-                ],
-                "personDefendant": {
-                  "personDetails": {
-                    "address": {
-                      "address1": "13 broad Street",
-                      "address2": "Cardiff",
-                      "address3": "Wales",
-                      "address4": "UK",
-                      "address5": "Earth",
-                      "postcode": "CF10 1FU"
-                    },
-                    "dateOfBirth": "1960-01-01",
-                    "firstName": "Eric",
-                    "gender": "MALE",
-                    "middleName": "mName1 mName2",
-                    "lastName": "Lassard",
-                    "title": "Mr"
-                  }
-                },
-                "aliases": [ 
-                    {
-                     "firstName": "aliasFirstName1",
-                     "lastName": "alisLastName1"
-                    },
-                    {
-                     "firstName": "aliasFirstName2",
-                     "lastName": "alisLastName2"
-                    }
-                  ],
-                "prosecutionCaseId": "D2B61C8A-0684-4764-B401-F0A788BC7CCF"
-              },
-              { 
-                "id": "${defendantIds[1]}",
-                "masterDefendantId": "1f6847a2-6663-44dd-b945-fe2c20961d0a",
-                "pncId": "2008/0056560Z",
-                "croNumber": "78182/63Q",
-                "offences": [
-                  {
-                    "id": "a63d9020-aa6b-4997-92fd-72a692b036de",
-                    "offenceLegislation": "Contrary to section 20 of the Offences Against the    Person Act 1861.",
-                    "offenceTitle": "Wound / inflict grievous bodily harm without intent",
-                    "wording": "on 01/08/2009 at  the County public house, unlawfully and maliciously wounded, John Smith",
-                    "listingNumber": 30,
-                    "offenceCode": "ABC001"
-                  }
-                ],
-                "personDefendant": {
-                  "personDetails": {
-                    "address": {
-                      "address1": "13 broad Street",
-                      "address2": "Cardiff",
-                      "address3": "Wales",
-                      "address4": "UK",
-                      "address5": "Earth",
-                      "postcode": "CF10 1FU"
-                    },
-                    "contact": {
-                      "home": "0207345678",
-                      "work": "0203788776",
-                      "mobile": "078590345677",
-                      "primaryEmail": "email@email.com"
-                    },
-                    "dateOfBirth": "1960-01-01",
-                    "firstName": "CAREY",
-                    "gender": "MALE",
-                    "lastName": "Mahoney",
-                    "title": "Mr"
+                    "title": "Mr",
+                    "nationalityCode": "GB",
+                    "nationalInsuranceNumber": "PC456743D"
                   }
                 },
                 "ethnicity": {
                    "observedEthnicityDescription": "observedEthnicityDescription",
                    "selfDefinedEthnicityDescription": "selfDefinedEthnicityDescription"
                 },
+                ${commonPlatformHearingSetup.aliases?.let { """ "aliases": [${commonPlatformHearingSetup.aliases.map { alias(it) }.joinToString(",")}], """.trimIndent() } ?: ""}
                 "prosecutionCaseId": "D2B61C8A-0684-4764-B401-F0A788BC7CCF"
-              },
-              { 
-                "id": "${defendantIds[2]}",
-                "masterDefendantId": "290e0457-1480-4e62-b3c8-7f29ef791c58",
-                "pncId": "20230583843L",
-                "croNumber": "15542/64K",
-                "offences": [
+              }
+  """.trimIndent()
+
+private fun alias(alias: CommonPlatformHearingSetupAlias) =
+  """
                   {
-                    "id": "a63d9020-aa6b-4997-92fd-72a692b036de",
-                    "offenceLegislation": "Contrary to section 20 of the Offences Against the    Person Act 1861.",
-                    "offenceTitle": "Wound / inflict grievous bodily harm without intent",
-                    "wording": "on 01/08/2009 at  the County public house, unlawfully and maliciously wounded, John Smith",
-                    "listingNumber": 30,
-                    "offenceCode": "ABC001"
+                    "firstName": "${alias.firstName}",
+                    "lastName": "${alias.lastName}"
                   }
-                ],
-                "personDefendant": {
-                  "personDetails": {
-                    "address": {
-                      "address1": "15 broad Street",
-                      "address2": "Cardiff",
-                      "address3": "Wales",
-                      "address4": "UK",
-                      "address5": "Earth",
-                      "postcode": "CF10 1FU"
-                    },
-                    "dateOfBirth": "1964-01-01",
-                    "firstName": "Leslie",
-                    "gender": "MALE",
-                    "lastName": "Barbara",
-                    "title": "Mr",
-                    "nationalityCode": "GB",
-                    "nationalInsuranceNumber": "PC456743D"
-                  }
-                },
-                "prosecutionCaseId": "D2B61C8A-0684-4764-B401-F0A788BC7CCF"
-              } 
-            ],
-            "id": "D2B61C8A-0684-4764-B401-F0A788BC7CCF",
-            "prosecutionCaseIdentifier": {
-              "caseURN": "25GD34377719"
-            }
-          }
-        ]
-      }
-    }
-""".trimIndent()
+  """.trimIndent()
