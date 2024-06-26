0cb8939f2c6b6168600bd600a380f80253d245b8
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-06-26 09:51:47 +0100

    CPR-215 search for matching records (#436)
    * CPR-215 Fix prison tests
    
    * CPR-215 Update libra tests
    
    * CPR-215 Update Probation test
    
    * CPR-215 Update common platform tests
    
    * CPR-215 Add test to verify search between source systems
    
    * CPR-215 Should expect 2 defendants create
    
    * CPR-215 Fix lint
    
    * CPR-215 Revert back pncId test
    
    * CPR-215 Simplify alais mapping

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/libra/LibraCourtCaseListenerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/libra/LibraCourtCaseListenerIntTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.listeners.court.libra
 
-import com.github.tomakehurst.wiremock.client.WireMock
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.untilAsserted
@@ -17,6 +16,7 @@ import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.specifications.P
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.LIBRA
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.COURT_MESSAGE_RECEIVED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_CANDIDATE_RECORD_SEARCH
@@ -59,6 +59,7 @@ class LibraCourtCaseListenerIntTest : MessagingMultiNodeTestBase() {
         "HIGH_CONFIDENCE_COUNT" to "0",
         "LOW_CONFIDENCE_COUNT" to "0",
       ),
+      times = 2,
     )
     checkTelemetry(CPR_RECORD_CREATED, mapOf("SOURCE_SYSTEM" to "LIBRA"))
 
@@ -139,6 +140,48 @@ class LibraCourtCaseListenerIntTest : MessagingMultiNodeTestBase() {
   }
 
   @Test
+  fun `should process and create libra message and link to different source system record`() {
+    val firstName = randomFirstName()
+
+    personRepository.saveAndFlush(
+      PersonEntity.from(
+        Person(
+          firstName = firstName,
+          lastName = "MORGAN",
+          dateOfBirth = LocalDate.of(1975, 1, 1),
+          addresses = listOf(Address("NT4 6YH")),
+          sourceSystemType = DELIUS,
+        ),
+      ),
+    )
+
+    val matchResponse = MatchResponse(matchProbabilities = mutableMapOf("0" to 0.9999999))
+    stubMatchScore(matchResponse)
+
+    val libraMessage = LibraMessage(firstName = firstName, cro = "", pncNumber = "")
+    val messageId1 = publishHMCTSMessage(libraHearing(libraMessage), LIBRA_COURT_CASE)
+    checkTelemetry(
+      COURT_MESSAGE_RECEIVED,
+      mapOf(
+        "EVENT_TYPE" to LIBRA_COURT_CASE.name,
+        "MESSAGE_ID" to messageId1,
+        "SOURCE_SYSTEM" to LIBRA.name,
+      ),
+    )
+
+    checkTelemetry(CPR_RECORD_CREATED, mapOf("SOURCE_SYSTEM" to "LIBRA"))
+    checkTelemetry(
+      CPR_CANDIDATE_RECORD_SEARCH,
+      mapOf(
+        "SOURCE_SYSTEM" to LIBRA.name,
+        "RECORD_COUNT" to "1",
+        "HIGH_CONFIDENCE_COUNT" to "1",
+        "LOW_CONFIDENCE_COUNT" to "0",
+      ),
+    )
+  }
+
+  @Test
   fun `should process and create new person with low score`() {
     val firstName = randomFirstName()
 
@@ -165,6 +208,7 @@ class LibraCourtCaseListenerIntTest : MessagingMultiNodeTestBase() {
         "HIGH_CONFIDENCE_COUNT" to "0",
         "LOW_CONFIDENCE_COUNT" to "1",
       ),
+      times = 2,
     )
 
     checkTelemetry(CPR_RECORD_CREATED, mapOf("SOURCE_SYSTEM" to "LIBRA"), times = 2)
@@ -300,18 +344,6 @@ class LibraCourtCaseListenerIntTest : MessagingMultiNodeTestBase() {
         "RECORD_COUNT" to "1000000",
         "SEARCH_VERSION" to SEARCH_VERSION,
       ),
-    )
-  }
-
-  private fun stubMatchScore(matchResponse: MatchResponse) {
-    wiremock.stubFor(
-      WireMock.post("/person/match")
-        .willReturn(
-          WireMock.aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(200)
-            .withBody(objectMapper.writeValueAsString(matchResponse)),
-        ),
     )
   }
 }
