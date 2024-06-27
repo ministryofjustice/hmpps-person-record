ecf7aed1aa389613a25365e9768ba03c4601ef76
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-06-27 12:35:28 +0100

    CPR-353 try deleting just one of the unused classes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/prisoner/PhysicalAttributes.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class PhysicalAttributes(
-  val sexCode: String? = null,
-  val raceCode: String? = null,
-)
