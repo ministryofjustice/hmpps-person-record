2c874ef68668a269fd2c7b50b4f9aeb59ed254a6
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-07-02 14:48:06 +0100

    Revert "CPR-359 drop unnecessary indexes"
    This reverts commit 343c6fea5240aa463d6490de2e5670eb868bbb76.

--- src/main/resources/db/migration/V34__drop_indexes_on_child_tables.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
--------------------------------------------------------
-DROP INDEX idx_address_fk_person_id;
-DROP INDEX idx_alias_fk_person_id;
-DROP INDEX idx_contact_fk_person_id;
------------------------------------------------------
-COMMIT;
