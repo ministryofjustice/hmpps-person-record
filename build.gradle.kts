b9a341f87aa36b2243f64a6637925f2a5a3c8b72
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-07-08 17:31:09 +0100

    CPR-347 use latest version of hmpps-kotlin-spring-boot-starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.1")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
