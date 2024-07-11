3b426174120905a0d886b8fce707e2526ae41232
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2024-07-11 08:16:54 +0100

    CPR-347-remove-jwtauth (#494)
    * CPR-347-remove-jwtauth
    removed custom jwt auth / decoder to use one provided by hmpps-kotlin-lib
    
    * CPR-347 set jwk url for all requests
    
    ---------
    
    Co-authored-by: Mark Rees <mark.rees@digital.justice.gov.uk>
    Co-authored-by: markreesmoj <76954782+markreesmoj@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("org.jmock:jmock:2.13.1")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2")
 }
 
 java {
