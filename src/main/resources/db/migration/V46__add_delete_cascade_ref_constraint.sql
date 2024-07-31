c6461367187eb3539761f212c261ff13e41f49a9
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-07-31 15:47:53 +0100

    CPR-417 Update migration

--- src/main/resources/db/migration/V46__add_delete_cascade_ref_constraint.sql
+++ src/main/resources/db/migration/V46__add_delete_cascade_ref_constraint.sql
@@ -4,7 +4,5 @@ BEGIN;
 ALTER TABLE IF EXISTS reference DROP CONSTRAINT fk_person_reference_id;
 ALTER TABLE IF EXISTS reference add constraint fk_person_reference_id foreign key (fk_person_id) references person ON DELETE CASCADE;
 
-DELETE FROM person WHERE source_system = 'LIBRA';
-
 -----------------------------------------------------
 COMMIT;
