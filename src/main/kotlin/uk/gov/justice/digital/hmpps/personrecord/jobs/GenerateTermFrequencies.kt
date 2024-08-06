8e7d57dde32189840d9d1fd302f0d5fd0af5caf3
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-08-06 14:44:24 +0100

    CPR-288 Simplify pnc frequency generation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/GenerateTermFrequencies.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/GenerateTermFrequencies.kt
@@ -12,13 +12,11 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.client.model.termfrequency.TermFrequency
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.termfrequency.PncFrequencyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PncFrequencyRepository
-import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.ReferenceRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.types.IdentifierType
 
 @RestController
 class GenerateTermFrequencies(
   private val pncFrequencyRepository: PncFrequencyRepository,
-  private val referenceRepository: ReferenceRepository,
 ) {
 
   @RequestMapping(method = [RequestMethod.POST], value = ["/generate/termfrequencies"])
@@ -31,33 +29,12 @@ class GenerateTermFrequencies(
     CoroutineScope(Dispatchers.Default).launch {
       log.info("Starting PNC term frequency generation")
       pncFrequencyRepository.deleteAll()
-      val totalElements = forPage { page ->
-        val pncFrequencyEntities = page.map { PncFrequencyEntity.from(it) }
-        pncFrequencyRepository.saveAllAndFlush(pncFrequencyEntities)
-      }
-      log.info("Finished PNC term frequency generation, Total PNC's: $totalElements")
+      pncFrequencyRepository.generatePncTermFrequency()
+      log.info("Finished PNC term frequency generation")
     }
   }
-
-  private inline fun forPage(page: (Page<TermFrequency>) -> Unit): Long {
-    var pageNum = 0
-    var currentPage: Page<TermFrequency>
-    do {
-      currentPage = executePagedQuery(pageNum)
-      if (currentPage.hasContent()) {
-        page(currentPage)
-      }
-      pageNum++
-    } while (currentPage.hasNext())
-    return currentPage.totalElements
-  }
-
-  private fun executePagedQuery(pageNum: Int): Page<TermFrequency> =
-    referenceRepository.getIdentifierTermFrequency(IdentifierType.PNC.name, PageRequest.of(pageNum, PAGE_SIZE))
-
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
     private const val OK = "OK"
-    private const val PAGE_SIZE = 500
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PncFrequencyRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PncFrequencyRepository.kt
@@ -1,8 +1,42 @@
 package uk.gov.justice.digital.hmpps.personrecord.jpa.repository
 
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.ListCrudRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.termfrequency.PncFrequencyEntity
 
 @Repository
-interface PncFrequencyRepository : JpaRepository<PncFrequencyEntity, Long>
+interface PncFrequencyRepository : CrudRepository<PncFrequencyEntity, Long>, ListCrudRepository<PncFrequencyEntity, Long> {
+
+  @Transactional
+  @Modifying
+  @Query(
+    """
+    INSERT INTO personmatchscore.pnc_frequency (pnc, frequency)
+    SELECT
+        ref.identifier_value as term, 
+        CAST(COUNT(*) AS FLOAT8) / (
+            SELECT 
+                COUNT(ref_inner.identifier_value) AS total 
+            FROM 
+                personrecordservice.reference ref_inner
+            WHERE 
+                ref_inner.identifier_type = 'PNC'
+                AND ref_inner.identifier_value IS NOT null
+        ) AS frequency
+    FROM
+        personrecordservice.reference ref
+    WHERE
+        ref.identifier_type = 'PNC'
+        AND ref.identifier_value IS NOT null
+    GROUP BY
+        ref.identifier_value
+    """,
+    nativeQuery = true,
+  )
+  fun generatePncTermFrequency(): Int
+}
