3ec01741ee02b47831053363c4eeea4060b2fa6e
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-08-12 16:33:08 +0100

    CPR-414 Update report

--- scripts/db/CPR-414-uuid-composition-report.sql
+++ scripts/db/CPR-414-uuid-composition-report.sql
@@ -1,18 +1,13 @@
 -- Number of orphaned v parented records by Source System
-select parented.source_system, orphaned.no_uuid, parented.has_uuid
-from (
-	select p1.source_system, count(*) as has_uuid
-	from personrecordservice.person p1
-	where p1.fk_person_key_id is not null
-	group by p1.source_system
-) as parented
-inner join (
-	select p2.source_system, count(*) as no_uuid
-	from personrecordservice.person p2
-	where p2.fk_person_key_id is null
-	group by p2.source_system
-) as orphaned
-on parented.source_system = orphaned.source_system;
+select p1.source_system, count(*) as has_uuid
+from personrecordservice.person p1
+where p1.fk_person_key_id is not null
+group by p1.source_system
+
+select p2.source_system, count(*) as no_uuid
+from personrecordservice.person p2
+where p2.fk_person_key_id is null
+group by p2.source_system
 
 -- NOMIS -> DELIUS linked records
 select count(pk.id) AS linked
