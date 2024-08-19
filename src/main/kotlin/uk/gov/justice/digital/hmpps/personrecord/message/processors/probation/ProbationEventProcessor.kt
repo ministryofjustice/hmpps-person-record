914b89cf5626c2fe7c98655ecb1c80f5ba519578
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-08-19 11:07:05 +0100

    Revert "CPR-337 Remove base class"
    This reverts commit d27b04db4ebe816c34dce534f85e71f3c9d28a96.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationEventProcessor.kt
@@ -1,17 +1,12 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.processors.probation
 
-import kotlinx.coroutines.runBlocking
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.client.CorePersonRecordAndDeliusClient
-import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
 import uk.gov.justice.digital.hmpps.personrecord.service.EventKeys
 import uk.gov.justice.digital.hmpps.personrecord.service.PersonService
-import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor
 import uk.gov.justice.digital.hmpps.personrecord.service.TelemetryService
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.MESSAGE_RECEIVED
 
@@ -20,15 +15,8 @@ class ProbationEventProcessor(
   val telemetryService: TelemetryService,
   val personService: PersonService,
   val personRepository: PersonRepository,
-  val corePersonRecordAndDeliusClient: CorePersonRecordAndDeliusClient,
-  @Value("\${retry.delay}")
-  private val retryDelay: Long = 0,
-) {
-
-  companion object {
-    private const val MAX_RETRY_ATTEMPTS: Int = 3
-    internal val log = LoggerFactory.getLogger(this::class.java)
-  }
+  corePersonRecordAndDeliusClient: CorePersonRecordAndDeliusClient,
+) : BaseProbationEventProcessor(corePersonRecordAndDeliusClient) {
 
   fun processEvent(crn: String, eventType: String) {
     telemetryService.trackEvent(
@@ -48,15 +36,5 @@ class ProbationEventProcessor(
         throw it
       },
     )
-  }
-
-  private fun getProbationCase(crn: String): Result<ProbationCase?> = runBlocking {
-    try {
-      return@runBlocking RetryExecutor.runWithRetry(MAX_RETRY_ATTEMPTS, retryDelay) {
-        Result.success(corePersonRecordAndDeliusClient.getProbationCase(crn))
-      }
-    } catch (e: Exception) {
-      Result.failure(e)
-    }
   }
 }
