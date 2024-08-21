ee6d66ac51d3f04379010efc754bf49f268c8744
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-08-21 14:37:06 +0100

    CPR-427 Map no fixed abode from NOMIS

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/prisoner/Address.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/prisoner/Address.kt
@@ -2,10 +2,14 @@ package uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Address(
   @JsonProperty("postalCode")
   val postcode: String? = null,
   val fullAddress: String? = null,
+  @JsonProperty("noFixedAddress")
+  val noFixedAbode: Boolean? = null,
+  val startDate: LocalDate? = null,
 )
