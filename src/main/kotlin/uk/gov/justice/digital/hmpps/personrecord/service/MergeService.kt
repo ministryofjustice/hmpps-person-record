cb0b841e3a1bca27e7989e33f5e20cc44f615a5a
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-08-22 11:12:22 +0100

    CPR-3840 Handle target record not found

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/MergeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/MergeService.kt
@@ -11,11 +11,13 @@ import org.springframework.orm.ObjectOptimisticLockingFailureException
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
 import org.springframework.orm.jpa.JpaSystemException
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.personrecord.client.model.merge.MergeEvent
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.service.PersonService.Companion.MAX_ATTEMPTS
 import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor.runWithRetry
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_MERGE_RECORD_NOT_FOUND
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_RECORD_MERGED
 
 @Service
@@ -34,26 +36,39 @@ class MergeService(
     ConstraintViolationException::class,
   )
 
-  fun processMerge(mergedRecord: Person, sourcePersonCallback: () -> PersonEntity?, targetPersonCallback: () -> PersonEntity?) = runBlocking {
+  fun processMerge(mergeEvent: MergeEvent, sourcePersonCallback: () -> PersonEntity?, targetPersonCallback: () -> PersonEntity?) = runBlocking {
     runWithRetry(MAX_ATTEMPTS, retryDelay, retryExceptions) {
-      processMergingOfRecords(mergedRecord, sourcePersonCallback, targetPersonCallback)
+      processMergingOfRecords(mergeEvent, sourcePersonCallback, targetPersonCallback)
     }
   }
 
-  private suspend fun processMergingOfRecords(mergedRecord: Person, sourcePersonCallback: () -> PersonEntity?, targetPersonCallback: () -> PersonEntity?) {
+  private suspend fun processMergingOfRecords(mergeEvent: MergeEvent, sourcePersonCallback: () -> PersonEntity?, targetPersonCallback: () -> PersonEntity?) {
     val (sourcePersonEntity, targetPersonEntity) = collectPeople(sourcePersonCallback, targetPersonCallback)
     when {
       sourcePersonEntity == null -> return // CPR-341
-      targetPersonEntity == null -> return // CPR-340
+      targetPersonEntity == null -> handleTargetRecordNotFound(mergeEvent)
       else -> when {
-        isSameUuid(sourcePersonEntity, targetPersonEntity) -> mergeRecord(mergedRecord, sourcePersonEntity, targetPersonEntity)
+        isSameUuid(sourcePersonEntity, targetPersonEntity) -> mergeRecord(mergeEvent, sourcePersonEntity, targetPersonEntity)
         else -> return // CPR-342
       }
     }
   }
 
-  private fun mergeRecord(mergedRecord: Person, sourcePersonEntity: PersonEntity?, targetPersonEntity: PersonEntity?) {
-    updateAndLinkRecords(mergedRecord, sourcePersonEntity, targetPersonEntity)
+  private fun handleTargetRecordNotFound(mergeEvent: MergeEvent) {
+    telemetryService.trackEvent(
+      CPR_MERGE_RECORD_NOT_FOUND,
+      mapOf(
+        EventKeys.RECORD_TYPE to RecordType.TARGET.name,
+        mergeEvent.sourceSystemId.first to mergeEvent.sourceSystemId.second,
+        mergeEvent.targetSystemId.first to mergeEvent.targetSystemId.second,
+        EventKeys.FROM_SOURCE_SYSTEM to mergeEvent.mergedRecord.sourceSystemType.name,
+        EventKeys.TO_SOURCE_SYSTEM to mergeEvent.mergedRecord.sourceSystemType.name,
+      ),
+    )
+  }
+
+  private fun mergeRecord(mergeEvent: MergeEvent, sourcePersonEntity: PersonEntity?, targetPersonEntity: PersonEntity?) {
+    updateAndLinkRecords(mergeEvent, sourcePersonEntity, targetPersonEntity)
     telemetryService.trackEvent(
       CPR_RECORD_MERGED,
       mapOf(
@@ -65,8 +80,8 @@ class MergeService(
     )
   }
 
-  private fun updateAndLinkRecords(mergedRecord: Person, sourcePersonEntity: PersonEntity?, targetPersonEntity: PersonEntity?) {
-    targetPersonEntity?.update(mergedRecord)
+  private fun updateAndLinkRecords(mergeEvent: MergeEvent, sourcePersonEntity: PersonEntity?, targetPersonEntity: PersonEntity?) {
+    targetPersonEntity?.update(mergeEvent.mergedRecord)
     sourcePersonEntity?.mergedTo = targetPersonEntity?.id
     personRepository.saveAllAndFlush(listOf(targetPersonEntity, sourcePersonEntity))
   }
@@ -83,6 +98,13 @@ class MergeService(
           targetPersonCallback()
         }.await(),
       )
+    }
+  }
+
+  companion object {
+    enum class RecordType {
+      TARGET,
+      SOURCE,
     }
   }
 }
