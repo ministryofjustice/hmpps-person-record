799429b133ed5b18f3148e61bd32a499d837557c
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-09-13 13:59:43 +0100

    CPR-453 Add migration

--- src/main/resources/db/migration/V60__add_trigram_filtering.sql
+++ src/main/resources/db/migration/V60__add_trigram_filtering.sql
@@ -3,8 +3,11 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 ALTER TABLE personrecordservice.person ADD COLUMN date_of_birth_text text default NULL;
+
 CREATE INDEX trgm_index_postcode ON personrecordservice.address USING gin (postcode gin_trgm_ops);
 CREATE INDEX trgm_index_date_of_birth ON personrecordservice.person USING gin (date_of_birth_text gin_trgm_ops);
+
+UPDATE personrecordservice.person SET date_of_birth_text = to_char(personrecordservice.person.date_of_birth, 'YYYY-MM-DD');
 
 -------------------------------------------------------
 COMMIT;
