4720d997744386b7a55f5df6650f324035ee4823
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-09-13 15:02:17 +0100

    Revert "CPR-453 Use postgres similarity to reduce number records on search (#…" (#610)
    This reverts commit 57ebc24fb1e9c273dc6915e09ac8a4d60c4607de.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/specifications/PersonSpecification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/specifications/PersonSpecification.kt
@@ -23,16 +23,15 @@ object PersonSpecification {
 
   const val FIRST_NAME = "firstName"
   const val LAST_NAME = "lastName"
-  const val DOB = "dateOfBirthText"
+  const val DOB = "dateOfBirth"
   const val SOURCE_SYSTEM = "sourceSystem"
 
   private const val IDENTIFIER_TYPE = "identifierType"
   private const val IDENTIFIER_VALUE = "identifierValue"
   private const val PERSON_KEY = "personKey"
   private const val POSTCODE = "postcode"
+  private const val DATE_FORMAT = "YYYY-MM-DD"
   private const val MERGED_TO = "mergedTo"
-
-  private const val SIMILARITY_THRESHOLD = 0.6
 
   fun exactMatch(input: String?, field: String): Specification<PersonEntity> {
     return Specification { root, _, criteriaBuilder ->
@@ -73,20 +72,9 @@ object PersonSpecification {
       postcodes.takeIf { it.isNotEmpty() }?.let {
         val addressJoin: Join<PersonEntity, AddressEntity> = root.join("addresses", LEFT)
         val postcodePredicates: Array<Predicate> = postcodes.map {
-          criteriaBuilder.and(
-            criteriaBuilder.greaterThanOrEqualTo(
-              criteriaBuilder.function(
-                "similarity",
-                Double::class.java,
-                addressJoin.get<String>(POSTCODE),
-                criteriaBuilder.literal(it),
-              ),
-              SIMILARITY_THRESHOLD,
-            ),
-            criteriaBuilder.le(
-              criteriaBuilder.function("levenshtein_less_equal", Integer::class.java, criteriaBuilder.literal(it), addressJoin.get<String>(POSTCODE), criteriaBuilder.literal(limit)),
-              limit,
-            ),
+          criteriaBuilder.le(
+            criteriaBuilder.function("levenshtein_less_equal", Integer::class.java, criteriaBuilder.literal(it), addressJoin.get<String>(POSTCODE), criteriaBuilder.literal(limit)),
+            limit,
           )
         }.toTypedArray()
         criteriaBuilder.or(*postcodePredicates)
@@ -97,22 +85,17 @@ object PersonSpecification {
   fun levenshteinDate(input: LocalDate?, field: String, limit: Int = 2): Specification<PersonEntity> {
     return Specification { root, _, criteriaBuilder ->
       input?.let {
-        criteriaBuilder.and(
-          criteriaBuilder.greaterThanOrEqualTo(
-            criteriaBuilder.function(
-              "similarity",
-              Double::class.java,
-              root.get<String>(field),
-              criteriaBuilder.literal(it.toString()),
-            ),
-            SIMILARITY_THRESHOLD,
+        val dbDateAsString = criteriaBuilder.function(
+          "TO_CHAR",
+          String::class.java,
+          root.get<LocalDate>(field),
+          criteriaBuilder.literal(DATE_FORMAT),
+        )
+        criteriaBuilder.le(
+          criteriaBuilder.function(
+            "levenshtein_less_equal", Integer::class.java, criteriaBuilder.literal(input.toString()), dbDateAsString, criteriaBuilder.literal(limit),
           ),
-          criteriaBuilder.le(
-            criteriaBuilder.function(
-              "levenshtein_less_equal", Integer::class.java, criteriaBuilder.literal(it.toString()), root.get<String>(field), criteriaBuilder.literal(limit),
-            ),
-            limit,
-          ),
+          limit,
         )
       } ?: criteriaBuilder.disjunction()
     }
