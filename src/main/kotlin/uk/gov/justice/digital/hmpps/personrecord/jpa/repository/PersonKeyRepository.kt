457a61c5d38fd13cb3dcd94c2a09d14123812ecf
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-10-07 13:45:50 +0100

    CPR-428 Remove JPA specification executor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonKeyRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonKeyRepository.kt
@@ -7,6 +7,6 @@ import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import java.util.*
 
 @Repository
-interface PersonKeyRepository : JpaSpecificationExecutor<PersonKeyEntity>, JpaRepository<PersonKeyEntity, Long> {
+interface PersonKeyRepository: JpaRepository<PersonKeyEntity, Long> {
   fun findByPersonId(personId: UUID?): PersonKeyEntity
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonRepository.kt
@@ -1,14 +1,13 @@
 package uk.gov.justice.digital.hmpps.personrecord.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.NOMIS
 
 @Repository
-interface PersonRepository : JpaSpecificationExecutor<PersonEntity>, JpaRepository<PersonEntity, Long> {
+interface PersonRepository: JpaRepository<PersonEntity, Long> {
 
   fun findByDefendantId(defendantId: String): PersonEntity?
 
