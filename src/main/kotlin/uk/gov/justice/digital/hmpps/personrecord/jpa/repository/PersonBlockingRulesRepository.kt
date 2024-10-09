65a9c240fac711da030738f02e3bb4a39bcb66c3
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2024-10-09 14:47:24 +0100

    CPR-472-add-paging (#646)
    * CPR-472-add-paging
    added paging to hopefully resolve memory isue
    
    * CPR-472-add-paging
    moved countquery out of scope
    
    * CPR-472-add-paging
    moved countquery out of scope
    
    * CPR-472-add-paging
    moved countquery out of scope

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonBlockingRulesRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/PersonBlockingRulesRepository.kt
@@ -2,6 +2,9 @@ package uk.gov.justice.digital.hmpps.personrecord.jpa.repository
 
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.queries.criteria.PersonSearchCriteria
@@ -11,10 +14,29 @@ class PersonBlockingRulesRepository {
   @PersistenceContext
   private val entityManager: EntityManager? = null
 
-  fun findMatchCandidates(searchCriteria: PersonSearchCriteria, personQuery: String): List<PersonEntity> {
+  fun findMatchCandidates(searchCriteria: PersonSearchCriteria, personQuery: String, pageable: Pageable, totalResults: Long = 0L): Page<PersonEntity> {
     val query = entityManager!!.createNativeQuery(personQuery, PersonEntity::class.java)
+
     query.setParameter("firstName", searchCriteria.firstName)
     query.setParameter("lastName", searchCriteria.lastName)
-    return query.resultList as List<PersonEntity>
+
+    // apply pagination
+    query.firstResult = pageable.offset.toInt()
+    query.maxResults = pageable.pageSize
+
+    val results = query.resultList as List<PersonEntity>
+
+    return PageImpl(results, pageable, totalResults)
+  }
+
+  fun countMatchCandidates(personQuery: String, person: PersonSearchCriteria): Long {
+    val queryString = "SELECT COUNT (*) FROM ($personQuery) AS total"
+
+    val query = entityManager!!.createNativeQuery(queryString)
+
+    query.setParameter("firstName", person.firstName)
+    query.setParameter("lastName", person.lastName)
+
+    return (query.singleResult as Number).toLong()
   }
 }
