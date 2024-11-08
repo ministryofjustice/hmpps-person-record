1200859615e21b487d515c16f589a88a5afaf9cd
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-11-08 11:15:00 +0000

    CPR-436 Add initial recluster check

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/ReclusterService.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.person
+
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
+import uk.gov.justice.digital.hmpps.personrecord.service.EventKeys
+import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor.ENTITY_RETRY_EXCEPTIONS
+import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor.runWithRetry
+import uk.gov.justice.digital.hmpps.personrecord.service.TelemetryService
+import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_UUID_RECLUSTER_NEEDS_ATTENTION
+
+@Component
+class ReclusterService(
+  private val telemetryService: TelemetryService,
+  @Value("\${retry.delay}") private val retryDelay: Long,
+) {
+
+  fun recluster(personEntity: PersonEntity) = runBlocking {
+    runWithRetry(MAX_ATTEMPTS, retryDelay, ENTITY_RETRY_EXCEPTIONS) {
+      handleRecluster(personEntity)
+    }
+  }
+
+  private fun handleRecluster(personEntity: PersonEntity) {
+    when {
+      clusterNeedsAttention(personEntity.personKey) -> telemetryService.trackPersonEvent(
+        CPR_UUID_RECLUSTER_NEEDS_ATTENTION,
+        personEntity,
+        mapOf(
+          EventKeys.UUID to personEntity.personKey?.personId.toString(),
+        ),
+      )
+    }
+  }
+
+  private fun clusterNeedsAttention(personKeyEntity: PersonKeyEntity?) = personKeyEntity?.status == UUIDStatusType.NEEDS_ATTENTION
+
+  companion object {
+    private const val MAX_ATTEMPTS = 5
+  }
+}
