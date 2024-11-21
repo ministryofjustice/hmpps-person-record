a38558570577e7230ab7e2080cefe03464119dd1
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-11-21 12:04:37 +0000

    CPR-500 Decouple event processing from person creation / update (#696)
    * CPR-500 Decouple event processing from person creation / update
    
    * CPR-500 Add back link logic for now

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonService.kt
@@ -1,77 +1,42 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.person
 
-import kotlinx.coroutines.runBlocking
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.service.ReadWriteLockService
-import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor.ENTITY_RETRY_EXCEPTIONS
-import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor.runWithRetry
 import uk.gov.justice.digital.hmpps.personrecord.service.TelemetryService
 import uk.gov.justice.digital.hmpps.personrecord.service.queue.QueueService
 import uk.gov.justice.digital.hmpps.personrecord.service.search.MatchResult
 import uk.gov.justice.digital.hmpps.personrecord.service.search.SearchService
-import uk.gov.justice.digital.hmpps.personrecord.service.type.NEW_OFFENDER_CREATED
-import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_ADDRESS_CHANGED
-import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_ALIAS_CHANGED
-import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_DETAILS_CHANGED
-import uk.gov.justice.digital.hmpps.personrecord.service.type.PRISONER_CREATED
-import uk.gov.justice.digital.hmpps.personrecord.service.type.PRISONER_UPDATED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
-import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_NEW_RECORD_EXISTS
-import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_UPDATE_RECORD_DOES_NOT_EXIST
 
 @Service
 class PersonService(
   private val telemetryService: TelemetryService,
   private val personRepository: PersonRepository,
-  private val readWriteLockService: ReadWriteLockService,
   private val searchService: SearchService,
-  private val personKeyService: PersonKeyService,
   private val queueService: QueueService,
-  @Value("\${retry.delay}") private val retryDelay: Long,
 ) {
 
-  fun processMessage(person: Person, event: String? = null, linkRecord: Boolean = true, callback: () -> PersonEntity?): PersonEntity = runBlocking {
-    runWithRetry(MAX_ATTEMPTS, retryDelay, ENTITY_RETRY_EXCEPTIONS) {
-      readWriteLockService.withWriteLock(person.sourceSystemType) { return@withWriteLock processPerson(person, event, linkRecord, callback) }
-    }
-  }
-
-  private fun processPerson(person: Person, event: String?, linkRecord: Boolean, callback: () -> PersonEntity?): PersonEntity {
-    val existingPersonEntity: PersonEntity? = callback()
-    val personEntity: PersonEntity = when {
-      (existingPersonEntity == null) -> handlePersonCreation(person, event, linkRecord)
-      else -> handlePersonUpdate(person, existingPersonEntity, event)
-    }
-    return personEntity
-  }
-
-  private fun handlePersonCreation(person: Person, event: String?, linkRecord: Boolean): PersonEntity {
-    if (isUpdateEvent(event)) {
-      telemetryService.trackPersonEvent(CPR_UPDATE_RECORD_DOES_NOT_EXIST, person)
-    }
-    val personEntity = createPersonEntity(person)
-    val personKey: PersonKeyEntity? = when {
-      linkRecord -> personKeyService.getPersonKey(personEntity)
-      else -> PersonKeyEntity.empty
-    }
-    linkToPersonKey(personEntity, personKey)
+  fun createPersonEntity(person: Person): PersonEntity {
+    val personEntity = createNewPersonEntity(person)
     telemetryService.trackPersonEvent(TelemetryEventType.CPR_RECORD_CREATED, person)
     return personEntity
   }
 
-  private fun handlePersonUpdate(person: Person, existingPersonEntity: PersonEntity, event: String?): PersonEntity {
-    if (isCreateEvent(event)) {
-      telemetryService.trackPersonEvent(CPR_NEW_RECORD_EXISTS, person)
-    }
+  fun updatePersonEntity(person: Person, existingPersonEntity: PersonEntity): PersonEntity {
     val updatedEntity = updateExistingPersonEntity(person, existingPersonEntity)
     telemetryService.trackPersonEvent(TelemetryEventType.CPR_RECORD_UPDATED, person)
     updatedEntity.personKey?.personId?.let { queueService.publishReclusterMessageToQueue(it) }
     return updatedEntity
+  }
+
+  fun linkPersonEntityToPersonKey(personEntity: PersonEntity, personKeyEntity: PersonKeyEntity?) {
+    personKeyEntity?.let {
+      personEntity.personKey = personKeyEntity
+      personRepository.saveAndFlush(personEntity)
+    }
   }
 
   private fun updateExistingPersonEntity(person: Person, personEntity: PersonEntity): PersonEntity {
@@ -79,33 +44,13 @@ class PersonService(
     return personRepository.saveAndFlush(personEntity)
   }
 
-  private fun createPersonEntity(person: Person): PersonEntity {
+  private fun createNewPersonEntity(person: Person): PersonEntity {
     val personEntity = PersonEntity.from(person)
     return personRepository.saveAndFlush(personEntity)
   }
 
-  private fun linkToPersonKey(personEntity: PersonEntity, personKeyEntity: PersonKeyEntity?) {
-    personKeyEntity.let {
-      personEntity.personKey = personKeyEntity
-      personRepository.saveAndFlush(personEntity)
-    }
-  }
-
-  private fun isUpdateEvent(event: String?) = listOf(
-    PRISONER_UPDATED,
-    OFFENDER_DETAILS_CHANGED,
-    OFFENDER_ALIAS_CHANGED,
-    OFFENDER_ADDRESS_CHANGED,
-  ).contains(event)
-
-  private fun isCreateEvent(event: String?) = listOf(PRISONER_CREATED, NEW_OFFENDER_CREATED).contains(event)
-
   fun searchBySourceSystem(person: Person): PersonEntity? {
     val highConfidenceMatches: List<MatchResult> = searchService.findCandidateRecordsBySourceSystem(person)
     return searchService.processCandidateRecords(highConfidenceMatches)
-  }
-
-  companion object {
-    const val MAX_ATTEMPTS: Int = 5
   }
 }
