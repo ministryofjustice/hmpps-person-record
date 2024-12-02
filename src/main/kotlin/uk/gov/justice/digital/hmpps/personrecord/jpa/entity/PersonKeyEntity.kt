4e61c8528a0abbc6594b646a5777a05613969d9a
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-12-02 16:58:25 +0000

    CPR-79 Remove redundant code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/PersonKeyEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/PersonKeyEntity.kt
@@ -45,10 +45,5 @@ class PersonKeyEntity(
     val empty: PersonKeyEntity? = null
 
     fun new(): PersonKeyEntity = PersonKeyEntity(personId = UUID.randomUUID())
-
-    fun PersonKeyEntity?.shouldCreateOrRetrieve(shouldCreate: () -> PersonKeyEntity, shouldRetrieve: (personKeyEntity: PersonKeyEntity) -> PersonKeyEntity): PersonKeyEntity = when {
-      this == PersonEntity.empty -> shouldCreate()
-      else -> shouldRetrieve(this)
-    }
   }
 }
