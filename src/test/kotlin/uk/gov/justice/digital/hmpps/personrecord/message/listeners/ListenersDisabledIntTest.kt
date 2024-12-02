929377ae3f3a00af19ca7f02cca0de13a7aeb117
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2024-12-02 13:55:09 +0000

    Speed up more (#708)
    * FIX: write a specific task to initialise database
    
    * FIX: try to make asserting quicker for when it sometimes fails
    
    * FIX: remove unused imports
    
    * FIX: add missing tests for seeding profile

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/ListenersDisabledIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/ListenersDisabledIntTest.kt
@@ -7,8 +7,12 @@ import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.personrecord.config.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.personrecord.message.listeners.court.CourtEventListener
+import uk.gov.justice.digital.hmpps.personrecord.message.listeners.cpr.ReclusterEventListener
 import uk.gov.justice.digital.hmpps.personrecord.message.listeners.prison.PrisonEventListener
+import uk.gov.justice.digital.hmpps.personrecord.message.listeners.prison.PrisonMergeEventListener
+import uk.gov.justice.digital.hmpps.personrecord.message.listeners.probation.ProbationDeletionEventListener
 import uk.gov.justice.digital.hmpps.personrecord.message.listeners.probation.ProbationEventListener
+import uk.gov.justice.digital.hmpps.personrecord.message.listeners.probation.ProbationMergeEventListener
 import kotlin.test.assertFailsWith
 
 @ActiveProfiles("seeding")
@@ -32,9 +36,37 @@ class ListenersDisabledIntTest : IntegrationTestBase() {
   }
 
   @Test
+  fun `should not have ProbationDeleteEventListener bean when seeding profile is active`() {
+    assertFailsWith<NoSuchBeanDefinitionException>(
+      block = { applicationContext.getBean(ProbationDeletionEventListener::class.java) },
+    )
+  }
+
+  @Test
+  fun `should not have ProbationMergeEventListener bean when seeding profile is active`() {
+    assertFailsWith<NoSuchBeanDefinitionException>(
+      block = { applicationContext.getBean(ProbationMergeEventListener::class.java) },
+    )
+  }
+
+  @Test
   fun `should not have PrisonEventListener bean when seeding profile is active`() {
     assertFailsWith<NoSuchBeanDefinitionException>(
       block = { applicationContext.getBean(PrisonEventListener::class.java) },
+    )
+  }
+
+  @Test
+  fun `should not have PrisonMargeEventListener bean when seeding profile is active`() {
+    assertFailsWith<NoSuchBeanDefinitionException>(
+      block = { applicationContext.getBean(PrisonMergeEventListener::class.java) },
+    )
+  }
+
+  @Test
+  fun `should not have ReclusterEventListener bean when seeding profile is active`() {
+    assertFailsWith<NoSuchBeanDefinitionException>(
+      block = { applicationContext.getBean(ReclusterEventListener::class.java) },
     )
   }
 }
