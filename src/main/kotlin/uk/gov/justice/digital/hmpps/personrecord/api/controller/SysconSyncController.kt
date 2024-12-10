ab1c2f8e1dccbc74f6fa1f1660aad397d68cab68
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-12-10 09:31:06 +0000

    Revert "CPR-520 copy and paste person as starting point for SysconSyncPrisoner"
    This reverts commit 885511f7493d99e9cf41b57dabcb0d2346b593e1.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/SysconSyncController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/SysconSyncController.kt
@@ -8,22 +8,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.constraints.NotBlank
-import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
-import uk.gov.justice.digital.hmpps.personrecord.api.model.SysconSyncPrisoner
 import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 
 @Tag(name = "Syscon Sync")
 @RestController
 @PreAuthorize("hasRole('${Roles.QUEUE_ADMIN}')") // TODO change me :-)
-class SysconSyncController {
+class SysconSyncController() {
 
   @Operation(description = "Create a prison record")
   @PutMapping("/syscon-sync/{prisonNumber}")
@@ -45,13 +42,7 @@ class SysconSyncController {
     @PathVariable(name = "prisonNumber")
     @Parameter(description = "The identifier of the offender source system (NOMIS)", required = true)
     prisonNumber: String,
-    @RequestBody prisoner: SysconSyncPrisoner,
   ): ResponseEntity<String> {
-    log.info("Prisoner {} in body {}", prisonNumber, prisoner.prisonNumber)
-    return ResponseEntity.status(CREATED).body("Record Created")
-  }
-
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
+    return ResponseEntity.status(HttpStatus.CREATED).body("Record Created")
   }
 }
