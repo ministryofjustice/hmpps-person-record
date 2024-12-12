db07463656404bb5d5745c27f5a1d14a93170ce2
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2024-12-12 14:03:57 +0000

    Revert "Revert "CPR-510 update person aggregate data view (#745)" (#746)"
    This reverts commit 4e2f544ee988e5c104db5b9c49d6331fd552043c.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/termfrequency/TermCleaningRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/termfrequency/TermCleaningRepository.kt
@@ -12,9 +12,15 @@ class TermCleaningRepository(
   @Transactional
   fun refreshPersonAggregateData() = entityManager.createNativeQuery(REFRESH_PERSON_AGGREGATE_SQL).executeUpdate()
 
+  @Transactional
+  fun refreshPersonCleanedData() = entityManager.createNativeQuery(REFRESH_PERSON_CLEANED_SQL).executeUpdate()
+
   companion object {
     private const val REFRESH_PERSON_AGGREGATE_SQL = """
       REFRESH MATERIALIZED VIEW CONCURRENTLY personrecordservice.person_aggregate_data;
+    """
+    private const val REFRESH_PERSON_CLEANED_SQL = """
+      REFRESH MATERIALIZED VIEW CONCURRENTLY personrecordservice.person_cleaned_data;
     """
   }
 }
