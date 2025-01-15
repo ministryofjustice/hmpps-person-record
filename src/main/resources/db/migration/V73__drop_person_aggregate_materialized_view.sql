76d5c1a0c09eda39a94188113a41e891dc5c3d7f
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-01-15 16:12:51 +0000

    CPR-510 drop database objects

---
+++ src/main/resources/db/migration/V73__drop_person_aggregate_materialized_view.sql
@@ -0,0 +1,7 @@
+BEGIN;
+-------------------------------------------------------
+
+DROP MATERIALIZED VIEW IF EXISTS personrecordservice.person_aggregate_data;
+
+-----------------------------------------------------
+COMMIT;
