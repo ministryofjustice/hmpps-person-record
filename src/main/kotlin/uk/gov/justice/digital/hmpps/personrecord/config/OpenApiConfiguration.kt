5b331065396b37040c405066aa2d435b06ce2c67
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-01-15 16:10:43 +0000

    CPR-510 remove all code and config relating to term frequency jobs (#785)
    * CPR-510 remove cronjob for term frequencies
    
    * CPR-510 remove all code relating to term frequency

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/OpenApiConfiguration.kt
@@ -29,7 +29,6 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
     .tags(
       listOf(
         Tag().name("Search").description("APIs for person search"),
-        Tag().name("Jobs").description("Batch job endpoints"),
       ),
     )
     .info(
