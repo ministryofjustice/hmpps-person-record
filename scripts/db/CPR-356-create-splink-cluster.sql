f358fe955521dcc067e1dd7c7f0c027eca68a589
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-01-16 13:06:04 +0000

    CPR-510 remove all references to term frequency tables in READMEs and scripts (#787)

--- scripts/db/CPR-356-create-splink-cluster.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS personmatchscore.splink_cluster (
-    person_id       BIGINT        DEFAULT 0,
-    source_dataset  TEXT        DEFAULT NULL,
-    cluster_high    BIGINT        DEFAULT 0,
-    cluster_medium  BIGINT        DEFAULT 0,
-    crn             TEXT        DEFAULT NULL,
-    prison_number TEXT        DEFAULT NULL
-);
