0de292bb82d9a1fbcf383aa02baa5866dbb3b9d5
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-01-17 09:50:59 +0000

    CPR-512 make retryexecutor a class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/RetryExecutor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/RetryExecutor.kt
@@ -5,11 +5,13 @@ import kotlinx.coroutines.delay
 import org.hibernate.StaleObjectStateException
 import org.hibernate.exception.ConstraintViolationException
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.dao.CannotAcquireLockException
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.orm.ObjectOptimisticLockingFailureException
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
 import org.springframework.orm.jpa.JpaSystemException
+import org.springframework.stereotype.Component
 import kotlin.math.min
 import kotlin.random.Random
 import kotlin.reflect.KClass
@@ -22,24 +24,22 @@ private const val JITTER_MIN = 0.8
 private const val JITTER_MAX = 1.2
 private const val MAX_DELAY_MILLIS: Long = 1000
 
-object RetryExecutor {
+@Component
+class RetryExecutor(@Value("\${retry.delay}") val delayMillis: Long) {
 
   suspend fun <T> runWithRetryHTTP(
-    delay: Long,
     action: suspend () -> T,
-  ): T = runWithRetry(HTTP_TRY_COUNT, delay, HTTP_RETRY_EXCEPTIONS, action)
+  ): T = runWithRetry(HTTP_TRY_COUNT, httpRetryExceptions, action)
 
   suspend fun <T> runWithRetryDatabase(
-    delay: Long,
     action: suspend () -> T,
-  ): T = runWithRetry(DB_TRY_COUNT, delay, ENTITY_RETRY_EXCEPTIONS, action)
+  ): T = runWithRetry(DB_TRY_COUNT, entityRetryExceptions, action)
   private suspend fun <T> runWithRetry(
     maxAttempts: Int,
-    delay: Long,
     exceptions: List<KClass<out Exception>>,
     action: suspend () -> T,
   ): T {
-    var currentDelay = delay
+    var currentDelay = delayMillis
     var lastException: Exception? = null
     val log = LoggerFactory.getLogger(this::class.java)
 
@@ -69,7 +69,7 @@ object RetryExecutor {
     throw lastException ?: RuntimeException("Unexpected error")
   }
 
-  private val ENTITY_RETRY_EXCEPTIONS = listOf(
+  private val entityRetryExceptions = listOf(
     ObjectOptimisticLockingFailureException::class,
     CannotAcquireLockException::class,
     JpaSystemException::class,
@@ -79,5 +79,5 @@ object RetryExecutor {
     StaleObjectStateException::class,
   )
 
-  private val HTTP_RETRY_EXCEPTIONS = listOf(feign.RetryableException::class, FeignException.InternalServerError::class, FeignException.ServiceUnavailable::class, FeignException.BadGateway::class)
+  private val httpRetryExceptions = listOf(feign.RetryableException::class, FeignException.InternalServerError::class, FeignException.ServiceUnavailable::class, FeignException.BadGateway::class)
 }
