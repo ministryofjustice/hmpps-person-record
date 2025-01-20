2de052bee1dcaa9aee03ab251d9427043167cb73
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-01-20 18:16:51 +0000

    CPR-566 tidy up functions, remove useless codes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/RandomTestData.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/RandomTestData.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.test
 
-import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.CROIdentifier
 import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.CROIdentifier.Companion.VALID_LETTERS
-import uk.gov.justice.digital.hmpps.personrecord.model.identifiers.PNCIdentifier
 import java.time.LocalDate
 import java.util.UUID
 
@@ -10,10 +8,7 @@ fun randomPnc(): String {
   val year = (1950..LocalDate.now().year).random().toString()
   val digits = randomDigit(7)
   val check = VALID_LETTERS[(year.takeLast(2) + digits).toInt().mod(VALID_LETTERS.length)]
-  if (PNCIdentifier.from("$year/$digits$check").valid) {
-    return "$year/$digits$check"
-  }
-  throw Exception("$year/$digits$check")
+  return "$year/$digits$check"
 }
 
 fun randomEmail(): String = randomLowerCaseString(8) + "." + randomDigit(4) + "@" + randomLowerCaseString(8) + ".co.uk"
@@ -25,16 +20,15 @@ fun randomCro(): String {
   val digits = randomDigit(6)
 
   val check = VALID_LETTERS[(year + digits).toInt().mod(VALID_LETTERS.length)]
-  if (CROIdentifier.from("$digits/$year$check").valid) {
-    return "$digits/$year$check"
-  }
-  throw Exception(CROIdentifier.from("$digits/$year$check").croId)
+  return "$digits/$year$check"
 }
 
 fun randomName(): String = randomLowerCaseString()
 
 fun randomEthnicity(): String = randomLowerCaseString()
+
 fun randomNationality(): String = randomLowerCaseString()
+
 fun randomReligion(): String = randomLowerCaseString()
 
 fun randomDriverLicenseNumber(): String = UUID.randomUUID().toString()
@@ -46,11 +40,13 @@ fun randomPrisonNumber(): String = randomLowerCaseString(2).uppercase() + random
 fun randomDefendantId(): String = UUID.randomUUID().toString()
 
 fun randomCrn(): String = randomLowerCaseString(1).uppercase() + randomDigit(6)
+
 fun randomNationalInsuranceNumber(): String = randomLowerCaseString(2).uppercase() + randomDigit(6) + randomLowerCaseString(1).uppercase()
 
 fun randomPostcode(): String = randomLowerCaseString(2).uppercase() + randomDigit(1) + " " + randomDigit(1) + randomLowerCaseString(2).uppercase()
 
 fun randomHearingId(): String = UUID.randomUUID().toString()
+
 private fun randomLowerCaseString(length: Int = 7): String = (1..length).map {
   ('a' + (Math.random() * 26).toInt())
 }.joinToString("")
