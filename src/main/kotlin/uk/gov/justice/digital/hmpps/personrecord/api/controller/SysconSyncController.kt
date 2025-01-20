02ae2cf8ae586d38b3140fd01258b5242566b4bf
Gary Newbould <gary.newbould@digital.justice.gov.uk>
2025-01-20 13:53:04 +0000

    CPR-556: Fix pipeline compile error

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/SysconSyncController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/SysconSyncController.kt
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.constraints.NotBlank
+import org.slf4j.LoggerFactory
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
