525ae67df995d3334fc06eaa2f58d9310b203189
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-02-04 09:22:06 +0000

    CPR-578 correct test configuration

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/LargeCourtMessageTestConfig.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/LargeCourtMessageTestConfig.kt
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("test", "local")
+@Profile("test")
 class LargeCourtMessageTestConfig {
 
   @Bean
