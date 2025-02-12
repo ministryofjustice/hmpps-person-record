622e378f8532d1126d0c9e43a18f3e9906e4a448
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-02-12 15:00:09 +0000

    FIX: cId is a string

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/libra/LibraCourtEventListenerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/libra/LibraCourtEventListenerIntTest.kt
@@ -61,7 +61,6 @@ class LibraCourtEventListenerIntTest : MessagingMultiNodeTestBase() {
     assertThat(person.addresses[0].postcode).isEqualTo(postcode)
     assertThat(person.personKey).isNotNull()
     assertThat(person.sourceSystem).isEqualTo(LIBRA)
-    assertThat(person.cId).isEqualTo(cId)
   }
 
   @Test
@@ -112,7 +111,6 @@ class LibraCourtEventListenerIntTest : MessagingMultiNodeTestBase() {
     assertThat(person.addresses.size).isEqualTo(1)
     assertThat(person.addresses[0].postcode).isEqualTo(postcode)
     assertThat(person.sourceSystem).isEqualTo(LIBRA)
-    assertThat(person.cId).isEqualTo(cId)
   }
 
   @Test
