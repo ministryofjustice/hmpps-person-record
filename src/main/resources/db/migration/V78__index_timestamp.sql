a06283a2c861bf2f48d5fbd849ec3c38561b2c38
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-02-13 11:54:22 +0000

    CPR-605-index-timezone

--- src/main/resources/db/migration/V78__index_timestamp.sql
+++ src/main/resources/db/migration/V78__index_timestamp.sql
@@ -4,14 +4,12 @@ ALTER TABLE IF EXISTS person DROP COLUMN created_at;
 ALTER TABLE IF EXISTS person DROP COLUMN last_modified_at;
 
 ALTER TABLE IF EXISTS person
-     ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+     ADD COLUMN created TIMESTAMP WITH TIME ZONE DEFAULT NULL;
 
 ALTER TABLE IF EXISTS person
-     ADD COLUMN last_modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+     ADD COLUMN last_modified TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
 
-
-CREATE INDEX idx_person_created_at ON person(created_at);
-CREATE INDEX idx_person_last_modified_at ON person(last_modified_at);
+CREATE INDEX idx_person_last_modified ON person(last_modified);
 
 
 
