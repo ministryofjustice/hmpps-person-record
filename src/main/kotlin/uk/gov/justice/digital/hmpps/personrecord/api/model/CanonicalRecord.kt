8998f4fd4fc0ecb41604dd3012582ab0fdeef223
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-02-17 11:39:48 +0000

    CPR-508-canonical-record-api CPR-508 basic canonical record model

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/CanonicalRecord.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/CanonicalRecord.kt
@@ -3,10 +3,21 @@ package uk.gov.justice.digital.hmpps.personrecord.api.model
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 
 data class CanonicalRecord(
-  val personId: String? = null,
+  val personId: String? = "",
+  val firstName: String? = "",
+  val middleNames: String? = "",
+  val lastName: String? = "",
 
 ) {
   companion object {
-    fun from(personKey: PersonKeyEntity): CanonicalRecord = CanonicalRecord(personId = personKey.personId.toString())
+    fun from(personKey: PersonKeyEntity): CanonicalRecord {
+      val latestPerson = personKey.personEntities.sortedByDescending { it.lastModified }.first()
+      return CanonicalRecord(
+        personId = personKey.personId.toString(),
+        firstName = latestPerson.firstName,
+        middleNames = latestPerson.middleNames,
+        lastName = latestPerson.lastName,
+      )
+    }
   }
 }
