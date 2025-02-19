424209c4cfae3ca662937b9529724804849521f0
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-02-19 16:06:01 +0000

    CPR-508-canonical-record-api CPR-508 pr review fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalAddress.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalAddress.kt
@@ -3,17 +3,17 @@ package uk.gov.justice.digital.hmpps.personrecord.api.model.canonical
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.AddressEntity
 
 data class CanonicalAddress(
-  val noFixedAbode: String? = null,
-  val startDate: String? = null,
-  val endDate: String? = null,
-  val postcode: String? = null,
+  val noFixedAbode: String? = "",
+  val startDate: String? = "",
+  val endDate: String? = "",
+  val postcode: String? = "",
 ) {
   companion object {
 
     fun from(addressEntity: AddressEntity): CanonicalAddress = CanonicalAddress(
       postcode = addressEntity.postcode,
-      startDate = addressEntity.startDate?.toString() ?: "",
-      endDate = addressEntity.endDate?.toString() ?: "",
+      startDate = addressEntity.startDate?.toString(),
+      endDate = addressEntity.endDate?.toString(),
       noFixedAbode = addressEntity.noFixedAbode.toString(),
     )
     fun fromAddressEntityList(addressEntity: List<AddressEntity>): List<CanonicalAddress> = addressEntity.map { from(it) }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalAlias.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalAlias.kt
@@ -3,10 +3,10 @@ package uk.gov.justice.digital.hmpps.personrecord.api.model.canonical
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PseudonymEntity
 
 data class CanonicalAlias(
-  val firstName: String? = null,
-  val lastName: String? = null,
-  val middleNames: String? = null,
-  val title: String? = null,
+  val firstName: String? = "",
+  val lastName: String? = "",
+  val middleNames: String? = "",
+  val title: String? = "",
 ) {
   companion object {
 
