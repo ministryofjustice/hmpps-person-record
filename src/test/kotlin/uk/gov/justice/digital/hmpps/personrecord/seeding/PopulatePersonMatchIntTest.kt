26a51f88964c67fca0443ed5c44d1eaf91e7b0ee
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-02-24 09:00:15 +0000

    FIX: use shared stub method

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulatePersonMatchIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulatePersonMatchIntTest.kt
@@ -1,10 +1,8 @@
 package uk.gov.justice.digital.hmpps.personrecord.seeding
 
-import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.equalToJson
 import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
-import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchRecord
@@ -140,17 +138,5 @@ class PopulatePersonMatchIntTest : WebTestBase() {
     }
   }
 
-  private fun stubPersonMigrate(scenario: String = BASE_SCENARIO, currentScenarioState: String = STARTED, nextScenarioState: String = STARTED) {
-    wiremock.stubFor(
-      WireMock.post("/person/migrate")
-        .inScenario(scenario)
-        .whenScenarioStateIs(currentScenarioState)
-        .willSetStateTo(nextScenarioState)
-        .willReturn(
-          WireMock.aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(200),
-        ),
-    )
-  }
+  private fun stubPersonMigrate() = stubPostRequest(url = "/person/migrate", body = "")
 }
