a228428cc49dc012a8b472173660fb8835591b91
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-02-26 14:44:31 +0000

    Revert "Revert "Revert "CPR-584 invoke person match (#854)" (#863)" (#864)"
    This reverts commit 7de60ebde8044e27d185c36b242d5b1c9e925da8.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PersonMatchClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PersonMatchClient.kt
@@ -2,23 +2,17 @@ package uk.gov.justice.digital.hmpps.personrecord.client
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchIdentifier
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchMigrateRequest
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchRecord
-import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchScore
 
 @FeignClient(
   name = "person-match",
   url = "\${person-match.base-url}",
 )
 interface PersonMatchClient {
-
-  @GetMapping("/person/score/{matchId}")
-  fun getPersonScores(@PathVariable matchId: String): List<PersonMatchScore>
 
   @PostMapping("/person")
   fun postPerson(@RequestBody personMatchRecord: PersonMatchRecord)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/IntegrationTestBase.kt
@@ -2,11 +2,8 @@ package uk.gov.justice.digital.hmpps.personrecord.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
-import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension
-import com.github.tomakehurst.wiremock.matching.UrlPattern
 import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.atMost
@@ -21,7 +18,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.MatchResponse
-import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchScore
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.OverrideMarkerEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
@@ -36,7 +32,6 @@ import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.MERG
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType
 import uk.gov.justice.digital.hmpps.personrecord.telemetry.TelemetryTestRepository
 import java.time.Duration
-import java.util.UUID
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
@@ -153,22 +148,9 @@ class IntegrationTestBase {
       scenario,
       currentScenarioState,
       nextScenarioState,
-      urlPattern = urlEqualTo("/person/match"),
+      url = "/person/match",
       status = status,
       body = objectMapper.writeValueAsString(matchResponse),
-    )
-  }
-
-  internal fun stubPersonMatchScores(matchId: UUID? = null, personMatchResponse: List<PersonMatchScore> = listOf(), scenario: String = BASE_SCENARIO, currentScenarioState: String = STARTED, nextScenarioState: String = STARTED, status: Int = 200) {
-    val matchIdUrlPattern: UrlPattern = matchId?.let { urlEqualTo("/person/score/$it") } ?: urlPathMatching("/person/score/.*") // Regex to match any matchId, as not known on create
-    val responseBody: List<PersonMatchScore> = personMatchResponse.isEmpty().let { listOf(PersonMatchScore(candidateMatchId = matchId.toString(), candidateMatchWeight = 1.0F, candidateMatchProbability = 1.0F)) }
-    stubGetRequest(
-      scenario,
-      currentScenarioState,
-      nextScenarioState,
-      urlPattern = matchIdUrlPattern,
-      status = status,
-      body = objectMapper.writeValueAsString(responseBody),
     )
   }
 
@@ -177,7 +159,7 @@ class IntegrationTestBase {
       scenario,
       currentScenarioState,
       nextScenarioState,
-      urlPattern = urlEqualTo("/person"),
+      url = "/person",
       status = status,
       body = body,
     )
@@ -188,7 +170,7 @@ class IntegrationTestBase {
       scenario,
       currentScenarioState,
       nextScenarioState,
-      urlPattern = urlEqualTo("/person"),
+      url = "/person",
       status = status,
       body = body,
     )
@@ -205,9 +187,9 @@ class IntegrationTestBase {
     )
   }
 
-  internal fun stubGetRequest(scenarioName: String? = BASE_SCENARIO, currentScenarioState: String? = STARTED, nextScenarioState: String? = STARTED, urlPattern: UrlPattern, body: String, status: Int = 200) {
+  internal fun stubGetRequest(scenarioName: String? = BASE_SCENARIO, currentScenarioState: String? = STARTED, nextScenarioState: String? = STARTED, url: String, body: String, status: Int = 200) {
     wiremock.stubFor(
-      WireMock.get(urlPattern)
+      WireMock.get(url)
         .inScenario(scenarioName)
         .whenScenarioStateIs(currentScenarioState)
         .willSetStateTo(nextScenarioState)
@@ -220,9 +202,9 @@ class IntegrationTestBase {
     )
   }
 
-  internal fun stubPostRequest(scenarioName: String? = BASE_SCENARIO, currentScenarioState: String? = STARTED, nextScenarioState: String? = STARTED, urlPattern: UrlPattern, body: String, status: Int = 200) {
+  internal fun stubPostRequest(scenarioName: String? = BASE_SCENARIO, currentScenarioState: String? = STARTED, nextScenarioState: String? = STARTED, url: String, body: String, status: Int = 200) {
     wiremock.stubFor(
-      WireMock.post(urlPattern)
+      WireMock.post(url)
         .inScenario(scenarioName)
         .whenScenarioStateIs(currentScenarioState)
         .willSetStateTo(nextScenarioState)
@@ -235,9 +217,9 @@ class IntegrationTestBase {
     )
   }
 
-  internal fun stubDeleteRequest(scenarioName: String? = BASE_SCENARIO, currentScenarioState: String? = STARTED, nextScenarioState: String? = STARTED, urlPattern: UrlPattern, body: String, status: Int = 200) {
+  internal fun stubDeleteRequest(scenarioName: String? = BASE_SCENARIO, currentScenarioState: String? = STARTED, nextScenarioState: String? = STARTED, url: String, body: String, status: Int = 200) {
     wiremock.stubFor(
-      WireMock.delete(urlPattern)
+      WireMock.delete(url)
         .inScenario(scenarioName)
         .whenScenarioStateIs(currentScenarioState)
         .willSetStateTo(nextScenarioState)
