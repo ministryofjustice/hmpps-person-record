2405a4310b5bd5ed421ae3a1e5efe0e0fbaa8540
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-02-27 16:33:53 +0000

    CPR-508 add text to swagger schema

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalIdentifier.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalIdentifier.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.model.canonical
 
+import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.ReferenceEntity
 
+@Schema(description = "Possible identifier types")
 data class CanonicalIdentifier(
   val identifierType: CanonicalIdentifierType,
   val identifierValue: List<String?> = emptyList(),
