beee65ae55620ae2b44046c85feeeff791bebc7c
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-03-10 13:05:39 +0000

    CPR-629 add missing import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PersonMatchClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PersonMatchClient.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.personrecord.client
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.MatchStatus
