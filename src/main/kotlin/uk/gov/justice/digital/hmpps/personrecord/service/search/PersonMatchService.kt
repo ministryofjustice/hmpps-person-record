01c62ae46d4653f5cd19112af3d00986fa157693
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-03-20 15:02:06 +0000

    CPR-585 Remove person match feature flag (#921)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/search/PersonMatchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/search/PersonMatchService.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.personrecord.service.search
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.client.PersonMatchClient
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
@@ -13,16 +12,12 @@ import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor
 class PersonMatchService(
   private val personMatchClient: PersonMatchClient,
   private val retryExecutor: RetryExecutor,
-  @Value("\${CALL_PERSON_MATCH}")
-  private var callPersonMatch: Boolean,
 ) {
 
   // Fire and forget, until consumed in CPR-585
   fun getScores(personEntity: PersonEntity) {
-    if (callPersonMatch) {
-      CoroutineScope(Dispatchers.Default).launch {
-        retryExecutor.runWithRetryHTTP { personMatchClient.getPersonScores(personEntity.matchId.toString()) }
-      }
+    CoroutineScope(Dispatchers.Default).launch {
+      retryExecutor.runWithRetryHTTP { personMatchClient.getPersonScores(personEntity.matchId.toString()) }
     }
   }
 }
