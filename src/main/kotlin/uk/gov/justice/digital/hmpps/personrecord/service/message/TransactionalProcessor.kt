f03dd521915bc4e155c2c72e987bbd284ee145a6
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-03-26 15:22:29 +0000

    CPR-652-resolve-with-retry use the @Retryable annotation in TransactionalProcessor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/TransactionalProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/TransactionalProcessor.kt
@@ -1,20 +1,36 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.message
 
 import kotlinx.coroutines.runBlocking
+import org.hibernate.StaleObjectStateException
+import org.hibernate.exception.ConstraintViolationException
+import org.springframework.dao.CannotAcquireLockException
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.orm.ObjectOptimisticLockingFailureException
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
+import org.springframework.orm.jpa.JpaSystemException
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor
 
 @Component
 class TransactionalProcessor(
   private val createUpdateService: CreateUpdateService,
-  private val retryExecutor: RetryExecutor,
+  // private val retryExecutor: RetryExecutor,
 ) {
 
+  @Retryable(
+    retryFor = [
+      ObjectOptimisticLockingFailureException::class,
+      CannotAcquireLockException::class,
+      JpaSystemException::class,
+      JpaObjectRetrievalFailureException::class,
+      DataIntegrityViolationException::class,
+      ConstraintViolationException::class,
+      StaleObjectStateException::class,
+    ],
+  )
   fun processMessage(person: Person, event: String? = null, callback: () -> PersonEntity?) = runBlocking {
-    retryExecutor.runWithRetryDatabase {
-      createUpdateService.processPerson(person, event, callback)
-    }
+    createUpdateService.processPerson(person, event, callback)
   }
 }
