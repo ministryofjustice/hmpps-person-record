f55bcf89e5fc9e3a3e23d00c5fb496806bbca8aa
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-03-27 15:13:45 +0000

    CPR-652 restart work on retrying one exception at a time

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/CreateUpdateService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/CreateUpdateService.kt
@@ -1,13 +1,7 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.message
 
+import jakarta.persistence.OptimisticLockException
 import kotlinx.coroutines.runBlocking
-import org.hibernate.StaleObjectStateException
-import org.hibernate.exception.ConstraintViolationException
-import org.springframework.dao.CannotAcquireLockException
-import org.springframework.dao.DataIntegrityViolationException
-import org.springframework.orm.ObjectOptimisticLockingFailureException
-import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
-import org.springframework.orm.jpa.JpaSystemException
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
@@ -39,13 +33,7 @@ class CreateUpdateService(
   @Retryable(
     backoff = Backoff(random = true, delay = 1000, maxDelay = 2000, multiplier = 1.5),
     retryFor = [
-      ObjectOptimisticLockingFailureException::class,
-      CannotAcquireLockException::class,
-      JpaSystemException::class,
-      JpaObjectRetrievalFailureException::class,
-      DataIntegrityViolationException::class,
-      ConstraintViolationException::class,
-      StaleObjectStateException::class,
+      OptimisticLockException::class,
     ],
   )
   @Transactional(isolation = Isolation.REPEATABLE_READ)
