5d91926f528bcf432cff25e1e43094a8af1eb6ec
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-03-28 14:32:34 +0000

    CPR-616 Remove old candidate search and person match score results (#948)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/queries/statementmodels/PreparedStringStatement.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.personrecord.jpa.repository.queries.statementmodels
-
-data class PreparedStringStatement(
-  val parameterName: String,
-  val value: String?,
-)
