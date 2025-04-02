0aa4950eb8b892eea25d04006cd75e5c4927faeb
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-04-02 10:08:50 +0100

    CPR-401 Leave in merged not null index

--- src/main/resources/db/migration/V85__remove_candidate_search_indexes.sql
+++ src/main/resources/db/migration/V85__remove_candidate_search_indexes.sql
@@ -6,7 +6,6 @@ DROP INDEX IF EXISTS idx_dob_month;
 DROP INDEX IF EXISTS idx_dob_year;
 DROP INDEX IF EXISTS idx_first_name_soundex;
 DROP INDEX IF EXISTS idx_last_name_soundex;
-DROP INDEX IF EXISTS idx_merged_to_not_null;
 DROP INDEX IF EXISTS idx_reference_identifier_type_and_value;
 
 -----------------------------------------------------
