0f580a31aedc6378d8e4687e4c3d168b6147e25b
samz-justice <sam.zaroug@justice.gov.uk>
2025-04-09 14:50:17 +0100

    CPR-623 recluster on clusters with override markers (#978)
    * CPR-623 covering test for mutual exclusion between clusters
    
    * CPR-617 Add defensive checks to matches (#977)
    
    * CPR-617 Add defensive checks to matches
    
    * CPR-617 Format
    
    * CPR-623 Add tests
    
    * CPR-623 Fix Handle matching to records in cluster
    
    * CPR-623 Fix
    
    * CPR-623 processing clusters with exclude markers
    
    * CPR-623 moved test for exclusion markers to own inner test class
    
    * CPR-623 added rest of cases from recluster clusters with exclusion markers from the confluence page
    
    * CPR-623 tidy up to test cases
    
    * CPR-623 revisiting tests after update to recluster rules on the exclusion markers
    
    * CPR-623 added new test for recluster when exclusion is from the changed record
    
    * CPR-623 Add rest of test for reclustering
    
    * CPR-623 Refactor test
    
    * CPR-623 Add log to override recluster scenario
    
    ---------
    
    Co-authored-by: H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
    Co-authored-by: Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/type/TelemetryEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/type/TelemetryEventType.kt
@@ -19,5 +19,6 @@ enum class TelemetryEventType(val eventName: String) {
   CPR_UUID_DELETED("CprUUIDHardDeleted"),
   CPR_RECLUSTER_UUID_MARKED_NEEDS_ATTENTION("CprUuidReclusterNeedsAttention"),
   CPR_RECLUSTER_CLUSTER_RECORDS_NOT_LINKED("CprReclusterClusterRecordsNotLinked"),
+  CPR_RECLUSTER_MATCHED_CLUSTERS_HAS_EXCLUSIONS("CprReclusterMatchedClustersHasExclusions"),
   CPR_RECLUSTER_MERGE("CprReclusterMerge"),
 }
