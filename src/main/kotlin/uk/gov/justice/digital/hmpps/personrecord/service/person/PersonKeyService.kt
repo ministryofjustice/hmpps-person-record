cc138a4e15cee60a74e9dd84f4eccd1815b09edc
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-04-11 11:00:27 +0100

    CPR-648 Run format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonKeyService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonKeyService.kt
@@ -5,13 +5,9 @@ import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
-import uk.gov.justice.digital.hmpps.personrecord.service.EventKeys
-import uk.gov.justice.digital.hmpps.personrecord.service.TelemetryService
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.personkey.PersonKeyCreated
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.personkey.PersonKeyFound
 import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
-import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_CANDIDATE_RECORD_FOUND_UUID
-import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_UUID_CREATED
 
 @Component
 class PersonKeyService(
