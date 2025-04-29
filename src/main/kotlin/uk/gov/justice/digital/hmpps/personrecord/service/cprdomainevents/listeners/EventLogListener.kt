d76c35fa9dc4845686e223021e2fca4d014565b4
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-04-29 11:47:18 +0100

    CPR-647 Simplify data passing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/listeners/EventLogListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/listeners/EventLogListener.kt
@@ -16,6 +16,6 @@ class EventLogListener(
   @EventListener
   @TransactionalEventListener
   fun onEventLogEvent(eventLog: RecordEventLog) {
-    eventLogService.logEvent(eventLog.personEntity, eventLog.eventType, eventLog.personKeyEntity, eventLog.clusterComposition)
+    eventLogService.logEvent(eventLog)
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/eventlog/EventLogService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/eventlog/EventLogService.kt
@@ -2,11 +2,9 @@ package uk.gov.justice.digital.hmpps.personrecord.service.eventlog
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.personrecord.client.model.match.isclustervalid.ValidCluster
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.EventLogEntity
-import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
-import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.EventLogRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.eventlog.RecordEventLog
 
 @Component
 class EventLogService(
@@ -14,17 +12,12 @@ class EventLogService(
   private val objectMapper: ObjectMapper,
 ) {
 
-  fun logEvent(
-    personEntity: PersonEntity,
-    eventType: CPRLogEvents,
-    personKeyEntity: PersonKeyEntity? = null,
-    clusterComposition: List<ValidCluster>? = null,
-  ): EventLogEntity = eventLogRepository.save(
+  fun logEvent(eventLog: RecordEventLog) = eventLogRepository.save(
     EventLogEntity.from(
-      personEntity,
-      eventType,
-      objectMapper.writeValueAsString(clusterComposition),
-      personKeyEntity,
+      eventLog.personEntity,
+      eventLog.eventType,
+      objectMapper.writeValueAsString(eventLog.clusterComposition),
+      eventLog.personKeyEntity,
     ),
   )
 }
