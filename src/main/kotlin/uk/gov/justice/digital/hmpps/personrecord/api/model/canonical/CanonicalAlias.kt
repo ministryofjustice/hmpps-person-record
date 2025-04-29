2aefeec58ad94586136f6c5202587f1ba45fbc76
samz-justice <sam.zaroug@justice.gov.uk>
2025-04-29 10:24:44 +0100

    CPR-642 added missing import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalAlias.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalAlias.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.personrecord.api.model.canonical
 
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PseudonymEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.NameType
 
 data class CanonicalAlias(
   @Schema(description = "Person alias first name", example = "Jon")
