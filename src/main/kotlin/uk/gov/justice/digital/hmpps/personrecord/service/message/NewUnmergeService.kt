27d015c36f69054b88ebf530d055c7f431d2eb67
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2025-04-29 15:45:48 +0100

    CPR-610 New unmerge service

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/NewUnmergeService.kt
@@ -0,0 +1,56 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.message
+
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.model.merge.UnmergeEvent
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
+import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
+import uk.gov.justice.digital.hmpps.personrecord.service.message.recluster.ClusterDetails
+import uk.gov.justice.digital.hmpps.personrecord.service.person.PersonService
+import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
+
+@Component
+class NewUnmergeService(
+  private val personService: PersonService,
+  private val personKeyRepository: PersonKeyRepository,
+  private val personRepository: PersonRepository,
+) {
+
+  @Transactional
+  fun processUnmerge(reactivated: PersonEntity, unmerged: PersonEntity) {
+    when {
+      clusterContainsAdditionalRecords(reactivated, unmerged) -> setClusterAsNeedsAttention(unmerged.personKey)
+      else -> unmerge(reactivated, unmerged)
+    }
+  }
+
+  private fun unmerge(reactivated: PersonEntity, unmerged: PersonEntity) {
+    unmerged.addExcludeOverrideMarker(excludeRecord = reactivated)
+    personRepository.save(unmerged)
+
+    reactivated.personKey?.let { reactivated.removePersonKeyLink() }
+    reactivated.removeMergedLink()
+
+    reactivated.addExcludeOverrideMarker(excludeRecord = unmerged)
+    personService.linkRecordToPersonKey(reactivated)
+  }
+
+  private fun setClusterAsNeedsAttention(cluster: PersonKeyEntity?) {
+    cluster?.let {
+      it.status = UUIDStatusType.NEEDS_ATTENTION
+      personKeyRepository.save(it)
+    }
+  }
+
+  private fun clusterContainsAdditionalRecords(reactivated: PersonEntity, unmerged: PersonEntity): Boolean {
+    val additionalRecords = unmerged.personKey?.let { cluster ->
+      cluster.personEntities.filter {
+        listOf(unmerged.id, reactivated.id).contains(it.id).not()
+      }
+    }
+    return (additionalRecords?.size ?: 0) > 0
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/NewMergeServiceIntTest.kt
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.personrecord.service
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.personrecord.config.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.OverrideMarkerType
+import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
+import uk.gov.justice.digital.hmpps.personrecord.service.message.NewUnmergeService
+
+
+class NewMergeServiceIntTest: IntegrationTestBase() {
+
+  @Autowired
+  lateinit var newUnmergeService: NewUnmergeService
+
+  @Test
+  fun `should unmerge 2 merged records that exist on same cluster`() {
+    val cluster = createPersonKey()
+    val reactivatedRecord = createPerson(createRandomProbationPersonDetails(), cluster)
+    val unmergedRecord = createPerson(createRandomProbationPersonDetails(), cluster)
+
+    stubPersonMatchScores()
+
+    newUnmergeService.processUnmerge(reactivatedRecord, unmergedRecord)
+
+    reactivatedRecord.assertExcludedFrom(unmergedRecord)
+    unmergedRecord.assertExcludedFrom(reactivatedRecord)
+
+    cluster.assertClusterStatus(UUIDStatusType.ACTIVE)
+    cluster.assertClusterIsOfSize(1)
+
+    unmergedRecord.assertLinkedToCluster(cluster)
+    reactivatedRecord.assertNotLinkedToCluster(cluster)
+  }
+
+  @Test
+  fun `should set to needs attention when extra records on the cluster`() {
+    val cluster = createPersonKey()
+    val reactivatedRecord = createPerson(createRandomProbationPersonDetails(), cluster)
+    val unmergedRecord = createPerson(createRandomProbationPersonDetails(), cluster)
+    createPerson(createRandomProbationPersonDetails(), cluster)
+
+    newUnmergeService.processUnmerge(reactivatedRecord, unmergedRecord)
+
+    cluster.assertClusterStatus(UUIDStatusType.NEEDS_ATTENTION)
+    cluster.assertClusterIsOfSize(3)
+  }
+
+  private fun PersonEntity.assertNotLinkedToCluster(cluster: PersonKeyEntity) = awaitAssert {
+    assertThat(personRepository.findByMatchId(this.matchId)?.personKey?.personUUID).isNotEqualTo(cluster.personUUID)
+  }
+
+  private fun PersonEntity.assertLinkedToCluster(cluster: PersonKeyEntity) = awaitAssert {
+    assertThat(personRepository.findByMatchId(this.matchId)?.personKey?.personUUID).isEqualTo(cluster.personUUID)
+  }
+
+  private fun PersonEntity.assertExcludedFrom(personEntity: PersonEntity) = awaitAssert {
+    assertThat(
+      personRepository.findByMatchId(this.matchId)?.overrideMarkers?.filter { it.markerType == OverrideMarkerType.EXCLUDE && it.markerValue == personEntity.id }
+    ).hasSize(1)
+  }
+}
