3de7ed403409a339e2a70ca7b09bc1b1b7ffce49
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-04-30 15:39:30 +0100

    CPR-642 remove unused migration

--- src/main/resources/db/migration/V93__DONOTCOMMIT_remove_names_dob_title.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
--------------------------------------------------------
-
-ALTER TABLE IF EXISTS person_key
-    add constraint merged_to_itself_check check (merged_to != id);
------------------------------------------------------
-COMMIT;
