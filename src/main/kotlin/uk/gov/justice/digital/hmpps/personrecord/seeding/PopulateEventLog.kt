5d0e4189e1c302b9000a40fbb765e9c3a66248b8
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-05-08 15:40:25 +0100

    CPR-680 populate event log table from person repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateEventLog.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateEventLog.kt
@@ -9,10 +9,17 @@ import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.EventLogEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.EventLogRepository
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
 
 @RestController
 @Profile("seeding")
-class PopulateEventLog {
+class PopulateEventLog(
+  private val eventLogRepository: EventLogRepository,
+  private val personRepository: PersonRepository,
+) {
 
   @Hidden
   @RequestMapping(method = [POST], value = ["/populateeventlog"])
@@ -23,6 +30,9 @@ class PopulateEventLog {
 
   suspend fun populateEventLog() {
     CoroutineScope(Dispatchers.Default).launch {
+      personRepository.findAll().forEach { person ->
+        eventLogRepository.saveAndFlush(EventLogEntity.from(personEntity = person, eventType = CPRLogEvents.CPR_RECORD_CREATED, personKeyEntity = person.personKey))
+      }
       log.info("Event log population finished")
     }
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateEventLogIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateEventLogIntTest.kt
@@ -1,20 +1,32 @@
 package uk.gov.justice.digital.hmpps.personrecord.seeding
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
+import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
 
 @ActiveProfiles("seeding")
 class PopulateEventLogIntTest : WebTestBase() {
   @Test
   fun `populate event log`() {
-    createPersonWithNewKey(Person(sourceSystem = DELIUS))
+    eventLogRepository.deleteAll()
+    personKeyRepository.deleteAll()
+    val key = createPersonKey()
+    val person = createPerson(Person(sourceSystem = DELIUS), key)
     webTestClient.post()
       .uri("/populateeventlog")
       .exchange()
       .expectStatus()
       .isOk
+
+    awaitAssert { assertThat(eventLogRepository.count()).isEqualTo(1) }
+    val entry = eventLogRepository.findAll().first()
+    assertThat(entry.uuid).isEqualTo(key.personUUID)
+    assertThat(entry.sourceSystem).isEqualTo(DELIUS)
+    assertThat(entry.eventType).isEqualTo(CPRLogEvents.CPR_RECORD_CREATED)
+    assertThat(entry.sourceSystemId).isEqualTo(person.crn)
   }
 }
