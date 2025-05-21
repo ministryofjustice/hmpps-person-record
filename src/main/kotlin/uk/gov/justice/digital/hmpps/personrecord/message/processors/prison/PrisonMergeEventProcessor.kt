b11e72f8144069a308706b4ee1c592da0b523a7f
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-05-21 13:33:39 +0100

    CPR-663 Remove merge received custom event (#1087)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonMergeEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonMergeEventProcessor.kt
@@ -1,22 +1,16 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.processors.prison
 
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
-import uk.gov.justice.digital.hmpps.personrecord.service.EventKeys
-import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.telemetry.RecordTelemetry
 import uk.gov.justice.digital.hmpps.personrecord.service.format.EncodingService
 import uk.gov.justice.digital.hmpps.personrecord.service.message.CreateUpdateService
 import uk.gov.justice.digital.hmpps.personrecord.service.message.MergeService
-import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.MERGE_MESSAGE_RECEIVED
 
 @Component
 class PrisonMergeEventProcessor(
-  private val publisher: ApplicationEventPublisher,
   private val personRepository: PersonRepository,
   private val mergeService: MergeService,
   private val encodingService: EncodingService,
@@ -24,17 +18,6 @@ class PrisonMergeEventProcessor(
 ) {
 
   fun processEvent(domainEvent: DomainEvent) {
-    publisher.publishEvent(
-      RecordTelemetry(
-        MERGE_MESSAGE_RECEIVED,
-        mapOf(
-          EventKeys.FROM_SOURCE_SYSTEM_ID to domainEvent.additionalInformation?.sourcePrisonNumber,
-          EventKeys.TO_SOURCE_SYSTEM_ID to domainEvent.additionalInformation?.prisonNumber,
-          EventKeys.EVENT_TYPE to domainEvent.eventType,
-          EventKeys.SOURCE_SYSTEM to SourceSystemType.NOMIS.name,
-        ),
-      ),
-    )
     encodingService.getPrisonerDetails(domainEvent.additionalInformation?.prisonNumber!!) {
       it?.let {
         val from: PersonEntity? = personRepository.findByPrisonNumber(domainEvent.additionalInformation.sourcePrisonNumber!!)
