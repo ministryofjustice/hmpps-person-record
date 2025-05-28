717f50b2896279779e6de5014bd2244077d61d39
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-05-28 14:35:58 +0100

    CPR-393 remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     }
   }
 
-  implementation("org.springframework.cloud:spring-cloud-dependencies:2024.0.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.10")
 
