16027228fdf097c2c9955db7bf4f8644f429b74f
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-06-02 16:10:03 +0100

    CPR-464 Handle Probabtion recovered domain event (#1110)
    * CPR-464 Test recovered domain event
    
    * CPR-464 format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/type/DomainEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/type/DomainEventType.kt
@@ -9,6 +9,7 @@ const val OFFENDER_PERSONAL_DETAILS_UPDATED = "probation-case.personal-details.u
 const val OFFENDER_ADDRESS_CREATED = "probation-case.address.created"
 const val OFFENDER_ADDRESS_UPDATED = "probation-case.address.updated"
 const val OFFENDER_ADDRESS_DELETED = "probation-case.address.deleted"
+const val OFFENDER_RECOVERED = "probation-case.engagement.recovered"
 
 const val OFFENDER_ALIAS_CHANGED = "OFFENDER_ALIAS_CHANGED"
 
