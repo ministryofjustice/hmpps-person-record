f45d8be4c7a841bb37929f07fc8fd267dc958bdc
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-06-04 15:32:18 +0100

    CPR-640 encoding service does nothing for prison requests (#1124)
    * CPR-640 encoding service does nothing for prison requests
    
    * CPR-640 simplify lets

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonMergeEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonMergeEventProcessor.kt
@@ -1,11 +1,11 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.processors.prison
 
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.PrisonerSearchClient
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.service.format.EncodingService
 import uk.gov.justice.digital.hmpps.personrecord.service.message.CreateUpdateService
 import uk.gov.justice.digital.hmpps.personrecord.service.message.MergeService
 
@@ -13,17 +13,19 @@ import uk.gov.justice.digital.hmpps.personrecord.service.message.MergeService
 class PrisonMergeEventProcessor(
   private val personRepository: PersonRepository,
   private val mergeService: MergeService,
-  private val encodingService: EncodingService,
+  private val prisonerSearchClient: PrisonerSearchClient,
   private val createUpdateService: CreateUpdateService,
 ) {
 
   fun processEvent(domainEvent: DomainEvent) {
-    encodingService.getPrisonerDetails(domainEvent.additionalInformation?.prisonNumber!!) {
-      it?.let {
-        val from: PersonEntity? = personRepository.findByPrisonNumber(domainEvent.additionalInformation.sourcePrisonNumber!!)
-        val to: PersonEntity = createUpdateService.processPerson(Person.from(it), shouldReclusterOnUpdate = false) { personRepository.findByPrisonNumber(domainEvent.additionalInformation.prisonNumber) }
-        mergeService.processMerge(from, to)
-      }
+    prisonerSearchClient.getPrisoner(domainEvent.additionalInformation?.prisonNumber!!)?.let {
+      val from: PersonEntity? =
+        personRepository.findByPrisonNumber(domainEvent.additionalInformation.sourcePrisonNumber!!)
+      val to: PersonEntity = createUpdateService.processPerson(
+        Person.from(it),
+        shouldReclusterOnUpdate = false,
+      ) { personRepository.findByPrisonNumber(domainEvent.additionalInformation.prisonNumber) }
+      mergeService.processMerge(from, to)
     }
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/format/EncodingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/format/EncodingService.kt
@@ -4,15 +4,12 @@ import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.client.CorePersonRecordAndDeliusClient
-import uk.gov.justice.digital.hmpps.personrecord.client.PrisonerSearchClient
 import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
-import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.Prisoner
 import uk.gov.justice.digital.hmpps.personrecord.service.RetryExecutor
 
 @Component
 class EncodingService(
   private var corePersonRecordAndDeliusClient: CorePersonRecordAndDeliusClient,
-  private var prisonerSearchClient: PrisonerSearchClient,
   private val retryExecutor: RetryExecutor,
 ) {
 
@@ -23,15 +20,6 @@ class EncodingService(
       }
     }
   }.fold(onSuccess, failedToRetrieveProbationCase())
-
-  fun getPrisonerDetails(prisonNumber: String, onSuccess: (value: Prisoner?) -> Any?) = runCatching {
-    prisonerSearchClient.getPrisoner(prisonNumber)
-  }.fold(onSuccess, failedToRetrievePrisoner())
-
-  private fun failedToRetrievePrisoner(): (exception: Throwable) -> Unit? = {
-    log.error("Error retrieving prisoner detail: ${it.message}")
-    throw it
-  }
 
   private fun failedToRetrieveProbationCase(): (exception: Throwable) -> Unit? = {
     log.error("Error retrieving new offender detail: ${it.message}")
