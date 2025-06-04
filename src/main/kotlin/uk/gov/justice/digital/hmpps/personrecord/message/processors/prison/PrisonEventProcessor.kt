6454ae92482d812e2ee5fcc062760b3017ea198d
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-06-04 15:20:36 +0100

    CPR-640 encoding service does nothing for prison requests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonEventProcessor.kt
@@ -1,24 +1,23 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.processors.prison
 
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.PrisonerSearchClient
+import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.Prisoner
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.service.format.EncodingService
 import uk.gov.justice.digital.hmpps.personrecord.service.message.CreateUpdateService
 
 @Component
 class PrisonEventProcessor(
   private val createUpdateService: CreateUpdateService,
   private val personRepository: PersonRepository,
-  private val encodingService: EncodingService,
+  private val prisonerSearchClient: PrisonerSearchClient,
 ) {
 
   fun processEvent(domainEvent: DomainEvent) {
     val prisonNumber = domainEvent.additionalInformation?.prisonNumber!!
-    encodingService.getPrisonerDetails(
-      prisonNumber,
-    ) {
+    prisonerSearchClient.getPrisoner(prisonNumber).let<Prisoner?, Any?> { it: Prisoner? ->
       it?.let {
         createUpdateService.processPerson(Person.from(it)) {
           personRepository.findByPrisonNumber(prisonNumber)
