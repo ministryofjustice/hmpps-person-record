e33fd62820682387d451f297f9bafe6200bd4a52
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-06-04 13:38:15 +0100

    CPR-717 Remove currently managed (#1121)
    * CPR-717 Remove currently managed
    
    * CPR-717 Remove migration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/match/MatchStatus.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.personrecord.client.model.match
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class MatchStatus(
-  val status: String? = null,
-)
