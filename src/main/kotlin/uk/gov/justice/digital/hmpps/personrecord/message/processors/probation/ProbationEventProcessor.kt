b92434cc701f4bff6b6c412b285396568642440c
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-06-06 17:08:36 +0100

    CPR-640 splitting retries

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationEventProcessor.kt
@@ -1,6 +1,12 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.processors.probation
 
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClientRequestException
+import org.springframework.web.reactive.function.client.WebClientResponseException.BadGateway
+import org.springframework.web.reactive.function.client.WebClientResponseException.InternalServerError
+import org.springframework.web.reactive.function.client.WebClientResponseException.ServiceUnavailable
 import uk.gov.justice.digital.hmpps.personrecord.client.CorePersonRecordAndDeliusClient
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
@@ -13,6 +19,15 @@ class ProbationEventProcessor(
   private val corePersonRecordAndDeliusClient: CorePersonRecordAndDeliusClient,
 ) {
 
+  @Retryable(
+    backoff = Backoff(delay = 200, random = true, multiplier = 3.0),
+    retryFor = [
+      InternalServerError::class,
+      BadGateway::class,
+      ServiceUnavailable::class,
+      WebClientRequestException::class,
+    ],
+  )
   fun processEvent(crn: String) {
     corePersonRecordAndDeliusClient.getProbationCase(crn).let {
       createUpdateService.processPerson(Person.from(it)) {
