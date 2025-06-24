9e074179d266673ae9f0b4316eaa6feca91c618f
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-06-24 17:38:09 +0100

    FIX: bump minor versions while skipping Kotlin upgrade (#1189)
    * FIX: bump minor versions while skipping Kotlin upgrade
    
    * FIX: remove little used dependency and format

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifierTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifierTest.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.personrecord.model.identifiers
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCro
-import kotlin.test.assertEquals
 
 class CROIdentifierTest {
   @Test
@@ -27,25 +26,25 @@ class CROIdentifierTest {
   @Test
   fun `should process a SF format CRO`() {
     val identifier = CROIdentifier.from("SF05/482703J")
-    assertEquals("SF05/482703J", identifier.croId)
+    assertThat("SF05/482703J").isEqualTo(identifier.croId)
   }
 
   @Test
   fun `should process a SF format CRO with limit serial section should not pad serial num`() {
     val identifier = CROIdentifier.from("SF83/50058Z")
-    assertEquals("SF83/50058Z", identifier.croId)
+    assertThat("SF83/50058Z").isEqualTo(identifier.croId)
   }
 
   @Test
   fun `should process a standard format CRO`() {
     val inputCroId = randomCro()
     val identifier = CROIdentifier.from(inputCroId)
-    assertEquals(inputCroId, identifier.croId)
+    assertThat(inputCroId).isEqualTo(identifier.croId)
   }
 
   @Test
   fun `should process a standard format CRO with limit serial section`() {
     val identifier = CROIdentifier.from("65656/91H")
-    assertEquals("065656/91H", identifier.croId)
+    assertThat("065656/91H").isEqualTo(identifier.croId)
   }
 }
