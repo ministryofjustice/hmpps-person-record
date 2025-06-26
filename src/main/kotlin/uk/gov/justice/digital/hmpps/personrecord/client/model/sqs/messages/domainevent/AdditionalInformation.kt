cd35e86ed5532dfa3059b80070bce9ed6174a4d0
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-06-26 08:17:04 +0100

    FIX: remove unused property (#1190)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/sqs/messages/domainevent/AdditionalInformation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/sqs/messages/domainevent/AdditionalInformation.kt
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class AdditionalInformation(
-  @JsonProperty("categoriesChanged")
-  val categoriesChanged: List<String>? = emptyList(),
 
   @JsonProperty("nomsNumber")
   val prisonNumber: String? = null,
