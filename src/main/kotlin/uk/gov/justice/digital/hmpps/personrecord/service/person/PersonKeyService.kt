bd1c807cdb7fc77797f4523e35e84a0dcdf4add4
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-02 12:10:19 +0100

    FIX: move methods and reduce dependencies

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonKeyService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonKeyService.kt
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
-import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.ACTIVE
-import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.eventlog.RecordEventLog
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.personkey.PersonKeyCreated
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.personkey.PersonKeyFound
-import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
 
 @Component
 class PersonKeyService(
@@ -29,17 +26,5 @@ class PersonKeyService(
     publisher.publishEvent(PersonKeyFound(personEntity, highConfidenceRecord.personKey!!))
     personEntity.personKey = highConfidenceRecord.personKey!!
     return personEntity
-  }
-
-  fun settingNeedsAttentionClusterToActive(personKeyEntity: PersonKeyEntity, changedRecord: PersonEntity) {
-    personKeyEntity.status = ACTIVE
-    personKeyRepository.save(personKeyEntity)
-    publisher.publishEvent(
-      RecordEventLog(
-        CPRLogEvents.CPR_NEEDS_ATTENTION_TO_ACTIVE,
-        changedRecord,
-        personKeyEntity,
-      ),
-    )
   }
 }
