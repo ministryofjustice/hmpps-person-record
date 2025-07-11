2f5f59ef3c2e89cfafc4b05041e50cc84492c949
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-07-11 08:14:44 +0100

    CPR-780 cluster in an easier to deal with format (#1224)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/ClustersController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/ClustersController.kt
@@ -15,6 +15,10 @@ import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.SourceS
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.COMMON_PLATFORM
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.LIBRA
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.NOMIS
 import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
 
 @RestController
@@ -34,11 +38,11 @@ class ClustersController(
     }.map {
       AdminCluster(
         uuid = it.personUUID.toString(),
-        recordComposition = SourceSystemComposition(
-          nomis = it.personEntities.getRecordCountBySourceSystem(SourceSystemType.NOMIS),
-          delius = it.personEntities.getRecordCountBySourceSystem(SourceSystemType.DELIUS),
-          commonPlatform = it.personEntities.getRecordCountBySourceSystem(SourceSystemType.COMMON_PLATFORM),
-          libra = it.personEntities.getRecordCountBySourceSystem(SourceSystemType.LIBRA),
+        recordComposition = listOf(
+          SourceSystemComposition(COMMON_PLATFORM, it.personEntities.getRecordCountBySourceSystem(COMMON_PLATFORM)),
+          SourceSystemComposition(DELIUS, it.personEntities.getRecordCountBySourceSystem(DELIUS)),
+          SourceSystemComposition(LIBRA, it.personEntities.getRecordCountBySourceSystem(LIBRA)),
+          SourceSystemComposition(NOMIS, it.personEntities.getRecordCountBySourceSystem(NOMIS)),
         ),
       )
     }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/controller/admin/ClustersApiIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/controller/admin/ClustersApiIntTest.kt
@@ -7,7 +7,14 @@ import org.springframework.core.ParameterizedTypeReference
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
 import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.AdminCluster
 import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.COMMON_PLATFORM
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.LIBRA
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.NOMIS
 import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
+import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.ACTIVE
+import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.MERGED
+import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.RECLUSTER_MERGE
 
 class ClustersApiIntTest : WebTestBase() {
 
@@ -38,17 +45,21 @@ class ClustersApiIntTest : WebTestBase() {
     assertThat(response.page).isEqualTo(0)
     assertThat(response.size).isEqualTo(20)
     assertThat(response.content[0].uuid).isEqualTo(person.personKey?.personUUID.toString())
-    assertThat(response.content[0].recordComposition.delius).isEqualTo(1)
-    assertThat(response.content[0].recordComposition.nomis).isEqualTo(0)
-    assertThat(response.content[0].recordComposition.libra).isEqualTo(0)
-    assertThat(response.content[0].recordComposition.commonPlatform).isEqualTo(0)
+    assertThat(response.content[0].recordComposition[0].count).isEqualTo(0)
+    assertThat(response.content[0].recordComposition[0].sourceSystem).isEqualTo(COMMON_PLATFORM)
+    assertThat(response.content[0].recordComposition[1].count).isEqualTo(1)
+    assertThat(response.content[0].recordComposition[1].sourceSystem).isEqualTo(DELIUS)
+    assertThat(response.content[0].recordComposition[2].count).isEqualTo(0)
+    assertThat(response.content[0].recordComposition[2].sourceSystem).isEqualTo(LIBRA)
+    assertThat(response.content[0].recordComposition[3].count).isEqualTo(0)
+    assertThat(response.content[0].recordComposition[3].sourceSystem).isEqualTo(NOMIS)
   }
 
   @Test
   fun `should not return clusters that are not NEEDS ATTENTION`() {
-    createPersonWithNewKey(createRandomProbationPersonDetails(), status = UUIDStatusType.ACTIVE)
-    createPersonWithNewKey(createRandomProbationPersonDetails(), status = UUIDStatusType.MERGED)
-    createPersonWithNewKey(createRandomProbationPersonDetails(), status = UUIDStatusType.RECLUSTER_MERGE)
+    createPersonWithNewKey(createRandomProbationPersonDetails(), status = ACTIVE)
+    createPersonWithNewKey(createRandomProbationPersonDetails(), status = MERGED)
+    createPersonWithNewKey(createRandomProbationPersonDetails(), status = RECLUSTER_MERGE)
 
     val responseType = object : ParameterizedTypeReference<PagedResponse<AdminCluster>>() {}
     val response = webTestClient.get()
@@ -98,16 +109,23 @@ class ClustersApiIntTest : WebTestBase() {
     assertThat(response.size).isEqualTo(20)
 
     assertThat(response.content[0].uuid).isEqualTo(cluster1.personUUID.toString())
-    assertThat(response.content[0].recordComposition.delius).isEqualTo(1)
-    assertThat(response.content[0].recordComposition.nomis).isEqualTo(1)
-    assertThat(response.content[0].recordComposition.libra).isEqualTo(1)
-    assertThat(response.content[0].recordComposition.commonPlatform).isEqualTo(1)
+    assertThat(response.content[0].recordComposition[0].count).isEqualTo(1)
+    assertThat(response.content[0].recordComposition[0].sourceSystem).isEqualTo(COMMON_PLATFORM)
+    assertThat(response.content[0].recordComposition[1].count).isEqualTo(1)
+    assertThat(response.content[0].recordComposition[1].sourceSystem).isEqualTo(DELIUS)
+    assertThat(response.content[0].recordComposition[2].count).isEqualTo(1)
+    assertThat(response.content[0].recordComposition[2].sourceSystem).isEqualTo(LIBRA)
+    assertThat(response.content[0].recordComposition[3].count).isEqualTo(1)
+    assertThat(response.content[0].recordComposition[3].sourceSystem).isEqualTo(NOMIS)
 
     assertThat(response.content[1].uuid).isEqualTo(cluster2.personUUID.toString())
-    assertThat(response.content[1].recordComposition.delius).isEqualTo(2)
-    assertThat(response.content[1].recordComposition.nomis).isEqualTo(0)
-    assertThat(response.content[1].recordComposition.libra).isEqualTo(0)
-    assertThat(response.content[1].recordComposition.commonPlatform).isEqualTo(0)
+    assertThat(response.content[1].recordComposition.first { it.sourceSystem == COMMON_PLATFORM }.count).isEqualTo(0)
+    assertThat(response.content[1].recordComposition[1].count).isEqualTo(2)
+    assertThat(response.content[1].recordComposition[1].sourceSystem).isEqualTo(DELIUS)
+    assertThat(response.content[1].recordComposition[2].count).isEqualTo(0)
+    assertThat(response.content[1].recordComposition[2].sourceSystem).isEqualTo(LIBRA)
+    assertThat(response.content[1].recordComposition[3].count).isEqualTo(0)
+    assertThat(response.content[1].recordComposition[3].sourceSystem).isEqualTo(NOMIS)
   }
 
   @Test
