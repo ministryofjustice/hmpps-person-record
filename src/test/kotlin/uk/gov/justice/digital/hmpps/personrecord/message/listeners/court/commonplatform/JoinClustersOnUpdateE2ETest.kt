70dfc50050981bd4e27c4b631ae4c2bfcb915dde
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-17 15:25:35 +0100

    CPR-766 another test showing how the system actually behaves

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/commonplatform/JoinClustersOnUpdateE2ETest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/commonplatform/JoinClustersOnUpdateE2ETest.kt
@@ -243,4 +243,105 @@ class JoinClustersOnUpdateE2ETest : MessagingTestBase() {
     assertThat(secondPersonRecord.personKey!!.personEntities.size).isEqualTo(2)
     assertThat(secondPersonRecord.personKey!!.personUUID).isEqualTo(firstPersonRecord.personKey!!.personUUID)
   }
+
+  @Test
+  fun `should create two people who do not match followed by a third which matches both and picks one - then when updating one of the original two the clusters are merged`() {
+    // we think our system behaves like this,
+    // but apparently it actually just picks one of the clusters even on update
+    // expected
+    /*
+    create A
+    create B, no match
+    create C, matches A and B, picks A
+    update B, matches A and C merges B into A
+     */
+    // actual
+    /*
+   ???
+     */
+
+    val firstCrnWithPnc = randomCrn()
+    val secondCrnWithCro = randomCrn()
+    val thirdCrnWithBoth = randomCrn()
+    val pnc = randomPnc()
+    val cro = randomCro()
+    val basePerson = createRandomProbationPersonDetails(firstCrnWithPnc)
+    val firstSetup = ApiResponseSetup(
+      crn = firstCrnWithPnc,
+      cro = "",
+      pnc = pnc,
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, firstSetup)
+
+    val firstPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(firstCrnWithPnc) })
+    assertThat(firstPersonRecord.getPrimaryName().lastName).isEqualTo(basePerson.lastName)
+    assertThat(firstPersonRecord.getPnc()).isEqualTo(pnc)
+    assertThat(firstPersonRecord.addresses.size).isEqualTo(1)
+    assertThat(firstPersonRecord.personKey!!.personEntities.size).isEqualTo(1)
+
+    checkTelemetry(
+      CPR_RECORD_CREATED,
+      mapOf("SOURCE_SYSTEM" to "DELIUS", "CRN" to firstCrnWithPnc),
+    )
+    val secondSetup = ApiResponseSetup(
+      crn = secondCrnWithCro,
+      cro = cro,
+      pnc = "",
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, secondSetup)
+
+    val secondPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(secondCrnWithCro) })
+    assertThat(secondPersonRecord.getPrimaryName().lastName).isEqualTo(basePerson.lastName)
+    assertThat(secondPersonRecord.getCro()).isEqualTo(cro)
+    assertThat(secondPersonRecord.personKey!!.personEntities.size).isEqualTo(1)
+    assertThat(secondPersonRecord.personKey!!.personUUID).isNotEqualTo(firstPersonRecord.personKey!!.personUUID)
+
+    val thirdSetup = ApiResponseSetup(
+      crn = thirdCrnWithBoth,
+      cro = cro,
+      pnc = pnc,
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, thirdSetup)
+
+    val thirdPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(thirdCrnWithBoth) })
+    assertThat(thirdPersonRecord.getPrimaryName().lastName).isEqualTo(basePerson.lastName)
+    assertThat(thirdPersonRecord.getCro()).isEqualTo(cro)
+    assertThat(thirdPersonRecord.getPnc()).isEqualTo(pnc)
+    assertThat(thirdPersonRecord.personKey!!.personEntities.size).isEqualTo(2)
+
+    val secondUpdate = ApiResponseSetup(
+      crn = secondCrnWithCro,
+      cro = cro,
+      pnc = "",
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress()), ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+    probationDomainEventAndResponseSetup(OFFENDER_ADDRESS_UPDATED, secondUpdate)
+    val secondPersonRecordUpdated = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(secondCrnWithCro) })
+    assertThat(secondPersonRecordUpdated.getPrimaryName().lastName).isEqualTo(basePerson.lastName)
+    assertThat(secondPersonRecordUpdated.getCro()).isEqualTo(cro)
+    assertThat(secondPersonRecordUpdated.personKey!!.personEntities.size).isEqualTo(3)
+  }
 }
