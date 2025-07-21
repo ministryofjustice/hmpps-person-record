8c0d2ce401e96c4d125041c6dc18fb3edab54f3a
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-21 10:40:08 +0100

    CPR-766 test to show current behaviour of exclude override markers

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/commonplatform/JoinClustersE2ETest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/commonplatform/JoinClustersE2ETest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.personrecord.config.MessagingTestBase
 import uk.gov.justice.digital.hmpps.personrecord.service.type.NEW_OFFENDER_CREATED
+import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_UNMERGED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_RECORD_CREATED
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCro
@@ -139,5 +140,65 @@ class JoinClustersE2ETest : MessagingTestBase() {
     assertThat(secondPersonRecord.getCro()).isEqualTo(cro)
     assertThat(secondPersonRecord.personKey!!.personEntities.size).isEqualTo(2)
     assertThat(secondPersonRecord.personKey!!.personUUID).isEqualTo(firstPersonRecord.personKey!!.personUUID)
+  }
+
+  @Test
+  fun `handle a new person which matches two clusters with override markers`() {
+    val firstCrn = randomCrn()
+    val secondCrn = randomCrn()
+    val thirdCrn = randomCrn()
+
+    val pnc = randomPnc()
+    val cro = randomCro()
+    val basePerson = createRandomProbationPersonDetails(firstCrn)
+    val firstSetup = ApiResponseSetup(
+      crn = firstCrn,
+      cro = cro,
+      pnc = pnc,
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, firstSetup)
+    val firstPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(firstCrn) })
+    assertThat(firstPersonRecord.personKey!!.personEntities.size).isEqualTo(1)
+
+    val secondSetup = ApiResponseSetup(
+      crn = secondCrn,
+      cro = cro,
+      pnc = pnc,
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+    probationUnmergeEventAndResponseSetup(OFFENDER_UNMERGED, secondCrn, firstCrn, reactivatedSetup = secondSetup)
+    val secondPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(secondCrn) })
+    assertThat(secondPersonRecord.personKey!!.personEntities.size).isEqualTo(1)
+    secondPersonRecord.assertExcludedFrom(firstPersonRecord)
+
+    // create a new person which matches both
+    val thirdSetup = ApiResponseSetup(
+      crn = thirdCrn,
+      cro = cro,
+      pnc = pnc,
+      firstName = basePerson.firstName,
+      middleName = basePerson.middleNames,
+      lastName = basePerson.lastName,
+      dateOfBirth = basePerson.dateOfBirth,
+      addresses = listOf(ApiResponseSetupAddress(postcode = basePerson.addresses.first().postcode, fullAddress = randomFullAddress())),
+      aliases = listOf(ApiResponseSetupAlias(firstName = basePerson.aliases.first().firstName!!, middleName = basePerson.aliases.first().middleNames!!, lastName = basePerson.aliases.first().lastName!!, dateOfBirth = basePerson.aliases.first().dateOfBirth!!)),
+    )
+
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, thirdSetup)
+    val thirdPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(thirdCrn) })
+    assertThat(thirdPersonRecord.personKey!!.personEntities.size).isEqualTo(1)
+    // see what happens... atm it just joins one so this fails
   }
 }
