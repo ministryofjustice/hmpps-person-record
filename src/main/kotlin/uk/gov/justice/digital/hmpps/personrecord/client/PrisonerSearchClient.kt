56044ce577ce30882db0a08bc27c54da299eb227
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-23 12:42:31 +0100

    FIX: remove unused code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonerSearchClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/PrisonerSearchClient.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.client
 
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.Prisoner
 import uk.gov.justice.digital.hmpps.personrecord.service.queue.discardNotFoundException
@@ -17,10 +15,4 @@ class PrisonerSearchClient(private val prisonerSearchWebClient: WebClient) {
     .bodyToMono(Prisoner::class.java)
     .discardNotFoundException()
     .block()
-
-  @PostMapping(value = ["/prisoner-search/prisoner-numbers"])
-  fun getPrisonNumbers(prisonNumbers: PrisonNumbers): List<Prisoner>? = prisonerSearchWebClient.post().uri("/prisoner-search/prisoner-numbers").bodyValue(prisonNumbers)
-    .retrieve().bodyToMono(object : ParameterizedTypeReference<List<Prisoner>>() {}).block()
 }
-
-class PrisonNumbers(val prisonerNumbers: List<String>)
