f0e906a69ad3a2344119d237fad97cf3e2ebe340
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-07-23 15:25:15 +0100

    CPR-781 add event log admin endpoint (#1245)
    * CPR-781 Add event log admin endpoint
    
    * CPR-781 format

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/controller/admin/EventLogApiIntTest.kt
@@ -0,0 +1,62 @@
+package uk.gov.justice.digital.hmpps.personrecord.controller.admin
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
+import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.AdminEventLogSummary
+import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.eventlog.RecordEventLog
+import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
+import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.EventLogService
+import java.util.UUID
+
+class EventLogApiIntTest : WebTestBase() {
+
+  @Autowired
+  lateinit var eventLogService: EventLogService
+
+  @Test
+  fun `should return not return anything if cluster does not exist`() {
+    val uuid = UUID.randomUUID().toString()
+    val response = webTestClient.get()
+      .uri(eventLogUrl(uuid))
+      .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody(AdminEventLogSummary::class.java)
+      .returnResult()
+      .responseBody!!
+
+    assertThat(response.uuid).isEqualTo(uuid)
+    assertThat(response.eventLogs.count()).isEqualTo(0)
+  }
+
+  @Test
+  fun `should return list of event logs`() {
+    val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+
+    eventLogService.logEvent(RecordEventLog(CPRLogEvents.CPR_RECORD_CREATED, person))
+    eventLogService.logEvent(RecordEventLog(CPRLogEvents.CPR_UUID_CREATED, person))
+
+    val response = webTestClient.get()
+      .uri(eventLogUrl(person.personKey?.personUUID.toString()))
+      .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody(AdminEventLogSummary::class.java)
+      .returnResult()
+      .responseBody!!
+
+    assertThat(response.uuid).isEqualTo(person.personKey?.personUUID.toString())
+    assertThat(response.eventLogs.count()).isEqualTo(2)
+    assertThat(response.eventLogs[0].eventType).isEqualTo(CPRLogEvents.CPR_UUID_CREATED.name)
+    assertThat(response.eventLogs[0].uuidStatusType).isEqualTo("ACTIVE")
+    assertThat(response.eventLogs[1].eventType).isEqualTo(CPRLogEvents.CPR_RECORD_CREATED.name)
+    assertThat(response.eventLogs[1].uuidStatusType).isEqualTo("ACTIVE")
+  }
+
+  private fun eventLogUrl(uuid: String) = "/admin/event-log/$uuid"
+}
