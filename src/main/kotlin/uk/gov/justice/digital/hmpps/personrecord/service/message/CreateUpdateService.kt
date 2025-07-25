e0e6bbbdc2b3b0de58a8bdcb13d769510145578b
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-07-25 16:16:09 +0100

    CPR-764 refactoring ahead of adding IDs (#1251)
    Co-authored-by: H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/CreateUpdateService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/CreateUpdateService.kt
@@ -1,20 +1,17 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.message
 
 import kotlinx.coroutines.runBlocking
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Isolation.REPEATABLE_READ
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity.Companion.exists
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonCreated
 import uk.gov.justice.digital.hmpps.personrecord.service.person.PersonService
 
 @Component
 class CreateUpdateService(
   private val personService: PersonService,
-  private val publisher: ApplicationEventPublisher,
 ) {
 
   @Transactional(isolation = REPEATABLE_READ)
@@ -24,23 +21,11 @@ class CreateUpdateService(
   ): PersonEntity = runBlocking {
     return@runBlocking findPerson().exists(
       no = {
-        create(person)
+        personService.createPersonEntity(person)
       },
       yes = {
-        update(person, it)
+        personService.updatePersonEntity(person, it)
       },
     )
-  }
-
-  private fun create(person: Person): PersonEntity {
-    val personEntity: PersonEntity = personService.createPersonEntity(person)
-    publisher.publishEvent(PersonCreated(personEntity))
-    return personEntity
-  }
-
-  private fun update(person: Person, existingPersonEntity: PersonEntity): PersonEntity {
-    val personUpdated = personService.updatePersonEntity(person, existingPersonEntity)
-    publisher.publishEvent(personUpdated)
-    return personUpdated.personEntity
   }
 }
