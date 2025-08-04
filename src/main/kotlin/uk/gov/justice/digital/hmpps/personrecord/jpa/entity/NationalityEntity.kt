a23a8652e87fc93d45b11b87e4b4047f829d363a
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-08-04 14:33:44 +0100

    CPR-744 Map to entities

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/NationalityEntity.kt
@@ -0,0 +1,45 @@
+package uk.gov.justice.digital.hmpps.personrecord.jpa.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.reference.NationalityCodeEntity
+import java.time.LocalDate
+
+@Entity
+@Table(name = "nationalities")
+class NationalityEntity(
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = null,
+
+  @ManyToOne
+  @JoinColumn(
+    name = "fk_nationality_code_id",
+    referencedColumnName = "id",
+  )
+  var nationalityCode: NationalityCodeEntity? = null,
+
+  @ManyToOne(optional = false)
+  @JoinColumn(
+    name = "fk_person_id",
+    referencedColumnName = "id",
+    nullable = false,
+  )
+  var person: PersonEntity? = null,
+
+  @Column(name = "start_date")
+  val startDate: LocalDate? = null,
+
+  @Column(name = "end_date")
+  val endDate: LocalDate? = null,
+
+  @Column(name = "notes")
+  val notes: String? = null,
+)
