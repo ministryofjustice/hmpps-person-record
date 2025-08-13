7e2910e63272a2ad5ab30eceb62768d9f8cbc954
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-08-13 15:23:44 +0100

    CPR-794 Extra fields on nationality for canonical api

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalNationality.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/canonical/CanonicalNationality.kt
@@ -2,12 +2,19 @@ package uk.gov.justice.digital.hmpps.personrecord.api.model.canonical
 
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import java.time.LocalDate
 
 data class CanonicalNationality(
   @Schema(description = "person nationality code", example = "GB")
   val code: String? = null,
   @Schema(description = "Person nationality description", example = "GB")
   val description: String? = null,
+  @Schema(description = "Person nationality start date", example = "01/01/2000")
+  val startDate: LocalDate? = null,
+  @Schema(description = "Person nationality end date", example = "12/08/2025")
+  val endDate: LocalDate? = null,
+  @Schema(description = "Person nationality notes", example = "Example notes")
+  val notes: String? = null,
 
 ) {
   companion object {
@@ -16,6 +23,9 @@ data class CanonicalNationality(
       CanonicalNationality(
         code = it.nationalityCode?.code,
         description = it.nationalityCode?.description,
+        startDate = it.startDate,
+        endDate = it.startDate,
+        notes = it.notes
       )
     }
   }
