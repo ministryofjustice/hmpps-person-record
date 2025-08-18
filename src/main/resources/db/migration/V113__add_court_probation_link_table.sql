2dfd951bf5be7b081818d8f4085628f8e74a4310
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-08-18 10:36:22 +0100

    CPR-829 Make unique

--- src/main/resources/db/migration/V113__add_court_probation_link_table.sql
+++ src/main/resources/db/migration/V113__add_court_probation_link_table.sql
@@ -2,8 +2,8 @@ BEGIN;
 -------------------------------------------------------
 
 CREATE TABLE court_probation_link_table(
-    defendant_id TEXT NOT NULL,
-    crn TEXT NOT NULL
+    defendant_id TEXT UNIQUE NOT NULL,
+    crn TEXT UNIQUE NOT NULL
 );
 CREATE INDEX idx_court_probation_defendant_id ON court_probation_link_table(defendant_id);
 
