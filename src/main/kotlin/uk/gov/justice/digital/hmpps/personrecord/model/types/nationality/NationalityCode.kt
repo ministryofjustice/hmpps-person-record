30e78fdbde1b5fbb71f0dc934ea3495e5e7228c7
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-08-29 11:04:42 +0100

    CPR-857 log unknown nationalities

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/types/nationality/NationalityCode.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/types/nationality/NationalityCode.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.personrecord.model.types.nationality
 
+import org.slf4j.LoggerFactory
 import uk.gov.justice.digital.hmpps.personrecord.extentions.nullIfBlank
+import uk.gov.justice.digital.hmpps.personrecord.model.types.nationality.NationalityCode.Companion.getNationalityOrUnknown
 
 enum class NationalityCode {
   AFGA,
@@ -247,22 +249,23 @@ enum class NationalityCode {
   ;
 
   companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     fun fromProbationMapping(code: String?): NationalityCode? = code?.getNationalityOrUnknown(
       PROBATION_NATIONALITY_MAPPING,
-    )
+    ).also { if (it == UNKNOWN) log.info("Unknown nationality code probation: $it") }
 
     fun fromPrisonMapping(code: String?): NationalityCode? = code?.getNationalityOrUnknown(
       PRISON_NATIONALITY_MAPPING,
-    )
+    ).also { if (it == UNKNOWN) log.info("Unknown nationality code prison: $it") }
 
     fun fromCommonPlatformMapping(code: String?): NationalityCode? = code?.getNationalityOrUnknown(
       COMMON_PLATFORM_NATIONALITY_MAPPING,
-    )
+    ).also { if (it == UNKNOWN) log.info("Unknown nationality code common platform: $it") }
 
     fun fromLibraMapping(code: String?): NationalityCode? = code?.getNationalityOrUnknown(
       LIBRA_NATIONALITY_MAPPINGS,
-    )
+    ).also { if (it == UNKNOWN) log.info("Unknown nationality code libra: $it") }
 
     private fun String?.getNationalityOrUnknown(nationalityMap: Map<String, NationalityCode>): NationalityCode? = this.normalize()?.let {
       nationalityMap[it] ?: UNKNOWN
