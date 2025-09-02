c132e3c0b92bc6003c373e699741b6f761ded6da
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-09-02 09:37:46 +0100

    CPR-873 simplify (#1373)
    * CPR-873 simplify
    
    * CPR-873 simplify test
    
    * CPR-873 simplify test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/types/TitleCodeTest.kt
@@ -0,0 +1,150 @@
+package uk.gov.justice.digital.hmpps.personrecord.model.types
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import uk.gov.justice.digital.hmpps.personrecord.client.model.court.commonplatform.Defendant
+import uk.gov.justice.digital.hmpps.personrecord.client.model.court.commonplatform.PersonDefendant
+import uk.gov.justice.digital.hmpps.personrecord.client.model.court.commonplatform.PersonDetails
+import uk.gov.justice.digital.hmpps.personrecord.client.model.court.event.LibraHearingEvent
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.Identifiers
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.Value
+import uk.gov.justice.digital.hmpps.personrecord.client.model.prisoner.Prisoner
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCId
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
+import uk.gov.justice.digital.hmpps.personrecord.test.randomDate
+import uk.gov.justice.digital.hmpps.personrecord.test.randomDefendantId
+import uk.gov.justice.digital.hmpps.personrecord.test.randomName
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPrisonNumber
+import java.util.stream.Stream
+import uk.gov.justice.digital.hmpps.personrecord.client.model.court.libra.Name as LibraName
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCaseName as OffenderName
+
+class TitleCodeTest {
+
+  @ParameterizedTest
+  @MethodSource("probationTitleCodes")
+  fun `should map all probation title codes to cpr title codes`(probationTitleCode: String?, cprTitleCode: String?) {
+    val probationCase = ProbationCase(identifiers = Identifiers(randomCrn()), title = Value(probationTitleCode), name = OffenderName(firstName = randomName()))
+    val person = Person.from(probationCase)
+    assertThat(person.titleCode?.name).isEqualTo(cprTitleCode)
+  }
+
+  @ParameterizedTest
+  @MethodSource("prisonTitleCodes")
+  fun `should map all prison title codes to cpr title codes`(prisonTitleCode: String?, cprTitleCode: String?) {
+    val prisoner = Prisoner(prisonNumber = randomPrisonNumber(), title = prisonTitleCode, lastName = randomName(), firstName = randomName(), dateOfBirth = randomDate())
+    val person = Person.from(prisoner)
+    assertThat(person.titleCode?.name).isEqualTo(cprTitleCode)
+  }
+
+  @ParameterizedTest
+  @MethodSource("commonPlatformTitleCodes")
+  fun `should map all common platform title codes to cpr title codes`(defendantTitleCode: String?, cprTitleCode: String?) {
+    val defendant = Defendant(id = randomDefendantId(), personDefendant = PersonDefendant(personDetails = PersonDetails(title = defendantTitleCode, lastName = randomName())))
+    val person = Person.from(defendant)
+    assertThat(person.titleCode?.name).isEqualTo(cprTitleCode)
+  }
+
+  @ParameterizedTest
+  @MethodSource("libraTitleCodes")
+  fun `should map all libra title codes to cpr title codes`(libraTitleCode: String?, cprTitleCode: String?) {
+    val libraHearingEvent = LibraHearingEvent(cId = randomCId(), name = LibraName(title = libraTitleCode))
+    val person = Person.from(libraHearingEvent)
+    assertThat(person.titleCode?.name).isEqualTo(cprTitleCode)
+  }
+
+  companion object {
+
+    @JvmStatic
+    fun probationTitleCodes(): Stream<Arguments> = Stream.of(
+      Arguments.of("MR", "MR"),
+      Arguments.of("MRS", "MRS"),
+      Arguments.of("MISS", "MISS"),
+      Arguments.of("MS", "MS"),
+      Arguments.of("MX", "MX"),
+      Arguments.of("REV", "REV"),
+      Arguments.of("DME", "DME"),
+      Arguments.of("DR", "DR"),
+      Arguments.of("LDY", "LDY"),
+      Arguments.of("LRD", "LRD"),
+      Arguments.of("SIR", "SIR"),
+      Arguments.of("Invalid", "UN"),
+      Arguments.of(null, null),
+    )
+
+    @JvmStatic
+    fun prisonTitleCodes(): Stream<Arguments> = Stream.of(
+      Arguments.of("Mr", "MR"),
+      Arguments.of("Mrs", "MRS"),
+      Arguments.of("Miss", "MISS"),
+      Arguments.of("Ms", "MS"),
+      Arguments.of("Reverend", "REV"),
+      Arguments.of("Father", "FR"),
+      Arguments.of("Imam", "IMAM"),
+      Arguments.of("Rabbi", "RABBI"),
+      Arguments.of("Brother", "BR"),
+      Arguments.of("Sister", "SR"),
+      Arguments.of("Dame", "DME"),
+      Arguments.of("Dr", "DR"),
+      Arguments.of("Lady", "LDY"),
+      Arguments.of("Lord", "LRD"),
+      Arguments.of("Sir", "SIR"),
+      Arguments.of("Invalid", "UN"),
+      Arguments.of(null, null),
+    )
+
+    @JvmStatic
+    fun commonPlatformTitleCodes(): Stream<Arguments> = Stream.of(
+      Arguments.of("Mr", "MR"),
+      Arguments.of("Mrs", "MRS"),
+      Arguments.of("Miss", "MISS"),
+      Arguments.of("Ms", "MS"),
+      Arguments.of("Reverend", "REV"),
+      Arguments.of("Father", "FR"),
+      Arguments.of("Imam", "IMAM"),
+      Arguments.of("Rabbi", "RABBI"),
+      Arguments.of("Brother", "BR"),
+      Arguments.of("Sister", "SR"),
+      Arguments.of("Dame", "DME"),
+      Arguments.of("Dr", "DR"),
+      Arguments.of("Lady", "LDY"),
+      Arguments.of("Lord", "LRD"),
+      Arguments.of("Sir", "SIR"),
+      Arguments.of("Invalid", "UN"),
+      Arguments.of(null, null),
+    )
+
+    @JvmStatic
+    fun libraTitleCodes(): Stream<Arguments> = Stream.of(
+      Arguments.of("Mr", "MR"),
+      Arguments.of("MR", "MR"),
+      Arguments.of("Mrs", "MRS"),
+      Arguments.of("MRS", "MRS"),
+      Arguments.of("Miss", "MISS"),
+      Arguments.of("MISS", "MISS"),
+      Arguments.of("Ms", "MS"),
+      Arguments.of("MS", "MS"),
+      Arguments.of("Mx", "MX"),
+      Arguments.of("MX", "MX"),
+      Arguments.of("Reverend", "REV"),
+      Arguments.of("Father", "FR"),
+      Arguments.of("Imam", "IMAM"),
+      Arguments.of("Rabbi", "RABBI"),
+      Arguments.of("Brother", "BR"),
+      Arguments.of("Sister", "SR"),
+      Arguments.of("Dame", "DME"),
+      Arguments.of("Dr", "DR"),
+      Arguments.of("DR", "DR"),
+      Arguments.of("Lady", "LDY"),
+      Arguments.of("Lord", "LRD"),
+      Arguments.of("Sir", "SIR"),
+      Arguments.of("Invalid", "UN"),
+      Arguments.of("", null),
+      Arguments.of(null, null),
+    )
+  }
+}
