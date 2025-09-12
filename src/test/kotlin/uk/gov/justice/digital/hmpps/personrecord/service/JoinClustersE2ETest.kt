d2c7acebe9d2157e513f900a1f3cae27987ca793
samz-justice <sam.zaroug@justice.gov.uk>
2025-09-12 16:20:11 +0100

    CPR-839 simulating a test scenario

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/JoinClustersE2ETest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/JoinClustersE2ETest.kt
@@ -2,22 +2,113 @@ package uk.gov.justice.digital.hmpps.personrecord.service
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.personrecord.client.PersonMatchClient
 import uk.gov.justice.digital.hmpps.personrecord.config.E2ETestBase
 import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.ACTIVE
 import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType.NEEDS_ATTENTION_EXCLUDE
 import uk.gov.justice.digital.hmpps.personrecord.service.type.NEW_OFFENDER_CREATED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_UNMERGED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_RECORD_CREATED
+import uk.gov.justice.digital.hmpps.personrecord.test.messages.CommonPlatformHearingSetup
+import uk.gov.justice.digital.hmpps.personrecord.test.messages.CommonPlatformHearingSetupAddress
+import uk.gov.justice.digital.hmpps.personrecord.test.messages.commonPlatformHearing
+import uk.gov.justice.digital.hmpps.personrecord.test.messages.libraHearing
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCId
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCro
+import uk.gov.justice.digital.hmpps.personrecord.test.randomDate
+import uk.gov.justice.digital.hmpps.personrecord.test.randomDefendantId
 import uk.gov.justice.digital.hmpps.personrecord.test.randomFullAddress
+import uk.gov.justice.digital.hmpps.personrecord.test.randomName
 import uk.gov.justice.digital.hmpps.personrecord.test.randomPnc
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPostcode
 import uk.gov.justice.digital.hmpps.personrecord.test.responses.ApiResponseSetup
 import uk.gov.justice.digital.hmpps.personrecord.test.responses.ApiResponseSetupAddress
 import uk.gov.justice.digital.hmpps.personrecord.test.responses.ApiResponseSetupAlias
 import uk.gov.justice.digital.hmpps.personrecord.test.responses.ApiResponseSetupSentences
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-class JoinClustersE2ETest : E2ETestBase() {
+class JoinClustersE2ETest() : E2ETestBase() {
+
+  @Autowired
+  private lateinit var personMatchClient: PersonMatchClient
+
+  @Test
+  fun `should duplicate the scenario from prod`() {
+    val deliusRecordCrn = randomCrn()
+    val pnc = randomPnc()
+    val cro = randomCro()
+    val defendantId = randomDefendantId()
+    val firstName = randomName()
+    val lastName = randomName()
+    val postCode1 = randomPostcode()
+    val postCode2 = randomPostcode()
+    val postCode3 = randomPostcode()
+    val date1 = randomDate()
+    val date2 = randomDate()
+    val date3 = randomDate()
+    // create Delius record
+    createRandomProbationPersonDetails(deliusRecordCrn)
+    val deliusSetup = ApiResponseSetup(
+      crn = deliusRecordCrn,
+      cro = cro,
+      pnc = pnc,
+      firstName = firstName,
+      middleName = null,
+      lastName = lastName,
+      dateOfBirth = date1,
+      addresses = listOf(
+        ApiResponseSetupAddress(postcode = postCode1, fullAddress = null),
+//        ApiResponseSetupAddress(postcode = postCode2, fullAddress = null),
+//        ApiResponseSetupAddress(postcode = postCode3, fullAddress = null)
+      ),
+      sentences = listOf(
+        ApiResponseSetupSentences(date1),
+        ApiResponseSetupSentences(date2),
+        ApiResponseSetupSentences(date3)
+      )
+    )
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, deliusSetup)
+    val deliusPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCrn(deliusRecordCrn) })
+
+    // create Libra Record
+    val cId = randomCId()
+    createRandomLibraPersonDetails(cId = cId)
+    publishLibraMessage(libraHearing(cro = cro, firstName = firstName, cId = cId, lastName = lastName, postcode = postCode1, dateOfBirth = date1.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))))
+    val libraPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByCId(cId) })
+
+    awaitAssert {
+      val scores = personMatchClient.getPersonScores(libraPersonRecord.matchId.toString())
+      println("libra record scores are - $scores")
+    }
+
+
+    // create CP record
+    publishCommonPlatformMessage(
+      commonPlatformHearing(
+        listOf(
+          CommonPlatformHearingSetup(
+            firstName = firstName,
+            pnc = pnc,
+            cro = cro,
+            lastName = lastName,
+            defendantId = defendantId,
+            address = CommonPlatformHearingSetupAddress(buildingName = "", buildingNumber = "", thoroughfareName = "", dependentLocality = "", postTown = "", postcode = postCode1),
+            dateOfBirth = date1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+          )
+        )
+      ),
+    )
+    // assert 1 cluster with 3 records
+    val cpPersonRecord = awaitNotNullPerson(timeout = 7, function = { personRepository.findByDefendantId(defendantId) })
+//    cpPersonRecord.personKey!!.assertClusterIsOfSize(2)
+    awaitAssert {
+      val scores = personMatchClient.getPersonScores(cpPersonRecord.matchId.toString())
+      println("cp record scores are - $scores")
+    }
+  }
 
   @Test
   fun `should create 2 people who do not match followed by a third which matches both and all end up on same cluster`() {
