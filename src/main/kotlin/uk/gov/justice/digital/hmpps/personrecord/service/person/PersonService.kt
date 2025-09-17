18bd39a3e215046c823cbe26da7b4892366e519a
samz-justice <sam.zaroug@justice.gov.uk>
2025-09-17 14:01:40 +0100

    CPR-839 update to test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PersonService.kt
@@ -4,20 +4,15 @@ import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchRecord
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
-import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
-import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.model.types.OverrideMarkerType
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonCreated
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonUpdated
 import uk.gov.justice.digital.hmpps.personrecord.service.message.recluster.ReclusterService
 import uk.gov.justice.digital.hmpps.personrecord.service.person.factories.PersonFactory
-import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchResult
 import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
 
 @Component
 class PersonService(
-  private val personKeyRepository: PersonKeyRepository,
   private val personFactory: PersonFactory,
   private val personKeyService: PersonKeyService,
   private val personMatchService: PersonMatchService,
@@ -62,12 +57,4 @@ class PersonService(
       reclusterService.recluster(personEntity)
     }
   }
-}
-
-private fun List<PersonMatchResult>.containsExcluded(): List<PersonKeyEntity> {
-  val allKeys = this.map { it.personEntity.personKey }
-  val allEntities = allKeys.flatMap { it?.personEntities!! }
-  val allEntityIds = allEntities.map { it.id }
-  val allOverrides = allEntities.map { it.overrideMarkers }.flatten().filter { it.markerType == OverrideMarkerType.EXCLUDE }
-  return allOverrides.filter { it.markerValue in allEntityIds }.map { it.person?.personKey!! }
 }
