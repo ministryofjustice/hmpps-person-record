2c7736051c5b1e822588a62620dc660b7b238fd9
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-09-22 15:19:53 +0100

    CPR-839-remove-exclude-override remove old override marker table

--- src/main/resources/db/migration/V121__remove_old_override_markers.sql
+++ src/main/resources/db/migration/V121__remove_old_override_markers.sql
@@ -6,5 +6,7 @@ DROP COLUMN exclude_override_markers;
 
 ALTER TABLE IF EXISTS personrecordservice.event_log
 DROP COLUMN include_override_markers;
+
+DROP TABLE IF EXISTS personrecordservice.override_marker;
 -----------------------------------------------------
 COMMIT;
