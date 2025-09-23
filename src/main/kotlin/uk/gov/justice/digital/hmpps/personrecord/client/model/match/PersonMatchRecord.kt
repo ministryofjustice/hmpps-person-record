2a1907b44325166195d7e8198a3b2c5077df0c05
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-09-23 14:10:34 +0100

    CPR-905 send masterDefendantId to hmpps-person-match

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/match/PersonMatchRecord.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/match/PersonMatchRecord.kt
@@ -20,6 +20,7 @@ data class PersonMatchRecord(
   val pncs: List<String> = listOf(),
   val sentenceDates: List<String> = listOf(),
   val sourceSystemId: String? = "",
+  val masterDefendantId: String? = "",
   val overrideMarker: String? = "",
   val overrideScopes: List<String> = emptyList(),
 ) {
@@ -42,6 +43,7 @@ data class PersonMatchRecord(
       pncs = personEntity.references.getType(IdentifierType.PNC).mapNotNull { it.identifierValue }.distinct().sorted(),
       sentenceDates = personEntity.sentenceInfo.mapNotNull { it.sentenceDate }.map { it.toString() }.distinct().sorted(),
       sourceSystemId = personEntity.extractSourceSystemId(),
+      masterDefendantId = personEntity.masterDefendantId,
       overrideMarker = personEntity.overrideMarker?.toString() ?: "",
       overrideScopes = personEntity.overrideScopes.map { overrideScope -> overrideScope.scope.toString() },
     )
