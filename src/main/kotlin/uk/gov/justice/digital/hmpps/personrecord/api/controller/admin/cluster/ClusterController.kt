6ef8249af600a67b9188ad69cef088f3903f32da
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-09-25 14:13:05 +0100

    CPR-847 Split out and add extra admin cluster endpoints

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/cluster/ClusterController.kt
@@ -0,0 +1,63 @@
+package uk.gov.justice.digital.hmpps.personrecord.api.controller.admin.cluster
+
+import io.swagger.v3.oas.annotations.Hidden
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
+import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
+import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.AdminClusterDetail
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
+import java.util.*
+
+@RestController
+class ClusterController(
+  private val personKeyRepository: PersonKeyRepository,
+  private val personMatchService: PersonMatchService,
+  private val personRepository: PersonRepository,
+) {
+
+  @Hidden
+  @PreAuthorize("hasRole('${Roles.PERSON_RECORD_ADMIN_READ_ONLY}')")
+  @GetMapping("/admin/cluster/{uuid}")
+  suspend fun getClusterFromUUID(
+    @PathVariable(name = "uuid") uuid: UUID,
+  ): AdminClusterDetail {
+    val personKeyEntity = withContext(Dispatchers.IO) {
+      personKeyRepository.findByPersonUUID(uuid)
+    } ?: throw ResourceNotFoundException(uuid.toString())
+    val clusterVisualisationSpec = personMatchService.retrieveClusterVisualisationSpec(personKeyEntity).spec
+    return AdminClusterDetail.from(personKeyEntity, clusterVisualisationSpec)
+  }
+
+  @Hidden
+  @PreAuthorize("hasRole('${Roles.PERSON_RECORD_ADMIN_READ_ONLY}')")
+  @GetMapping("/admin/cluster/offender/{crn}")
+  suspend fun getClusterFromCRN(
+    @PathVariable(name = "crn") crn: String,
+  ): AdminClusterDetail {
+    val personKeyEntity = withContext(Dispatchers.IO) {
+      personRepository.findByCrn(crn)?.personKey
+    } ?: throw ResourceNotFoundException(crn)
+    val clusterVisualisationSpec = personMatchService.retrieveClusterVisualisationSpec(personKeyEntity).spec
+    return AdminClusterDetail.from(personKeyEntity, clusterVisualisationSpec)
+  }
+
+  @Hidden
+  @PreAuthorize("hasRole('${Roles.PERSON_RECORD_ADMIN_READ_ONLY}')")
+  @GetMapping("/admin/cluster/prisoner/{prisonNumber}")
+  suspend fun getClusterFromPrisonNumber(
+    @PathVariable(name = "prisonNumber") prisonNumber: String,
+  ): AdminClusterDetail {
+    val personKeyEntity = withContext(Dispatchers.IO) {
+      personRepository.findByPrisonNumber(prisonNumber)?.personKey
+    } ?: throw ResourceNotFoundException(prisonNumber)
+    val clusterVisualisationSpec = personMatchService.retrieveClusterVisualisationSpec(personKeyEntity).spec
+    return AdminClusterDetail.from(personKeyEntity, clusterVisualisationSpec)
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/cluster/ClusterApiIntTest.kt
@@ -0,0 +1,252 @@
+package uk.gov.justice.digital.hmpps.personrecord.api.controller.admin.cluster
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
+import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.AdminClusterDetail
+import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPrisonNumber
+import java.util.UUID
+
+class ClusterApiIntTest : WebTestBase() {
+
+  @Nested
+  inner class UUIDClusterDetail {
+
+    @Test
+    fun `should return cluster record with one record details`() {
+      val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+
+      stubVisualiseCluster()
+
+      val response = webTestClient.get()
+        .uri(uuidClusterUrl(person.personKey?.personUUID.toString()))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody(AdminClusterDetail::class.java)
+        .returnResult()
+        .responseBody!!
+
+      assertThat(response.uuid).isEqualTo(person.personKey?.personUUID.toString())
+      assertThat(response.status).isEqualTo("ACTIVE")
+      assertThat(response.records.size).isEqualTo(1)
+      assertThat(response.records[0].sourceSystem).isEqualTo("DELIUS")
+      assertThat(response.records[0].sourceSystemId).isEqualTo(person.crn)
+      assertThat(response.records[0].firstName).isEqualTo(person.getPrimaryName().firstName)
+      assertThat(response.records[0].middleName).isEqualTo(person.getPrimaryName().middleNames)
+      assertThat(response.records[0].lastName).isEqualTo(person.getPrimaryName().lastName)
+    }
+
+    @Test
+    fun `should return cluster record with multiple records details`() {
+      val personKey = createPersonKey()
+        .addPerson(createPerson(createRandomProbationPersonDetails()))
+        .addPerson(createPerson(createRandomLibraPersonDetails()))
+        .addPerson(createPerson(createRandomPrisonPersonDetails()))
+        .addPerson(createPerson(createRandomCommonPlatformPersonDetails()))
+
+      stubVisualiseCluster()
+
+      val response = webTestClient.get()
+        .uri(uuidClusterUrl(personKey.personUUID.toString()))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody(AdminClusterDetail::class.java)
+        .returnResult()
+        .responseBody!!
+
+      assertThat(response.uuid).isEqualTo(personKey.personUUID.toString())
+      assertThat(response.status).isEqualTo("ACTIVE")
+      assertThat(response.records.size).isEqualTo(4)
+    }
+
+    @Test
+    fun `should return not found if cluster does not exist`() {
+      webTestClient.get()
+        .uri(uuidClusterUrl(UUID.randomUUID().toString()))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isNotFound
+    }
+
+    @Test
+    fun `should return Access Denied 403 when role is wrong`() {
+      val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+      val expectedErrorMessage = "Forbidden: Access Denied"
+      webTestClient.get()
+        .uri(uuidClusterUrl(person.personKey?.personUUID.toString()))
+        .authorised(listOf("UNSUPPORTED-ROLE"))
+        .exchange()
+        .expectStatus()
+        .isForbidden
+        .expectBody()
+        .jsonPath("userMessage")
+        .isEqualTo(expectedErrorMessage)
+    }
+
+    @Test
+    fun `should return UNAUTHORIZED 401 when role is not set`() {
+      val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+      webTestClient.get()
+        .uri(uuidClusterUrl(person.personKey?.personUUID.toString()))
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    private fun uuidClusterUrl(uuid: String) = ADMIN_UUID_CLUSTER_URL + uuid
+  }
+
+  @Nested
+  inner class CRNClusterDetail {
+
+    @Test
+    fun `should return cluster record with one record details`() {
+      val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+
+      stubVisualiseCluster()
+
+      val response = webTestClient.get()
+        .uri(crnClusterUrl(person.crn!!))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody(AdminClusterDetail::class.java)
+        .returnResult()
+        .responseBody!!
+
+      assertThat(response.uuid).isEqualTo(person.personKey?.personUUID.toString())
+      assertThat(response.status).isEqualTo("ACTIVE")
+      assertThat(response.records.size).isEqualTo(1)
+      assertThat(response.records[0].sourceSystem).isEqualTo("DELIUS")
+      assertThat(response.records[0].sourceSystemId).isEqualTo(person.crn)
+      assertThat(response.records[0].firstName).isEqualTo(person.getPrimaryName().firstName)
+      assertThat(response.records[0].middleName).isEqualTo(person.getPrimaryName().middleNames)
+      assertThat(response.records[0].lastName).isEqualTo(person.getPrimaryName().lastName)
+    }
+
+    @Test
+    fun `should return not found if cluster does not exist`() {
+      webTestClient.get()
+        .uri(crnClusterUrl(randomCrn()))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isNotFound
+    }
+
+    @Test
+    fun `should return Access Denied 403 when role is wrong`() {
+      val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+      val expectedErrorMessage = "Forbidden: Access Denied"
+      webTestClient.get()
+        .uri(crnClusterUrl(person.crn!!))
+        .authorised(listOf("UNSUPPORTED-ROLE"))
+        .exchange()
+        .expectStatus()
+        .isForbidden
+        .expectBody()
+        .jsonPath("userMessage")
+        .isEqualTo(expectedErrorMessage)
+    }
+
+    @Test
+    fun `should return UNAUTHORIZED 401 when role is not set`() {
+      val person = createPersonWithNewKey(createRandomProbationPersonDetails())
+      webTestClient.get()
+        .uri(crnClusterUrl(person.crn!!))
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    private fun crnClusterUrl(crn: String) = ADMIN_CRN_CLUSTER_URL + crn
+  }
+
+  @Nested
+  inner class PrisonNumberClusterDetail {
+
+    @Test
+    fun `should return cluster record with one record details`() {
+      val person = createPersonWithNewKey(createRandomPrisonPersonDetails())
+
+      stubVisualiseCluster()
+
+      val response = webTestClient.get()
+        .uri(prisonNumberClusterUrl(person.prisonNumber!!))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody(AdminClusterDetail::class.java)
+        .returnResult()
+        .responseBody!!
+
+      assertThat(response.uuid).isEqualTo(person.personKey?.personUUID.toString())
+      assertThat(response.status).isEqualTo("ACTIVE")
+      assertThat(response.records.size).isEqualTo(1)
+      assertThat(response.records[0].sourceSystem).isEqualTo("NOMIS")
+      assertThat(response.records[0].sourceSystemId).isEqualTo(person.prisonNumber)
+      assertThat(response.records[0].firstName).isEqualTo(person.getPrimaryName().firstName)
+      assertThat(response.records[0].middleName).isEqualTo(person.getPrimaryName().middleNames)
+      assertThat(response.records[0].lastName).isEqualTo(person.getPrimaryName().lastName)
+    }
+
+    @Test
+    fun `should return not found if cluster does not exist`() {
+      webTestClient.get()
+        .uri(prisonNumberClusterUrl(randomPrisonNumber()))
+        .authorised(roles = listOf(Roles.PERSON_RECORD_ADMIN_READ_ONLY))
+        .exchange()
+        .expectStatus()
+        .isNotFound
+    }
+
+    @Test
+    fun `should return Access Denied 403 when role is wrong`() {
+      val person = createPersonWithNewKey(createRandomPrisonPersonDetails())
+      val expectedErrorMessage = "Forbidden: Access Denied"
+      webTestClient.get()
+        .uri(prisonNumberClusterUrl(person.prisonNumber!!))
+        .authorised(listOf("UNSUPPORTED-ROLE"))
+        .exchange()
+        .expectStatus()
+        .isForbidden
+        .expectBody()
+        .jsonPath("userMessage")
+        .isEqualTo(expectedErrorMessage)
+    }
+
+    @Test
+    fun `should return UNAUTHORIZED 401 when role is not set`() {
+      val person = createPersonWithNewKey(createRandomPrisonPersonDetails())
+      webTestClient.get()
+        .uri(prisonNumberClusterUrl(person.prisonNumber!!))
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    private fun prisonNumberClusterUrl(prisonNumber: String) = ADMIN_PRISON_NUMBER_CLUSTER_URL + prisonNumber
+  }
+
+  private fun stubVisualiseCluster() = stubPostRequest(
+    url = "/visualise-cluster",
+    status = 200,
+    responseBody = """ { "spec": {} } """.trimIndent(),
+  )
+
+  companion object {
+    private const val ADMIN_UUID_CLUSTER_URL = "/admin/cluster/"
+    private const val ADMIN_CRN_CLUSTER_URL = "/admin/cluster/offender/"
+    private const val ADMIN_PRISON_NUMBER_CLUSTER_URL = "/admin/cluster/prisoner/"
+  }
+}
