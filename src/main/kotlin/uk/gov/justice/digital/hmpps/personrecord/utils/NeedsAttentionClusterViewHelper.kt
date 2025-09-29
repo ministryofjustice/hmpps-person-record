9f6cca811a3b3f6b2edff5fe2d24afabe22fdaed
samz-justice <sam.zaroug@justice.gov.uk>
2025-09-29 14:30:26 +0100

    CPR-912 upate to util class to object

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/utils/NeedsAttentionClusterViewHelper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/utils/NeedsAttentionClusterViewHelper.kt
@@ -3,11 +3,9 @@ package uk.gov.justice.digital.hmpps.personrecord.utils
 import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.AdminCluster
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
 
-class NeedsAttentionClusterViewHelper {
-  companion object {
-    fun process(clusters: List<AdminCluster>): List<AdminCluster> {
-      val partition = clusters.partition { it.recordComposition.any { it.sourceSystem != DELIUS && it.count > 0 } }
-      return partition.second + partition.first
-    }
+object NeedsAttentionClusterViewHelper {
+  fun process(clusters: List<AdminCluster>): List<AdminCluster> {
+    val partition = clusters.partition { it.recordComposition.any { it.sourceSystem != DELIUS && it.count > 0 } }
+    return partition.second + partition.first
   }
 }
