32881fe11b80b2b9717f0704f5ce7bf4c38af7c1
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-09-30 14:51:23 +0100

    Revert "CPR-912 do the pagination after we get all the records from the db (#…"
    This reverts commit 73d73b773e7abcb44323e3c593604dfe43ee04a9.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/cluster/ClustersController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/cluster/ClustersController.kt
@@ -2,6 +2,9 @@ package uk.gov.justice.digital.hmpps.personrecord.api.controller.admin.cluster
 
 import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.media.Schema
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.data.domain.Pageable
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
 import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.AdminCluster
 import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.cluster.SourceSystemComposition
-import uk.gov.justice.digital.hmpps.personrecord.extensions.sort
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
@@ -27,12 +29,14 @@ class ClustersController(
   @Hidden
   @PreAuthorize("hasRole('${Roles.PERSON_RECORD_ADMIN_READ_ONLY}')")
   @GetMapping("/admin/clusters")
-  fun getClusters(
+  suspend fun getClusters(
     @RequestParam(defaultValue = "1") page: Int,
-  ): PaginatedResponse {
-    val paginatedClusters = personKeyRepository.findAllByStatusOrderById(UUIDStatusType.NEEDS_ATTENTION)
-
-    val clusters = paginatedClusters.map {
+  ): PaginatedResponse<AdminCluster> {
+    val evaluatedPageable: Pageable = Pageable.ofSize(DEFAULT_PAGE_SIZE).withPage(page - 1)
+    val paginatedClusters = withContext(Dispatchers.IO) {
+      personKeyRepository.findAllByStatusOrderById(UUIDStatusType.NEEDS_ATTENTION, evaluatedPageable)
+    }
+    val clusters = paginatedClusters.content.map {
       AdminCluster(
         uuid = it.personUUID.toString(),
         recordComposition = listOf(
@@ -43,35 +47,15 @@ class ClustersController(
         ),
       )
     }
-
-    return paginate(clusters.sort(), page)
-  }
-
-  private fun paginate(
-    clusters: List<AdminCluster>,
-    page: Int,
-  ): PaginatedResponse {
-    val totalClusters = clusters.size
-    val first = (page - 1) * DEFAULT_PAGE_SIZE
-    val isLastPage = (totalClusters - first) <= DEFAULT_PAGE_SIZE
-
-    val totalPages = when {
-      totalClusters > 0 -> (totalClusters / DEFAULT_PAGE_SIZE) + 1
-      else -> 0
-    }
-
-    val last = when {
-      isLastPage -> (totalClusters % DEFAULT_PAGE_SIZE) + first
-      else -> first + DEFAULT_PAGE_SIZE
-    }
-
-    val content = clusters.subList(first, last)
-
     return PaginatedResponse(
-      content = content,
+      content = clusters,
       pagination = Pagination(
-        isLastPage = isLastPage,
-        totalPages = totalPages,
+        isLastPage = paginatedClusters.isLast,
+        count = paginatedClusters.numberOfElements,
+        page = paginatedClusters.number + 1,
+        perPage = paginatedClusters.size,
+        totalCount = paginatedClusters.totalElements,
+        totalPages = paginatedClusters.totalPages,
       ),
     )
   }
@@ -83,14 +67,22 @@ class ClustersController(
   }
 }
 
-class PaginatedResponse(
-  val content: List<AdminCluster>,
+class PaginatedResponse<T>(
+  val content: List<T>,
   val pagination: Pagination,
 )
 
 data class Pagination(
   @Schema(description = "Is the current page the last one?", example = "true")
   val isLastPage: Boolean,
+  @Schema(description = "The number of results in `data` for the current page", example = "1")
+  val count: Int,
+  @Schema(description = "The current page number", example = "1")
+  val page: Int,
+  @Schema(description = "The maximum number of results in `data` for a page", example = "10")
+  val perPage: Int,
+  @Schema(description = "The total number of results in `data` across all pages", example = "1")
+  val totalCount: Long,
   @Schema(description = "The total number of pages", example = "1")
   val totalPages: Int,
 )
