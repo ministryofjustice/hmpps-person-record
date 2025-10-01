8d1f2506c76a08aa95e3ddffb40eaf526e10a9a3
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-10-01 11:08:04 +0100

    CPR-914 delius update master defendant id make seperate component for update/create for

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationEventProcessor.kt
@@ -3,22 +3,15 @@ package uk.gov.justice.digital.hmpps.personrecord.message.processors.probation
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Isolation.REPEATABLE_READ
 import org.springframework.transaction.annotation.Transactional
-import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.service.message.CreateUpdateService
 
 @Component
 class ProbationEventProcessor(
-  private val createUpdateService: CreateUpdateService,
-  private val personRepository: PersonRepository,
+  private val probationProcessor: ProbationProcessor,
 ) {
 
   @Transactional(isolation = REPEATABLE_READ)
   fun processEvent(person: Person) {
-    val offender = personRepository.findByCrn(person.crn!!)
-    person.masterDefendantId = offender?.masterDefendantId
-    createUpdateService.processPerson(person) {
-      personRepository.findByCrn(person.crn)
-    }
+    probationProcessor.processProbationEvent(person)
   }
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/probation/ProbationProcessor.kt
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.personrecord.message.processors.probation;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository;
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Person;
+import uk.gov.justice.digital.hmpps.personrecord.service.message.CreateUpdateService;
+
+
+@Component
+class ProbationProcessor(
+  private val personRepository:PersonRepository,
+  private val createUpdateService:CreateUpdateService,
+)
+{
+
+  fun processProbationEvent(person: Person) {
+    val offender = personRepository.findByCrn(person.crn!!)
+    person.masterDefendantId = offender?.masterDefendantId
+    createUpdateService.processPerson(person) {
+      personRepository.findByCrn(person.crn)
+    }
+  }
+
+}
