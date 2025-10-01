338aa24fe38316ea23cbd7e5951d3045445e2cb3
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-10-01 16:46:51 +0100

    CPR-914 retryable only works if the method is called from another class because of AOP proxying

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/prison/PrisonMergeEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/prison/PrisonMergeEventListener.kt
@@ -3,17 +3,17 @@ package uk.gov.justice.digital.hmpps.personrecord.message.listeners.prison
 import io.awspring.cloud.sqs.annotation.SqsListener
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.message.processors.prison.PrisonMergeEventProcessor
+import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventProcessor
 import uk.gov.justice.digital.hmpps.personrecord.service.queue.Queues
-import uk.gov.justice.digital.hmpps.personrecord.service.queue.SQSListenerService
 
 @Component
 class PrisonMergeEventListener(
-  private val sqsListenerService: SQSListenerService,
+  private val domainEventProcessor: DomainEventProcessor,
   private val mergeEventProcessor: PrisonMergeEventProcessor,
 ) {
 
   @SqsListener(Queues.PRISON_MERGE_EVENT_QUEUE_ID, factory = "hmppsQueueContainerFactoryProxy")
-  fun onDomainEvent(rawMessage: String) = sqsListenerService.processDomainEvent(rawMessage) {
+  fun onDomainEvent(rawMessage: String) = domainEventProcessor.processDomainEvent(rawMessage) {
     mergeEventProcessor.processEvent(it)
   }
 }
