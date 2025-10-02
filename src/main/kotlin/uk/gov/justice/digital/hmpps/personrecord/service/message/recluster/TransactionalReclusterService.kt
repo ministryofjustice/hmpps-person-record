e0008a2cc920415c6636b54359d1ca64665ac33d
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-10-02 16:02:03 +0100

    CPR-877 retry is in but est failing for some reason

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/recluster/TransactionalReclusterService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/message/recluster/TransactionalReclusterService.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.message.recluster
 
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.reactive.function.client.WebClientException
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 
 @Component
@@ -9,6 +12,11 @@ class TransactionalReclusterService(
   private val reclusterService: ReclusterService,
 ) {
 
+  @Retryable( maxAttempts = 2,
+    backoff = Backoff(delay = 200, random = true, multiplier = 3.0),
+    retryFor = [
+      WebClientException::class,
+    ])
   @Transactional
   fun triggerRecluster(person: PersonEntity) = reclusterService.recluster(person)
 }
