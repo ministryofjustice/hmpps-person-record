186a8f12178fdb2745c1c0ef092c3476c797946d
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-10-09 10:41:32 +0100

    CPR-736 Drop correct column

--- src/main/resources/db/migration/V126__remove_old_country_field.sql
+++ src/main/resources/db/migration/V126__remove_old_country_field.sql
@@ -5,7 +5,7 @@ ALTER TABLE IF EXISTS person
     DROP COLUMN birth_country;
 
 ALTER TABLE IF EXISTS address
-    DROP COLUMN country_code;
+    DROP COLUMN country;
 
 -----------------------------------------------------
 COMMIT;
