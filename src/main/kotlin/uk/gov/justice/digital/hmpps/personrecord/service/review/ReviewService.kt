02c0d163714a00c4a9c62bfd399219f9a275b185
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-10-17 15:34:51 +0100

    CPR-902 Create new review service

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/review/ReviewService.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.review
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.review.ReviewEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.ReviewRepository
+import uk.gov.justice.digital.hmpps.personrecord.model.types.review.ClusterType
+
+@Component
+class ReviewService(
+  private val reviewRepository: ReviewRepository,
+) {
+
+  fun raiseForReview(primary: PersonKeyEntity, additional: List<PersonKeyEntity>? = null): ReviewEntity {
+    val reviewEntity = primary.getIfReviewExists()
+    return when {
+      reviewEntity == null -> primary.createNewReview(additional)
+      else -> reviewEntity
+    }
+  }
+
+  private fun PersonKeyEntity.createNewReview(additional: List<PersonKeyEntity>? = null): ReviewEntity {
+    val review = ReviewEntity.new()
+    review.addPrimary(this)
+    additional?.let { review.addAdditional(it) }
+    return reviewRepository.save(review)
+  }
+
+  private fun PersonKeyEntity.getIfReviewExists() = reviewRepository.findByClustersClusterTypeAndClustersPersonKey(ClusterType.PRIMARY, this)
+}
