109df3e3f032a7b9457e99c23fd3803f0f0663f6
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-10-20 09:32:55 +0100

    CPR-902 Create new review service (#1511)
    * CPR-902 Create new review service
    
    * CPR-902 Add index to cluster type
    
    * CPR-902 Correct migration version number

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/ReviewRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/repository/ReviewRepository.kt
@@ -2,7 +2,11 @@ package uk.gov.justice.digital.hmpps.personrecord.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.review.ReviewEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.review.ClusterType
 
 @Repository
-interface ReviewRepository : JpaRepository<ReviewEntity, Long>
+interface ReviewRepository : JpaRepository<ReviewEntity, Long> {
+  fun findByClustersClusterTypeAndClustersPersonKey(clusterType: ClusterType, personKeyEntity: PersonKeyEntity): ReviewEntity?
+}
