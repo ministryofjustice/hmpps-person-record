0a71c8f81d1b4caf3484860963b45e6af74495b5
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-10-23 13:00:44 +0100

    CPR-896 Add prison prefix

--- src/main/resources/db/migration/V137__add_contact_extension.sql
+++ src/main/resources/db/migration/V137__add_contact_extension.sql
@@ -1,14 +1,14 @@
 BEGIN;
 -------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS additional_contact_information
+CREATE TABLE IF NOT EXISTS prison_additional_contact_information
 (
     id                                      SERIAL      PRIMARY KEY,
     extension                               TEXT        NULL,
     fk_contact_id                           BIGINT      NULL,
     CONSTRAINT fk_contact_id FOREIGN KEY (fk_contact_id) references contact (id)
 );
-CREATE INDEX idx_additional_contact_information_fk_contact_id ON additional_contact_information(fk_contact_id);
+CREATE INDEX idx_prison_additional_contact_information_fk_contact_id ON prison_additional_contact_information(fk_contact_id);
 
 -----------------------------------------------------
 COMMIT;
