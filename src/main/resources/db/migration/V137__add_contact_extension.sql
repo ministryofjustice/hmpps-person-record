c0f0f5155b2522d21268bc4cd3e66e321393eb51
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-10-23 11:11:11 +0100

    CPR-896 Add index and foreign key

--- src/main/resources/db/migration/V137__add_contact_extension.sql
+++ src/main/resources/db/migration/V137__add_contact_extension.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS additional_contact_information
 (
     id                                      SERIAL      PRIMARY KEY,
     extension                               TEXT        NULL,
-    fk_contact_id                           BIGINT      NULL
+    fk_contact_id                           BIGINT      NULL,
+    CONSTRAINT fk_contact_id FOREIGN KEY (fk_contact_id) references contact (id)
 );
+CREATE INDEX idx_additional_contact_information_fk_contact_id ON additional_contact_information(fk_contact_id);
 
 -----------------------------------------------------
 COMMIT;
