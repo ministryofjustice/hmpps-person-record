30440e539dc6d0ff76dec5812004d33e7488a39c
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-11-06 10:39:28 +0000

    CPR-79 refactor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/builder/address/CommonPlatformAddressBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/builder/address/CommonPlatformAddressBuilder.kt
@@ -9,24 +9,20 @@ object CommonPlatformAddressBuilder {
 
   fun build(person: Person, personEntity: PersonEntity?): List<Address> {
     val primaryAddress = person.addresses.firstOrNull()
-    return rebuildAddresses(primaryAddress, personEntity?.addresses)
-  }
-
-  private fun rebuildAddresses(address: Address?, existingAddresses: MutableList<AddressEntity>?): List<Address> {
-    address?.setToPrimary()
-    val newAddresses = mutableListOf(address)
-    val otherAddresses: List<Address>? = extract(existingAddresses, address)
+    primaryAddress?.setToPrimary()
+    val newAddresses = mutableListOf(primaryAddress)
+    val otherAddresses: List<Address>? = extract(personEntity?.addresses, primaryAddress)
     otherAddresses?.forEach { it.setToPrevious() }
     otherAddresses?.let { newAddresses.addAll(it) }
     return newAddresses.mapNotNull { it }
   }
 
-  fun extract(addresses: List<AddressEntity>?, newAddress: Address?): List<Address>? {
-    if (newAddress == null) return addresses?.map { Address.from(it) }
-    return addresses?.filter { address ->
-      newAddress.compareAddressTo(Address.from(address)) == false
-    }?.map { Address.from(it) }
-  }
+  fun extract(addresses: List<AddressEntity>?, newAddress: Address?): List<Address>? = when {
+    newAddress == null -> addresses
+    else -> addresses?.filter { address ->
+      newAddress.compareAddressTo(Address.from(address))
+    }
+  }?.map { Address.from(it) }
 
-  private fun Address?.compareAddressTo(anotherAddress: Address): Boolean = this?.copy(recordType = null) == anotherAddress.copy(recordType = null)
+  private fun Address.compareAddressTo(anotherAddress: Address): Boolean = this.copy(recordType = null) == anotherAddress.copy(recordType = null)  == false
 }
