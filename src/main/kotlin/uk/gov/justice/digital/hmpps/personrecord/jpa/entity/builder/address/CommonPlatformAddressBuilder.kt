09d3228276c11ea60ac38f330a74f212d10109b0
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-11-06 12:10:28 +0000

    CPR-79 rename

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/builder/address/CommonPlatformAddressBuilder.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/builder/address/CommonPlatformAddressBuilder.kt
@@ -8,9 +8,11 @@ import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 object CommonPlatformAddressBuilder {
 
   fun build(person: Person, personEntity: PersonEntity?): List<Address> {
+    // if null, return previous addresses
+    // if new, set to primary and return previous addresses
     val primaryAddress = person.addresses.firstOrNull()?.setToPrimary()
-    val otherAddresses = removePrimaryAddress(personEntity?.addresses, primaryAddress)
-    return listOf(*otherAddresses, primaryAddress).mapNotNull { it }
+    val previousAddresses = removePrimaryAddress(personEntity?.addresses, primaryAddress)
+    return listOf(*previousAddresses, primaryAddress).mapNotNull { it }
   }
 
   fun removePrimaryAddress(addresses: List<AddressEntity>?, newAddress: Address?): Array<Address> = when {
