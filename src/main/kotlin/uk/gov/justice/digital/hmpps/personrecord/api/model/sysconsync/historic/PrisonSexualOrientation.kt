6716762da0538507bd2d6f7c1a120f62e3b60450
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-11-06 10:47:38 +0000

    CPR-956 Add swagger docs to api input

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/historic/PrisonSexualOrientation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/historic/PrisonSexualOrientation.kt
@@ -1,10 +1,15 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class PrisonSexualOrientation(
+  @Schema(description = "The sexual orientation code", example = "BIS")
   val sexualOrientationCode: String,
+  @Schema(description = "The sexual orientation start date", example = "1980-01-01")
   val startDate: LocalDate? = null,
+  @Schema(description = "The sexual orientation end date", example = "2000-01-01")
   val endDate: LocalDate? = null,
+  @Schema(description = "Flag indicating the current sexual orientation status", example = "true")
   val current: Boolean,
 )
