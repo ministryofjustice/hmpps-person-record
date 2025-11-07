c4083ad2cc117849ed9200e9316ec7ec25adaaad
H-Iwanejko <163879459+H-Iwanejko@users.noreply.github.com>
2025-11-07 14:01:22 +0000

    CPR-956 add historic sexual orientation endpoint (#1556)
    * CPR-956 Add sexual orientation controller
    
    * CPR-956 Add swagger docs to api input
    
    * CPR-956 Add Test
    
    * CPR-956 Add update test
    
    * CPR-956 Add role based test to endpoint
    
    * CPR-956 Format
    
    * CPR-956 Add creation endpoint to syscon controller
    
    * CPR-956 Add creation endpoint
    
    * CPR-956 Remove update temp test
    
    * CPR-956 Fix blank whitespace
    
    * CPR-956 Switch to post
    
    * CPR-956 Update enum name
    
    * CPR-956 Remove redundnat assertions
    
    * CPR-956 Format
    
    * CPR-956 Add user id field

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/prison/PrisonSexualOrientationEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/prison/PrisonSexualOrientationEntity.kt
@@ -8,9 +8,12 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import uk.gov.justice.digital.hmpps.personrecord.model.types.RecordType
+import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonSexualOrientation
+import uk.gov.justice.digital.hmpps.personrecord.model.types.PrisonRecordType
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SexualOrientation
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
 
 @Entity
 @Table(name = "prison_sexual_orientation")
@@ -19,6 +22,9 @@ class PrisonSexualOrientationEntity(
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   val id: Long? = null,
+
+  @Column(name = "cpr_sexual_orientation_id", nullable = false)
+  val cprSexualOrientationId: UUID,
 
   @Column(name = "prison_number")
   val prisonNumber: String? = null,
@@ -33,7 +39,47 @@ class PrisonSexualOrientationEntity(
   @Column(name = "end_date")
   val endDate: LocalDate? = null,
 
+  @Column(name = "create_user_id")
+  val createUserId: String? = null,
+
+  @Column(name = "create_date_time")
+  val createDateTime: LocalDateTime? = null,
+
+  @Column(name = "create_display_name")
+  val createDisplayName: String? = null,
+
+  @Column(name = "modify_date_time")
+  val modifyDateTime: LocalDateTime? = null,
+
+  @Column(name = "modify_user_id")
+  val modifyUserId: String? = null,
+
+  @Column(name = "modify_display_name")
+  val modifyDisplayName: String? = null,
+
   @Column(name = "record_type")
   @Enumerated(STRING)
-  val recordType: RecordType,
-)
+  val prisonRecordType: PrisonRecordType? = null,
+
+) {
+  companion object {
+
+    fun from(sexualOrientation: PrisonSexualOrientation): PrisonSexualOrientationEntity = PrisonSexualOrientationEntity(
+      cprSexualOrientationId = UUID.randomUUID(),
+      prisonNumber = sexualOrientation.prisonNumber,
+      sexualOrientationCode = SexualOrientation.from(sexualOrientation),
+      startDate = sexualOrientation.startDate,
+      endDate = sexualOrientation.endDate,
+      createUserId = sexualOrientation.createUserId,
+      createDateTime = sexualOrientation.createDateTime,
+      createDisplayName = sexualOrientation.createDisplayName,
+      modifyDateTime = sexualOrientation.modifyDateTime,
+      modifyUserId = sexualOrientation.modifyUserId,
+      modifyDisplayName = sexualOrientation.modifyDisplayName,
+      prisonRecordType = when (sexualOrientation.current) {
+        true -> PrisonRecordType.CURRENT
+        false -> PrisonRecordType.HISTORIC
+      },
+    )
+  }
+}
