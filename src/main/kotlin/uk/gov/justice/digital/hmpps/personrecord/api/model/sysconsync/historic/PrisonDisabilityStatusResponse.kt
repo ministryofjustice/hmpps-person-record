86edbd2707bd4cfbe8933d1d26868dd90ba603b0
samz-justice <sam.zaroug@justice.gov.uk>
2025-11-10 14:14:02 +0000

    CPR-959 removed wildcard import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/historic/PrisonDisabilityStatusResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/historic/PrisonDisabilityStatusResponse.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic
 
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.prison.PrisonDisabilityStatusEntity
-import java.util.*
+import java.util.UUID
 
 data class PrisonDisabilityStatusResponse(
   val cprDisabilityStatusId: UUID,
