f22dfe57303277cd05c708ffd120a51fe10fabea
samz-justice <sam.zaroug@justice.gov.uk>
2025-11-11 10:49:22 +0000

    CPR-959 added update api for disability status (#1566)
    * CPR-959 added update api for disability status
    
    * CPR-959 slight refactor to common disabilty status creatin
    
    * CPR-959 refactor test
    
    * CPR-959 refactoring test
    
    ---------
    
    Co-authored-by: Mark Rees <mark.rees@digital.justice.gov.uk>
    Co-authored-by: markreesmoj <76954782+markreesmoj@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/syscon/historic/SysconDisabilityStatusController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/syscon/historic/SysconDisabilityStatusController.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.controller.syscon.historic
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -8,14 +9,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
+import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
 import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonDisabilityStatus
 import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonDisabilityStatusResponse
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.prison.PrisonDisabilityStatusEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.prison.PrisonDisabilityStatusRepository
+import java.util.UUID
 
 @Tag(name = "Syscon Sync")
 @RestController
@@ -38,5 +43,30 @@ class SysconDisabilityStatusController(
   ): ResponseEntity<PrisonDisabilityStatusResponse> {
     val prisonDisabilityStatusEntity = prisonDisabilityStatusRepository.save(PrisonDisabilityStatusEntity.from(disabilityStatus))
     return ResponseEntity(PrisonDisabilityStatusResponse.from(prisonDisabilityStatusEntity), HttpStatus.CREATED)
+  }
+
+  @Operation(description = "Update a prisoner disability status")
+  @PutMapping("/syscon-sync/disability-status/{cprDisabilityStatusId}")
+  @ApiResponses(
+    ApiResponse(
+      responseCode = "200",
+      description = "Disability Status updated in CPR",
+    ),
+  )
+  @Transactional
+  fun updateDisabilityStatus(
+    @PathVariable(name = "cprDisabilityStatusId")
+    @Parameter(description = "The identifier of the prison disability status", required = true)
+    cprDisabilityStatusId: UUID,
+    @RequestBody disabilityStatus: PrisonDisabilityStatus,
+  ): String {
+    prisonDisabilityStatusRepository.findByCprDisabilityStatusId(cprDisabilityStatusId)
+      ?.update(disabilityStatus)
+      ?: throw ResourceNotFoundException(cprDisabilityStatusId.toString())
+    return OK
+  }
+
+  companion object {
+    private const val OK = "OK"
   }
 }
