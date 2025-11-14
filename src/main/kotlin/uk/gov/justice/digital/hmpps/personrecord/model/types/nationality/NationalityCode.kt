4bce16204a553b66d787225dd8fa604cbf74fcb4
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-11-14 11:45:38 +0000

    CPR-931 further polishing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/types/nationality/NationalityCode.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/types/nationality/NationalityCode.kt
@@ -256,12 +256,12 @@ enum class NationalityCode(val description: String) {
 
     fun fromCommonPlatformMapping(code: String?): NationalityCode? = getNationalityOrUnknown(COMMON_PLATFORM_NATIONALITY_MAPPING, code, "common platform")
 
-    private fun getNationalityOrUnknown(nationalityMap: Map<String, NationalityCode>, code: String?, sourceSystem: String): NationalityCode? = code.normalize()?.let {
-      nationalityMap[it] ?: UNKNOWN.also {
-        if (!code.normalize().equals("UNKNOWN")) log.info("Unknown nationality code $sourceSystem: $code")
+    private fun getNationalityOrUnknown(nationalityMap: Map<String, NationalityCode>, code: String?, sourceSystem: String): NationalityCode? = code.normalise()?.let { normalisedCode ->
+      nationalityMap[normalisedCode] ?: UNKNOWN.also {
+        if (normalisedCode != "UNKNOWN") log.info("Unknown nationality code $sourceSystem: $code")
       }
     }
 
-    private fun String?.normalize(): String? = this?.trim().nullIfBlank()?.uppercase()
+    private fun String?.normalise(): String? = this?.trim().nullIfBlank()?.uppercase()
   }
 }
