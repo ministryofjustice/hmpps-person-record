f71c19ed54da93ac60873e983077cda3a5ade8d6
Henry Iwanejko <henry.iwanejko@justice.gov.uk>
2025-11-17 14:46:59 +0000

    CPR-960 log execution results for calculate status reason

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/CalculateStatusReason.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/CalculateStatusReason.kt
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personrecord.jobs.MigrateNationalities.Companion
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
@@ -26,13 +27,18 @@ class CalculateStatusReason(
   @PostMapping("/admin/calculate-status-reason")
   suspend fun postRecluster(): String {
     CoroutineScope(Dispatchers.Default).launch {
-      forPage { clusters ->
+      val executionResults = forPage { clusters ->
         clusters.forEach {
           it.setAsActive()
           val activeCluster = personKeyRepository.save(it)
           transactionalReclusterService.recluster(activeCluster.personEntities.first())
         }
       }
+      log.info(
+        JOB_NAME +
+          "total elements: ${executionResults.totalElements}, " +
+          "elapsed time: ${executionResults.elapsedTime}",
+      )
     }
     return OK
   }
@@ -64,7 +70,7 @@ class CalculateStatusReason(
 
   companion object {
     private const val OK = "OK"
-    private const val BATCH_SIZE = 500
+    private const val BATCH_SIZE = 100
     private val log = LoggerFactory.getLogger(this::class.java)
     private const val JOB_NAME = "JOB: recalculate-status-reason: "
   }
