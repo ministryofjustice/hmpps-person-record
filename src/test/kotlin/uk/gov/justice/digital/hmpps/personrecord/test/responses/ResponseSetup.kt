efd2c0eb8ab50f8be3467b12b90f774d77c20bf3
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2025-11-21 14:57:17 +0000

    CPR-975 map address delius

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/responses/ResponseSetup.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/test/responses/ResponseSetup.kt
@@ -55,7 +55,7 @@ data class ApiResponseSetup(
   val religion: String? = null,
   val prisonNumber: String? = null,
   val ethnicity: String? = null,
-  val addresses: List<ApiResponseSetupAddress> = listOf(ApiResponseSetupAddress(postcode = randomPostcode(), fullAddress = randomFullAddress(), buildingName = randomName(), streetName = randomName(), district = randomName(), townCity = randomName(), county = randomName(), uprn = randomUprn())),
+  val addresses: List<ApiResponseSetupAddress> = listOf(ApiResponseSetupAddress(postcode = randomPostcode(), fullAddress = randomFullAddress())),
   val nationalInsuranceNumber: String? = null,
   val email: String? = randomEmail(),
   val dateOfBirth: LocalDate? = randomDate(),
