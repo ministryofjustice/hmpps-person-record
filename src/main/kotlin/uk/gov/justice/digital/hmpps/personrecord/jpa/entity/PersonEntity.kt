79c75704d6a9497704c7408cb4457d65a1cbf612
Luke Bevan <6418390+lbevan@users.noreply.github.com>
2025-11-25 15:24:21 +0000

    CPR-930 Drop ethnicity and title code columns (#1631)
    * CPR-930 Drop ethnicity and title code columns
    
    * CPR-930 Remove drop script
    
    Ensure columns are unmapped first.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/PersonEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/PersonEntity.kt
@@ -23,7 +23,6 @@ import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.builder.ReferenceBui
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.builder.SentenceInfoBuilder.buildSentenceInfo
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.reference.EthnicityCodeEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.model.types.EthnicityCode
 import uk.gov.justice.digital.hmpps.personrecord.model.types.NameType.ALIAS
 import uk.gov.justice.digital.hmpps.personrecord.model.types.NameType.PRIMARY
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SexualOrientation
@@ -114,9 +113,6 @@ class PersonEntity(
     referencedColumnName = "id",
   )
   var ethnicityCodeLegacy: EthnicityCodeEntity? = null,
-
-//  @Column(name = "ethnicity_code")   -- TODO -reintroduce this after we fix the titleCode mapping
-  var ethnicityCode: EthnicityCode? = null,
 
   @Column(name = "date_of_death")
   var dateOfDeath: LocalDate? = null,
