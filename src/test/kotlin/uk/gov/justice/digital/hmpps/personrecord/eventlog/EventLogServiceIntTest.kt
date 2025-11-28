50a25ff3150c3ea58b4b8acbcadeff3309ecc3b4
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-11-28 17:03:10 +0000

    CPR-930 remove another largely duplicated test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/eventlog/EventLogServiceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/eventlog/EventLogServiceIntTest.kt
@@ -11,78 +11,19 @@ import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.Probation
 import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.Sentences
 import uk.gov.justice.digital.hmpps.personrecord.config.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
-import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.eventlog.RecordEventLog
 import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
 import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.EventLogService
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCro
-import uk.gov.justice.digital.hmpps.personrecord.test.randomDate
 import uk.gov.justice.digital.hmpps.personrecord.test.randomLongPnc
 import uk.gov.justice.digital.hmpps.personrecord.test.randomName
-import uk.gov.justice.digital.hmpps.personrecord.test.randomPostcode
 import java.time.LocalDate
 
 class EventLogServiceIntTest : IntegrationTestBase() {
 
   @Autowired
   lateinit var eventLogService: EventLogService
-
-  @Test
-  fun `should map person to event log`() {
-    val personEntity = createPersonWithNewKey(
-      Person.from(
-        ProbationCase(
-          name = ProbationCaseName(firstName = randomName(), middleNames = randomName(), lastName = randomName()),
-          dateOfBirth = randomDate(),
-          identifiers = Identifiers(crn = randomCrn(), cro = randomCro(), pnc = randomLongPnc()),
-          addresses = listOf(ProbationAddress(postcode = randomPostcode())),
-          aliases = listOf(
-            ProbationCaseAlias(
-              name = ProbationCaseName(firstName = randomName(), lastName = randomName()),
-              dateOfBirth = randomDate(),
-            ),
-          ),
-          sentences = listOf(Sentences(sentenceDate = randomDate())),
-        ),
-      ),
-    )
-
-    val eventLog = eventLogService.logEvent(RecordEventLog(CPRLogEvents.CPR_RECORD_UPDATED, personEntity))
-
-    assertThat(eventLog).isNotNull()
-    assertThat(eventLog.sourceSystemId).isEqualTo(personEntity.crn)
-    assertThat(eventLog.sourceSystem).isEqualTo(SourceSystemType.DELIUS)
-    assertThat(eventLog.matchId).isEqualTo(personEntity.matchId)
-    assertThat(eventLog.personUUID).isEqualTo(personEntity.personKey?.personUUID)
-    assertThat(eventLog.uuidStatusType).isEqualTo(UUIDStatusType.ACTIVE)
-    assertThat(eventLog.firstName).isEqualTo(personEntity.getPrimaryName().firstName)
-    assertThat(eventLog.middleNames).isEqualTo(personEntity.getPrimaryName().middleNames)
-    assertThat(eventLog.lastName).isEqualTo(personEntity.getPrimaryName().lastName)
-    assertThat(eventLog.dateOfBirth).isEqualTo(personEntity.getPrimaryName().dateOfBirth)
-    assertThat(eventLog.firstNameAliases.size).isEqualTo(1)
-    assertThat(eventLog.firstNameAliases.first()).isEqualTo(personEntity.getAliases().first().firstName)
-    assertThat(eventLog.lastNameAliases.size).isEqualTo(1)
-    assertThat(eventLog.lastNameAliases.first()).isEqualTo(personEntity.getAliases().first().lastName)
-    assertThat(eventLog.lastNameAliases.size).isEqualTo(1)
-    assertThat(eventLog.dateOfBirthAliases.first()).isEqualTo(personEntity.getAliases().first().dateOfBirth)
-    assertThat(eventLog.postcodes.size).isEqualTo(1)
-    assertThat(eventLog.postcodes.first()).isEqualTo(personEntity.addresses.first().postcode)
-    assertThat(eventLog.cros.size).isEqualTo(1)
-    assertThat(eventLog.cros.first()).isEqualTo(personEntity.getCro())
-    assertThat(eventLog.pncs.size).isEqualTo(1)
-    assertThat(eventLog.pncs.first()).isEqualTo(personEntity.getPnc())
-    assertThat(eventLog.sentenceDates.size).isEqualTo(1)
-    assertThat(eventLog.sentenceDates.first()).isEqualTo(personEntity.sentenceInfo.first().sentenceDate)
-    assertThat(eventLog.recordMergedTo).isNull()
-    assertThat(eventLog.eventTimestamp).isNotNull()
-    assertThat(eventLog.eventType).isEqualTo(CPRLogEvents.CPR_RECORD_UPDATED)
-    assertThat(eventLog.overrideMarker).isNull()
-    assertThat(eventLog.overrideScopes).isEmpty()
-    assertThat(eventLog.statusReason).isNull()
-    assertThat(eventLog.masterDefendantId).isNull()
-  }
 
   @Test
   fun `should map exclude override to event log`() {
