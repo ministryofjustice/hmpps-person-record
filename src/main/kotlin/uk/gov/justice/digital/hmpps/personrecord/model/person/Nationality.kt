d7550e4b7ee9b6831cf5ad27e2886eb72be6fe56
Luke Bevan <6418390+lbevan@users.noreply.github.com>
2025-11-28 11:42:38 +0000

    CPR-931 Remove Nationality data class (#1643)
    Use the NationalityCode directly.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/person/Nationality.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.personrecord.model.person
-import uk.gov.justice.digital.hmpps.personrecord.model.types.nationality.NationalityCode
-
-data class Nationality(
-  var code: NationalityCode?,
-)
