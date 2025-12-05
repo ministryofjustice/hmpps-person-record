b4caa8fecc8651df80cd10078373a7703c87edf3
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-12-05 10:46:46 +0000

    CPR-930 drop ethnicity_codes table (#1671)
    * CPR-930 remove foreign key from person table before dropping ethnicity code table
    
    * CPR-930 drop ethnicity_codes table

---
+++ src/main/resources/db/migration/V163__drop_ethnicity_codes_foreign_key.sql
@@ -0,0 +1,8 @@
+BEGIN;
+-------------------------------------------------------
+
+ALTER TABLE IF EXISTS personrecordservice.person
+DROP COLUMN fk_ethnicity_code_id;
+
+-----------------------------------------------------
+COMMIT;
