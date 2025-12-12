e0c0425318eae9e088daa1781f6e824c87d8c29a
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-12-12 13:15:37 +0000

    CPR-1010 admin endpoint for reclustering twins (#1688)
    * CPR-1010 starting point for reclustering twins
    
    * CPR-1010 format
    
    * CPR-1010 start looking at is-cluster-valid response
    
    * CPR-1010 correct setup for a cluster split - not sure if it matters whether twins detection is in place
    
    * CPR-1010 split into x clusters
    
    * CPR-1010 handle splitting multiple records into clusters for twins
    
    * CPR-1010 add some logging:
    
    * CPR-1010 formatting

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/ReclusterTwinsController.kt
@@ -0,0 +1,73 @@
+package uk.gov.justice.digital.hmpps.personrecord.api.controller.admin
+
+import io.swagger.v3.oas.annotations.Hidden
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personrecord.api.model.admin.AdminTwin
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
+
+@RestController
+class ReclusterTwinsController(
+  private val personKeyRepository: PersonKeyRepository,
+  private val personMatchService: PersonMatchService,
+  private val reclusterTwinsService: ReclusterTwinsService,
+) {
+
+  @Hidden
+  @PostMapping("/admin/recluster-twins")
+  suspend fun postRecluster(
+    @RequestBody adminTwins: List<AdminTwin>,
+  ) {
+    CoroutineScope(Dispatchers.Default).launch {
+      log.info("$RECLUSTER_TWIN_PROCESS_PREFIX Triggered. Number of records: ${adminTwins.size}.")
+      triggerRecluster(adminTwins)
+      log.info("$RECLUSTER_TWIN_PROCESS_PREFIX Complete.")
+    }
+  }
+
+  fun triggerRecluster(adminReclusterRecords: List<AdminTwin>) {
+    adminReclusterRecords.forEachPersonAndLog(RECLUSTER_TWIN_PROCESS_NAME) {
+      val isClusterValidResponse = personMatchService.examineIsClusterValid(it)
+      if (isClusterValidResponse.isClusterValid) {
+        log.info("No twins found in ${it.personUUID}")
+      } else {
+        log.info("Splitting ${it.personUUID} into ${isClusterValidResponse.clusters.size} clusters")
+        log.info("Cluster ${it.personUUID} will keep matchIds ${isClusterValidResponse.clusters.first().map{ it }}")
+        val clustersToSplitOff = isClusterValidResponse.clusters.subList(1, isClusterValidResponse.clusters.size)
+        clustersToSplitOff.forEach { cluster ->
+          reclusterTwinsService.split(cluster)
+        }
+      }
+    }
+  }
+
+  private fun searchForPersonByIdentifier(record: AdminTwin): PersonKeyEntity? = personKeyRepository.findByPersonUUID(record.uuid)
+
+  private fun List<AdminTwin>.forEachPersonAndLog(processName: String, action: (PersonKeyEntity) -> Unit) {
+    val total = this.count()
+    log.info("Starting $processName, count: $total")
+    this.forEachIndexed { idx, record ->
+      val itemNumber = idx + 1
+      log.info("Processing $processName, item: $itemNumber/$total")
+      searchForPersonByIdentifier(record)?.let {
+        when {
+          it.isNotMerged() -> action(it)
+          else -> log.info("Skipping $processName, item: $itemNumber/$total it has been merged")
+        }
+      } ?: log.info("Error $processName, record not found. id: ${record.uuid} item: $itemNumber/$total")
+    }
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+    private const val RECLUSTER_TWIN_PROCESS_PREFIX = "ADMIN TWIN RECLUSTER: "
+    private const val RECLUSTER_TWIN_PROCESS_NAME = "Recluster Twins"
+  }
+}
