a51cd397ac0f4829bbad2f0f7a4b63be52735b35
Luke Bevan <6418390+lbevan@users.noreply.github.com>
2025-12-12 13:22:57 +0000

    CPR-987 Prison religion entity ref types and validation (#1696)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/historic/PrisonReligion.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/historic/PrisonReligion.kt
@@ -17,10 +17,10 @@ data class PrisonReligion(
   val startDate: LocalDate? = null,
   @Schema(description = "The religion end date", example = "2000-01-01")
   val endDate: LocalDate? = null,
-  @Schema(description = "The religion modify date and time", example = "2000-01-01 12:00:00")
-  val modifyDateTime: LocalDateTime? = null,
-  @Schema(description = "The religion modify user id", example = "12345")
-  val modifyUserId: String? = null,
+  @Schema(description = "The religion modify date and time", example = "2000-01-01 12:00:00", required = true)
+  val modifyDateTime: LocalDateTime,
+  @Schema(description = "The religion modify user id", example = "12345", required = true)
+  val modifyUserId: String,
   @Schema(description = "Flag indicating the current religion", example = "true", required = true)
   val current: Boolean,
 )
