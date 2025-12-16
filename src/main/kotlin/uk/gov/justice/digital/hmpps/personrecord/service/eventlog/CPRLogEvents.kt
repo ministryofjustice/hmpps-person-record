e0ccd8fa044a5c666b4209ac8ce3b6d5b69fe30c
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-12-16 14:02:41 +0000

    CPR-1010 remove twins splitting code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/eventlog/CPRLogEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/eventlog/CPRLogEvents.kt
@@ -9,13 +9,15 @@ enum class CPRLogEvents {
   CPR_RECORD_UNMERGED,
   CPR_RECORD_UPDATED,
   CPR_UUID_CREATED,
-  CPR_UUID_SPLIT,
-  CPR_UUID_LEAVE,
   CPR_UUID_DELETED,
   CPR_UUID_MERGED,
   CPR_RECORD_SEEDED,
 
   // Deprecated Events. Still kept in enum so we can still decode old events.
+  @Deprecated("Used when splitting possible twins on the new cluster")
+  CPR_UUID_SPLIT,
+  @Deprecated("Used when splitting possible twins to indicate the record which left the old cluster")
+  CPR_UUID_LEAVE,
   @Deprecated("Replaced by setting NEEDS ATTENTION on create / update")
   CPR_RECLUSTER_NEEDS_ATTENTION,
 
