5c0cb203f86c22a20f839d6d5fe1bddc271a3729
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-01-06 17:30:19 +0000

    CPR-1031 Use StdDeserializer instead of ValueDeserializer

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifierDeserializer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/CROIdentifierDeserializer.kt
@@ -2,11 +2,11 @@ package uk.gov.justice.digital.hmpps.personrecord.model.identifiers
 
 import tools.jackson.core.JsonParser
 import tools.jackson.databind.DeserializationContext
-import tools.jackson.databind.ValueDeserializer
+import tools.jackson.databind.deser.std.StdDeserializer
 
-class CROIdentifierDeserializer : ValueDeserializer<CROIdentifier>() {
+class CROIdentifierDeserializer : StdDeserializer<CROIdentifier>(CROIdentifier::class.java) {
 
-  override fun deserialize(parser: JsonParser?, context: DeserializationContext?): CROIdentifier = CROIdentifier.from(parser?.text ?: "")
+  override fun deserialize(parser: JsonParser?, context: DeserializationContext?): CROIdentifier = CROIdentifier.from(parser?.string ?: "")
 
   override fun getNullValue(ctxt: DeserializationContext?): CROIdentifier = CROIdentifier.from()
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/PNCIdentifierDeserializer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/identifiers/PNCIdentifierDeserializer.kt
@@ -2,11 +2,11 @@ package uk.gov.justice.digital.hmpps.personrecord.model.identifiers
 
 import tools.jackson.core.JsonParser
 import tools.jackson.databind.DeserializationContext
-import tools.jackson.databind.ValueDeserializer
+import tools.jackson.databind.deser.std.StdDeserializer
 
-class PNCIdentifierDeserializer : ValueDeserializer<PNCIdentifier>() {
+class PNCIdentifierDeserializer : StdDeserializer<PNCIdentifier>(PNCIdentifier::class.java) {
 
-  override fun deserialize(parser: JsonParser?, context: DeserializationContext?): PNCIdentifier = PNCIdentifier.from(parser?.text ?: "")
+  override fun deserialize(parser: JsonParser?, context: DeserializationContext?): PNCIdentifier = PNCIdentifier.from(parser?.string ?: "")
 
   override fun getNullValue(ctxt: DeserializationContext?): PNCIdentifier = PNCIdentifier.from()
 }
