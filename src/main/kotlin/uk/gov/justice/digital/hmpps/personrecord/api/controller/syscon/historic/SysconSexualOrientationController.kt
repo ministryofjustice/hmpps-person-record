d65322cb86597e86aaaca1dc7e738ecbfcbfb2a9
Josh Parker <joshua.parker2@justice.gov.uk>
2026-01-07 15:14:56 +0000

    CPR-1011 Updated SysconSexualOrientationController to update sexual orientation on the person table

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/syscon/historic/SysconSexualOrientationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/syscon/historic/SysconSexualOrientationController.kt
@@ -6,43 +6,57 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.PERSON_RECORD_SYSCON_SYNC_WRITE
+import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
 import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonSexualOrientation
-import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.prison.PrisonSexualOrientationEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.prison.PrisonSexualOrientationRepository
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SexualOrientation
+import uk.gov.justice.digital.hmpps.personrecord.service.person.PersonService
 
 @Tag(name = "Syscon Sync")
 @RestController
 @PreAuthorize("hasRole('${Roles.PERSON_RECORD_SYSCON_SYNC_WRITE}')")
 class SysconSexualOrientationController(
   private val prisonSexualOrientationRepository: PrisonSexualOrientationRepository,
+  private val personRepository: PersonRepository,
+  private val personService: PersonService,
 ) {
 
-  @Operation(description = "Creates a new prison sexual orientation record, or updates the existing one if a record matching the prison number is found")
+  @Operation(description = "Update the sexual orientation by prison number. Role required is **$PERSON_RECORD_SYSCON_SYNC_WRITE**.")
   @PostMapping("/syscon-sync/sexual-orientation/{prisonNumber}")
   @ApiResponses(
     ApiResponse(
       responseCode = "200",
-      description = "Sexual Orientation saved in CPR",
+      description = "Sexual Orientation updated in CPR",
     ),
   )
-  @Transactional
-  fun saveSexualOrientation(
-    @PathVariable("prisonNumber")
-    @Parameter(description = "The identifier of the offender source system (NOMIS)", required = true)
-    prisonNumber: String,
+  @Transactional(isolation = Isolation.REPEATABLE_READ)
+  fun updateSexualOrientation(
+    @PathVariable @Parameter(description = "The identifier of the offender source system (NOMIS)", required = true) prisonNumber: String,
     @RequestBody sexualOrientation: PrisonSexualOrientation,
   ): String {
-    prisonSexualOrientationRepository.findByPrisonNumber(prisonNumber)
-      ?.update(sexualOrientation)
-      ?: prisonSexualOrientationRepository.save(PrisonSexualOrientationEntity.from(prisonNumber, sexualOrientation))
-
+    personRepository.findByPrisonNumber(prisonNumber)?.processUpdate(sexualOrientation)
+      ?: throw ResourceNotFoundException(prisonNumber)
     return OK
+  }
+
+  private fun PersonEntity.processUpdate(prisonSexualOrientation: PrisonSexualOrientation) {
+    val person = Person.from(
+      this.apply {
+        sexualOrientation = prisonSexualOrientation.sexualOrientationCode?.let { SexualOrientation.fromPrison(it) }
+      },
+    )
+    personService.processPerson(person) { this }
   }
 
   companion object {
