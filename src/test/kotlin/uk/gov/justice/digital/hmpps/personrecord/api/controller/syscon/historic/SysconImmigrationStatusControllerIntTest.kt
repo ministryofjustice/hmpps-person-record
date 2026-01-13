6c50cbc0b325ae15a5355f974711c25116e62612
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-01-13 11:25:53 +0000

    CPR-997 Update Syscon Immigration Status endpoint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/syscon/historic/SysconImmigrationStatusControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/syscon/historic/SysconImmigrationStatusControllerIntTest.kt
@@ -4,12 +4,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.PERSON_RECORD_SYSCON_SYNC_WRITE
 import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonImmigrationStatus
-import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonImmigrationStatusResponse
 import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.prison.PrisonImmigrationStatusRepository
-import uk.gov.justice.digital.hmpps.personrecord.model.types.PrisonRecordType
 import uk.gov.justice.digital.hmpps.personrecord.test.randomBoolean
 import uk.gov.justice.digital.hmpps.personrecord.test.randomDate
 import uk.gov.justice.digital.hmpps.personrecord.test.randomDateTime
@@ -18,126 +16,86 @@ import uk.gov.justice.digital.hmpps.personrecord.test.randomPrisonNumber
 
 class SysconImmigrationStatusControllerIntTest : WebTestBase() {
 
-  @Autowired
-  private lateinit var prisonImmigrationStatusRepository: PrisonImmigrationStatusRepository
-
-  @Nested
-  inner class Creation {
-
-    @Test
-    fun `should store current immigration status against a prison number`() {
-      val prisonNumber = randomPrisonNumber()
-      val currentImmigrationStatus = createRandomPrisonImmigrationStatus(prisonNumber, current = true)
-
-      val historicImmigrationStatus = createRandomPrisonImmigrationStatus(prisonNumber, current = false)
-
-      val currentCreationResponse = webTestClient
-        .post()
-        .uri("/syscon-sync/immigration-status")
-        .bodyValue(currentImmigrationStatus)
-        .authorised(roles = listOf(Roles.PERSON_RECORD_SYSCON_SYNC_WRITE))
-        .exchange()
-        .expectStatus()
-        .isCreated
-        .expectBody(PrisonImmigrationStatusResponse::class.java)
-        .returnResult()
-        .responseBody!!
-
-      val current = awaitNotNull { prisonImmigrationStatusRepository.findByCprImmigrationStatusId(currentCreationResponse.cprImmigrationStatusId) }
-
-      assertThat(current.interestToImmigration).isEqualTo(currentImmigrationStatus.interestToImmigration)
-      assertThat(current.prisonNumber).isEqualTo(prisonNumber)
-      assertThat(current.startDate).isEqualTo(currentImmigrationStatus.startDate)
-      assertThat(current.endDate).isEqualTo(currentImmigrationStatus.endDate)
-      assertThat(current.createUserId).isEqualTo(currentImmigrationStatus.createUserId)
-      assertThat(current.createDateTime).isEqualTo(currentImmigrationStatus.createDateTime)
-      assertThat(current.createDisplayName).isEqualTo(currentImmigrationStatus.createDisplayName)
-      assertThat(current.modifyDateTime).isEqualTo(currentImmigrationStatus.modifyDateTime)
-      assertThat(current.modifyUserId).isEqualTo(currentImmigrationStatus.modifyUserId)
-      assertThat(current.modifyDisplayName).isEqualTo(currentImmigrationStatus.modifyDisplayName)
-      assertThat(current.prisonRecordType).isEqualTo(PrisonRecordType.CURRENT)
-
-      val historicCreationResponse = webTestClient
-        .post()
-        .uri("/syscon-sync/immigration-status")
-        .bodyValue(historicImmigrationStatus)
-        .authorised(roles = listOf(Roles.PERSON_RECORD_SYSCON_SYNC_WRITE))
-        .exchange()
-        .expectStatus()
-        .isCreated
-        .expectBody(PrisonImmigrationStatusResponse::class.java)
-        .returnResult()
-        .responseBody!!
-
-      val historic = awaitNotNull { prisonImmigrationStatusRepository.findByCprImmigrationStatusId(historicCreationResponse.cprImmigrationStatusId) }
-
-      assertThat(historic.interestToImmigration).isEqualTo(historicImmigrationStatus.interestToImmigration)
-      assertThat(historic.prisonNumber).isEqualTo(prisonNumber)
-      assertThat(historic.startDate).isEqualTo(historicImmigrationStatus.startDate)
-      assertThat(historic.endDate).isEqualTo(historicImmigrationStatus.endDate)
-      assertThat(historic.createUserId).isEqualTo(historicImmigrationStatus.createUserId)
-      assertThat(historic.createDateTime).isEqualTo(historicImmigrationStatus.createDateTime)
-      assertThat(historic.createDisplayName).isEqualTo(historicImmigrationStatus.createDisplayName)
-      assertThat(historic.modifyDateTime).isEqualTo(historicImmigrationStatus.modifyDateTime)
-      assertThat(historic.modifyUserId).isEqualTo(historicImmigrationStatus.modifyUserId)
-      assertThat(historic.modifyDisplayName).isEqualTo(historicImmigrationStatus.modifyDisplayName)
-      assertThat(historic.prisonRecordType).isEqualTo(PrisonRecordType.HISTORIC)
-    }
-  }
-
   @Nested
   inner class Update {
 
     @Test
-    fun `should update a immigration status`() {
+    fun `should update person immigration status`() {
       val prisonNumber = randomPrisonNumber()
+      createPerson(createRandomPrisonPersonDetails(prisonNumber))
 
-      val currentSexualOrientation = createRandomPrisonImmigrationStatus(prisonNumber, current = true)
+      val originalEntity = awaitNotNull { personRepository.findByPrisonNumber(prisonNumber) }
+      assertThat(originalEntity.immigrationStatus).isNull()
 
-      val currentCreationResponse = webTestClient
-        .post()
-        .uri("/syscon-sync/immigration-status")
-        .bodyValue(currentSexualOrientation)
-        .authorised(roles = listOf(Roles.PERSON_RECORD_SYSCON_SYNC_WRITE))
+      val immigrationStatus = createPrisonImmigrationStatus(prisonNumber, true)
+      postImmigrationStatus(prisonNumber, immigrationStatus)
+
+      awaitAssert {
+        val updatedEntity = personRepository.findByPrisonNumber(prisonNumber)!!
+        assertThat(updatedEntity.immigrationStatus).isEqualTo(immigrationStatus.interestToImmigration)
+        assertThat(updatedEntity.getPrimaryName().dateOfBirth).isEqualTo(originalEntity.getPrimaryName().dateOfBirth)
+        assertThat(updatedEntity.getPrimaryName().firstName).isEqualTo(originalEntity.getPrimaryName().firstName)
+        assertThat(updatedEntity.getPrimaryName().lastName).isEqualTo(originalEntity.getPrimaryName().lastName)
+      }
+    }
+
+    @Test
+    fun `should return 404 not found when person with prison number does not exist`() {
+      val prisonNumber = randomPrisonNumber()
+      val expectedErrorMessage = "Not found: $prisonNumber"
+      webTestClient.post()
+        .uri(immigrationUrl(prisonNumber))
+        .bodyValue(createPrisonImmigrationStatus(prisonNumber, randomBoolean()))
+        .authorised(roles = listOf(PERSON_RECORD_SYSCON_SYNC_WRITE))
         .exchange()
         .expectStatus()
-        .isCreated
-        .expectBody(PrisonImmigrationStatusResponse::class.java)
-        .returnResult()
-        .responseBody!!
-
-      val current = awaitNotNull { prisonImmigrationStatusRepository.findByCprImmigrationStatusId(currentCreationResponse.cprImmigrationStatusId) }
-
-      assertThat(current.interestToImmigration).isEqualTo(currentSexualOrientation.interestToImmigration)
-
-      val updatedImmigrationStatus = createRandomPrisonImmigrationStatus(prisonNumber, current = false)
-
-      webTestClient
-        .put()
-        .uri("/syscon-sync/immigration-status/${current.cprImmigrationStatusId}")
-        .bodyValue(updatedImmigrationStatus)
-        .authorised(roles = listOf(Roles.PERSON_RECORD_SYSCON_SYNC_WRITE))
-        .exchange()
-        .expectStatus()
-        .isOk
-
-      val updated = awaitNotNull { prisonImmigrationStatusRepository.findByCprImmigrationStatusId(currentCreationResponse.cprImmigrationStatusId) }
-
-      assertThat(updated.interestToImmigration).isEqualTo(updatedImmigrationStatus.interestToImmigration)
-      assertThat(updated.prisonNumber).isEqualTo(prisonNumber)
-      assertThat(updated.startDate).isEqualTo(updatedImmigrationStatus.startDate)
-      assertThat(updated.endDate).isEqualTo(updatedImmigrationStatus.endDate)
-      assertThat(updated.createUserId).isEqualTo(updatedImmigrationStatus.createUserId)
-      assertThat(updated.createDateTime).isEqualTo(updatedImmigrationStatus.createDateTime)
-      assertThat(updated.createDisplayName).isEqualTo(updatedImmigrationStatus.createDisplayName)
-      assertThat(updated.modifyDateTime).isEqualTo(updatedImmigrationStatus.modifyDateTime)
-      assertThat(updated.modifyUserId).isEqualTo(updatedImmigrationStatus.modifyUserId)
-      assertThat(updated.modifyDisplayName).isEqualTo(updatedImmigrationStatus.modifyDisplayName)
-      assertThat(updated.prisonRecordType).isEqualTo(PrisonRecordType.HISTORIC)
+        .isNotFound
+        .expectBody()
+        .jsonPath("userMessage")
+        .isEqualTo(expectedErrorMessage)
     }
   }
 
-  private fun createRandomPrisonImmigrationStatus(prisonNumber: String, current: Boolean): PrisonImmigrationStatus = PrisonImmigrationStatus(
+  @Nested
+  inner class Auth {
+
+    @Test
+    fun `should return Access Denied 403 when role is wrong`() {
+      val expectedErrorMessage = "Forbidden: Access Denied"
+      webTestClient.post()
+        .uri(immigrationUrl(randomPrisonNumber()))
+        .bodyValue(createPrisonImmigrationStatus(randomPrisonNumber(), randomBoolean()))
+        .authorised(listOf("UNSUPPORTED-ROLE"))
+        .exchange()
+        .expectStatus()
+        .isForbidden
+        .expectBody()
+        .jsonPath("userMessage")
+        .isEqualTo(expectedErrorMessage)
+    }
+
+    @Test
+    fun `should return UNAUTHORIZED 401 when role is not set`() {
+      webTestClient.post()
+        .uri(immigrationUrl(randomPrisonNumber()))
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+  }
+
+  private fun postImmigrationStatus(prisonNumber: String, immigrationStatus: Any) {
+    webTestClient
+      .post()
+      .uri(immigrationUrl(prisonNumber))
+      .bodyValue(immigrationStatus)
+      .authorised(roles = listOf(PERSON_RECORD_SYSCON_SYNC_WRITE))
+      .exchange()
+      .expectStatus()
+      .isOk
+  }
+
+  private fun createPrisonImmigrationStatus(prisonNumber: String, current: Boolean): PrisonImmigrationStatus = PrisonImmigrationStatus(
     prisonNumber = prisonNumber,
     interestToImmigration = randomBoolean(),
     startDate = randomDate(),
@@ -150,4 +108,6 @@ class SysconImmigrationStatusControllerIntTest : WebTestBase() {
     modifyDisplayName = randomName(),
     current = current,
   )
+
+  private fun immigrationUrl(prisonNumber: String) = "/syscon-sync/immigration-status/$prisonNumber"
 }
