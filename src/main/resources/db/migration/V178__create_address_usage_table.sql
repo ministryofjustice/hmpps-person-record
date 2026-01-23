a0b89a327fdcffe432b27dbbd38484af0c9aed95
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-01-23 15:44:19 +0000

    CPR-1048 Create address_usage table

---
+++ src/main/resources/db/migration/V178__create_address_usage_table.sql
@@ -0,0 +1,15 @@
+BEGIN;
+-------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS personrecordservice.address_usage
+(
+    id                                      SERIAL      PRIMARY KEY,
+    fk_address_id                           BIGINT      NOT NULL,
+    usage_code                              TEXT        NOT NULL,
+    active                                  BOOLEAN     NOT NULL,
+    CONSTRAINT fk_address_id FOREIGN KEY (fk_address_id) references address (id) ON DELETE CASCADE
+);
+CREATE INDEX idx_address_usage_fk_address_id ON address_usage(fk_address_id);
+
+-----------------------------------------------------
+COMMIT;
