61a32669a844ac109382d8b3fcb4ba66bfd2a0df
samz-justice <sam.zaroug@justice.gov.uk>
2026-01-26 10:05:21 +0000

    CPR-1050 update to query to find clusters with more that one delius record

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/ServiceNowDeliusMergeRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/ServiceNowDeliusMergeRequest.kt
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import java.time.LocalDateTime
 
 @RestController
 class ServiceNowDeliusMergeRequest(private val personRepository: PersonRepository) {
@@ -17,10 +19,16 @@ class ServiceNowDeliusMergeRequest(private val personRepository: PersonRepositor
   }
 
   fun generate() {
-    // something along these lines to find cluster updates yesterday with more than 1 Delius record (not working at the moment)
-
-//    personRepository.findByLastModifiedBetween(LocalDateTime.now().minusDays(1), LocalDateTime.now().minusDays(1).plusHours(1))
-//      .filter  { person -> person.personKey?.personEntities?.filter { it.sourceSystem == DELIUS }?.size > 1}
+    // find cluster updates yesterday with more than 1 Delius record (not working at the moment)
+    personRepository.findByLastModifiedBetween(
+      LocalDateTime.now().minusDays(1),
+      LocalDateTime.now().minusDays(1).plusHours(1),
+    )
+      .filter { person ->
+        person.personKey?.personEntities?.filter {
+          it.sourceSystem == SourceSystemType.DELIUS
+        }?.size!! > 1
+      }
 
     // identify 5 clusters with more than 1 Delius record
     // call servicenow
