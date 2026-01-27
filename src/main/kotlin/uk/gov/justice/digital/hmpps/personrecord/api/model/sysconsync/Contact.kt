fbd81ece86a4681e2face29192e4d91e3b4086a6
Luke Bevan <6418390+lbevan@users.noreply.github.com>
2026-01-27 11:48:12 +0000

    CPR-1028 Amend Syscon PUT API contract to include contacts at address level (#1775)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/Contact.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/Contact.kt
@@ -1,20 +1,13 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.personrecord.model.types.ContactType
 
 data class Contact(
   @Schema(description = "The contact value", example = "01234567890")
   val value: String? = null,
   @Schema(description = "The contact type", example = "HOME")
-  val type: ContactType? = null,
+  val type: ContactType,
   @Schema(description = "The contact extension", example = "235")
   val extension: String? = null,
-  @NotNull
-  @Schema(description = "Is the contact for a person", example = "false", required = true)
-  val isPersonContact: Boolean,
-  @NotNull
-  @Schema(description = "Is the contact for an address", example = "true", required = true)
-  val isAddressContact: Boolean,
 )
