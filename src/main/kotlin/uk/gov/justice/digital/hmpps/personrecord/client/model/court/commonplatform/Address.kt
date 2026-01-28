d366aa5151b45da2aa7b511c0c67bfdc16bdb98a
Josh Parker <joshua.parker2@justice.gov.uk>
2026-01-28 10:54:17 +0000

    CPR WIP

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/court/commonplatform/Address.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/client/model/court/commonplatform/Address.kt
@@ -1,14 +1,20 @@
 package uk.gov.justice.digital.hmpps.personrecord.client.model.court.commonplatform
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import jakarta.validation.constraints.NotBlank
+import kotlin.reflect.full.memberProperties
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Address(
-  val address1: @NotBlank String,
+  val address1: String? = null,
   val address2: String? = null,
   val address3: String? = null,
   val address4: String? = null,
   val address5: String? = null,
   val postcode: String? = null,
-)
+) {
+
+  fun allPropertiesOrNull(): Address? = this.takeIf { it.allPropertiesNotNull() }
+
+  private fun allPropertiesNotNull(): Boolean = this::class.memberProperties
+    .all { it.call(this) == null }.not()
+}
