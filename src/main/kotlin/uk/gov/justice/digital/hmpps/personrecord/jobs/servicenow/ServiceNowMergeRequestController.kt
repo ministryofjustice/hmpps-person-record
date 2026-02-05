175ecad4351c8a5a71abf68fd1cebd009323ce23
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-02-05 09:49:01 +0000

    CPR-1050 add transaction

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/servicenow/ServiceNowMergeRequestController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/servicenow/ServiceNowMergeRequestController.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.personrecord.jobs.servicenow
 import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.RestController
@@ -24,6 +25,7 @@ class ServiceNowMergeRequestController(
 ) {
 
   @Hidden
+  @Transactional
   @RequestMapping(method = [POST], value = ["/jobs/service-now/generate-delius-merge-requests"])
   fun collectAndReport(): String {
     getClustersForMergeRequests().forEach {
