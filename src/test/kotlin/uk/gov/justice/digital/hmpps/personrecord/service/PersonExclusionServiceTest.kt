fbccf1fff5bd5e15fbb14c08dd115e4c6a7e349b
Josh Parker <joshua.parker2@justice.gov.uk>
2026-02-13 14:22:18 +0000

    CPR-1043 added roleback test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/PersonExclusionServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/PersonExclusionServiceTest.kt
@@ -1,12 +1,18 @@
 package uk.gov.justice.digital.hmpps.personrecord.service
 
+import org.junit.jupiter.api.Assertions.assertThrows
 import com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doThrow
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
 import uk.gov.justice.digital.hmpps.personrecord.config.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonKeyRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
 import uk.gov.justice.digital.hmpps.personrecord.test.randomPrisonNumber
 
 class PersonExclusionServiceTest : IntegrationTestBase() {
@@ -14,13 +20,16 @@ class PersonExclusionServiceTest : IntegrationTestBase() {
   @Autowired
   private lateinit var personExclusionService: PersonExclusionService
 
-  @BeforeEach
-  fun beforeEach() {
-    stubDeletePersonMatch()
-  }
+  @MockitoSpyBean
+  private lateinit var mockPersonKeyRepository: PersonKeyRepository
+
+  @MockitoSpyBean
+  private lateinit var mockPersonMatchService: PersonMatchService
 
   @Test
   fun `cluster size greater than 1 - removes person from cluster - attaches person to new cluster - deletes from person match`() {
+    stubDeletePersonMatch()
+
     val prisonerNumberOne = randomPrisonNumber()
     val prisonerNumberTwo = randomPrisonNumber()
     val originalPersonKeyEntity = createPersonKey()
@@ -50,6 +59,8 @@ class PersonExclusionServiceTest : IntegrationTestBase() {
 
   @Test
   fun `cluster size of 1 - only deletes from person match`() {
+    stubDeletePersonMatch()
+
     val prisonerNumberOne = randomPrisonNumber()
     val originalPersonKeyEntity = createPersonKey()
       .addPerson(createRandomPrisonPersonDetails(prisonerNumberOne))
@@ -61,6 +72,30 @@ class PersonExclusionServiceTest : IntegrationTestBase() {
       assertThat(personOne.personKey!!.personUUID).isEqualTo(originalPersonKeyEntity.personUUID)
       assertThat(personOne.personKey!!.personEntities.size).isEqualTo(1)
       wiremock.verify(1, deleteRequestedFor(urlEqualTo("/person")))
+    }
+  }
+
+  @Test
+  fun `error writing to db - does not call person match to delete`() {
+    val prisonerNumberOne = randomPrisonNumber()
+    val prisonerNumberTwo = randomPrisonNumber()
+    val originalPersonKeyEntity = createPersonKey()
+      .addPerson(createRandomPrisonPersonDetails(prisonerNumberOne))
+      .addPerson(createRandomPrisonPersonDetails(prisonerNumberTwo))
+
+    awaitAssert {
+      val clusterBeforeExclusion = personKeyRepository.findByPersonUUID(originalPersonKeyEntity.personUUID)
+      assertThat(clusterBeforeExclusion!!.personEntities.size).isEqualTo(2)
+    }
+
+    doThrow(RuntimeException()).whenever(mockPersonKeyRepository).save(any())
+
+    assertThrows(RuntimeException::class.java) { personExclusionService.exclude { personRepository.findByPrisonNumber(prisonerNumberTwo) } }
+
+    awaitAssert {
+      val clusterAfterExclusion = personKeyRepository.findByPersonUUID(originalPersonKeyEntity.personUUID)
+      assertThat(clusterAfterExclusion!!.personEntities.size).isEqualTo(2)
+      wiremock.verify(0, deleteRequestedFor(urlEqualTo("/person")))
     }
   }
 }
