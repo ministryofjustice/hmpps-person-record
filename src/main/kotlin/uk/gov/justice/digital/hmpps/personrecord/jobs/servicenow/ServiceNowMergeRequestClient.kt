d4ca0ac7ae088e9f8fbe3e4568a3a00ceb9d2e48
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2026-02-23 10:53:25 +0000

    CPR-1050-enable-job-prod enable job in prod

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/servicenow/ServiceNowMergeRequestClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/servicenow/ServiceNowMergeRequestClient.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.personrecord.jobs.servicenow
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
-@Profile("!prod")
 @Component
 class ServiceNowMergeRequestClient(private val serviceNowWebClient: WebClient, @Value($$"${service-now.sysparm-id}")private val sysParmId: String) {
   fun postRecords(payload: ServiceNowMergeRequestPayload) = serviceNowWebClient
