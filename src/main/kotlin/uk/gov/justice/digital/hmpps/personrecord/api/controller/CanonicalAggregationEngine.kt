1dd3815aaae68a184ecb6d6a47735f227ab539db
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-03-03 11:01:35 +0000

    Canonical record structure

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/CanonicalAggregationEngine.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/CanonicalAggregationEngine.kt
@@ -1,7 +1,19 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.controller
 
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalAddress
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalAlias
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalEthnicity
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalIdentifiers
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalNationality
 import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalRecord
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalRecord.Companion.getAddresses
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalRecord.Companion.getAliases
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalReligion
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalSex
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalSexualOrientation
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalTitle
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonKeyEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 
@@ -11,7 +23,37 @@ class CanonicalAggregationEngine(
 ) {
 
   fun get(personKeyEntity: PersonKeyEntity): CanonicalRecord {
-    val canonicalRecord = CanonicalRecord.from(personKeyEntity)
+    val canonicalRecord = fromAggregation(personKeyEntity)
     return canonicalRecord
+  }
+
+  fun fromAggregation(personKey: PersonKeyEntity): CanonicalRecord {
+    val latestPerson = personKey.personEntities.sortedByDescending { it.lastModified }.first()
+    return CanonicalRecord(
+      cprUUID = personKey.personUUID.toString(),
+      firstName = latestPerson.getPrimaryName().firstName,
+      middleNames = latestPerson.getPrimaryName().middleNames,
+      lastName = latestPerson.getPrimaryName().lastName,
+      dateOfBirth = latestPerson.getPrimaryName().dateOfBirth?.toString(),
+      disability = latestPerson.disability,
+      interestToImmigration = latestPerson.immigrationStatus,
+      title = CanonicalTitle.from(latestPerson.getPrimaryName().titleCode),
+      sex = CanonicalSex.from(latestPerson.getPrimaryName().sexCode),
+      sexualOrientation = CanonicalSexualOrientation.from(latestPerson.sexualOrientation),
+      religion = CanonicalReligion.from(latestPerson.religion),
+      ethnicity = CanonicalEthnicity.from(latestPerson.ethnicityCode),
+      aliases = getAliases(latestPerson),
+      addresses = getAddresses(latestPerson),
+      identifiers = CanonicalIdentifiers.from(personKey.personEntities),
+      nationalities = CanonicalNationality.from(latestPerson),
+    )
+  }
+
+  private fun getAddresses(person: PersonEntity): List<CanonicalAddress> {
+    TODO("Not yet implemented")
+  }
+
+  private fun getAliases(person: PersonEntity): List<CanonicalAlias> {
+    TODO("Not yet implemented")
   }
 }
