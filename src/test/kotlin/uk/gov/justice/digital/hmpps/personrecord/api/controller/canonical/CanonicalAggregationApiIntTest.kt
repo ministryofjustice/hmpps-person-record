db5eb65229f4c945621ba3a77260f6be0ea1f521
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2026-03-03 10:47:04 +0000

    canonical-record-spike create tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/canonical/CanonicalAggregationApiIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/canonical/CanonicalAggregationApiIntTest.kt
@@ -3,25 +3,34 @@ package uk.gov.justice.digital.hmpps.personrecord.api.controller.canonical
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.API_READ_ONLY
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalAlias
 import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalRecord
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalSex
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalTitle
 import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.Identifiers
 import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
 import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCaseName
 import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Alias
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomName
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPrisonNumber
 
 class CanonicalAggregationApiIntTest : WebTestBase() {
   @Test
   fun `should return latest modified from 2 records  aggregate `() {
-    val latestFirstName = randomName()
+    val prisonDetails = createRandomPrisonPersonDetails()
+    val probationDetails = createRandomProbationPersonDetails()
+
+
+    val prisonPerson = createPerson(prisonDetails)
+    val probationPerson = createPerson(probationDetails)
+
     val personKey = createPersonKey()
-      .addPerson(
-        Person.from(ProbationCase(name = ProbationCaseName(firstName = randomName(), middleNames = randomName(), lastName = randomName()), identifiers = Identifiers(crn = randomCrn()))),
-      ).addPerson(
-        Person.from(ProbationCase(name = ProbationCaseName(firstName = latestFirstName, middleNames = randomName(), lastName = randomName()), identifiers = Identifiers(crn = randomCrn()))),
-      )
+      .addPerson(prisonPerson)
+      .addPerson(probationPerson)
+
 
     val responseBody = webTestClient.get()
       .uri(canonicalAPIUrlAggregate(personKey.personUUID.toString()))
@@ -33,7 +42,14 @@ class CanonicalAggregationApiIntTest : WebTestBase() {
       .returnResult()
       .responseBody!!
 
-    assertThat(responseBody.firstName).isEqualTo(latestFirstName)
+
+    val canonicalAlias = CanonicalAlias.from(prisonPerson)?.plus( CanonicalAlias.from(probationPerson))
+
+
+    assertThat(responseBody.firstName).isEqualTo(probationDetails.firstName)
+    assertThat(responseBody.aliases).isEqualTo(canonicalAlias?.get(0))
+
+
   }
 
   private fun canonicalAPIUrlAggregate(uuid: String) = "/canonical-record/$uuid"
