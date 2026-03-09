3db14c29810e3b978840f09efa42db91fbbcfd59
Josh Parker <joshua.parker2@justice.gov.uk>
2026-03-09 15:49:53 +0000

    CPR-1104: moved shared pojo into seperate file

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/prison/PrisonReligionGetResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/prison/PrisonReligionGetResponse.kt
@@ -2,8 +2,6 @@ package uk.gov.justice.digital.hmpps.personrecord.api.model.prison
 
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.prison.PrisonReligionEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.types.toReligionCodeDescription
-import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class PrisonReligionGetResponse(
   val prisonNumber: String,
@@ -24,34 +22,6 @@ data class PrisonReligionGetResponse(
         modifyUserId = prisonReligionEntity.modifyUserId,
         current = prisonReligionEntity.prisonRecordType.value,
       ),
-    )
-  }
-}
-
-data class PrisonReligionGet(
-  val religionCode: String? = null,
-  val religionDescription: String? = null,
-  val changeReasonKnown: Boolean? = null,
-  val comments: String? = null,
-  val verified: Boolean? = null,
-  val startDate: LocalDate? = null,
-  val endDate: LocalDate? = null,
-  val modifyDateTime: LocalDateTime? = null,
-  val modifyUserId: String? = null,
-  val current: Boolean? = null,
-) {
-  companion object {
-    fun from(prisonReligionEntity: PrisonReligionEntity): PrisonReligionGet = PrisonReligionGet(
-      religionCode = prisonReligionEntity.code,
-      religionDescription = prisonReligionEntity.code.toReligionCodeDescription(),
-      changeReasonKnown = prisonReligionEntity.changeReasonKnown,
-      comments = prisonReligionEntity.comments,
-      verified = prisonReligionEntity.verified,
-      startDate = prisonReligionEntity.startDate,
-      endDate = prisonReligionEntity.endDate,
-      modifyDateTime = prisonReligionEntity.modifyDateTime,
-      modifyUserId = prisonReligionEntity.modifyUserId,
-      current = prisonReligionEntity.prisonRecordType.value,
     )
   }
 }
