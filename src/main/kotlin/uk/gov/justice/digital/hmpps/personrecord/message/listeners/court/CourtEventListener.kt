4194d6c5e51af63a2354874b21faed5cb8f8338e
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-03-10 17:08:16 +0000

    FIX: add profile annotation to as few classes as possible

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/CourtEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/court/CourtEventListener.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.message.listeners.court
 
 import io.awspring.cloud.sqs.annotation.SqsListener
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.client.model.court.MessageType.COMMON_PLATFORM_HEARING
 import uk.gov.justice.digital.hmpps.personrecord.client.model.court.MessageType.LIBRA_COURT_CASE
@@ -10,7 +9,6 @@ import uk.gov.justice.digital.hmpps.personrecord.message.processors.court.LibraE
 import uk.gov.justice.digital.hmpps.personrecord.service.queue.Queues
 import uk.gov.justice.digital.hmpps.personrecord.service.queue.SQSListenerService
 
-@Profile("!webtest")
 @Component
 class CourtEventListener(
   private val commonPlatformEventProcessor: CommonPlatformEventProcessor,
