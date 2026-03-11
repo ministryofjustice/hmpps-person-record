4f7145c002b7d85d0c4502edd24b363251fdac70
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-03-11 20:46:19 +0000

    FIX: introduce WebTestSetup to do all web test setup. WebTestBase now just excludes messaging configuration

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/QueueAdminIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/QueueAdminIntTest.kt
@@ -1,27 +1,9 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.controller
 
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.web.reactive.server.WebTestClient
-import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.QUEUE_ADMIN
-import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
+import uk.gov.justice.digital.hmpps.personrecord.config.WebTestSetup
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles("test")
-@AutoConfigureWebTestClient
-class QueueAdminIntTest {
-
-  @Autowired
-  lateinit var webTestClient: WebTestClient
-
-  @Autowired
-  internal lateinit var jwtAuthorisationHelper: JwtAuthorisationHelper
-
-  fun WebTestClient.RequestHeadersSpec<*>.authorised(roles: List<String> = listOf(QUEUE_ADMIN)): WebTestClient.RequestBodySpec = headers(jwtAuthorisationHelper.setAuthorisationHeader(roles = roles)) as WebTestClient.RequestBodySpec
+class QueueAdminIntTest : WebTestSetup() {
 
   @Test
   fun `should retry dead letter queue`() {
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/WebTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/WebTestBase.kt
@@ -2,81 +2,17 @@ package uk.gov.justice.digital.hmpps.personrecord.config
 
 import org.mockito.Mockito
 import org.mockito.kotlin.whenever
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
 import org.springframework.test.context.TestPropertySource
-import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.reactive.server.expectBody
 import software.amazon.awssdk.services.sns.SnsAsyncClient
-import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.QUEUE_ADMIN
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.HmppsTopic
-import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
 
-@AutoConfigureWebTestClient
 @TestPropertySource(properties = ["spring.autoconfigure.exclude=uk.gov.justice.hmpps.sqs.HmppsSqsConfiguration"])
 @Import(WebTestBase.WebTestConfig::class)
-abstract class WebTestBase : IntegrationTestBase() {
-
-  @Autowired
-  lateinit var webTestClient: WebTestClient
-
-  @Autowired
-  internal lateinit var jwtAuthorisationHelper: JwtAuthorisationHelper
-
-  fun WebTestClient.RequestHeadersSpec<*>.authorised(roles: List<String> = listOf(QUEUE_ADMIN)): WebTestClient.RequestBodySpec = headers(jwtAuthorisationHelper.setAuthorisationHeader(roles = roles)) as WebTestClient.RequestBodySpec
-
-  protected final inline fun <reified T : Any> sendPostRequestAsserted(
-    url: String,
-    body: Any,
-    roles: List<String>,
-    expectedStatus: HttpStatus,
-    sendAuthorised: Boolean = true,
-  ): WebTestClient.BodySpec<T, *> = sendRequestAsserted(url, body, roles, expectedStatus, sendAuthorised, HttpMethod.POST)
-
-  protected final inline fun <reified T : Any> sendPutRequestAsserted(
-    url: String,
-    body: Any,
-    roles: List<String>,
-    expectedStatus: HttpStatus,
-    sendAuthorised: Boolean = true,
-  ): WebTestClient.BodySpec<T, *> = sendRequestAsserted(url, body, roles, expectedStatus, sendAuthorised, HttpMethod.PUT)
-
-  protected final inline fun <reified T : Any> sendGetRequestAsserted(
-    url: String,
-    roles: List<String>,
-    expectedStatus: HttpStatus,
-    sendAuthorised: Boolean = true,
-  ): WebTestClient.BodySpec<T, *> = sendRequestAsserted(url, null, roles, expectedStatus, sendAuthorised, HttpMethod.GET)
-
-  protected final inline fun <reified T : Any> sendRequestAsserted(
-    url: String,
-    body: Any?,
-    roles: List<String>,
-    expectedStatus: HttpStatus,
-    sendAuthorised: Boolean = true,
-    methodType: HttpMethod,
-  ): WebTestClient.BodySpec<T, *> {
-    val requestSpec = webTestClient
-      .method(methodType)
-      .uri(url)
-
-    val requestSpecReady = when (methodType) {
-      HttpMethod.GET -> requestSpec
-      else -> requestSpec.bodyValue(body!!)
-    }
-
-    val responseSpec = when (sendAuthorised) {
-      true -> requestSpecReady.authorised(roles).exchange()
-      false -> requestSpecReady.exchange()
-    }.expectStatus().isEqualTo(expectedStatus.value())
-    return responseSpec.expectBody<T>()
-  }
+abstract class WebTestBase : WebTestSetup() {
 
   @TestConfiguration
   class WebTestConfig {
