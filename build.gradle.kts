463544342449c652c390e8bca2ca453a52a51b7b
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2026-03-18 08:15:36 +0000

    FIX: upgrade swagger-ui to fix dompurify CVE (#1890)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ kotlin {
 }
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.6"
   kotlin("plugin.spring") version "2.3.10"
   kotlin("jvm") version "2.3.10"
   kotlin("plugin.jpa") version "2.3.10"
