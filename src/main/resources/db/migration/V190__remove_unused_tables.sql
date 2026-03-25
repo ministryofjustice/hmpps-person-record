89ffffaf6aebd727f54c6f8d56a9641b5ddaf299
JunaedAhmedMOJ <junaed.ahmed@digital.justice.gov.uk>
2026-03-25 13:54:17 +0000

    CPR-1073-remove-unused-tables remove pr_baak and temp_uuids for prod and pre prod

---
+++ src/main/resources/db/migration/V190__remove_unused_tables.sql
@@ -0,0 +1,9 @@
+BEGIN;
+-------------------------------------------------------
+
+DROP TABLE IF EXISTS personrecordservice.temp_uuids;
+DROP TABLE IF EXISTS personrecordservice.pr_bak;
+
+
+-----------------------------------------------------
+COMMIT;
