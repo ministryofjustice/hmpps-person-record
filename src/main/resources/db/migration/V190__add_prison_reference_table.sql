c467e6b70c4ab470539a518059dccb08a3a9ceb9
jldparker527 <joshua.parker2@justice.gov.uk>
2026-03-26 16:46:34 +0000

    CPR-1064 added new migration file to add prison reference table (#1911)

---
+++ src/main/resources/db/migration/V190__add_prison_reference_table.sql
@@ -0,0 +1,17 @@
+BEGIN;
+-------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS personrecordservice.prison_reference
+(
+    id                                      SERIAL      PRIMARY KEY,
+    update_id                               UUID        DEFAULT gen_random_uuid() NOT NULL,
+    fk_pseudonym_id                         BIGINT      NOT NULL,
+    identifier_type                         TEXT        DEFAULT NULL,
+    identifier_value                        TEXT        DEFAULT NULL,
+    identifier_comment                      TEXT        DEFAULT NULL,
+    version                                 int4        NOT NULL DEFAULT 0
+    );
+ALTER TABLE IF EXISTS prison_reference add constraint fk_pseudonym_reference_id foreign key (fk_pseudonym_id) references pseudonym ON DELETE CASCADE;
+
+-----------------------------------------------------
+COMMIT;
