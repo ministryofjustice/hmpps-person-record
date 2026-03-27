917edce51d015897ccb06e9bdb29243de95ce67f
Josh Parker <joshua.parker2@justice.gov.uk>
2026-03-27 11:50:09 +0000

    CPR-1064 made GET /person/prison return canonical record

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/prison/PrisonGetHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/prison/PrisonGetHandler.kt
@@ -5,36 +5,20 @@ import org.springframework.http.HttpStatus.MOVED_PERMANENTLY
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
-import uk.gov.justice.digital.hmpps.personrecord.api.model.prison.PrisonCanonicalRecord
+import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalRecord
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
-import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.prison.PrisonReligionRepository
 import java.net.URI
 
 @Component
-class PrisonGetHandler(
-  private val personRepository: PersonRepository,
-  private val prisonReligionRepository: PrisonReligionRepository,
-) {
+class PrisonGetHandler(private val personRepository: PersonRepository) {
 
-  fun get(prisonNumber: String): ResponseEntity<PrisonCanonicalRecord> {
+  fun get(prisonNumber: String): ResponseEntity<CanonicalRecord> {
     val personEntity = personRepository.findByPrisonNumber(prisonNumber)
     return when {
       personEntity == null -> throw ResourceNotFoundException(prisonNumber)
       personEntity.hasMergedIntoAnotherPerson() -> respondWithRedirect(getMergedToPrisonNumber(personEntity))
-      else -> {
-        val prisonReligionEntities = prisonReligionRepository
-          .findByPrisonNumberOrderByStartDateDescCreateDateTimeDesc(prisonNumber)
-
-        ResponseEntity.ok(
-          (
-            PrisonCanonicalRecord.from(
-              personEntity,
-              prisonReligionEntities,
-            )
-            ),
-        )
-      }
+      else -> ResponseEntity.ok(CanonicalRecord.from(personEntity))
     }
   }
 
@@ -42,7 +26,7 @@ class PrisonGetHandler(
 
   private fun PersonEntity.hasMergedIntoAnotherPerson() = !this.isNotMerged()
 
-  private fun respondWithRedirect(targetPrisonNumber: String): ResponseEntity<PrisonCanonicalRecord> = ResponseEntity
+  private fun respondWithRedirect(targetPrisonNumber: String): ResponseEntity<CanonicalRecord> = ResponseEntity
     .status(MOVED_PERMANENTLY)
     .location(URI("/person/prison/$targetPrisonNumber"))
     .build()
