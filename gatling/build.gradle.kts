12b89fea8c1a4857f7b1d2cd503c7eda93906270
Kaustav Surai <kaustav.surai@opencastsoftware.com>
2026-04-13 15:51:21 +0100

    updated config

--- gatling/build.gradle.kts
+++ gatling/build.gradle.kts
@@ -1,18 +1,15 @@
 plugins {
     kotlin("jvm") version "2.3.0"
-    id("io.gatling.gradle") version "3.15.0"
-    application
+    kotlin("plugin.allopen")
+    id("io.gatling.gradle") version "3.15.0.1"
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
     gatling("org.postgresql:postgresql:42.7.7")
 }
 
@@ -28,4 +25,24 @@ tasks.register<JavaExec>("generateTestData") {
     group = "application"
     classpath = sourceSets.getByName("gatling").runtimeClasspath
     mainClass.set("com.moj.cpr.perf.helper.CsvGenerator")
+}
+
+tasks.register<Exec>("gatlingRunCi") {
+  group = "gatling"
+  description = "Run un-attended in github ci"
+
+  val clientId = (project.findProperty("CLIENT_ID") as String?)
+  val clientSecret = (project.findProperty("CLIENT_SECRET") as String?)
+
+
+  environment("CLIENT_ID", clientId ?: "")
+  environment("CLIENT_SECRET", clientSecret ?: "")
+
+  val args = mutableListOf("gatlingRun")
+  args += listOf("--all")
+
+  workingDir = project.rootDir
+  val wrapper = if (org.gradle.internal.os.OperatingSystem.current().isWindows) "gradlew.bat" else "./gradlew"
+  println("[GATLING][Gradle] $wrapper ${args.joinToString(" ")}")
+  commandLine(wrapper, *args.toTypedArray())
 }
