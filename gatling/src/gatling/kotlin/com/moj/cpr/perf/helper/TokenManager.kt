1c760d62d102119babc1938a7f5d8828608d4431
Kaustav Surai <kaustav.surai@opencastsoftware.com>
2026-04-14 15:54:37 +0100

    debug

--- gatling/src/gatling/kotlin/com/moj/cpr/perf/helper/TokenManager.kt
+++ gatling/src/gatling/kotlin/com/moj/cpr/perf/helper/TokenManager.kt
@@ -28,7 +28,8 @@ object TokenManager {
       }
       val basicAuth =
         Base64.getEncoder().encodeToString("${AppConfig.clientId.trim()}:${AppConfig.clientSecret.trim()}".toByteArray())
-      println(basicAuth)
+      println(AppConfig.clientId.trim())
+      println(AppConfig.clientSecret.trim())
       println(AppConfig.tokenUrl)
 
       val request = HttpRequest.newBuilder().uri(URI.create(AppConfig.tokenUrl))
