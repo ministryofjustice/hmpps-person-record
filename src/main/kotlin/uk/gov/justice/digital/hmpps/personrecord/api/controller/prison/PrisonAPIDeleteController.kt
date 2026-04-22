f113332b0efcdb8dc9e20f1c9f437cc6505bef00
Josh Parker <joshua.parker2@justice.gov.uk>
2026-04-22 13:56:18 +0100

    CPR-1138 added prison delete endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/prison/PrisonAPIDeleteController.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.personrecord.api.controller.prison
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.PERSON_RECORD_SYSCON_SYNC_WRITE
+import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
+import uk.gov.justice.digital.hmpps.personrecord.service.message.PersonDeletionService
+
+@Tag(name = "HMPPS Person API")
+@RestController
+@PreAuthorize("hasRole('$PERSON_RECORD_SYSCON_SYNC_WRITE')")
+class PrisonAPIDeleteController(
+  private val personDeletionService: PersonDeletionService,
+  private val personRepository: PersonRepository,
+) {
+
+  @ApiResponses(
+    ApiResponse(
+      responseCode = "200",
+      description = "OK",
+    ),
+  )
+  @DeleteMapping("/person/prison/{prisonNumber}")
+  fun deleteByPrisonNumber(@PathVariable(name = "prisonNumber") prisonNumber: String): ResponseEntity<Unit> {
+    personDeletionService.processDelete {
+      personRepository.findByPrisonNumber(prisonNumber)
+        ?: throw ResourceNotFoundException("Person '$prisonNumber' does not exist")
+    }
+    return ResponseEntity.ok().build()
+  }
+}
