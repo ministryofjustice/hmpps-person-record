f7b00fce4bec344f970a8ac216007c7091b8388f
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-04-23 17:06:49 +0100

    Pin swagger-ui to 5.32.2
    See the following for more info:
     - https://github.com/ministryofjustice/hmpps-gradle-spring-boot/blob/main/release-notes/10.x.md
     - https://github.com/ministryofjustice/cmd-api/pull/388/changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
   implementation("software.amazon.sns:sns-extended-client:2.1.0")
   implementation("com.jayway.jsonpath:json-path:3.0.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+  constraints {
+    implementation("org.webjars:swagger-ui:5.32.2")
+  }
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
