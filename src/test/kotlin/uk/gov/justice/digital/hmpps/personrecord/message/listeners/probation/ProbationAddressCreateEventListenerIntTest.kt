fc3520a6166eb722ffad8940e991a48cee3320ff
Josh Parker <joshua.parker2@justice.gov.uk>
2026-04-27 17:11:41 +0100

    CPR-1140 added address assertions

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/probation/ProbationAddressCreateEventListenerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/probation/ProbationAddressCreateEventListenerIntTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest
 import tools.jackson.module.kotlin.readValue
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationAddress
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.SQSMessage
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.AdditionalInformation
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
@@ -11,8 +12,6 @@ import uk.gov.justice.digital.hmpps.personrecord.config.MessagingMultiNodeTestBa
 import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_ADDRESS_CREATED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_ADDRESS_CREATED
 import uk.gov.justice.digital.hmpps.personrecord.test.randomAddressNumber
-import uk.gov.justice.digital.hmpps.personrecord.test.randomBuildingNumber
-import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomDate
 import uk.gov.justice.digital.hmpps.personrecord.test.randomDigit
 import uk.gov.justice.digital.hmpps.personrecord.test.randomFullAddress
@@ -23,52 +22,99 @@ import uk.gov.justice.digital.hmpps.personrecord.test.randomPostcode
 import uk.gov.justice.digital.hmpps.personrecord.test.randomUprn
 import uk.gov.justice.digital.hmpps.personrecord.test.responses.ApiResponseSetupAddress
 import uk.gov.justice.digital.hmpps.personrecord.test.responses.probationAddress
-import java.util.UUID
 
 class ProbationAddressCreateEventListenerIntTest : MessagingMultiNodeTestBase() {
 
   @Test
   fun `consuming address created event - saves address - triggers correct events`() {
-    val probationAddressId = randomDigit()
-    stubGetRequestToProbation(probationAddressId)
-
-    val crn = randomCrn()
-    val existingPersonEntity = createPerson(createRandomProbationPersonDetails(crn = crn).copy(addresses = emptyList()))
+    val probationAddress = randomProbationAddress()
+    val cprPerson = createRandomProbationPersonDetails().copy(addresses = emptyList())
     createPersonKey()
-      .addPerson(existingPersonEntity)
+      .addPerson(cprPerson)
 
-    publishProbationDomainEvent(OFFENDER_ADDRESS_CREATED, crn, additionalInformation = AdditionalInformation(addressId = probationAddressId))
+    stubGetRequestToProbation(probationAddress)
+    publishProbationDomainEvent(OFFENDER_ADDRESS_CREATED, cprPerson.crn!!, AdditionalInformation(addressId = probationAddress.addressId))
 
     awaitAssert {
-      val actualPersonEntity = personRepository.findByCrn(crn)!!
-      assertThat(actualPersonEntity.addresses.size).isEqualTo(1)
-      val actualAddressEntity = actualPersonEntity.addresses.first()
-      // ....
+      assertAddressSaved(cprPerson.crn, probationAddress)
+      assertPublishedDomainEvent(cprPerson.crn, probationAddress.addressId!!)
+      // recluster triggered
 
-      assertPublishedDomainEvent(crn, actualAddressEntity.updateId!!, probationAddressId)
+      // telemetry?
+      // event logs?
     }
   }
 
-  private fun stubGetRequestToProbation(probationAddressId: String) {
-    ApiResponseSetupAddress(
-      addressId = probationAddressId,
+  private fun randomProbationAddress(): ProbationAddress {
+    val startDate = randomDate()
+    val endDate = startDate.plusYears(10)
+    return ProbationAddress(
       noFixedAbode = true,
-      startDate = randomDate(),
+      startDate = startDate,
+      endDate = endDate,
       postcode = randomPostcode(),
       fullAddress = randomFullAddress(),
-      buildingName = randomBuildingNumber(),
+      buildingName = randomName(),
       addressNumber = randomAddressNumber(),
       streetName = randomLowerCaseString(),
       district = randomName(),
       townCity = randomName(),
       county = randomName(),
       uprn = randomUprn(),
-      notes = randomName(),
+      notes = randomLowerCaseString(),
       telephoneNumber = randomPhoneNumber(),
-    ).also { stubGetRequest(url = "/person/address/${it.addressId}", body = probationAddress(it)) }
+      addressId = randomDigit(),
+    )
   }
 
-  private fun assertPublishedDomainEvent(crn: String, cprAddressId: UUID, probationAddressId: String) {
+  private fun stubGetRequestToProbation(probationAddress: ProbationAddress) {
+    stubGetRequest(
+      url = "/person/address/${probationAddress.addressId}",
+      body = probationAddress(
+        address = ApiResponseSetupAddress(
+          addressId = probationAddress.addressId,
+          noFixedAbode = probationAddress.noFixedAbode,
+          startDate = probationAddress.startDate,
+          endDate = probationAddress.endDate,
+          postcode = probationAddress.postcode,
+          fullAddress = probationAddress.fullAddress,
+          buildingName = probationAddress.buildingName,
+          addressNumber = probationAddress.addressId,
+          streetName = probationAddress.streetName,
+          district = probationAddress.district,
+          townCity = probationAddress.townCity,
+          county = probationAddress.county,
+          uprn = probationAddress.uprn,
+          notes = probationAddress.notes,
+          telephoneNumber = probationAddress.telephoneNumber,
+        ),
+      ),
+    )
+  }
+
+  private fun assertAddressSaved(crn: String, probationAddress: ProbationAddress) {
+    val actualPersonEntity = personRepository.findByCrn(crn)!!
+    assertThat(actualPersonEntity.addresses.size).isEqualTo(1)
+    val actualAddressEntity = actualPersonEntity.addresses.first()
+    assertThat(actualAddressEntity.noFixedAbode).isEqualTo(probationAddress.noFixedAbode)
+    assertThat(actualAddressEntity.startDate).isEqualTo(probationAddress.startDate)
+    assertThat(actualAddressEntity.endDate).isEqualTo(probationAddress.endDate)
+    assertThat(actualAddressEntity.postcode).isEqualTo(probationAddress.postcode)
+    assertThat(actualAddressEntity.fullAddress).isEqualTo(probationAddress.fullAddress)
+    assertThat(actualAddressEntity.buildingName).isEqualTo(probationAddress.buildingName)
+    assertThat(actualAddressEntity.postTown).isEqualTo(probationAddress.townCity)
+    assertThat(actualAddressEntity.county).isEqualTo(probationAddress.county)
+    assertThat(actualAddressEntity.uprn).isEqualTo(probationAddress.uprn)
+//    assertThat(actualAddressEntity.addressNumber)
+//    assertThat(actualAddressEntity.streetName)
+//    assertThat(actualAddressEntity.district)
+//    assertThat(actualAddressEntity.notes)
+//    assertThat(actualAddressEntity.telephoneNumber)
+//    assertThat(actualAddressEntity.addressId)
+  }
+
+  private fun assertPublishedDomainEvent(crn: String, probationAddressId: String) {
+    val cprAddressUpdateId = personRepository.findByCrn(crn)!!.addresses.first().updateId!!.toString()
     expectOneMessageOn(testOnlyCPREventsQueue)
     val actualDomainEvent = testOnlyCPREventsQueue?.sqsClient?.receiveMessage(ReceiveMessageRequest.builder().queueUrl(testOnlyCPREventsQueue?.queueUrl).build())!!.get()
     val sqsMessage = actualDomainEvent.messages()?.first()?.let { jsonMapper.readValue<SQSMessage>(it.body()) }!!
@@ -79,10 +125,10 @@ class ProbationAddressCreateEventListenerIntTest : MessagingMultiNodeTestBase() 
     assertThat(domainEvent.personReference.identifiers.first().type).isEqualTo("CRN")
     assertThat(domainEvent.personReference.identifiers.first().value).isEqualTo(crn)
     assertThat(domainEvent.additionalInformation?.deliusAddressId).isEqualTo(probationAddressId)
-    assertThat(domainEvent.additionalInformation?.cprAddressId).isEqualTo(cprAddressId.toString())
+    assertThat(domainEvent.additionalInformation?.cprAddressId).isEqualTo(cprAddressUpdateId)
     assertThat(domainEvent.version).isEqualTo(1) // TODO
     assertThat(domainEvent.description).isEqualTo("Address was created in Core Person Record")
-    assertThat(domainEvent.detailUrl).isEqualTo("http://localhost:8080/person/probation/$crn/address/$cprAddressId")
+    assertThat(domainEvent.detailUrl).isEqualTo("http://localhost:8080/person/probation/$crn/address/$cprAddressUpdateId")
     assertThat(domainEvent.occurredAt).isNotBlank
   }
 }
