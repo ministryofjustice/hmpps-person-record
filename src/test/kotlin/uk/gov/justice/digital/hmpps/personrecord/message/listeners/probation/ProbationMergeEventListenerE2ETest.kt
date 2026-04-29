a8b5c248eb9ca2cd571cabb90720475e7278a3d5
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-04-29 17:12:04 +0100

    CPR-1164 Fix merges with clusters that have an outstanding review

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/probation/ProbationMergeEventListenerE2ETest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/probation/ProbationMergeEventListenerE2ETest.kt
@@ -2,15 +2,49 @@ package uk.gov.justice.digital.hmpps.personrecord.message.listeners.probation
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
 import uk.gov.justice.digital.hmpps.personrecord.config.E2ETestBase
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.types.UUIDStatusType
 import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
+import uk.gov.justice.digital.hmpps.personrecord.service.type.NEW_OFFENDER_CREATED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_MERGED
+import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_UNMERGED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.TelemetryEventType.CPR_RECORD_MERGED
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomDefendantId
+import uk.gov.justice.digital.hmpps.personrecord.test.responses.ApiResponseSetup
 import kotlin.jvm.optionals.getOrNull
 
 class ProbationMergeEventListenerE2ETest : E2ETestBase() {
+
+  @Test
+  fun `process offender merge event with source record having a review`() {
+    val targetCrn = randomCrn()
+    val sourceCrn = randomCrn()
+    val sourcePerson = createPersonWithNewKey(createRandomProbationPersonDetails(sourceCrn))
+    val targetPersonDetails = createRandomProbationCase(targetCrn)
+    val targetPerson = createPersonWithNewKey(Person.from(targetPersonDetails))
+
+    // 1. merge the records
+    probationMergeEventAndResponseSetup(
+      OFFENDER_MERGED,
+      sourceCrn = sourcePerson.crn!!,
+      targetCrn = targetPerson.crn!!,
+    )
+    sourcePerson.assertMergedTo(targetPerson)
+
+    // 2. unmerge the records
+    val targetUnmergeSetup = ApiResponseSetup.from(targetPersonDetails).copy(crn = sourceCrn)
+    probationUnmergeEventAndResponseSetup(OFFENDER_UNMERGED, sourceCrn, targetCrn, reactivatedSetup = targetUnmergeSetup)
+    sourcePerson.assertNotMerged()
+
+    // 3. create new person with same target details - should get added to cluster
+    probationDomainEventAndResponseSetup(NEW_OFFENDER_CREATED, targetUnmergeSetup.copy(crn = randomCrn()))
+    targetPerson.personKey!!.getReview().hasReviewSize(1)
+
+    // TODO: merge again
+  }
 
   @Test
   fun `processes offender merge event with records on same cluster`() {
