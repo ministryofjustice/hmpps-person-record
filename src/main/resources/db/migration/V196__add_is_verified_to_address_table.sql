b0304f44bb580c63c9e83cc167149c9dcc79b5a8
Josh Parker <joshua.parker2@justice.gov.uk>
2026-05-07 09:31:14 +0100

    CPR-1172 changed data type

--- src/main/resources/db/migration/V196__add_is_verified_to_address_table.sql
+++ src/main/resources/db/migration/V196__add_is_verified_to_address_table.sql
@@ -2,7 +2,7 @@ BEGIN;
 -------------------------------------------------------
 
 ALTER TABLE IF EXISTS personrecordservice.address
-    ADD COLUMN is_verified BIGINT NULL;
+    ADD COLUMN is_verified BOOLEAN NULL;
 
 -----------------------------------------------------
 COMMIT;
