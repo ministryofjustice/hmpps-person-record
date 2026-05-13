197e5eff8b822d2f768fe0996ea7676dadb901f0
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-05-13 17:04:45 +0100

    CPR-1181 Use UK timezone for timestamp conversion

--- src/main/resources/db/migration/V198__modify_address_date_timestampz.sql
+++ src/main/resources/db/migration/V198__modify_address_date_timestampz.sql
@@ -3,11 +3,10 @@ BEGIN;
 
 ALTER TABLE IF EXISTS personrecordservice.address
     ALTER COLUMN start_date TYPE timestamptz
-    USING start_date AT TIME ZONE 'UTC';
+    USING start_date AT TIME ZONE 'Europe/London';
 
 ALTER TABLE IF EXISTS personrecordservice.address
     ALTER COLUMN end_date TYPE timestamptz
-    USING end_date AT TIME ZONE 'UTC';
-
+    USING end_date AT TIME ZONE 'Europe/London';
 -----------------------------------------------------
 COMMIT;
