6b572fe4aeae85cf3814df7dcd2e71f17a800374
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-05-20 17:21:59 +0100

    CPR-1141 hard code page size

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateFromProbationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateFromProbationController.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.personrecord.client.CorePersonRecordAndDeliusClient
@@ -14,7 +13,6 @@ import uk.gov.justice.digital.hmpps.personrecord.client.CorePersonRecordAndDeliu
 @RestController
 class PopulateFromProbationController(
   val corePersonRecordAndDeliusClient: CorePersonRecordAndDeliusClient,
-  @Value("\${populate-from-probation.page-size}") val pageSize: Int,
   private val retryableProbationUpdater: RetryableProbationUpdater,
 ) {
 
@@ -27,23 +25,23 @@ class PopulateFromProbationController(
 
   suspend fun populatePages() {
     CoroutineScope(Dispatchers.Default).launch {
-      // todo check all fields
       val totalPages = corePersonRecordAndDeliusClient.getProbationCases(
         CorePersonRecordAndDeliusClientPageParams(
           0,
-          pageSize,
+          PAGE_SIZE,
         ),
       )?.page?.totalPages ?: 1
 
       log.info("Starting address updating, total pages: $totalPages")
       for (page in 0..<totalPages) {
-        retryableProbationUpdater.repopulateProbationRecord(CorePersonRecordAndDeliusClientPageParams(page, pageSize))
+        retryableProbationUpdater.repopulateProbationRecord(CorePersonRecordAndDeliusClientPageParams(page, PAGE_SIZE))
       }
-      log.info("finished add seeding finished, approx records ${totalPages * pageSize}")
+      log.info("finished add seeding finished, approx records ${totalPages * PAGE_SIZE}")
     }
   }
 
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
+    private const val PAGE_SIZE = 1000
   }
 }
