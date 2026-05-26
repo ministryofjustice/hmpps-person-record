79fd89fde08bb45f0dc85704d5042097185354bb
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-05-26 14:09:48 +0100

    CPR-1147 remove unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateFromProbationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/seeding/PopulateFromProbationController.kt
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Hidden
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.launch
-import org.hibernate.query.Page.page
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
