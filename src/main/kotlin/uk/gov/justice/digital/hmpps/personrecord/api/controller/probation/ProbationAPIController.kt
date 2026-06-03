cb97909487dd9488e7249b5df42a5a4c6f40298f
jldparker527 <joshua.parker2@justice.gov.uk>
2026-06-03 10:46:42 +0100

    CPR-1148 stop saving of addresses on PUT /person/probation/{defendantId} (#2092)
    * CPR-1148 stop saving of addresses on PUT /person/probation/{defendantId}
    
    * CPR-1148 added test for no saving address on probation person create
    
    * CPR-1148 added feature flag
    
    * CPR-1148 removed feature flag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/probation/ProbationAPIController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/probation/ProbationAPIController.kt
@@ -23,6 +23,7 @@ import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.PROBATION_A
 import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
 import uk.gov.justice.digital.hmpps.personrecord.api.model.canonical.CanonicalRecord
 import uk.gov.justice.digital.hmpps.personrecord.client.model.offender.ProbationCase
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.AddressEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
@@ -98,7 +99,7 @@ class ProbationAPIController(
     val person = Person.from(probationCase)
     person.masterDefendantId = masterDefendantId
 
-    val offender: PersonEntity = personService.processPerson(person) {
+    val offender: PersonEntity = personService.processPerson(person, setOf(AddressEntity::class)) {
       personRepository.findByCrn(probationCase.identifiers.crn!!)
     }
 
