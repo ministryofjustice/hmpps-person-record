f47691fa71bd45bee81f99cb063ac304ff40bcc3
jldparker527 <joshua.parker2@justice.gov.uk>
2026-06-05 13:58:16 +0100

    CPR 1145 added sas listener for address updates (#2006)
    * CPR-1145 added listener for sas address updated
    
    * CPR-1145 added first test for sas address update
    
    * CPR-1145 added more config
    
    * CPR-1145 added SAS client id and secret env var
    
    * CPR-1145 added cprsasevents queue for profile test
    
    * CPR-1145 updates address and triggers re-cluster
    
    * CPR-1145 updated tests
    
    * CPR-1145 publish address update event to domain events
    
    * CPR-1145 fixed test
    
    * CPR-1145 added negative tests
    
    * CPR-1145 making proper use of the detail url from the event
    
    * CPR-1145 added sas base urls for preprod and dev
    
    * CPR-1145 made callback to sas with their address id not crp address update id
    
    * CPR-1145 reverted publishing address update events to domain topic
    
    * CPR-1145 updated sas event listener to use address service
    
    * CPR-1145 updated sas get response body
    
    * CPR-1145 made non upsert for time being
    
    * CPR-1145 updated sas address response body
    
    * CPR-1148 updated sas address response body
    
    * CPR-1145 removed creating random sas address id in tests
    
    * CPR-1145 feature flagged sas event listener
    
    * CPR-1148 added queue alerts names to dev and preprod
    
    * CPR-1145 changed response body key name
    
    * CPR-1148 removed pointless else block
    
    * CPR-1145 updated sas address update response body fields
    
    * CPR-1145 set correct environment variable name for SAS client secret
    
    ---------
    
    Co-authored-by: Mark Rees <mark.rees@digital.justice.gov.uk>

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/sas/SasAddressUpdatedEventListenerIntTest.kt
@@ -0,0 +1,196 @@
+package uk.gov.justice.digital.hmpps.personrecord.message.listeners.sas
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sas.SasAddressData
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sas.SasAddressStatus
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sas.SasAddressType
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sas.SasGetAddressResponse
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.AdditionalInformation
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
+import uk.gov.justice.digital.hmpps.personrecord.config.MessagingMultiNodeTestBase
+import uk.gov.justice.digital.hmpps.personrecord.extensions.toUkLocalDate
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
+import uk.gov.justice.digital.hmpps.personrecord.service.type.SAS_ADDRESS_UPDATED
+import uk.gov.justice.digital.hmpps.personrecord.test.randomAddressStatusCode
+import uk.gov.justice.digital.hmpps.personrecord.test.randomAddressUsageCode
+import uk.gov.justice.digital.hmpps.personrecord.test.randomBoolean
+import uk.gov.justice.digital.hmpps.personrecord.test.randomBuildingNumber
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCountryCode
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
+import uk.gov.justice.digital.hmpps.personrecord.test.randomLowerCaseString
+import uk.gov.justice.digital.hmpps.personrecord.test.randomName
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPostcode
+import uk.gov.justice.digital.hmpps.personrecord.test.randomUprn
+import java.time.LocalDate
+import java.util.UUID
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sas.Address as SasAddress
+
+class SasAddressUpdatedEventListenerIntTest : MessagingMultiNodeTestBase() {
+
+  @Nested
+  inner class Successful {
+
+    @Test
+    fun `consumes sas update event - updates address`() {
+      val existingPersonEntity = createPerson(createRandomProbationPersonDetails().copy(addresses = listOf(Address(postcode = randomPostcode()))))
+      val existingAddressEntity = existingPersonEntity.addresses.first()
+      createPersonKey()
+        .addPerson(existingPersonEntity)
+
+      val sasCallbackResponse = createSasAddressGetResponse(existingPersonEntity.crn, existingAddressEntity.updateId)
+
+      stubPersonMatchUpsert()
+      stubPersonMatchScores()
+      stubGetRequestToSas(sasCallbackResponse)
+        .also { stubPersonMatchScores() }
+
+      publishSasAddressUpdateEvent(existingPersonEntity.crn!!)
+
+      assertAddressUpdated(existingPersonEntity.crn, sasCallbackResponse)
+    }
+  }
+
+  @Nested
+  inner class FailureScenarios {
+
+    @Test
+    fun `address not returned from sas - pushes event to dead letter queue`() {
+      val existingPersonEntity = createPerson(createRandomProbationPersonDetails().copy(addresses = listOf(Address(postcode = randomPostcode()))))
+      val existingAddressEntity = existingPersonEntity.addresses.first()
+      createPersonKey()
+        .addPerson(existingPersonEntity)
+
+      stubGetRequestToSas(status = 404)
+
+      publishSasAddressUpdateEvent(existingPersonEntity.crn!!)
+
+      expectNoMessagesOn(sasEventsQueue)
+      expectOneMessageOnDlq(sasEventsQueue)
+      expectNoMessagesOn(testOnlyCPRDomainEventsQueue)
+      val actualAddress = personRepository.findByCrn(existingPersonEntity.crn!!)!!.addresses.first()
+      assertThat(actualAddress.postcode).isEqualTo(existingAddressEntity.postcode)
+    }
+
+    @Test
+    fun `cpr address does not exist - pushed to dead letter queue`() {
+      val existingPersonEntity = createPerson(createRandomProbationPersonDetails().copy(addresses = listOf(Address(postcode = randomPostcode()))))
+      createPersonKey()
+        .addPerson(existingPersonEntity)
+
+      val nonExistingAddressUpdateId = UUID.randomUUID()
+      val sasCallbackResponse = createSasAddressGetResponse(existingPersonEntity.crn, nonExistingAddressUpdateId)
+
+      stubGetRequestToSas(sasCallbackResponse)
+
+      publishSasAddressUpdateEvent(existingPersonEntity.crn!!)
+
+      expectNoMessagesOn(sasEventsQueue)
+      expectOneMessageOnDlq(sasEventsQueue)
+      expectNoMessagesOn(testOnlyCPRDomainEventsQueue)
+    }
+
+    @Test
+    fun `cpr person does not exist - pushes to dead letter queue`() {
+      val existingPersonEntity = createPerson(createRandomProbationPersonDetails().copy(addresses = listOf(Address(postcode = randomPostcode()))))
+      val existingAddressEntity = existingPersonEntity.addresses.first()
+      createPersonKey()
+        .addPerson(existingPersonEntity)
+
+      val nonExistingPersonCrn = randomCrn()
+      val sasCallbackResponse = createSasAddressGetResponse(nonExistingPersonCrn, existingAddressEntity.updateId)
+
+      stubGetRequestToSas(sasCallbackResponse)
+
+      publishSasAddressUpdateEvent(nonExistingPersonCrn)
+
+      expectNoMessagesOn(sasEventsQueue)
+      expectOneMessageOnDlq(sasEventsQueue)
+      expectNoMessagesOn(testOnlyCPRDomainEventsQueue)
+    }
+  }
+
+  private fun createSasAddressGetResponse(crn: String?, cprAddressUpdateId: UUID?) = SasGetAddressResponse(
+    data = SasAddressData(
+      crn = crn!!,
+      cprAddressId = cprAddressUpdateId.toString(),
+      startDate = LocalDate.now().minusYears(10),
+      endDate = LocalDate.now().plusYears(10),
+      noFixedAbode = randomBoolean(),
+      typeVerified = randomBoolean(),
+      address = SasAddress(
+        postcode = randomPostcode(),
+        subBuildingName = randomName(),
+        buildingName = randomName(),
+        buildingNumber = randomBuildingNumber(),
+        thoroughfareName = randomName(),
+        dependentLocality = randomName(),
+        postTown = randomPostcode(),
+        county = randomName(),
+        countryCode = randomCountryCode().name,
+        uprn = randomUprn(),
+      ),
+      statusCode = SasAddressStatus(
+        code = randomAddressStatusCode().name,
+        description = randomLowerCaseString(),
+      ),
+      usage = SasAddressType(
+        code = randomAddressUsageCode().name,
+        description = randomLowerCaseString(),
+      ),
+    ),
+  )
+
+  private fun publishSasAddressUpdateEvent(crn: String) {
+    publishDomainEvent(
+      SAS_ADDRESS_UPDATED,
+      DomainEvent(
+        eventType = SAS_ADDRESS_UPDATED,
+        detailUrl = "/accommodations/1234",
+        additionalInformation = AdditionalInformation(
+          sourceCrn = crn,
+        ),
+      ),
+    )
+  }
+
+  private fun stubGetRequestToSas(
+    sasCallbackResponse: SasGetAddressResponse? = null,
+    status: Int = 200,
+  ) {
+    stubGetRequest(
+      url = "/accommodations/1234",
+      body = jsonMapper.writeValueAsString(sasCallbackResponse),
+      status = status,
+    )
+  }
+
+  private fun assertAddressUpdated(crn: String?, expected: SasGetAddressResponse) {
+    awaitAssert {
+      val expectedSasAddress = expected.data
+      val actualPersonEntity = personRepository.findByCrn(crn!!)!!
+      assertThat(actualPersonEntity.addresses.size).isEqualTo(1)
+      val actualAddressEntity = actualPersonEntity.addresses.first()
+      assertThat(actualAddressEntity.startDate!!.toUkLocalDate()).isEqualTo(expectedSasAddress.startDate)
+      assertThat(actualAddressEntity.endDate!!.toUkLocalDate()).isEqualTo(expectedSasAddress.endDate)
+      assertThat(actualAddressEntity.noFixedAbode).isEqualTo(expectedSasAddress.noFixedAbode)
+      assertThat(actualAddressEntity.isVerified).isEqualTo(expectedSasAddress.typeVerified)
+      assertThat(actualAddressEntity.postcode).isEqualTo(expectedSasAddress.address.postcode)
+      assertThat(actualAddressEntity.subBuildingName).isEqualTo(expectedSasAddress.address.subBuildingName)
+      assertThat(actualAddressEntity.buildingName).isEqualTo(expectedSasAddress.address.buildingName)
+      assertThat(actualAddressEntity.buildingNumber).isEqualTo(expectedSasAddress.address.buildingNumber)
+      assertThat(actualAddressEntity.thoroughfareName).isEqualTo(expectedSasAddress.address.thoroughfareName)
+      assertThat(actualAddressEntity.dependentLocality).isEqualTo(expectedSasAddress.address.dependentLocality)
+      assertThat(actualAddressEntity.postTown).isEqualTo(expectedSasAddress.address.postTown)
+      assertThat(actualAddressEntity.county).isEqualTo(expectedSasAddress.address.county)
+      assertThat(actualAddressEntity.countryCode!!.name).isEqualTo(expectedSasAddress.address.countryCode)
+      assertThat(actualAddressEntity.uprn).isEqualTo(expectedSasAddress.address.uprn)
+      assertThat(actualAddressEntity.statusCode!!.name).isEqualTo(expectedSasAddress.statusCode!!.code)
+      val actualAddressUsages = actualAddressEntity.usages
+      assertThat(actualAddressUsages.size).isEqualTo(1)
+      assertThat(actualAddressUsages.first().usageCode.name).isEqualTo(expectedSasAddress.usage!!.code)
+      assertThat(actualAddressUsages.first().active).isEqualTo(true)
+    }
+  }
+}
