848ed47ac2164ad38a642baed35f4b74a5140f13
Josh Parker <joshua.parker2@justice.gov.uk>
2026-06-17 15:45:55 +0100

    CPR-1167 refactored tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/probation/ProbationAddressDeletedEventListenerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/listeners/probation/ProbationAddressDeletedEventListenerIntTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.personrecord.message.listeners.probation
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_ADDRESS_DELETED
 import uk.gov.justice.digital.hmpps.personrecord.service.type.OFFENDER_ADDRESS_DELETED
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomDigit
@@ -15,15 +16,21 @@ class ProbationAddressDeletedEventListenerIntTest : ProbationEventListenerTestBa
     val personEntity = createPersonWithNewKey(
       createRandomProbationPersonDetails().copy(addresses = listOf(Address.from(probationAddress)!!)),
     )
+    val addressEntity = personEntity.addresses.first()
 
     stubPersonMatchUpsert()
     stubPersonMatchScores()
     publishProbationAddressEvent(personEntity.crn, probationAddress.deliusAddressId, OFFENDER_ADDRESS_DELETED)
 
     expectNoMessagesOnQueueOrDlq(probationEventsQueue)
-    expectOneMessageOn(testOnlyCPRDomainEventsQueue)
     val actualPersonEntity = personRepository.findByCrn(personEntity.crn!!)!!
     assertThat(actualPersonEntity.addresses.size).isEqualTo(0)
+
+    assertDomainEventPublishedAfterDeliusAddressDeleteEvent(
+      expectedEventType = CPR_PROBATION_ADDRESS_DELETED,
+      crn = personEntity.crn!!,
+      cprAddressUpdateId = addressEntity.updateId.toString(),
+    )
   }
 
   @Test
@@ -32,16 +39,22 @@ class ProbationAddressDeletedEventListenerIntTest : ProbationEventListenerTestBa
     val personEntity = createPersonWithNewKey(
       createRandomProbationPersonDetails().copy(addresses = listOf(Address.from(probationAddress)!!)),
     )
+    val addressEntity = personEntity.addresses.first()
 
     stubPersonMatchUpsert()
     stubPersonMatchScores()
     publishProbationAddressEvent(randomCrn(), probationAddress.deliusAddressId, OFFENDER_ADDRESS_DELETED)
 
     expectNoMessagesOnQueueOrDlq(probationEventsQueue)
-    expectOneMessageOn(testOnlyCPRDomainEventsQueue)
 
     val actualPersonEntity = personRepository.findByCrn(personEntity.crn!!)!!
     assertThat(actualPersonEntity.addresses.size).isEqualTo(0)
+
+    assertDomainEventPublishedAfterDeliusAddressDeleteEvent(
+      expectedEventType = CPR_PROBATION_ADDRESS_DELETED,
+      crn = personEntity.crn!!,
+      cprAddressUpdateId = addressEntity.updateId.toString(),
+    )
   }
 
   @Test
