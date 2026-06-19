b254326b522f804d75d961c07ee234d55e04342e
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-06-19 14:14:59 +0100

    CPR-1152 use native java methods

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/SwaggerAvoidSameClassNameTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/SwaggerAvoidSameClassNameTest.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.model
 
 import org.junit.jupiter.api.Test
-import wiremock.com.github.jknack.handlebars.internal.Files
 import java.io.File
 import java.nio.file.Paths
 
@@ -11,7 +10,7 @@ class SwaggerAvoidSameClassNameTest {
   fun `ensure there are not two classes with the same name with Swagger annotations`() {
     val kotlinFilesLocation = Paths.get("src/main/kotlin").toAbsolutePath()
     val ls = File(kotlinFilesLocation.toString()).walkTopDown().filter {
-      it.isFile() && Files.read(it, Charsets.UTF_8).contains("@Schema")
+      it.isFile() && it.readText().contains("@Schema")
     }.map { it.name }.groupingBy { it }.eachCount().filter { it.value > 1 }
 
     if (ls.isNotEmpty()) {
