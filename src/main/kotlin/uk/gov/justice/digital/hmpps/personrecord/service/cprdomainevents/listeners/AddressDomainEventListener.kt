27acad90ce35baa1caadc888f5146d234674ddfc
jldparker527 <joshua.parker2@justice.gov.uk>
2026-06-19 14:59:16 +0100

    simplified AddressDomainEventListner logic (#2143)
    * simplified AddressDomainEventListner logic
    
    * CPR-1153 refactored address event listening to use strategy pattern
    
    * CPR-1153 changed class names
    
    * CPR-1153 renamed function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/listeners/AddressDomainEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/listeners/AddressDomainEventListener.kt
@@ -1,92 +1,26 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.listeners
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.AdditionalInformation
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonIdentifier
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonReference
-import uk.gov.justice.digital.hmpps.personrecord.extensions.UK_ZONE
-import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.AddressEntity
-import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
-import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType.DELIUS
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.address.AddressCreated
 import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.address.AddressUpdated
-import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventPublisher
-import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_ADDRESS_CREATED
-import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_ADDRESS_UPDATED
-import java.time.Instant
-import java.time.format.DateTimeFormatter
 
 @Profile("!preprod & !prod")
 @Component
-class AddressDomainEventListener(
-  private val domainEventPublisher: DomainEventPublisher,
-  @Value($$"${core-person-record.base-url}") private val baseUrl: String,
-) {
+class AddressDomainEventListener(addressDomainEventStrategies: List<AddressEventPublisher>) {
+
+  private val strategies = addressDomainEventStrategies.associateBy { it.sourceSystemType }
 
   @TransactionalEventListener
   fun onAddressCreated(addressCreated: AddressCreated) {
-    publishAddressDomainEvent(
-      addressEntity = addressCreated.addressEntity,
-      eventSource = addressCreated.eventSource.identifier,
-      action = "created",
-      config = buildAddressDomainEventConfig(addressCreated.addressEntity.person!!.sourceSystem, CPR_PROBATION_ADDRESS_CREATED),
-    )
+    val sourceSystem = addressCreated.addressEntity.person!!.sourceSystem
+    strategies[sourceSystem]?.onCreate(addressCreated)
   }
 
   @TransactionalEventListener
   fun onAddressUpdated(addressUpdated: AddressUpdated) {
-    publishAddressDomainEvent(
-      addressEntity = addressUpdated.addressEntity,
-      eventSource = addressUpdated.eventSource.identifier,
-      action = "updated",
-      config = buildAddressDomainEventConfig(addressUpdated.addressEntity.person!!.sourceSystem, CPR_PROBATION_ADDRESS_UPDATED),
-    )
-  }
-
-  private fun publishAddressDomainEvent(
-    addressEntity: AddressEntity,
-    eventSource: String,
-    action: String,
-    config: AddressDomainEventConfig?,
-  ) {
-    config ?: return
-    val sourceSystemId = addressEntity.person!!.extractSourceSystemId()!!
-    val addressId = addressEntity.updateId
-    val detailUrl = "$baseUrl/person/${config.urlPathSegment}/$sourceSystemId/address/$addressId"
-
-    domainEventPublisher.publish(
-      DomainEvent(
-        eventType = config.eventType,
-        description = "A ${config.typeDescription} address has been $action for a person",
-        detailUrl = detailUrl,
-        occurredAt = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(UK_ZONE).format(Instant.now()),
-        additionalInformation = AdditionalInformation(
-          outboundCprAddressId = addressId.toString(),
-          outboundDeliusAddressId = addressEntity.deliusAddressId,
-        ),
-        personReference = PersonReference(
-          identifiers = listOf(
-            PersonIdentifier(config.identifierName, sourceSystemId),
-          ),
-        ),
-      ),
-      attributes = mapOf("eventSource" to eventSource),
-    )
-  }
-
-  private fun buildAddressDomainEventConfig(sourceSystem: SourceSystemType, eventType: String): AddressDomainEventConfig? = when (sourceSystem) {
-    DELIUS -> AddressDomainEventConfig(eventType, "CRN", "probation", "probation")
-    else -> null // temporary flow for other source systems we haven't mapped yet
+    val sourceSystem = addressUpdated.addressEntity.person!!.sourceSystem
+    strategies[sourceSystem]?.onUpdate(addressUpdated)
   }
 }
-
-data class AddressDomainEventConfig(
-  val eventType: String,
-  val identifierName: String,
-  val urlPathSegment: String,
-  val typeDescription: String,
-)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/listeners/AddressEventPublisher.kt
@@ -0,0 +1,80 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.listeners
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.AdditionalInformation
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonIdentifier
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonReference
+import uk.gov.justice.digital.hmpps.personrecord.extensions.UK_ZONE
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.AddressEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.address.AddressCreated
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.address.AddressUpdated
+import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventPublisher
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_ADDRESS_CREATED
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_ADDRESS_UPDATED
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+interface AddressEventPublisher {
+  val sourceSystemType: SourceSystemType
+  fun onCreate(addressCreated: AddressCreated)
+  fun onUpdate(addressUpdated: AddressUpdated)
+}
+
+@Component
+class ProbationAddressEventPublisher(
+  private val domainEventPublisher: DomainEventPublisher,
+  @Value($$"${core-person-record.base-url}") private val baseUrl: String,
+) : AddressEventPublisher {
+  override val sourceSystemType = SourceSystemType.DELIUS
+
+  override fun onCreate(addressCreated: AddressCreated) {
+    publishAddressDomainEvent(
+      addressEntity = addressCreated.addressEntity,
+      eventSource = addressCreated.eventSource.identifier,
+      action = "created",
+      eventType = CPR_PROBATION_ADDRESS_CREATED,
+    )
+  }
+
+  override fun onUpdate(addressUpdated: AddressUpdated) {
+    publishAddressDomainEvent(
+      addressEntity = addressUpdated.addressEntity,
+      eventSource = addressUpdated.eventSource.identifier,
+      action = "updated",
+      eventType = CPR_PROBATION_ADDRESS_UPDATED,
+    )
+  }
+
+  private fun publishAddressDomainEvent(
+    addressEntity: AddressEntity,
+    eventSource: String,
+    action: String,
+    eventType: String,
+  ) {
+    val crn = addressEntity.person!!.extractSourceSystemId()!!
+    val addressId = addressEntity.updateId
+    val detailUrl = "$baseUrl/person/probation/$crn/address/$addressId"
+
+    domainEventPublisher.publish(
+      DomainEvent(
+        eventType = eventType,
+        description = "A probation address has been $action for a person",
+        detailUrl = detailUrl,
+        occurredAt = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(UK_ZONE).format(Instant.now()),
+        additionalInformation = AdditionalInformation(
+          outboundCprAddressId = addressId.toString(),
+          outboundDeliusAddressId = addressEntity.deliusAddressId,
+        ),
+        personReference = PersonReference(
+          identifiers = listOf(
+            PersonIdentifier("CRN", crn),
+          ),
+        ),
+      ),
+      attributes = mapOf("eventSource" to eventSource),
+    )
+  }
+}
