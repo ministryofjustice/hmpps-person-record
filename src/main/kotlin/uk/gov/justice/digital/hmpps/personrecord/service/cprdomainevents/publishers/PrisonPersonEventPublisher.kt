15ad5497fc83a291c1543302807fb4f2a4945d5c
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-06-22 14:32:04 +0100

    CPR-1159 Refactor publishing of Person domain events

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/publishers/PrisonPersonEventPublisher.kt
@@ -0,0 +1,63 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.publishers
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonIdentifier
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonReference
+import uk.gov.justice.digital.hmpps.personrecord.extensions.asStringWithUkZone
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonCreated
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonDeleted
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonUpdated
+import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventPublisher
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PRISON_PERSON_CREATED
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_PERSON_CREATED
+import java.time.Instant
+
+@Component
+class PrisonPersonEventPublisher(
+  private val domainEventPublisher: DomainEventPublisher,
+  @Value($$"${core-person-record.base-url}") private val baseUrl: String,
+): PersonEventPublisher {
+  override val sourceSystemType = SourceSystemType.NOMIS
+
+  override fun onCreate(personCreated: PersonCreated) {
+    publishPersonDomainEvent(
+      personCreated.personEntity,
+      CPR_PRISON_PERSON_CREATED
+    )
+  }
+
+  override fun onUpdate(personUpdated: PersonUpdated) {
+    throw NotImplementedError("Person update events are not currently published for prison records")
+  }
+
+  override fun onDelete(personDeleted: PersonDeleted) {
+    throw NotImplementedError("Person delete events are not currently published for prison records")
+  }
+
+  private fun publishPersonDomainEvent(
+    personEntity: PersonEntity,
+    eventType: String,
+  ) {
+    val noms = personEntity.extractSourceSystemId()!!
+    val sourceSystemId = personEntity.extractSourceSystemId()
+    val detailUrl = "$baseUrl/person/prison/$sourceSystemId"
+
+    domainEventPublisher.publish(
+      DomainEvent(
+        eventType = eventType,
+        description = "A prison person record has been created",
+        detailUrl = detailUrl,
+        occurredAt = Instant.now().asStringWithUkZone(),
+        personReference = PersonReference(
+          identifiers = listOf(
+            PersonIdentifier("NOMS", noms),
+          )
+        )
+      ),
+    )
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/publishers/ProbationPersonEventPublisher.kt
@@ -0,0 +1,62 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.publishers
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.DomainEvent
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonIdentifier
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonReference
+import uk.gov.justice.digital.hmpps.personrecord.extensions.asStringWithUkZone
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonCreated
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonDeleted
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonUpdated
+import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventPublisher
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_PERSON_CREATED
+import java.time.Instant
+
+@Component
+class ProbationPersonEventPublisher(
+  private val domainEventPublisher: DomainEventPublisher,
+  @Value($$"${core-person-record.base-url}") private val baseUrl: String,
+): PersonEventPublisher {
+  override val sourceSystemType = SourceSystemType.DELIUS
+
+  override fun onCreate(personCreated: PersonCreated) {
+    publishPersonDomainEvent(
+      personCreated.personEntity,
+      CPR_PROBATION_PERSON_CREATED
+    )
+  }
+
+  override fun onUpdate(personUpdated: PersonUpdated) {
+    throw NotImplementedError("Person update events are not currently published for probation records")
+  }
+
+  override fun onDelete(personDeleted: PersonDeleted) {
+    throw NotImplementedError("Person delete events are not currently published for probation records")
+  }
+
+  private fun publishPersonDomainEvent(
+    personEntity: PersonEntity,
+    eventType: String,
+  ) {
+    val crn = personEntity.extractSourceSystemId()!!
+    val sourceSystemId = personEntity.extractSourceSystemId()
+    val detailUrl = "$baseUrl/person/probation/$sourceSystemId"
+
+    domainEventPublisher.publish(
+      DomainEvent(
+        eventType = eventType,
+        description = "A probation person record has been created",
+        detailUrl = detailUrl,
+        occurredAt = Instant.now().asStringWithUkZone(),
+        personReference = PersonReference(
+          identifiers = listOf(
+            PersonIdentifier("CRN", crn),
+          )
+        )
+      ),
+    )
+  }
+}
