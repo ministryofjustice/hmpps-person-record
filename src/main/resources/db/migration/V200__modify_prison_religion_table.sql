12df5ff4d16cbc981c86c70dae7b6e3c2932f2d8
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-06-23 15:47:10 +0100

    SDIT-3925: 🗃️ Change not null fields and add indices (#2146)
    * SDIT-3925: 🗃️ Change not null fields and add indices
    
    * SDIT-3925 remove truncate statement from flyway script
    
    ---------
    
    Co-authored-by: Mark Rees <mark.rees@digital.justice.gov.uk>
    Co-authored-by: Luke Bevan <6418390+lbevan@users.noreply.github.com>

---
+++ src/main/resources/db/migration/V200__modify_prison_religion_table.sql
@@ -0,0 +1,16 @@
+BEGIN;
+-------------------------------------------------------
+
+ALTER TABLE IF EXISTS personrecordservice.prison_religion
+    ALTER COLUMN religion_code SET NOT NULL,
+    ALTER COLUMN start_date SET NOT NULL,
+    ALTER COLUMN change_reason_known SET NOT NULL;
+
+CREATE INDEX idx_prison_religion_prison_number
+    ON personrecordservice.prison_religion (prison_number);
+
+CREATE UNIQUE INDEX unq_idx_prison_religion_update_id
+    ON personrecordservice.prison_religion (update_id);
+
+-----------------------------------------------------
+COMMIT;
