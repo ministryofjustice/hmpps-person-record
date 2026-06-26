9749a9d5b6cd653b4ff527e38945914fd6f08142
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-06-26 09:35:28 +0100

    CPR-1232 remove default implementation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/updater/PersonUpdater.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jpa/entity/updater/PersonUpdater.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.personrecord.jpa.entity.updater
 
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import java.time.LocalDateTime
 import kotlin.reflect.KClass
 
 interface PersonUpdater {
@@ -10,24 +9,5 @@ interface PersonUpdater {
     person: Person,
     childrenToIgnore: Set<KClass<*>>,
     personEntity: PersonEntity,
-  ) {
-    personEntity.defendantId = person.defendantId
-    personEntity.crn = person.crn
-    personEntity.prisonNumber = person.prisonNumber
-    personEntity.masterDefendantId = person.masterDefendantId
-    personEntity.religion = person.religion
-    personEntity.cId = person.cId
-    personEntity.sexualOrientation = person.sexualOrientation
-    personEntity.lastModified = LocalDateTime.now()
-    personEntity.dateOfDeath = person.dateOfDeath
-    personEntity.ethnicityCode = person.ethnicityCode
-    personEntity.genderIdentity = person.genderIdentity
-    personEntity.selfDescribedGenderIdentity = person.selfDescribedGenderIdentity
-    personEntity.disability = person.disability
-    personEntity.immigrationStatus = person.immigrationStatus
-    personEntity.birthplace = person.birthplace
-    personEntity.birthCountryCode = person.birthCountryCode
-    personEntity.nationalityNotes = person.nationalityNotes
-    personEntity.updateChildEntities(person, childrenToIgnore)
-  }
+  )
 }
