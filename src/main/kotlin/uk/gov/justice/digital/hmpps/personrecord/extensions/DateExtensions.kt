75f0fa18bc327d47fa04af624567ad4d047bde57
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2026-07-15 16:32:41 +0100

    CPR-1234 fix ZonedDateTime conversion (#2211)
    * CPR-1234 failing test
    
    * CPR-1234 use UK timezone when converting from ZonedDateTime to LocalDateTime
    
    * CPR-1234 rename
    
    * CPR-1234 simplify address date setup for probation
    
    * CPR-1234 simplify test response setup for ZonedDateTime
    
    * CPR-1234 remove unused function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/extensions/DateExtensions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/extensions/DateExtensions.kt
@@ -4,11 +4,13 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.format.DateTimeFormatter
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
-val UK_ZONE: ZoneId = ZoneId.of("Europe/London")
+private val UK_ZONE: ZoneId = ZoneId.of("Europe/London")
 
-// Instant extensions
-fun Instant.asStringWithUkZone(): String = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(UK_ZONE).format(this)
+fun Instant.asStringWithUkZone(): String = ISO_OFFSET_DATE_TIME.withZone(UK_ZONE).format(this)
 
 fun LocalDate.toLocalDateTime(): LocalDateTime = this.atStartOfDay()
+fun LocalDateTime.toZonedDateTimeUk(): ZonedDateTime = this.atZone(UK_ZONE)
+fun ZonedDateTime.toLocalDateTimeUk(): LocalDateTime = this.withZoneSameInstant(UK_ZONE).toLocalDateTime()
