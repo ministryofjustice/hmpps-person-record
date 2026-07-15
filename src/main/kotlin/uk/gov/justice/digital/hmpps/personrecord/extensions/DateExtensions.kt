62684630b009d40f4fac1ea071d65c68c0318a97
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-07-15 16:24:57 +0100

    CPR-1234 remove unused function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/extensions/DateExtensions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/extensions/DateExtensions.kt
@@ -11,8 +11,6 @@ private val UK_ZONE: ZoneId = ZoneId.of("Europe/London")
 
 fun Instant.asStringWithUkZone(): String = ISO_OFFSET_DATE_TIME.withZone(UK_ZONE).format(this)
 
-fun LocalDateTime.asStringWithUkZone(): String = ISO_OFFSET_DATE_TIME.withZone(UK_ZONE).format(this.atZone(UK_ZONE))
-
 fun LocalDate.toLocalDateTime(): LocalDateTime = this.atStartOfDay()
 fun LocalDateTime.toZonedDateTimeUk(): ZonedDateTime = this.atZone(UK_ZONE)
 fun ZonedDateTime.toLocalDateTimeUk(): LocalDateTime = this.withZoneSameInstant(UK_ZONE).toLocalDateTime()
