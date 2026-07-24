492d3c6ec08270215a555620bec99c3c8052ebee
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-07-24 14:01:37 +0100

    Revert "CPR-1236 Handle missing Delius addresses in address usage migration (#2233)"
    This reverts commit 38dd6d2aa898d980e5c8e537c763c967084a57d8.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/RetryableAddressMigrator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/admin/RetryableAddressMigrator.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.controller.admin
 
 import jakarta.persistence.OptimisticLockException
-import org.slf4j.LoggerFactory
 import org.springframework.dao.CannotAcquireLockException
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
@@ -11,7 +10,6 @@ import uk.gov.justice.digital.hmpps.personrecord.client.CorePersonRecordAndDeliu
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.AddressRepository
 import uk.gov.justice.digital.hmpps.personrecord.service.DomainEventSource.DELIUS
 import uk.gov.justice.digital.hmpps.personrecord.service.address.AddressService
-import uk.gov.justice.digital.hmpps.personrecord.service.queue.DiscardableNotFoundException
 
 @Service
 class RetryableAddressMigrator(
@@ -29,20 +27,11 @@ class RetryableAddressMigrator(
     ],
   )
   fun migrateUsage(deliusAddressId: Long) {
-    try {
-      corePersonRecordAndDeliusClient.getAddressIgnoreNotFound(deliusAddressId)?.let {
-        addressService.processAddress(
-          address = it,
-          findAddress = { addressRepository.findByDeliusAddressId(deliusAddressId) },
-          eventSource = DELIUS,
-        )
-      }
-    } catch (_: DiscardableNotFoundException) {
-      log.info("Address with deliusAddressId $deliusAddressId not found in Delius, discarding")
-    }
-  }
-
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
+    val deliusAddress = corePersonRecordAndDeliusClient.getAddress(deliusAddressId)
+    addressService.processAddress(
+      address = deliusAddress!!,
+      findAddress = { addressRepository.findByDeliusAddressId(deliusAddressId) },
+      eventSource = DELIUS,
+    )
   }
 }
