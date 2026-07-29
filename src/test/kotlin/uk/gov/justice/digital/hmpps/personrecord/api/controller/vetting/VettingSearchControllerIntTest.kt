ee245ea74b77e26e98eb9a77dce817e40cfdd491
Josh Parker <joshua.parker2@justice.gov.uk>
2026-07-29 09:50:51 +0100

    CPR-1216 auth tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/vetting/VettingSearchControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/vetting/VettingSearchControllerIntTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.controller.vetting
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
 import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.API_READ_ONLY
@@ -9,41 +10,81 @@ import uk.gov.justice.digital.hmpps.personrecord.api.model.vetting.VettingSearch
 import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchScore
 import uk.gov.justice.digital.hmpps.personrecord.config.WebTestBase
 import uk.gov.justice.digital.hmpps.personrecord.model.types.NameType
+import uk.gov.justice.digital.hmpps.personrecord.test.randomDate
+import uk.gov.justice.digital.hmpps.personrecord.test.randomLowerCaseString
 
 class VettingSearchControllerIntTest : WebTestBase() {
 
-  @Test
-  fun `should return strongest matching person`() {
-    val cluster = createPersonKey()
-      .addPerson(createRandomPrisonPersonDetails())
-      .addPerson(createRandomPrisonPersonDetails())
-    val strongestMatchPerson = cluster.personEntities.first()
-    val primaryPseudonym = strongestMatchPerson.pseudonyms.first { it.nameType == NameType.PRIMARY }
+  @Nested
+  inner class Success {
 
-    val personMatchScoreResponse = PersonMatchScore(
-      candidateMatchId = strongestMatchPerson.matchId.toString(),
-      candidateMatchProbability = 0.9999F,
-      candidateMatchWeight = JOIN_THRESHOLD + 1,
-      candidateShouldJoin = true,
-      candidateShouldFracture = false,
-    )
+    @Test
+    fun `should return strongest matching person`() {
+      val cluster = createPersonKey()
+        .addPerson(createRandomPrisonPersonDetails())
+        .addPerson(createRandomPrisonPersonDetails())
+      val strongestMatchPerson = cluster.personEntities.first()
+      val primaryPseudonym = strongestMatchPerson.pseudonyms.first { it.nameType == NameType.PRIMARY }
 
-    authSetup()
-    stubPostRequest(
-      url = "/person/search",
-      responseBody = jsonMapper.writeValueAsString(listOf(personMatchScoreResponse)),
-    )
+      val personMatchScoreResponse = PersonMatchScore(
+        candidateMatchId = strongestMatchPerson.matchId.toString(),
+        candidateMatchProbability = 0.9999F,
+        candidateMatchWeight = JOIN_THRESHOLD + 1,
+        candidateShouldJoin = true,
+        candidateShouldFracture = false,
+      )
 
-    val responseBody = sendPostRequestAsserted<VettingSearchResponse>(
-      url = "/person/search",
-      roles = listOf(API_READ_ONLY),
-      expectedStatus = HttpStatus.OK,
-      body = VettingSearchRequest(
-        fullName = """${primaryPseudonym.firstName} ${primaryPseudonym.middleNames} ${primaryPseudonym.lastName}""",
-        dateOfBirth = primaryPseudonym.dateOfBirth!!,
-      ),
-    ).returnResult().responseBody!!
+      authSetup()
+      stubPostRequest(
+        url = "/person/search",
+        responseBody = jsonMapper.writeValueAsString(listOf(personMatchScoreResponse)),
+      )
 
-    assertThat(responseBody.name.firstName).isEqualTo(primaryPseudonym.firstName)
+      val responseBody = sendPostRequestAsserted<VettingSearchResponse>(
+        url = "/person/search",
+        roles = listOf(API_READ_ONLY),
+        expectedStatus = HttpStatus.OK,
+        body = VettingSearchRequest(
+          fullName = """${primaryPseudonym.firstName} ${primaryPseudonym.middleNames} ${primaryPseudonym.lastName}""",
+          dateOfBirth = primaryPseudonym.dateOfBirth!!,
+        ),
+      ).returnResult().responseBody!!
+
+      assertThat(responseBody.name.firstName).isEqualTo(primaryPseudonym.firstName)
+      assertThat(responseBody.name.middleNames).isEqualTo(primaryPseudonym.middleNames)
+      assertThat(responseBody.name.lastName).isEqualTo(primaryPseudonym.lastName)
+      assertThat(responseBody.name.dateOfBirth).isEqualTo(primaryPseudonym.dateOfBirth)
+    }
+  }
+
+  @Nested
+  inner class Auth {
+
+    @Test
+    fun `should return UNAUTHORIZED 401 when role is not set`() {
+      sendPostRequestAsserted<Unit>(
+        url = "/person/search",
+        body = VettingSearchRequest(
+          fullName = """${randomLowerCaseString()} ${randomLowerCaseString()} ${randomLowerCaseString()}""",
+          dateOfBirth = randomDate(),
+        ),
+        roles = listOf(),
+        expectedStatus = HttpStatus.UNAUTHORIZED,
+        sendAuthorised = false,
+      )
+    }
+
+    @Test
+    fun `should return Access Denied 403 when role is wrong`() {
+      sendPostRequestAsserted<Unit>(
+        url = "/person/search",
+        body = VettingSearchRequest(
+          fullName = """${randomLowerCaseString()} ${randomLowerCaseString()} ${randomLowerCaseString()}""",
+          dateOfBirth = randomDate(),
+        ),
+        roles = listOf("UNSUPPORTED_ROLE"),
+        expectedStatus = HttpStatus.FORBIDDEN,
+      )
+    }
   }
 }
