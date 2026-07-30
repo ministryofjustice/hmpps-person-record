f82129f005d00093e386841b9b5734b0a5170ab0
Josh Parker <joshua.parker2@justice.gov.uk>
2026-07-30 15:20:57 +0100

    CPR-1216 refactored handler logic

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/vetting/VettingSearchHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/vetting/VettingSearchHandler.kt
@@ -7,11 +7,13 @@ import uk.gov.justice.digital.hmpps.personrecord.api.model.vetting.VettingSearch
 import uk.gov.justice.digital.hmpps.personrecord.api.model.vetting.VettingSearchRequest
 import uk.gov.justice.digital.hmpps.personrecord.api.model.vetting.VettingSearchResponse
 import uk.gov.justice.digital.hmpps.personrecord.client.PersonMatchClient
+import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchScore
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Alias
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Reference
+import java.util.SortedSet
 import java.util.UUID
 
 @Component
@@ -21,26 +23,35 @@ class VettingSearchHandler(
 ) {
 
   fun search(vettingSearchRequest: VettingSearchRequest): VettingSearchResponse? {
-    val personMatchScoresSorted = personMatchClient.vettingSearch(vettingSearchRequest).sortedByDescending { it.candidateMatchProbability }
+    val personMatchScoresSorted = getPersonMatchScoresSortedByMatchProbabilityDescending(vettingSearchRequest)
     if (personMatchScoresSorted.isEmpty()) return null
 
-    val searchDataOrderedByMatchProbability = mutableListOf<VettingSearchData>()
-    val completedClustersByClusterId = mutableMapOf<Long, Boolean>()
-    personMatchScoresSorted.forEach { personMatchScore ->
-      val personEntity = personRepository.findByMatchId(UUID.fromString(personMatchScore.candidateMatchId))!!
-      val clusterId = personEntity.personKey!!.id!!
-      if (completedClustersByClusterId.contains(clusterId)) return@forEach
+    val strongestPersonsAcrossUniqueClusters = findStrongestPersonsAcrossUniqueClusters(personMatchScoresSorted)
+    return constructSearchResult(strongestPersonsAcrossUniqueClusters)
+  }
 
+  private fun getPersonMatchScoresSortedByMatchProbabilityDescending(vettingSearchRequest: VettingSearchRequest): SortedSet<PersonMatchScore> = personMatchClient.vettingSearch(vettingSearchRequest)
+    .toSortedSet { match1, match2 -> match2.candidateMatchProbability.compareTo(match1.candidateMatchProbability) }
+
+  private fun findStrongestPersonsAcrossUniqueClusters(personMatchScoresSorted: SortedSet<PersonMatchScore>): Set<PersonEntity> {
+    val strongestPersonsInClusters = mutableMapOf<Long, PersonEntity>()
+    personMatchScoresSorted.forEach {
+      val personEntity = personRepository.findByMatchId(UUID.fromString(it.candidateMatchId))!!
+      if (strongestPersonsInClusters.contains(personEntity.personKey!!.id!!)) return@forEach
+      strongestPersonsInClusters[personEntity.personKey!!.id!!] = personEntity
+    }
+    return strongestPersonsInClusters.values.toSet()
+  }
+
+  private fun constructSearchResult(personEntities: Set<PersonEntity>): VettingSearchResponse {
+    val searchDataOrderedByMatchProbability = personEntities.map { personEntity ->
       val rootPersonData = toVettingSearchData(personEntity)
-
       val childPersonData = personEntity.personKey!!.personEntities
         .filter { it != personEntity }
         .map { toVettingSearchData(it) }
       rootPersonData.linkedRecords = childPersonData
-      searchDataOrderedByMatchProbability.add(rootPersonData)
-      completedClustersByClusterId[clusterId] = true
+      rootPersonData
     }
-
     return VettingSearchResponse(searchDataOrderedByMatchProbability)
   }
 
