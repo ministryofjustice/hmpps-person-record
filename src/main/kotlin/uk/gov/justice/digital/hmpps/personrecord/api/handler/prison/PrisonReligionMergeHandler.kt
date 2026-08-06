57b2a03e113e56c1cfe8c0a2e89d84c8eaeb462a
Richard Popple <2195350+richardpopple@users.noreply.github.com>
2026-08-06 11:10:14 +0100

    SDIT-4058 wip for the merging of religions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/prison/PrisonReligionMergeHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/prison/PrisonReligionMergeHandler.kt
@@ -2,14 +2,10 @@ package uk.gov.justice.digital.hmpps.personrecord.api.handler.prison
 
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import uk.gov.justice.digital.hmpps.personrecord.api.controller.exceptions.ResourceNotFoundException
-import uk.gov.justice.digital.hmpps.personrecord.api.model.prison.PrisonReligionMapping
-import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.historic.PrisonReligionHistory
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.prison.PrisonReligionEntity
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.prison.PrisonReligionRepository
-import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.types.PrisonRecordType
 import uk.gov.justice.digital.hmpps.personrecord.service.person.PersonService
 
