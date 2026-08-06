2d2a1bb93feef6afa2dd3441958f37b500a69f22
Richard Popple <2195350+richardpopple@users.noreply.github.com>
2026-08-06 11:09:46 +0100

    SDIT-4058 wip for the merging of religions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonMergeEventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/message/processors/prison/PrisonMergeEventProcessor.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.personrecord.message.processors.prison
 
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.personrecord.api.handler.prison.PrisonReligionMergeHandler
 import uk.gov.justice.digital.hmpps.personrecord.client.PrisonerSearchClient
 import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PrisonPersonMerged
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
@@ -15,6 +16,7 @@ class PrisonMergeEventProcessor(
   private val mergeService: MergeService,
   private val prisonerSearchClient: PrisonerSearchClient,
   private val personService: PersonService,
+  private val prisonReligionMergeHandler: PrisonReligionMergeHandler
 ) {
 
   @Transactional
