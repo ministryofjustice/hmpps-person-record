d2d3917eb204506a7172a54fc238adf28d81f191
jldparker527 <joshua.parker2@justice.gov.uk>
2026-08-06 14:14:37 +0100

    CPR-1216 Vetting search endpoint (#2245)
    * CPR-1216 added base controller
    
    * CPR-1216 added test
    
    * CPR-1216 auth tests
    
    * CPR-1216 returns linked records in response
    
    * CPR-1216 added assertions
    
    * CPR-1216 changed swagger tag name
    
    * CPR-1216 changed identifiers key in the response to use Reference
    
    * CPR-1216 resolved swagger not displaying linkedRecords correctly
    
    * CPR-1216 resolved swagger not displaying status correctly
    
    * CPR-1216 find the strongest match from person match response by candidateMatchProbability
    
    * CPR-1216 changed the way we return search data
    
    * CPR-1216 refactored handler logic
    
    * CPR-1216 updated tests
    
    * CPR-1216 type change from SortedSet to List
    
    * CPR-1216 returns empty list in response if no matches found
    
    * CPR-1216 update to sort by match weight
    
    * CPR-1216 clean up
    
    * CPR-1216 added wrapper classes for search response
    
    * CPR-1216 added new role
    
    * CPR-1216 name changes
    
    * CPR-1216 class name changes
    
    * CPR-1216 update search response to use canonical objects
    
    * CPR-1216 tidy up schema example
    
    * CPR-1216
    
    * CPR-1216 update api description
    
    * CPR-1216 updated api description
    
    * CPR-1216 change type name

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/controller/search/PersonSearchController.kt
@@ -0,0 +1,47 @@
+package uk.gov.justice.digital.hmpps.personrecord.api.controller.search
+
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.API_SEARCH_ONLY
+import uk.gov.justice.digital.hmpps.personrecord.api.handler.search.PersonSearchHandler
+import uk.gov.justice.digital.hmpps.personrecord.api.model.search.PersonSearchRequest
+import uk.gov.justice.digital.hmpps.personrecord.api.model.search.PersonSearchResponse
+
+@Tag(name = "Search")
+@RestController
+@Profile("!preprod & !prod")
+class PersonSearchController(
+  private val personSearchHandler: PersonSearchHandler,
+) {
+
+  @ApiResponses(
+    ApiResponse(
+      responseCode = "200",
+      description = "The objects in the top level array will be ordered by the strongest match descending (first record strongest, last record weakest).",
+      content = [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = PersonSearchResponse::class),
+        ),
+      ],
+    ),
+  )
+  @PreAuthorize("hasRole('$API_SEARCH_ONLY')")
+  @PostMapping("/person/search")
+  fun personSearch(
+    @RequestBody personSearchRequest: PersonSearchRequest,
+  ): ResponseEntity<PersonSearchResponse> {
+    val result = personSearchHandler.search(personSearchRequest)
+    return ResponseEntity(result, HttpStatus.OK)
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/handler/search/PersonSearchHandler.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.personrecord.api.handler.search
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.api.model.search.PersonSearchRequest
+import uk.gov.justice.digital.hmpps.personrecord.api.model.search.PersonSearchResponse
+import uk.gov.justice.digital.hmpps.personrecord.api.model.search.SearchData
+import uk.gov.justice.digital.hmpps.personrecord.client.PersonMatchClient
+import uk.gov.justice.digital.hmpps.personrecord.client.model.match.PersonMatchScore
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.repository.PersonRepository
+import java.util.UUID
+
+@Component
+class PersonSearchHandler(
+  private val personRepository: PersonRepository,
+  private val personMatchClient: PersonMatchClient,
+) {
+
+  fun search(personSearchRequest: PersonSearchRequest): PersonSearchResponse {
+    val personMatchScoresSortedDescending = getPersonMatchScoresSortedByMatchWeightDescending(personSearchRequest)
+    val strongestPersonsAcrossUniqueClusters = findStrongestPersonsAcrossUniqueClusters(personMatchScoresSortedDescending)
+    return buildSearchResult(strongestPersonsAcrossUniqueClusters)
+  }
+
+  private fun getPersonMatchScoresSortedByMatchWeightDescending(personSearchRequest: PersonSearchRequest) = personMatchClient.search(personSearchRequest)
+    .sortedByDescending { it.candidateMatchWeight }
+
+  private fun findStrongestPersonsAcrossUniqueClusters(personMatchScoresSortedDescending: List<PersonMatchScore>) = personMatchScoresSortedDescending
+    .map { personRepository.findByMatchId(UUID.fromString(it.candidateMatchId))!! }
+    .distinctBy { it.personKey!!.id!! }
+
+  private fun buildSearchResult(personEntities: List<PersonEntity>): PersonSearchResponse {
+    val searchDataOrderedByMatchProbability = personEntities.map { personEntity ->
+      val rootPersonData = SearchData.from(personEntity)
+      val childPersonData = personEntity.personKey!!.personEntities
+        .filter { it != personEntity }
+        .map { SearchData.from(it) }
+      rootPersonData.linkedRecords = childPersonData
+      rootPersonData
+    }
+    return PersonSearchResponse(searchDataOrderedByMatchProbability)
+  }
+}
