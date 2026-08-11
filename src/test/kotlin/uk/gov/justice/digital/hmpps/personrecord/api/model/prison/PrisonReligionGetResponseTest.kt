d11183babf9509622eed272f92ba93517f6fbc5e
Richard Popple <2195350+richardpopple@users.noreply.github.com>
2026-08-11 15:45:22 +0100

    SDIT-4058 adding the updateId to the ReligionHistory

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/prison/PrisonReligionGetResponseTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/prison/PrisonReligionGetResponseTest.kt
@@ -49,6 +49,7 @@ class PrisonReligionGetResponseTest {
         current = prisonReligionEntity.prisonRecordType.value,
         createDateTime = prisonReligionEntity.createDateTime,
         createUserId = prisonReligionEntity.createUserId,
+        updatedId = prisonReligionEntity.updateId.toString()
       ),
     )
     assertThat(actual).usingRecursiveComparison().isEqualTo(expected)
