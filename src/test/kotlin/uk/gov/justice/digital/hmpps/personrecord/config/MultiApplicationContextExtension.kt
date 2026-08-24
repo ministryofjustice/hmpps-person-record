c8c9e21440460333adccabc69c7ad1356942bb44
Luke Bevan <Luke.Bevan@justice.gov.uk>
2026-08-24 09:55:25 +0100

    Fix MultiApplicationContextExtension context clean-up to prevent test bleed

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/MultiApplicationContextExtension.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/MultiApplicationContextExtension.kt
@@ -1,34 +1,27 @@
 package uk.gov.justice.digital.hmpps.personrecord.config
 
+import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import uk.gov.justice.digital.hmpps.personrecord.HmppsPersonRecord
 
-class MultiApplicationContextExtension :
-  BeforeAllCallback,
-  AutoCloseable {
+class MultiApplicationContextExtension : BeforeAllCallback, AfterAllCallback {
 
-  private val instance1: SpringApplicationBuilder = SpringApplicationBuilder(HmppsPersonRecord::class.java)
-    .profiles("test", "test-instance-1")
+  private var instance1: ConfigurableApplicationContext? = null
 
   override fun beforeAll(context: ExtensionContext) {
-    if (!started) {
-      started = true
-      instance1.run()
+    val existingContext = SpringExtension.getApplicationContext(context)
 
-      context.root.getStore(ExtensionContext.Namespace.GLOBAL).put(
-        "test-instance-extension",
-        this,
-      )
-    }
+    instance1 = SpringApplicationBuilder(HmppsPersonRecord::class.java)
+      .profiles(*existingContext.environment.activeProfiles, "test-instance-1")
+      .run()
   }
 
-  override fun close() {
-    instance1.context()?.close()
-  }
-
-  companion object {
-    private var started = false
+  override fun afterAll(context: ExtensionContext) {
+    instance1?.close()
+    instance1 = null
   }
 }
