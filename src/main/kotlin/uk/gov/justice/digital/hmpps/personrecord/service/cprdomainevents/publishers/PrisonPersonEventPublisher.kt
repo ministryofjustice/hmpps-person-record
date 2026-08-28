24d5ec8f231a9fbd33cd2fd081cfd5da89d53a8a
Josh Parker <joshua.parker2@justice.gov.uk>
2026-08-28 12:00:39 +0100

    removed the need for each person publisher to make it's own implementation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/publishers/PrisonPersonEventPublisher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/publishers/PrisonPersonEventPublisher.kt
@@ -3,45 +3,19 @@ package uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.publis
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.CprPersonCreated
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonIdentifier
-import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonReference
-import uk.gov.justice.digital.hmpps.personrecord.extensions.asStringWithUkZone
-import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
-import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonCreated
 import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventPublisher
 import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PRISON_PERSON_CREATED
-import java.time.Instant
 
 @Profile("!preprod & !prod")
 @Component
 class PrisonPersonEventPublisher(
-  private val domainEventPublisher: DomainEventPublisher,
+  domainEventPublisher: DomainEventPublisher,
   @Value($$"${core-person-record.base-url}") private val baseUrl: String,
-) : PersonEventPublisher {
+) : PersonEventPublisher(domainEventPublisher, baseUrl) {
   override val sourceSystemType = SourceSystemType.NOMIS
-
-  override fun onCreate(personCreated: PersonCreated) {
-    publishPersonDomainEvent(personCreated.personEntity)
-  }
-
-  private fun publishPersonDomainEvent(personEntity: PersonEntity) {
-    val prisonNumber = personEntity.extractSourceSystemId()!!
-    val detailUrl = "$baseUrl/person/prison/$prisonNumber"
-
-    domainEventPublisher.publish(
-      CprPersonCreated(
-        eventType = CPR_PRISON_PERSON_CREATED,
-        description = "A prison person record has been created",
-        detailUrl = detailUrl,
-        occurredAt = Instant.now().asStringWithUkZone(),
-        personReference = PersonReference(
-          identifiers = listOf(
-            PersonIdentifier("prisonNumber", prisonNumber),
-          ),
-        ),
-      ),
-    )
-  }
+  override val createEventType = CPR_PRISON_PERSON_CREATED
+  override val path = "/person/prison/"
+  override val description = "A prison person record has been created"
+  override val sourceSystemIdField = "prisonNumber"
 }
