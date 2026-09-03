8a50b653380c115f6b425f150a204c5bb3fffd6c
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2026-09-03 11:38:13 +0100

    FIX: run s3 bucket setup before check (#2345)

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,12 @@ tasks.register<Test>("e2eTest") {
   onlyIf { gradle.startParameter.taskNames.contains("e2eTest") }
 }
 
+tasks.register<Exec>("setUpS3Bucket") {
+  description = "creates S3 buckets in localstack for testing"
+  executable = "sh"
+  args = listOf("-c", "./src/test/resources/localstack/setup-aws.sh")
+}
+
 tasks {
   test {
     exclude("**/InitialiseDatabase.class")
@@ -75,7 +81,7 @@ tasks {
   }
 
   getByName("check") {
-    dependsOn(":ktlintCheck")
+    dependsOn(":ktlintCheck", "setUpS3Bucket")
   }
 
   getByName("koverHtmlReport") {
