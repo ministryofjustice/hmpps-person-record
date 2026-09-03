7159b5adc1731012aa9b8ffebe1cbea69a2c7565
Josh Parker <joshua.parker2@justice.gov.uk>
2026-09-03 12:07:47 +0100

    split out publisher interface into action based

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/cprdomainevents/publishers/person/ProbationPersonCreatedEventPublisher.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.publishers.person
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.CprPersonCreated
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonIdentifier
+import uk.gov.justice.digital.hmpps.personrecord.client.model.sqs.messages.domainevent.PersonReference
+import uk.gov.justice.digital.hmpps.personrecord.extensions.asStringWithUkZone
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.types.SourceSystemType
+import uk.gov.justice.digital.hmpps.personrecord.service.cprdomainevents.events.person.PersonCreated
+import uk.gov.justice.digital.hmpps.personrecord.service.queue.DomainEventPublisher
+import uk.gov.justice.digital.hmpps.personrecord.service.type.CPR_PROBATION_PERSON_CREATED
+import java.time.Instant
+
+@Component
+class ProbationPersonCreatedEventPublisher(
+  private val domainEventPublisher: DomainEventPublisher,
+  @Value($$"${core-person-record.base-url}") private val baseUrl: String,
+) : PersonCreatedEventPublisher {
+  override val sourceSystemType = SourceSystemType.DELIUS
+
+  override fun onCreate(personCreated: PersonCreated) {
+    publishPersonDomainEvent(personCreated.personEntity)
+  }
+
+  private fun publishPersonDomainEvent(personEntity: PersonEntity) {
+    val crn = personEntity.extractSourceSystemId()!!
+
+    domainEventPublisher.publish(
+      CprPersonCreated(
+        eventType = CPR_PROBATION_PERSON_CREATED,
+        description = "A probation person record has been created",
+        detailUrl = "$baseUrl/person/probation/$crn",
+        occurredAt = Instant.now().asStringWithUkZone(),
+        personReference = PersonReference(
+          identifiers = listOf(
+            PersonIdentifier("CRN", crn),
+          ),
+        ),
+      ),
+    )
+  }
+}
