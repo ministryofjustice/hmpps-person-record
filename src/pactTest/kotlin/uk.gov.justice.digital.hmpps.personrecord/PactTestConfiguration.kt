e0c659552364e828b197eec03d16c5508a81ca73
David Sharrocks <david.sharrocks@justice.gov.uk>
2026-09-04 12:11:40 +0100

    Fix pactTest ktlint violations

--- src/pactTest/kotlin/uk.gov.justice.digital.hmpps.personrecord/PactTestConfiguration.kt
+++ src/pactTest/kotlin/uk.gov.justice.digital.hmpps.personrecord/PactTestConfiguration.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.hmpps.personrecord
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Import
 import uk.gov.justice.digital.hmpps.personrecord.api.controller.probation.ProbationAPIController
 import uk.gov.justice.digital.hmpps.personrecord.api.controller.probation.ProbationAddressCreateAPIController
@@ -21,6 +21,6 @@ import uk.gov.justice.digital.hmpps.personrecord.config.SecurityConfiguration
     "org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration",
     "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration",
     "uk.gov.justice.hmpps.sqs.HmppsSqsConfiguration",
-  ]
+  ],
 )
 class PactTestConfiguration
