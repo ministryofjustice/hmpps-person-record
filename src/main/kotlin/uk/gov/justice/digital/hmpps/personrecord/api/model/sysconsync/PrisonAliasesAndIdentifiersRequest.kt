88ab9e836766b013e05c99b1a98e3d8ed9b671b5
Richard Popple <2195350+richardpopple@users.noreply.github.com>
2026-09-07 11:14:16 +0100

    SDIT-4164 addressing PR comments

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/PrisonAliasesAndIdentifiersRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/api/model/sysconsync/PrisonAliasesAndIdentifiersRequest.kt
@@ -1,11 +1,6 @@
 package uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync
 
-import jakarta.validation.Valid
-import jakarta.validation.constraints.NotEmpty
-
 data class PrisonAliasesAndIdentifiersRequest(
-  @Valid
-  @NotEmpty
   val aliases: List<PrisonAlias>,
   val identifiers: List<PrisonIdentifier>,
 )
