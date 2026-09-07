ecdf7ad3b461e58cc2ec1ecee27c1adcb8e2b267
Josh Parker <joshua.parker2@justice.gov.uk>
2026-09-07 14:47:48 +0100

    CPR-1123 made batch job class generic

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/BatchJobManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/jobs/BatchJobManager.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.personrecord.jobs
 import io.awspring.cloud.sqs.annotation.SqsListener
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.beans.factory.support.DefaultListableBeanFactory
@@ -12,20 +13,42 @@ import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
+enum class BatchType {
+  SPLIT_CLUSTERS,
+}
+
 @Component
 @ConditionalOnProperty(name = ["batch.enabled"], havingValue = "true")
-class SplitClustersJob : BeanFactoryPostProcessor {
+class BatchJobManager(
+  @param:Value("\${batch.type}") private val batchType: BatchType,
+) {
 
   @EventListener
   fun onApplicationEvent(event: ContextRefreshedEvent) {
-    splitClusters()
+    runJob()
     event.closeApplication()
   }
 
-  fun splitClusters() = runBlocking {
-    LOG.info("Started running splitting of clusters")
+  fun runJob() = runBlocking {
+    LOG.info("Running batch job type {}", batchType)
+    when (batchType) {
+      BatchType.SPLIT_CLUSTERS -> { }
+    }
+    LOG.info("Finished batch job type {}", batchType)
+  }
 
-    LOG.info("Finished running splitting of clusters")
+  private fun ContextRefreshedEvent.closeApplication() = (this.applicationContext as ConfigurableApplicationContext).close()
+
+  companion object {
+    private val LOG = LoggerFactory.getLogger(BatchJobManager::class.java)
+  }
+}
+
+@Component
+@ConditionalOnProperty(name = ["batch.enabled"], havingValue = "true")
+class SqsListenerSuppressor : BeanFactoryPostProcessor {
+  companion object {
+    private val LOG = LoggerFactory.getLogger(this::class.java)
   }
 
   override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
@@ -45,11 +68,5 @@ class SplitClustersJob : BeanFactoryPostProcessor {
     return beanClass
       ?.methods
       ?.any { it.isAnnotationPresent(SqsListener::class.java) } == true
-  }
-
-  private fun ContextRefreshedEvent.closeApplication() = (this.applicationContext as ConfigurableApplicationContext).close()
-
-  companion object {
-    private val LOG = LoggerFactory.getLogger(SplitClustersJob::class.java)
   }
 }
