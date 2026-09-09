caad841d04df2aa165a054b6c3982006c3d2dc52
Josh Parker <joshua.parker2@justice.gov.uk>
2026-09-09 11:54:38 +0100

    SDIT-4164 stopped MessagingMultiNodeTestBase using 2 instances

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/MessagingMultiNodeTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/config/MessagingMultiNodeTestBase.kt
@@ -1,6 +1,3 @@
 package uk.gov.justice.digital.hmpps.personrecord.config
 
-import org.junit.jupiter.api.extension.ExtendWith
-
-@ExtendWith(MultiApplicationContextExtension::class)
 abstract class MessagingMultiNodeTestBase : MessagingTestBase()
