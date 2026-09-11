073802a4315d156c629f99a67db1a3599c9225b9
David Sharrocks <david.sharrocks@justice.gov.uk>
2026-09-11 18:15:43 +0100

    CPR-1341 Addressing PR Comments.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/pacttest/ProbationProviderPactTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/pacttest/ProbationProviderPactTest.kt
@@ -7,14 +7,13 @@ import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.PROBATION_A
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
 import uk.gov.justice.digital.hmpps.personrecord.model.person.AddressUsage
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Contact
-import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
-import uk.gov.justice.digital.hmpps.personrecord.model.types.TitleCode
 import uk.gov.justice.digital.hmpps.personrecord.test.randomAddressStatusCode
 import uk.gov.justice.digital.hmpps.personrecord.test.randomAddressUsageCode
 import uk.gov.justice.digital.hmpps.personrecord.test.randomBoolean
 import uk.gov.justice.digital.hmpps.personrecord.test.randomBuildingNumber
 import uk.gov.justice.digital.hmpps.personrecord.test.randomContactType
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCountryCode
+import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
 import uk.gov.justice.digital.hmpps.personrecord.test.randomName
 import uk.gov.justice.digital.hmpps.personrecord.test.randomPhoneNumber
 import uk.gov.justice.digital.hmpps.personrecord.test.randomPostcode
@@ -29,48 +28,38 @@ class ProbationProviderPactTest : AbstractProviderPactTests() {
 
   @State("An address exists for CRN and address ID")
   fun anAddressExistsForCrnAndAddressId(): Map<String, String> {
-    val crn = REQUESTED_CRN
-    deleteAllPersonData()
-
+    val crn = randomCrn()
     val person = createProbationPersonWithAddress(crn)
-    val persistedAddress = addressRepository.findById(person.addresses.first().id!!).orElseThrow()
+    val createdAddress = addressRepository.findById(person.addresses.first().id!!).orElseThrow()
 
     return mapOf(
       "crn" to crn,
-      "cprAddressId" to persistedAddress.updateId.toString(),
+      "cprAddressId" to createdAddress.updateId.toString(),
     )
   }
 
   @State("A probation address can be created for CRN")
   fun aProbationAddressCanBeCreatedForCrn(): Map<String, String> {
-    deleteAllPersonData()
-
     stubNoMatchesPersonMatch()
     stubPersonMatchUpsert()
 
-    val person = createProbationPerson(REQUESTED_CRN)
+    val person = createProbationPerson(randomCrn())
     return mapOf("crn" to person.crn!!)
   }
 
   @State("A probation person exists for CRN")
   fun aProbationPersonExistsForCrn(): Map<String, String> {
-    deleteAllPersonData()
-
-    val person = createProbationPersonWithAddress(REQUESTED_CRN)
+    val person = createProbationPersonWithAddress(randomCrn())
     return mapOf("crn" to person.crn!!)
   }
 
   private fun createProbationPerson(crn: String) = createPersonWithNewKey(
-    buildPactPerson(crn).copy(addresses = emptyList()),
+    createRandomProbationPersonDetails(crn).copy(addresses = emptyList()),
   )
 
   private fun createProbationPersonWithAddress(crn: String) = createPersonWithNewKey(
-    buildPactPerson(crn),
+    createRandomProbationPersonDetails(crn),
     configure = addAddressToRecord(buildPactAddress()),
-  )
-
-  private fun buildPactPerson(crn: String): Person = createRandomProbationPersonDetails(crn).copy(
-    titleCode = TitleCode.MR,
   )
 
   private fun buildPactAddress(): Address = Address(
@@ -93,8 +82,4 @@ class ProbationProviderPactTest : AbstractProviderPactTests() {
     usages = listOf(AddressUsage(randomAddressUsageCode(), randomBoolean())),
     contacts = listOf(Contact(randomContactType(), randomPhoneNumber(), "+44")),
   )
-
-  private companion object {
-    const val REQUESTED_CRN = "X12345"
-  }
 }
