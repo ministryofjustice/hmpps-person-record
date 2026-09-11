698cb9b9c84a84c1aecef8f4c2b566009a9f7a62
Mark Rees <mark.rees@digital.justice.gov.uk>
2026-09-11 11:47:11 +0100

    SDIT-4164 put implementation and interface of PrisonPersonServiceProxy together

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PrisonPersonServiceProxy.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/person/PrisonPersonServiceProxy.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.personrecord.service.person
 
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PersonEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.PseudonymEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 
 interface PrisonPersonServiceProxy {
@@ -8,4 +11,26 @@ interface PrisonPersonServiceProxy {
     person: Person,
     findPerson: () -> PersonEntity?,
   ): PersonEntity
+}
+
+@Component
+@Profile("preprod | prod")
+class PrisonPersonServiceProxyProd(
+  private val personService: PersonService,
+) : PrisonPersonServiceProxy {
+  override fun processPerson(
+    person: Person,
+    findPerson: () -> PersonEntity?,
+  ): PersonEntity = personService.processPerson(person) { findPerson() }
+}
+
+@Component
+@Profile("!preprod & !prod")
+class PrisonPersonServiceProxyDev(
+  private val personService: PersonService,
+) : PrisonPersonServiceProxy {
+  override fun processPerson(
+    person: Person,
+    findPerson: () -> PersonEntity?,
+  ): PersonEntity = personService.processPerson(person, setOf(PseudonymEntity::class)) { findPerson() }
 }
