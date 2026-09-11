4fac73a32976e63c07ba52f01f4de12ceb626792
David Sharrocks <david.sharrocks@justice.gov.uk>
2026-09-11 10:59:37 +0100

    CPR-1341 Fixing formatting issues.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/pacttest/ProbationProviderPactTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/pacttest/ProbationProviderPactTest.kt
@@ -7,13 +7,9 @@ import uk.gov.justice.digital.hmpps.personrecord.api.constants.Roles.PROBATION_A
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
 import uk.gov.justice.digital.hmpps.personrecord.model.person.AddressUsage
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Contact
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.jdbc.core.JdbcTemplate
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
 import uk.gov.justice.digital.hmpps.personrecord.model.types.TitleCode
 import uk.gov.justice.digital.hmpps.personrecord.test.*
-
-@Autowired
-private lateinit var jdbcTemplate: JdbcTemplate
 
 class ProbationProviderPactTest : AbstractProviderPactTests() {
   override fun rolesFor(request: HttpRequest): List<String> = when (request.method.uppercase()) {
@@ -21,85 +17,74 @@ class ProbationProviderPactTest : AbstractProviderPactTests() {
     else -> listOf(API_READ_ONLY)
   }
 
-  // TODO: To be updated with CPR-1328 with actual state values.
   @State("An address exists for CRN and address ID")
   fun anAddressExistsForCrnAndAddressId(): Map<String, String> {
-    val person = createPersonWithNewKey(
-      createRandomProbationPersonDetails(),
-      configure = addAddressToRecord(
-        Address(
-          noFixedAbode = randomBoolean(),
-          startDate = randomZonedDateTime(),
-          endDate = randomZonedDateTime(),
-          postcode = randomPostcode(),
-          buildingName = randomName(),
-          subBuildingName = randomName(),
-          buildingNumber = randomBuildingNumber(),
-          thoroughfareName = randomName(),
-          dependentLocality = randomName(),
-          postTown = randomName(),
-          county = randomName(),
-          countryCode = randomCountryCode(),
-          uprn = randomUprn(),
-          statusCode = randomAddressStatusCode(),
-          comment = randomName(),
-          isVerified = randomBoolean(),
-          usages = listOf(AddressUsage(randomAddressUsageCode(), randomBoolean())),
-          contacts = listOf(Contact(randomContactType(), randomPhoneNumber(), "+44")),
-        ),
-      ),
-    )
+    val crn = REQUESTED_CRN
+    deleteAllPersonData()
+
+    val person = createProbationPersonWithAddress(crn)
+    val persistedAddress = addressRepository.findById(person.addresses.first().id!!).orElseThrow()
 
     return mapOf(
-      "crn" to person.crn!!,
-      "cprAddressId" to person.addresses.first().updateId.toString(),
+      "crn" to crn,
+      "cprAddressId" to persistedAddress.updateId.toString(),
     )
   }
 
-  // TODO: To be updated with CPR-1328 with actual state values.
   @State("A probation address can be created for CRN")
   fun aProbationAddressCanBeCreatedForCrn(): Map<String, String> {
+    deleteAllPersonData()
+
     stubNoMatchesPersonMatch()
     stubPersonMatchUpsert()
 
-    val person = createPersonWithNewKey(
-      createRandomProbationPersonDetails()
-    )
-    return mapOf(
-      "crn" to person.crn!!,
-    )
+    val person = createProbationPerson(REQUESTED_CRN)
+    return mapOf("crn" to person.crn!!)
   }
 
-  // TODO: To be updated with CPR-1328 with actual state values.
   @State("A probation person exists for CRN")
   fun aProbationPersonExistsForCrn(): Map<String, String> {
-    val person = createPersonWithNewKey(
-      createRandomProbationPersonDetails().copy(titleCode = TitleCode.MR),
-      configure = addAddressToRecord(
-        Address(
-          noFixedAbode = randomBoolean(),
-          startDate = randomZonedDateTime(),
-          endDate = randomZonedDateTime(),
-          postcode = randomPostcode(),
-          buildingName = randomName(),
-          subBuildingName = randomName(),
-          buildingNumber = randomBuildingNumber(),
-          thoroughfareName = randomName(),
-          dependentLocality = randomName(),
-          postTown = randomName(),
-          county = randomName(),
-          countryCode = randomCountryCode(),
-          uprn = randomUprn(),
-          statusCode = randomAddressStatusCode(),
-          comment = randomName(),
-          isVerified = randomBoolean(),
-          usages = listOf(AddressUsage(randomAddressUsageCode(), randomBoolean())),
-          contacts = listOf(Contact(randomContactType(), randomPhoneNumber(), "+44")),
-        ),
-      ),
-    )
-    return mapOf(
-      "crn" to person.crn!!,
-    )
+    deleteAllPersonData()
+
+    val person = createProbationPersonWithAddress(REQUESTED_CRN)
+    return mapOf("crn" to person.crn!!)
+  }
+
+  private fun createProbationPerson(crn: String) = createPersonWithNewKey(
+    buildPactPerson(crn).copy(addresses = emptyList()),
+  )
+
+  private fun createProbationPersonWithAddress(crn: String) = createPersonWithNewKey(
+    buildPactPerson(crn),
+    configure = addAddressToRecord(buildPactAddress()),
+  )
+
+  private fun buildPactPerson(crn: String): Person = createRandomProbationPersonDetails(crn).copy(
+    titleCode = TitleCode.MR,
+  )
+
+  private fun buildPactAddress(): Address = Address(
+    noFixedAbode = randomBoolean(),
+    startDate = randomZonedDateTime(),
+    endDate = randomZonedDateTime(),
+    postcode = randomPostcode(),
+    buildingName = randomName(),
+    subBuildingName = randomName(),
+    buildingNumber = randomBuildingNumber(),
+    thoroughfareName = randomName(),
+    dependentLocality = randomName(),
+    postTown = randomName(),
+    county = randomName(),
+    countryCode = randomCountryCode(),
+    uprn = randomUprn(),
+    statusCode = randomAddressStatusCode(),
+    comment = randomName(),
+    isVerified = randomBoolean(),
+    usages = listOf(AddressUsage(randomAddressUsageCode(), randomBoolean())),
+    contacts = listOf(Contact(randomContactType(), randomPhoneNumber(), "+44")),
+  )
+
+  private companion object {
+    const val REQUESTED_CRN = "X12345"
   }
 }
