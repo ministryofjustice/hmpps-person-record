ebf647d08eed03c7547d751141858a49cceabf11
richardpopple <2195350+richardpopple@users.noreply.github.com>
2026-09-23 10:28:16 +0100

    SDIT-4238 Change signature to allow updates to not change address or contacts (#2414)
    * SDIT-4238 change signature to allow updates to not change address or contacts
    
    * SDIT-4238 adding tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/PersonServiceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personrecord/service/PersonServiceIntTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.personrecord.service
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.verifyNoInteractions
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,14 +9,23 @@ import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
 import uk.gov.justice.digital.hmpps.personrecord.client.model.court.libra.LibraHearingEvent
 import uk.gov.justice.digital.hmpps.personrecord.client.model.court.libra.Name
 import uk.gov.justice.digital.hmpps.personrecord.config.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.AddressEntity
+import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.ContactEntity
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Address
+import uk.gov.justice.digital.hmpps.personrecord.model.person.Contact
 import uk.gov.justice.digital.hmpps.personrecord.model.person.Person
+import uk.gov.justice.digital.hmpps.personrecord.model.types.ContactType.MOBILE
 import uk.gov.justice.digital.hmpps.personrecord.service.eventlog.CPRLogEvents
 import uk.gov.justice.digital.hmpps.personrecord.service.message.recluster.ReclusterService
 import uk.gov.justice.digital.hmpps.personrecord.service.person.PersonService
 import uk.gov.justice.digital.hmpps.personrecord.service.search.PersonMatchService
+import uk.gov.justice.digital.hmpps.personrecord.test.randomBoolean
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCId
 import uk.gov.justice.digital.hmpps.personrecord.test.randomCrn
+import uk.gov.justice.digital.hmpps.personrecord.test.randomFullAddress
 import uk.gov.justice.digital.hmpps.personrecord.test.randomName
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPhoneNumber
+import uk.gov.justice.digital.hmpps.personrecord.test.randomPostcode
 import uk.gov.justice.digital.hmpps.personrecord.test.randomPrisonNumber
 
 class PersonServiceIntTest : IntegrationTestBase() {
@@ -28,6 +38,54 @@ class PersonServiceIntTest : IntegrationTestBase() {
 
   @MockitoSpyBean
   private lateinit var mockPersonMatchService: PersonMatchService
+
+  @Nested
+  inner class AddressContactsIgnore {
+
+    @Test
+    fun `should not set contacts when specified in creation`() {
+      stubPersonMatchUpsert()
+      stubPersonMatchScores()
+      val personToCreate = createRandomPrisonPersonDetails(randomPrisonNumber()).copy(contacts = listOf(Contact(MOBILE, randomPhoneNumber(), "+44")))
+      assertThat(personToCreate.contacts).isNotEmpty()
+      val created = personService.processPerson(personToCreate, setOf(ContactEntity::class)) { null }
+      assertThat(created.contacts).isEmpty()
+    }
+
+    @Test
+    fun `should not set contacts when specified in update`() {
+      val personWithNoContact = createRandomPrisonPersonDetails(randomPrisonNumber())
+      val created = createPerson(personWithNoContact)
+      assertThat(created.contacts).isEmpty()
+      val personWithContact = personWithNoContact.copy(contacts = listOf(Contact(MOBILE, randomPhoneNumber(), "+44")))
+      val updated = personService.processPerson(personWithContact, setOf(ContactEntity::class)) { created }
+      assertThat(updated.contacts).isEmpty()
+    }
+
+    @Test
+    fun `should not set addresses when specified in creation`() {
+      stubPersonMatchUpsert()
+      stubPersonMatchScores()
+      val personToCreate = createRandomPrisonPersonDetails(randomPrisonNumber())
+      assertThat(personToCreate.addresses).isNotEmpty()
+      val created = personService.processPerson(personToCreate, setOf(AddressEntity::class)) { null }
+      assertThat(created.addresses).isEmpty()
+    }
+
+    @Test
+    fun `should not set addresses when specified in update`() {
+      val personNoAddress = createRandomPrisonPersonDetails(randomPrisonNumber()).copy(addresses = emptyList())
+      val created = createPerson(personNoAddress)
+      assertThat(created.addresses).isEmpty()
+      val personWithAddress = personNoAddress.copy(
+        addresses = listOf(
+          Address(postcode = randomPostcode(), fullAddress = randomFullAddress(), noFixedAbode = randomBoolean()),
+        ),
+      )
+      val updated = personService.processPerson(personWithAddress, setOf(AddressEntity::class)) { created }
+      assertThat(updated.addresses).isEmpty()
+    }
+  }
 
   @Test
   fun `should log record update when change in matching fields`() {
