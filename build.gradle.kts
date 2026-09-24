f4e287d1e63d37d385ce4d50df4accd08195f1cd
David Sharrocks <30368099+SharrocksDA@users.noreply.github.com>
2026-09-24 09:48:37 +0100

    Add pact record deployment step (#2423)
    * CPR-1414 Add reusable and manually runnable Pact Broker deployment workflow
    
    * CPR-1414 Add code to test workflow on branch
    
    * Adding test workflow for dispatches
    
    * Update deployment workflows to use Pact Broker deployment step v2
    
    * Remove commented-out pactbroker configuration and simplify workflow inputs
    
    * Remove unused Pact tests workflow configuration
    
    * Add deployment recording steps for dev and preprod environments
    
    * Refactor deployment workflows to remove unused steps and simplify configuration
    
    * Add push trigger for all branches in pipeline workflow
    
    * Simplify run-name in Pact Provider Verification workflow and remove unused consumer input

--- build.gradle.kts
+++ build.gradle.kts
@@ -122,9 +122,6 @@ tasks.named<Test>("pactTest") {
       ?: System.getenv("PACT_BROKER_URL")
       ?: "https://pact-broker-prod.apps.live-1.cloud-platform.service.justice.gov.uk",
   )
-//  systemProperty("pactbroker.host", System.getProperty("pactbroker.host") ?: System.getenv("PACT_BROKER_HOST") ?: "")
-//  systemProperty("pactbroker.port", System.getProperty("pactbroker.port") ?: System.getenv("PACT_BROKER_PORT") ?: "")
-//  systemProperty("pactbroker.scheme", System.getProperty("pactbroker.scheme") ?: System.getenv("PACT_BROKER_SCHEME") ?: "")
   systemProperty("pactbroker.auth.username", System.getenv("PACT_BROKER_USERNAME") ?: "")
   systemProperty("pactbroker.auth.password", System.getenv("PACT_BROKER_PASSWORD") ?: "")
 
