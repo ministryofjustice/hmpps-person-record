d181046dc05525501f02ea4ee5819cde5fc365c7
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-09-25 14:41:22 +0100

    SDIT-4237: ✨ Add in create / modify fields to address and contact tables (#2421)
    Co-authored-by: jldparker527 <joshua.parker2@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/person/AddressUsage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personrecord/model/person/AddressUsage.kt
@@ -4,18 +4,37 @@ import uk.gov.justice.digital.hmpps.personrecord.api.model.probation.ProbationCr
 import uk.gov.justice.digital.hmpps.personrecord.api.model.sysconsync.PrisonAddressUsage
 import uk.gov.justice.digital.hmpps.personrecord.jpa.entity.AddressUsageEntity
 import uk.gov.justice.digital.hmpps.personrecord.model.types.AddressUsageCode
+import java.time.LocalDateTime
 
 data class AddressUsage(
   val addressUsageCode: AddressUsageCode,
   val isActive: Boolean,
+  val createDateTime: LocalDateTime? = null,
+  val createUserId: String? = null,
+  val modifyDateTime: LocalDateTime? = null,
+  val modifyUserId: String? = null,
 ) {
 
   companion object {
 
-    fun from(usage: PrisonAddressUsage): AddressUsage = AddressUsage(usage.addressUsageCode, usage.isActive)
+    fun from(usage: PrisonAddressUsage): AddressUsage = AddressUsage(
+      usage.addressUsageCode,
+      usage.isActive,
+      usage.createDateTime,
+      usage.createUserId,
+      usage.modifyDateTime,
+      usage.modifyUserId,
+    )
 
     fun from(usage: ProbationCreateAddressUsage): AddressUsage = AddressUsage(usage.usageCode, usage.isActive)
 
-    fun from(usage: AddressUsageEntity): AddressUsage = AddressUsage(usage.usageCode, usage.active)
+    fun from(usage: AddressUsageEntity): AddressUsage = AddressUsage(
+      usage.usageCode,
+      usage.active,
+      usage.createDateTime,
+      usage.createUserId,
+      usage.modifyDateTime,
+      usage.modifyUserId,
+    )
   }
 }
